bd511ecc1ae6ea1196d6b118bbb38b35532d6267
iefremov@unipro.ru <iefremov@unipro.ru>
2011-11-01 06:27:50 +0000

    fixing occasionally damaged test
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@14 2ecb45c1-dfb3-dc95-330a-13f216d23a04

--- src/LangSpecTest/13_Types/13_4_Interface_Types/13_4_Interface_Types_A12_t06.dart
+++ src/LangSpecTest/13_Types/13_4_Interface_Types/13_4_Interface_Types_A12_t06.dart
@@ -1,19 +1,49 @@
-./rs.sh BadNumberFormatException
-./rs.sh ClosureArgumentMismatchException
-./rs.sh EmptyQueueException
-./rs.sh Exception
-./rs.sh ExpectException
-./rs.sh IllegalAccessException
-./rs.sh IllegalArgumentException
-./rs.sh IllegalJSRegExpException
-./rs.sh IndexOutOfRangeException
-./rs.sh IntegerDivisionByZeroException
-./rs.sh NoMoreElementsException
-./rs.sh NoSuchMethodException
-./rs.sh NotImplementedException
-./rs.sh NullPointerException
-./rs.sh ObjectNotClosureException
-./rs.sh OutOfMemoryException
-./rs.sh StackOverflowException
-./rs.sh UnsupportedOperationException
-./rs.sh WrongArgumentCountException
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion A type T is more specific than a type S, written T << S, if one of the following conditions is met:
+ * T is S.
+ * T is ⊥
+ * S is Dynamic.
+ * S is a direct supertype of T.
+ * T is a type variable and S is the upper bound of T.
+ * T is of the form I<T1, ..., Tn > and S is of the form I <S1, ..., Sn> and Ti << Si 1 <= i <= n.
+ * T << U and U << S.
+ * T is a subtype of S, written T <: S, iff [⊥/Dynamic]T << S.
+ * A type T may be assigned to a type S, written T <=> S, if either T <: S or S <: T .
+ * @description Checks that two generic types are not assignable even if just one type argument is not assignable.
+ * Using complex function types as type arguments.
+ * @static-type-error
+ * @author iefremov
+ * @reviewer rodionov
+ */
+
+interface I {}
+interface J extends I {}
+interface K extends J {}
+class C implements K {}
+
+typedef Map<List, List<Map<num,List>>> complexFunction_t1();
+typedef t1_1();
+typedef Map<List<int>, List> t1_2();
+typedef Map<List<int>, List<Map>> t1_3();
+typedef Map<List<complexFunction_t1>, List<Map<int, List<Map<complexFunction_t1, complexFunction_t2>>>>> t1_4();
+
+typedef badFunction(int x); //not assignable to complexFunction_t1
+
+typedef complexFunction_t1 complexFunction_t2(complexFunction_t1 f1, complexFunction_t1 f2);
+typedef t1_1 t2_1(t1_2 f1, badFunction f2); //not assignable to complexFunction_t2 because of badFunction
+
+typedef K typeParameter1(J j, complexFunction_t1 f1, [num i, complexFunction_t2 f2]);
+//      <=>              <=>  <=>                     <=>    <=/=>
+typedef C typeParameter2(K j, t1_4 f1,               [int i, t2_1 f2]);
+
+
+class Generic<G> {}
+
+main() {
+  Generic<typeParameter1> g = new Generic<typeParameter2>();
+}
