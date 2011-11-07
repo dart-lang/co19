4458f229448c93882f01af25cfe8d996eb5fcc55
iefremov@unipro.ru <iefremov@unipro.ru>
2011-11-07 10:55:15 +0000

    Fixing wrong Math.atan2() test (issue 8)
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@22 2ecb45c1-dfb3-dc95-330a-13f216d23a04

--- src/LibTest/core/Math/atan2/Math_atan2_A01_t03.dart
+++ src/LibTest/core/Math/atan2/Math_atan2_A01_t03.dart
@@ -16,18 +16,23 @@ main() {
   Expect.equals(0.0, Math.atan2(0.0, double.INFINITY));
   Expect.equals(0.0, Math.atan2(1, double.INFINITY));
   Expect.equals(0.0, Math.atan2(0.0, 0.0));
-  Expect.equals(0.0, Math.atan2(-1, double.INFINITY));
+
+  Expect.equals(-0.0, Math.atan2(-0.0, 10));
+  Expect.equals(-0.0, Math.atan2(-0.0, 0.0));
+  Expect.equals(-0.0, Math.atan2(-1, double.INFINITY));
   
   Expect.equals(Math.PI, Math.atan2(0.0, -0.1));
-  Expect.equals(Math.PI, Math.atan2(-0.0, -0.1));
   Expect.equals(Math.PI, Math.atan2(0.0, -10));
   Expect.equals(Math.PI, Math.atan2(0.0, double.NEGATIVE_INFINITY));
   Expect.equals(Math.PI, Math.atan2(1, double.NEGATIVE_INFINITY));
-  
+  Expect.equals(Math.PI, Math.atan2(0.0, -0.0));
+
+  Expect.equals(-Math.PI, Math.atan2(-0.0, -0.1));
+  Expect.equals(-Math.PI, Math.atan2(-0.0, -0.0));
   Expect.equals(-Math.PI, Math.atan2(-1, double.NEGATIVE_INFINITY));
   
   Expect.equals(Math.PI/2, Math.atan2(0.1, 0.0));
-  Expect.equals(Math.PI/2, Math.atan2(10, 0.0));
+  Expect.equals(Math.PI/2, Math.atan2(10, -0.0));
   Expect.equals(Math.PI/2, Math.atan2(double.INFINITY, 0.0));
   Expect.equals(Math.PI/2, Math.atan2(double.INFINITY, 0.0));
   Expect.equals(Math.PI/2, Math.atan2(double.INFINITY, -1));
