ba3113d3ae8acadc541e3153f9f4055c8697d5bf
iefremov@unipro.ru <iefremov@unipro.ru>
2011-11-18 13:14:06 +0000

    Fixing integer modulo tests (issue 48)
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@49 2ecb45c1-dfb3-dc95-330a-13f216d23a04

--- src/LibTest/core/int/operator%/int_operator%_A01_t01.dart
+++ src/LibTest/core/int/operator%/int_operator%_A01_t01.dart
@@ -9,11 +9,10 @@
  * the left operand is the dividend and the right operand is the divisor.
  * @description Checks that this operator returns correct value for integer
  *              arguments (including 0).
- * Assumptions taken from ECMA-262 spec, ch. 11.5.3.
  * @author vasya
  * @reviewer msyabro
  * @reviewer rodionov
- * @needsreview verify whether Ecma-262 rules should really be applied
+ * @needsreview modulo operatio is not specified (issue 493).
  */
 
 main() {
@@ -24,8 +23,8 @@ main() {
 
   Expect.equals(2, 17 % 3);
   Expect.equals(2, 17 % -3);
-  Expect.equals(-2, (-17) % 3);
-  Expect.equals(-2, (-17) % -3);
+  Expect.equals(1, (-17) % 3);
+  Expect.equals(1, (-17) % -3);
 
   Expect.equals(2147483647, 2147483647 % 2147483648);
   Expect.equals(0, 2147483647 % 2147483647);
@@ -42,6 +41,4 @@ main() {
   // 2^100 % 3^50
   Expect.equals(691521709937297972926156,
       1267650600228229401496703205376 % 717897987691852588770249);
-
-  Expect.isTrue((1 % 0).isNAN());
 }
--- src/LibTest/core/int/operator%/int_operator%_A01_t02.dart
+++ src/LibTest/core/int/operator%/int_operator%_A01_t02.dart
@@ -9,11 +9,9 @@
  * the left operand is the dividend and the right operand is the divisor.
  * @description Checks that this operator returns correct value for double
  *              arguments (including special values) and that it is double.
- * Assumptions taken from ECMA-262 spec, ch. 11.5.3.
  * @author rodionov
  * @reviewer pagolubev
- * @needsreview verify whether Ecma-262 rules should really be applied.
- * other % tests assume the result is always not less than zero.
+ * @needsreview modulo operation is not specified (issue 493).
  */
 
 main() {
@@ -22,8 +20,8 @@ main() {
 
   check(0.25, 1, 0.375);
   check(0.25, 1, -0.375);
-  check(-0.25, -1, 0.375);
-  check(-0.25, -1, -0.375);
+  check(0.125, -1, 0.375);
+  check(0.125, -1, -0.375);
 
   final double MIN_DOUBLE = Math.pow(2.0, -1074);
   final double NEG_MIN_DOUBLE = -1 * MIN_DOUBLE; 
@@ -37,21 +35,17 @@ main() {
   check(1, 1, MAX_DOUBLE);
   check(1, 1, NEG_MAX_DOUBLE);
   check(1, 1, double.INFINITY);
-  check(-1, -1, double.INFINITY);
+  check(double.INFINITY, -1, double.INFINITY);
   check(1, 1, double.NEGATIVE_INFINITY);
-  check(-1, -1, double.NEGATIVE_INFINITY);
+  check(double.INFINITY, -1, double.NEGATIVE_INFINITY);
   check(0x1000000000000000000000, 0x1000000000000000000000, double.INFINITY);
-  check(-0x1000000000000000000000, -0x1000000000000000000000, double.INFINITY);
+  check(double.INFINITY, -0x1000000000000000000000, double.INFINITY);
   check(0x1000000000000000000000, 0x1000000000000000000000, double.NEGATIVE_INFINITY);
-  check(-0x1000000000000000000000, -0x1000000000000000000000, double.NEGATIVE_INFINITY);
+  check(double.INFINITY, -0x1000000000000000000000, double.NEGATIVE_INFINITY);
   
-  Expect.isTrue((1 % double.NAN).isNAN());
-  Expect.isTrue((0x1000000000000000000000 % double.NAN).isNAN());
-  Expect.isTrue(((-0x1000000000000000000000) % double.NAN).isNAN());
-  Expect.isTrue((1 % 0.0).isNAN());
-  Expect.isTrue((1 % -0.0).isNAN());
-  Expect.isTrue((0x1000000000000000000000 % 0.0).isNAN());
-  Expect.isTrue(((-0x1000000000000000000000) % 0.0).isNAN());
+  Expect.isTrue((1 % double.NAN).isNaN());
+  Expect.isTrue((0x1000000000000000000000 % double.NAN).isNaN());
+  Expect.isTrue(((-0x1000000000000000000000) % double.NAN).isNaN());
 }
 
 void check(num ex, int a, double b) {
