d611f28e866c420e39118801927f97e357b6c8f8
iefremov@unipro.ru <iefremov@unipro.ru>
2012-02-29 12:59:57 +0000

    adding more checks against prohibited operator overloadings
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@138 2ecb45c1-dfb3-dc95-330a-13f216d23a04

---
+++ src/Language/07_Classes/1_Instance_Methods/2_Operators_A02_t18.dart
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion The following names are allowed for user-defined operators: 
+ * ==, <, >, <=,>=, -, +, /, ~/, *, %, |, ^, &, <<, >>, >>>, []=, [], ~, negate.
+ * @description Checks that operator ++ cannot be defined in a user class.
+ * @compile-error
+ * @author iefremov
+ */
+
+class C {
+  operator ++() {}
+}
+
+main() {
+  try {
+    var b = (new C());
+    b++;
+  } catch(var x) {}
+}
+
---
+++ src/Language/07_Classes/1_Instance_Methods/2_Operators_A02_t20.dart
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion The following names are allowed for user-defined operators: 
+ * ==, <, >, <=,>=, -, +, /, ~/, *, %, |, ^, &, <<, >>, >>>, []=, [], ~, negate.
+ * @description Checks that operator is cannot be defined in a user class.
+ * @compile-error
+ * @author iefremov
+ */
+
+class C {
+  operator is(x) {}
+}
+
+main() {
+  try {
+    var b = (new C());
+    (b is Dynamic);
+  } catch(var x) {}
+}
+
