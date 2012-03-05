d92bc4b95ceb061ddc41a940ed7746803f8eb08a
msyabro@unipro.ru <msyabro@unipro.ru>
2012-03-05 08:41:20 +0000

    Issue 86: Date.toString always must print at least 4 digits for the year.
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@148 2ecb45c1-dfb3-dc95-330a-13f216d23a04

--- src/LibTest/core/Date/toString_A02_t01.dart
+++ src/LibTest/core/Date/toString_A02_t01.dart
@@ -5,17 +5,18 @@
  */
 /**
  * @assertion String toString() returns string constructed for the [timeZone] of this instance. 
- * @description check various dates
+ * @description Checks various dates
  * @author hlodvig
  * @reviewer msyabro
  * @needsreview implementation based test. It would be good to define format of the returned string in the API
  */
 
 main() {
-  Expect.equals("1-01-01 00:00:00.000", new Date(1, 1, 1, 0, 0, 0, 0).toString());
-  Expect.equals("1-01-01 00:00:00.000Z", new Date.withTimeZone(1, 1, 1, 0, 0, 0, 0, new TimeZone.utc()).toString());
+  Expect.equals("0001-01-01 00:00:00.000", new Date(1, 1, 1, 0, 0, 0, 0).toString());
+  Expect.equals("0001-01-01 00:00:00.000Z", new Date.withTimeZone(1, 1, 1, 0, 0, 0, 0, new TimeZone.utc()).toString());
   Expect.equals("2001-01-01 00:00:00.000", new Date(2001, 1, 1, 0, 0, 0, 0).toString());
   Expect.equals("2001-12-31 23:59:59.999", new Date(2001, 12, 31, 23, 59, 59, 999).toString());
   Expect.equals("2001-01-01 00:00:00.000Z", new Date.withTimeZone(2001, 1, 1, 0, 0, 0, 0, new TimeZone.utc()).toString());
   Expect.equals("2001-12-31 23:59:59.999Z", new Date.withTimeZone(2001, 12, 31, 23, 59, 59, 999, new TimeZone.utc()).toString());
+  Expect.equals("999999-01-01 00:00:00.000", new Date(999999, 1, 1, 0, 0, 0, 0).toString());
 }
