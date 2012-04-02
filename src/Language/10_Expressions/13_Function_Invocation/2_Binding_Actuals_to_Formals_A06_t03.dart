a5d80ae552c8fa636a4117f93f70336cacdc7745
msyabro@unipro.ru <msyabro@unipro.ru>
2012-04-02 07:29:54 +0000

    Add new tests for binding function arguments.
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@189 2ecb45c1-dfb3-dc95-330a-13f216d23a04

---
+++ src/Language/10_Expressions/13_Function_Invocation/2_Binding_Actuals_to_Formals_A06_t03.dart
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Let Ti be the static type of ai, let Si be the type of pi, i from 1 to n + k and let
+ * Sq be the type of the named parameter q of f. It is a static warning if m < n or if m > n + k.
+ * @description Checks that it is a static warning if the number of argument is less than number
+ * of positional parameters of an instance method.
+ * @static-warning
+ * @author msyabro
+ * @reviewer iefremov
+ */
+
+class C {
+  func(p1, p2, p3) {}
+}
+
+main() {
+  try {
+   new C().func(1);
+  } catch(var e) {}
+}
---
+++ src/Language/10_Expressions/13_Function_Invocation/2_Binding_Actuals_to_Formals_A06_t05.dart
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Let Ti be the static type of ai, let Si be the type of pi, i from 1 to n + k and let
+ * Sq be the type of the named parameter q of f. It is a static warning if m < n or if m > n + k.
+ * @description Checks that it is a static warning if the number of argument is more than number
+ * of parameters of a top-level function.
+ * @static-warning
+ * @author msyabro
+ * @reviewer iefremov
+ */
+
+func(p1, p2, [p3]) {}
+
+main() {
+  try {
+    func(1, 2 ,3, 4);
+  } catch(var e) {}
+}
