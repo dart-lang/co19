9293a75393a3f297b0db8423356af041f5729c2b
iefremov@unipro.ru <iefremov@unipro.ru>
2012-04-03 05:18:16 +0000

    Fixing a wrong test (issue 107)
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@199 2ecb45c1-dfb3-dc95-330a-13f216d23a04

--- src/Language/10_Expressions/08_Function_Expressions_A01_t05.dart
+++ src/Language/10_Expressions/08_Function_Expressions_A01_t05.dart
@@ -19,16 +19,23 @@
  * @note issue 1189
  */
 
+interface I {}
+class C implements I{}
+typedef tf();
+
 main() {
-// as an invocation of function expression
   () {}();
   vf() {}();
-  int if1(k) {return k;}();
-  int if2(int k) {return k;}();
+  int if1(k) {return k;}(1);
+  int if2(int k) {return k;}(1);
   (int if3() => 1)();
   (() => 1)();
-  I if4(C x, y) => new C()();
+  (I if4(C x, y) => new C())(null, null);
   C cf() {}();
-  C cf2(C p1, tf p2, [tf p3] => null)();
+  (C cf2(C p1, tf p2, [tf p3]) => null)(null, null);
+
   tf tf4(tf p1, tf p2, [tf p3]) => null();
+  try {
+    (tf tf5(tf p1, tf p2, [tf p3]) => null())(null, null); //null() is parsed as a closure invocation
+  } catch(var x){}
 }
