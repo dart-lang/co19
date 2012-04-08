e7b25dbea00da4a81e7bf7d77f32f315844a3b84
iefremov@unipro.ru <iefremov@unipro.ru>
2012-04-08 13:34:11 +0000

    Couple of new tests against ch. 8 Interfaces
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@211 2ecb45c1-dfb3-dc95-330a-13f216d23a04

---
+++ src/Language/08_Interfaces/3_Factories_and_Constructors_A01_t11.dart
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion interfaceDefinition:
+ * interface identifier typeParameters? superinterfaces? factorySpecification? '{'
+ *   (interfaceMemberDefinition)* '}'
+ * ; 
+ * factorySpecification:
+ *   default qualified typeParameters?
+ * ;
+ * @description Checks that the 'default' keyword cannot be replaced with 'factory'.
+ * @author kaigorodov
+ * @reviewer iefremov
+ * @compile-error
+ */
+
+interface I factory A {
+  I();
+}
+
+class A implements I {}
+
+main() {
+  try {
+    new A();
+  } catch(var e) {}
+}
+
---
+++ src/Language/08_Interfaces/3_Factories_and_Constructors_A01_t12.dart
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion interfaceDefinition:
+ * interface identifier typeParameters? superinterfaces? factorySpecification? '{'
+ *   (interfaceMemberDefinition)* '}'
+ * ; 
+ * superinterfaces:
+ *   extends typeList
+ * ;
+ * factorySpecification:
+ *   default qualified typeParameters?
+ * ;
+ * @description Checks that a factory clause can not be declared outside class.
+ * @author kaigorodov
+ * @reviewer iefremov
+ * @compile-error
+ */
+
+factory F.I() {}
+
+class F {
+}
+
+interface I extends SI default F {
+  I();
+}
+
+interface SI {}
+
+main() {
+  try {
+    new I();
+  } catch(var x) {}
+}
