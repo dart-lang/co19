6235751076fbfdd61537304b044070b011ce3847
iefremov@unipro.ru <iefremov@unipro.ru>
2012-04-10 08:11:22 +0000

    New tests against built-in identifiers added to chapter 10.28
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@221 2ecb45c1-dfb3-dc95-330a-13f216d23a04

---
+++ src/Language/10_Expressions/28_Identifier_Reference_A07_t03.dart
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is a compile-time error to use a built-in identifier other than Dynamic 
+ * as a type annotation.
+ * @description Checks that it is a compile-time error when a built-in identifier
+ * "call" is used as a type annotation of a local variable.
+ * @compile-error
+ * @author rodionov
+ * @reviewer iefremov
+ * @note http://code.google.com/p/dart/issues/detail?id=2492 
+ */
+
+main() {
+  try {
+    call foo;
+  } catch(var x) {}
+}
---
+++ src/Language/10_Expressions/28_Identifier_Reference_A07_t11.dart
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is a compile-time error to use a built-in identifier other than Dynamic 
+ * as a type annotation.
+ * @description Checks that it is a compile-time error when a built-in identifier
+ * "set" is used as a type annotation of a local variable.
+ * @compile-error
+ * @author rodionov
+ * @reviewer iefremov
+ */
+
+main() {
+  try {
+    set foo;
+  } catch(var x) {}
+}
