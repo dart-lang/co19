a3a35232115792a1bc347c1938ae6f41119b0aee
iefremov@unipro.ru <iefremov@unipro.ru>
2012-04-25 12:11:11 +0000

    Some new tests against compile-time constants
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@240 2ecb45c1-dfb3-dc95-330a-13f216d23a04

--- src/Language/10_Expressions/01_Constants_A15_t46.dart
+++ src/Language/10_Expressions/01_Constants_A15_t46.dart
@@ -6,17 +6,19 @@
 /**
  * @assertion  All possible constant expressions are prescribed in assertions
  * 01_Constants_A01 - 01_Constants_A14. There are no other constant expressions.
- * @description Checks that an expression of the form e1 ~/ e2 cannot be used to initialize
- * a constant variable if e1 and e2 are both doubles.
- * @author iefremov
+ * @description Checks that it is a compile-time error when an expression of the form e1 + e2
+ * where e1 or e2 does not evaluate to a numeric value, is used to initialize a constant variable. 
  * @compile-error
+ * @author kaigorodov
  * @reviewer rodionov
  */
 
-const m = 3.14 ~/ 2.7;
+final constList = const [
+  true + 1,
+];
 
 main() {
   try {
-    print(m);
+    constList is List;
   } catch(var x) {}
 }
