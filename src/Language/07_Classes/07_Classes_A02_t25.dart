ea961703acf27cc4fd753fdd7407220e65d13777
iefremov@unipro.ru <iefremov@unipro.ru>
2012-04-25 12:34:32 +0000

    Adding some new tests against class declaration grammar
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@242 2ecb45c1-dfb3-dc95-330a-13f216d23a04

---
+++ src/Language/07_Classes/07_Classes_A02_t25.dart
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion classDefinition:
+ * abstract? class identifier typeParameters? superclass? interfaces?
+ *   '{' classMemberDefinition* '}'
+* ;
+ * classMemberDefinition:
+ *   declaration ';' |
+ *   methodSignature functionBody
+ * ;
+ * declaration:
+ *   constantConstructorSignature (redirection | initializers)?
+ *   | constructorSignature (redirection | initializers)?
+ *   | abstract getterSignature
+ *   | abstract setterSignature
+ *   | abstract operatorSignature
+ *   | abstract functionSignature
+ *   | static final type? staticFinalDeclarationList
+ *   | static? initializedVariableDeclaration
+ * ;
+ * @description Checks that it is a compile-time error if an operator signature is declared final.
+ * @compile-error
+ * @author kaigorodov
+ * @reviewer iefremov
+ */
+
+class A {
+  final operator==(A other) {}
+}
+
+main() {
+  try {
+    A a = new A();
+  } catch(var e) {}
+}
---
+++ src/Language/07_Classes/07_Classes_A02_t27.dart
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion classDefinition:
+ * abstract? class identifier typeParameters? superclass? interfaces?
+ *   '{' classMemberDefinition* '}'
+* ;
+ * classMemberDefinition:
+ *   declaration ';' |
+ *   methodSignature functionBody
+ * ;
+ * declaration:
+ *   constantConstructorSignature (redirection | initializers)?
+ *   | constructorSignature (redirection | initializers)?
+ *   | abstract getterSignature
+ *   | abstract setterSignature
+ *   | abstract operatorSignature
+ *   | abstract functionSignature
+ *   | static final type? staticFinalDeclarationList
+ *   | static? initializedVariableDeclaration
+ * ;
+ * @description Checks that it is a compile-time error if a function signature is declared final.
+ * @compile-error
+ * @author kaigorodov
+ * @reviewer iefremov
+ */
+
+class A {
+  final void a() {}
+}
+
+main() {
+  try {
+    A a = new A();
+    a.a();
+  } catch(var e) {}
+}
