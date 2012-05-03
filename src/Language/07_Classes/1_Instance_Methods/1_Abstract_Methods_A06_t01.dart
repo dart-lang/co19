b84cab1485f8440adaa29c56890a665b6c6a84b7
iefremov@unipro.ru <iefremov@unipro.ru>
2012-05-03 13:33:06 +0000

    Replacing an obsolete test with a new one (ch. 7.1.1)
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@271 2ecb45c1-dfb3-dc95-330a-13f216d23a04

--- src/Language/07_Classes/1_Instance_Methods/1_Abstract_Methods_A06_t01.dart
+++ src/Language/07_Classes/1_Instance_Methods/1_Abstract_Methods_A06_t01.dart
@@ -4,28 +4,20 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion Unless explicitly stated otherwise, all ordinary rules that apply to methods
- * apply to abstract methods.
- * 7.1: It is a static warning if a class C declares an instance method
- * named n and a static member named n is declared in a superclass of C.
- * @description Checks that a static warning is produced when a class declares an abstract
- * method with the same name as a static variable in its superclass.
- * @static-warning
- * @author rodionov
- * @needsreview issue 986
+ * @assertion It is a compile-time error to specify a body for an abstract method.
+ * @description Checks that a compile-time error is produced when an abstract method
+ * has a body.
+ * @compile-error
+ * @author msyabro
  */
 
-class A {
-  static var f;
-}
-
-class C extends A {
-  abstract f();
+class C {
+  abstract m1() {}
 }
 
 main() {
   try {
-    new C().f();
-  } catch (NoSuchMethodException ok) {}
+    new C().m1();
+  } catch(var ok){}
 }
 
