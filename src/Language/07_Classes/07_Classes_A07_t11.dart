9cbfec816fd4b71b1e278e260854c817b68795ff
iefremov@unipro.ru <iefremov@unipro.ru>
2012-05-03 13:50:10 +0000

    Adding a couple of new tests against ch. 7. Classes
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@272 2ecb45c1-dfb3-dc95-330a-13f216d23a04

---
+++ src/Language/07_Classes/07_Classes_A07_t11.dart
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is a compile-time error if a class declares two members of the same name,
+ * except that a getter and a setter may be declared with the same name provided
+ * both are instance members or both are static members.
+ * @description Checks that it is a compile-time error if a class declares 
+ * a variable, function, and an abstract getter of the same name.
+ * @compile-error
+ * @author kaigorodov
+ * @reviewer iefremov
+ */
+
+class C {
+  var f = 0;
+  abstract String get f();
+  String f(int f);
+}
+
+main() {
+  try {
+    C c = new C();
+  } catch(var e) {}
+}
+
---
+++ src/Language/07_Classes/07_Classes_A10_t01.dart
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion The interface of class C is an implicit interface that declares instance members
+ * that correspond to the instance members declared by C, and whose direct superinterfaces
+ * are the direct superinterfaces of C. When a class name appears as type or interface,
+ * that name denotes the interface of the class.
+ * @description Checks that a class can be implemented as an interface.
+ * @author msyabro
+ * @reviewer iefremov
+ */
+
+abstract class A {}
+class C {}
+
+class B implements A {}
+class D implements C {}
+
+main() {
+  var b = new B();
+  Expect.isTrue(b is A);
+
+  var d = new D();
+  Expect.isTrue(d is C);
+}
