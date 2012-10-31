46aa5ec3ea232be17e31d131e1834015acef6cea
kaigorodov@unipro.ru <kaigorodov@unipro.ru>
2012-10-31 11:22:44 +0000

    new tests against Statements/Try added.
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@430 2ecb45c1-dfb3-dc95-330a-13f216d23a04

--- src/Language/12_Statements/10_Try_A10_t01.dart
+++ src/Language/12_Statements/10_Try_A10_t01.dart
@@ -4,8 +4,13 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion Execution of a try statement of the form try s1 on − catch1 . . . on − catchn
- * ﬁnally sf; proceeds as follows:
+ * @assertion Execution of a try statement of the form
+ *   try
+ *       s1
+ *   on−catch1 ...
+ *   on−catchn
+ *   ﬁnally sf;
+  proceeds as follows:
  *   The statement s1 is executed in the dynamic scope of the exception handler
  * deﬁned by the try statement. Then, the ﬁnally clause is executed.
  *   Whether any of the on-catch clauses is executed depends on whether a matching
@@ -15,8 +20,41 @@
  * matches are found, the handler will execute the ﬁnally clause.
  *   If a matching on-catch was found, it will execute ﬁrst, and then the ﬁnally
  * clause will be executed.
- * @author TODO
+ * @description checks the execution order of the substatements of the try statement.
+ * @author kaigorodov
  */
 
+class E0 implements Exception {}
+class E1 implements Exception {}
+class E2 extends E1 {}
+class E3 extends E1 {}
+
+void test(void f(), int expected) {
+  int log=0;
+  void append(int digit) {log=log*10+digit;}
+
+  try {
+    append(1);
+    f();
+    append(2);
+  } on E2 catch(e) {
+    append(3);
+  } on E1 catch(e) {
+    append(4);
+  } catch(e) {
+    append(5);
+  } finally {
+    append(6);
+  }
+
+  Expect.equals(expected, log);
+}
+
 void main() {
+  test((){}, 126);
+  test((){throw new E0();}, 156);
+  test((){throw new E1();}, 146);
+  test((){throw new E2();}, 136);
+  test((){throw new E3();}, 146);
+  test((){throw 1;}, 156);
 }
--- src/Language/12_Statements/10_Try_A12_t01.dart
+++ src/Language/12_Statements/10_Try_A12_t01.dart
@@ -4,10 +4,67 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion A try statement of the form try s1 on − catch1 . . . on − catchn; is equivalent
- * to the statement try s1 on − catch1 ... on − catchn ﬁnally {};.
- * @author TODO
+ * @assertion A try statement of the form
+ *   try s1 on−catch1 ... on−catchn;
+ * is equivalent to the statement
+ *   try s1 on−catch1 ... on−catchn ﬁnally {};
+ * @description checks that the both forms are equivalent
+ * @author kaigorodov
  */
 
+class E0 implements Exception {}
+class E1 implements Exception {}
+class E2 extends E1 {}
+class E3 extends E1 {}
+
+int test1(void f()) {
+  int log=0;
+  void append(int digit) {log=log*10+digit;}
+
+  try {
+    append(1);
+    f();
+    append(2);
+  } on E2 catch(e) {
+    append(3);
+  } on E1 catch(e) {
+    append(4);
+  } catch(e) {
+    append(5);
+  }
+
+  return log;
+}
+
+int test2(void f()) {
+  int log=0;
+  void append(int digit) {log=log*10+digit;}
+
+  try {
+    append(1);
+    f();
+    append(2);
+  } on E2 catch(e) {
+    append(3);
+  } on E1 catch(e) {
+    append(4);
+  } catch(e) {
+    append(5);
+  }
+  finally {}
+
+  return log;
+}
+
+void test(void f()) {
+  Expect.equals(test1(f), test2(f));
+}
+
 void main() {
+  test((){});
+  test((){throw new E0();});
+  test((){throw new E1();});
+  test((){throw new E2();});
+  test((){throw new E3();});
+  test((){throw 1;});
 }
