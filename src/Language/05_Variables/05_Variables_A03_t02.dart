ec747c53edf84ea07cb87c06d4ff74828db578e7
kaigorodov@unipro.ru <kaigorodov@unipro.ru>
2013-07-25 09:29:00 +0000

    renamed according to new spec chapter numeration
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@540 2ecb45c1-dfb3-dc95-330a-13f216d23a04

--- src/Language/05_Variables/05_Variables_A03_t02.dart
+++ src/Language/05_Variables/05_Variables_A03_t02.dart
@@ -11,27 +11,14 @@
  * A library variable is implicitly static.
  * It is a compile-time error to preface a top-level variable declaration with
  * the built-in identiﬁer static.
- * @description Checks that a static variable is not associated with a particular instance.
+ * @description Checks that it is a compile-time error to preface a top-level
+ * variable declaration with the built-in identiﬁer static.
+ * @compile-error
  * @author kaigorodov
- * @reviewer iefremov
  */
-import "../../Utils/expect.dart";
  
-class Cl {
-  static var _foo;
-  int get foo {
-    return _foo;
-  }
-  void set foo(int val) {
-    _foo=val;
-  }
-}
+static var foo=1;
 
 main() {
-  Cl c1=new Cl();
-  Cl c2=new Cl();
-  c2.foo=1;
-  Expect.equals(1, c1.foo);
-  c2.foo=2;
-  Expect.equals(2, c1.foo);
+  Expect.equals(1, foo);
 }
---
+++ src/Language/12_Expressions/12_Instance_Creation_A01_t01.dart
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is a static type warning if the type T in an instance creation
+ * expression of one of the forms
+ * new T.id(a1 , . . . , an , xn+1 : an+1 , . . . , xn+k : an+k ),
+ * new T (a1 , . . . , an , xn+1 : an+1 , . . . , xn+k : an+k ),
+ * const T.id(a1 , . . . , an , xn+1 : an+1 , . . . , xn+k : an+k ),
+ * const T (a1 , . . . , an , xn+1 : an+1 , . . . , xn+k : an+k ) is malformed or malbounded.
+ * @description Checks that it is a static-warning if a constructor of
+ * a non-generic type invoked by a new expression is passed any type arguments.
+ * @static-warning
+ * @author msyabro
+ * @reviewer kaigorodov
+ */
+
+class A {
+}
+
+main() {
+  try {
+    var o = new A<int>();
+  } catch(e) {}
+}
