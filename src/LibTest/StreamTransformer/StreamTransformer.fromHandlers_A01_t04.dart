ade2056ce2d3eab29903013e0357d7431acac20a
Ilya.Vazhev@gmail.com <Ilya.Vazhev@gmail.com>
2013-10-22 14:54:53 +0000

    new async/StreamTransformer tests
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@635 2ecb45c1-dfb3-dc95-330a-13f216d23a04

---
+++ src/LibTest/StreamTransformer/StreamTransformer.fromHandlers_A01_t04.dart
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion factory StreamTransformer.fromHandlers({void handleData(S data,
+ *   EventSink<T> sink), void handleError(Object error, StackTrace stackTrace,
+ *   EventSink<T> sink), void handleDone(EventSink<T> sink)})
+ * Creates a StreamTransformer that delegates events to the given functions.
+ *
+ * @description Checks the case when all handlers are provided, errors
+ * converted to data, and handleDone() adds extra data elements.
+ * @author ilya
+ */
+import "dart:async";
+import "../../../Utils/async_utils.dart";
+import "../../../Utils/expect.dart";
+
+main() {
+  var s = new Stream.fromIterable([1,2,3,4,5]);
+  
+  // odd numbers as data events, even numbers as error events
+  s = s.map((x) => x.isOdd ? x : throw x);
+
+  var tr = new StreamTransformer.fromHandlers(
+      handleData:(x, sink) { sink.add(x); },
+      handleError: (x, st, sink) { sink.add(x); },
+      handleDone: (sink) { sink.add(6); sink.add(7); sink.close(); }
+  );
+  
+  asyncStart();
+  s.transform(tr).toList().then((x) {
+    Expect.listEquals([1,2,3,4,5,6,7], x);
+    asyncEnd();
+  });
+}
---
+++ src/LibTest/StreamTransformer/StreamTransformer_A03_t02.dart
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion factory StreamTransformer(StreamSubscription<T>
+ *   transformer(Stream<S> stream, bool cancelOnError))
+ * Creates a StreamTransformer.
+ *
+ * The result of invoking the transformer closure is a StreamSubscription.
+ * The bound stream-transformer (created by the bind method above) then
+ * sets the handlers it received as part of the listen call.
+ *
+ * @description Checks expected data events on transformed stream.
+ * @author ilya
+ */
+import "dart:async";
+import "../../../Utils/async_utils.dart";
+import "../../../Utils/expect.dart";
+
+// transform: skip evens, produce number and its double, convert errors to data
+myTransformer() {
+  return new StreamTransformer((s, _) {
+    var c = new StreamController();
+    s.listen((x) {
+      if (x.isOdd) {
+        c.add(x);
+        c.add(x*2);
+      }
+    }, onError:(x) {
+      c.add(x);
+    }, onDone:() {
+      c.close();
+    });
+    return c.stream.listen(null);
+  });
+}
+
+main() {
+  var c = new StreamController();
+  c.add(1);
+  c.add(2);
+  c.add(3);
+  c.add(4);
+  c.add(5);
+  c.addError(1);
+  c.addError(2);
+  c.addError(3);
+  c.addError(4);
+  c.addError(5);
+
+  asyncStart();
+  
+  c.stream.transform(myTransformer()).toList().then((x) {
+    Expect.listEquals([1,2,3,6,5,10,1,2,3,4,5], x);
+    asyncEnd();
+  });
+
+  c.close();
+}
