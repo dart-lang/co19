d5049c083a423d6c1a6d94a5ad2a1b7db0e94d1b
Ilya.Vazhev@gmail.com <Ilya.Vazhev@gmail.com>
2014-03-05 09:19:07 +0000

    fixed 671
    git-svn-id: http://co19.googlecode.com/svn/trunk/co19/tests/co19@700 2ecb45c1-dfb3-dc95-330a-13f216d23a04

--- src/Language/07_Classes/10_Superinterfaces_A07_t03.dart
+++ src/Language/07_Classes/10_Superinterfaces_A07_t03.dart
@@ -5,8 +5,7 @@
  */
 /**
  * @assertion Let C be a concrete class that does not declare its own
- * noSuchMethod() method and is not annotated with a metadata declaration
- * of the form @proxy, where proxy is declared in dart:core.
+ * noSuchMethod().
  * It is a static warning if the implicit interface
  * of C includes an instance member m of type F and C does not declare or
  * inherit a corresponding instance member m of type F' such that F' <: F.
