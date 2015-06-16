1cefd8126d73efba4461b8d5b6ab4995fc718faf
Sergey G. Grekhov <sgrekhov@unipro.ru>
2015-06-16 13:28:28 +0600

    Test commit. Layout test fixed (use NullTreeSanitizer)

--- LayoutTests/fast/text-autosizing/text-removal_t01.dart
+++ LayoutTests/fast/text-autosizing/text-removal_t01.dart
@@ -4,11 +4,12 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion 
- * @description 
+ * @assertion
+ * @description
  */
 import "dart:html";
 import "../../../Utils/expect.dart";
+import "../../testharness.dart";
 
 const String htmlEL1 = r'''
 <meta name="viewport" content="width=800">
@@ -35,7 +36,7 @@ const String htmlEL2 = r'''
 ''';
 
 void main() {
-    document.head.appendHtml(htmlEL1);
+    document.head.appendHtml(htmlEL1, treeSanitizer: new NullTreeSanitizer());
     document.body.appendHtml(htmlEL2);
     var forceLayout1 = document.body.offsetTop;
     var span1 = document.getElementById('span1');
