85facc77794dbaceeb122f98d42babddccb6373f
Sergey G. Grekhov <sgrekhov@unipro.ru>
2015-07-01 14:27:11 +0600

    Superclasses tests are actual now and new tests added

--- Language/07_Classes/9_Superclasses_A02_t07.dart
+++ Language/07_Classes/9_Superclasses_A02_t07.dart
@@ -6,19 +6,16 @@
 /**
  * @assertion It is a compile-time error if the extends clause of a class C specifies an
  * enumerated type, a malformed type or a deferred type as a superclass.
- * @description Checks that it is a compile-time error when the type expression
- * in a class's extends clause denotes a function type.
+ * @description Checks that it is a compile-time error if enumeration is used as a
+ * superclass
  * @compile-error
- * @author rodionov
- * @reviewer iefremov
+ * @author sgrekhov@unipro.ru
  */
 
-typedef void foo();
 
-class A extends foo {}
+enum E {a, b, c}
+
+class A extends E {}
 
 main() {
-  try {
-    A a = new A();
-  } catch (e){}
 }
