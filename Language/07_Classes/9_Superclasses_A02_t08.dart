5e7d4a6c3d76263e10e43f95e555fd39daee38b5
Sergey G. Grekhov <sgrekhov@unipro.ru>
2015-07-03 14:04:43 +0600

    Test for deferred type as a superclass added

---
+++ Language/07_Classes/9_Superclasses_A02_t08.dart
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is a compile-time error if the extends clause of a class C specifies an
+ * enumerated type, a malformed type or a deferred type as a superclass.
+ * @description Checks that it is a compile-time error if deferred type is used as a
+ * superclass
+ * @compile-error
+ * @author sgrekhov@unipro.ru
+ */
+
+
+import "dart:core" deferred as core;
+
+class A extends core.ArgumentError {}
+
+main() {
+}
