fd077f6d8815c8e730d89bbc846cd301e729e5b3
Sergey G. Grekhov <sgrekhov@unipro.ru>
2015-09-01 15:27:27 +0600

    Compile-time errors tests added

---
+++ Language/Errors_and_Warnings/compile_error_t05.dart
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Compile-time errors are errors that preclude execution.
+ * A compile-time error must be reported by a Dart compiler before the
+ * erroneous code is executed.
+ * @description Checks that erroneous code is not executed if there is
+ * a compile-time error. Test error in class constructor
+ * @compile-error
+ * @author sgrekhov@unipro.ru
+ */
+
+import '../../Utils/expect.dart';
+
+class C {
+  С() {
+    var a = ;
+  }
+}
+
+main() {
+  C c = new C();
+  Expect.fail("Erroneous code must not be executed");
+}
