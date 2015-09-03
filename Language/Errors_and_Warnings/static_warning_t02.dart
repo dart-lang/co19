d83d507ad2d56d1711098820b23722d607b2615f
Sergey G. Grekhov <sgrekhov@unipro.ru>
2015-09-03 14:16:25 +0600

    Errors and Warnings tests. Added static warning tests

---
+++ Language/Errors_and_Warnings/static_warning_t02.dart
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Static warnings are those errors reported by the static checker.
+ * They have no effect on execution
+ * @description Checks that static warning in class constructor has
+ * no effect on execution
+ * @static-warning
+ * @author sgrekhov@unipro.ru
+ */
+
+import '../../Utils/expect.dart';
+
+class C {
+  C() {
+    while ("") { // static type warning
+    }
+  }
+}
+
+main() {
+  int i = 0;
+  try {
+    C c = new C();
+    i++;
+  } catch (e) {
+    Expect.fail("No exeption should be thrown");
+  }
+  Expect.equals(1, i, "There should be no effect on execution");
+}
---
+++ Language/Errors_and_Warnings/static_warning_t04.dart
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Static warnings are those errors reported by the static checker.
+ * They have no effect on execution
+ * @description Checks that static warning in top-level function has
+ * no effect on execution
+ * @static-warning
+ * @author sgrekhov@unipro.ru
+ */
+
+import '../../Utils/expect.dart';
+
+void f() {
+  while ("") { // static type warning
+  }
+}
+
+main() {
+  int i = 0;
+  try {
+    f();
+    i++;
+  } catch (e) {
+    Expect.fail("No exeption should be thrown");
+  }
+  Expect.equals(1, i, "There should be no effect on execution");
+}
