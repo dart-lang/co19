b618859e63455033d9f95404b1d3ec67bd7cf798
Sergey G. Grekhov <sgrekhov@unipro.ru>
2015-09-03 11:45:40 +0600

    Minor code style changes

--- Utils/async_utils.dart
+++ Utils/async_utils.dart
@@ -7,17 +7,17 @@ import "expect.dart";
 const ONE_MS = const Duration(milliseconds: 1);
 
 Duration durationMs(delay) {
-  return delay == null? Duration.ZERO : ONE_MS * delay;
+  return delay == null ? Duration.ZERO : ONE_MS * delay;
 }
 
-Future runLater(void action(), [int delay=0]) {
+Future runLater(void action(), [int delay = 0]) {
   asyncStart();
   return new Future.delayed(durationMs(delay), (){
     action();
     asyncEnd();
   });
 }
-  
+
 Future runAfter(Future f, void action()) {
   asyncStart();
   return f.whenComplete((){
@@ -25,12 +25,12 @@ Future runAfter(Future f, void action()) {
     asyncEnd();
   });
 }
-  
+
 /*----------------------------*/
 
 /**
  * Let the test driver know the test is asynchronous and
- * continues after the method main() exits.  
+ * continues after the method main() exits.
  * see co19 issue #423
  * http://code.google.com/p/co19/issues/detail?id=423
  */
@@ -51,14 +51,14 @@ void  asyncStart() {
 
 void  asyncMultiStart(int delta) {
 //  print("asyncMultiStart $delta");
-  _asyncCounter+=delta;
+  _asyncCounter += delta;
 }
 
 void  asyncEnd() {
 //  print("asyncEnd");
-  Expect.isFalse(_asyncCounter==0, "asyncEnd: _asyncCounter==0");
+  Expect.isFalse(_asyncCounter == 0, "asyncEnd: _asyncCounter==0");
   _asyncCounter--;
-  if (_asyncCounter==0) {
+  if (_asyncCounter == 0) {
     print("unittest-suite-success");
     _completer.complete(null);
   }
@@ -81,7 +81,8 @@ abstract class StreamListener<T> {
     if (subscription!=null) {
        subscription.cancel();
     }
-    subscription=stream.listen(onData, onError:onError, onDone:onDone, cancelOnError:cancelOnError);
+    subscription=stream.listen(onData, onError:onError, onDone:onDone,
+      cancelOnError:cancelOnError);
     return subscription;
   }
 }
@@ -90,22 +91,22 @@ abstract class StreamListener<T> {
 
 class Sync2<T> {
   Function fire;
-  bool firstPut=false, secondPut=false;
+  bool firstPut = false, secondPut = false;
   T val1, val2;
-  
+
   Sync2(this.fire);
-  
+
   void put1(T val) {
     val1=val;
-    firstPut=true;
+    firstPut = true;
     if (secondPut) {
       fire(val1, val2);
     }
   }
-  
+
   void put2(T val) {
-    val2=val;
-    secondPut=true;
+    val2 = val;
+    secondPut = true;
     if (firstPut) {
       fire(val1, val2);
     }
