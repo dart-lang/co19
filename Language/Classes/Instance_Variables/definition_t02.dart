fc7a2bb51a6e9c6169f56fdf8ebaf233c89cd430
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2015-09-04 16:34:40 +0600

    fix typo

--- Language/Classes/Instance_Variables/definition_t02.dart
+++ Language/Classes/Instance_Variables/definition_t02.dart
@@ -7,7 +7,7 @@
  * @assertion Instance variables are variables whose declarations are
  * immediately contained within a class declaration and that are not declared
  * static. The instance variables of a class C are those instance variables
- * declared by C and the instancev ariables inherited by C from its superclass.
+ * declared by C and the instance variables inherited by C from its superclass.
  * @description Checks that class instance contains declared instance variable
  * and inherited from superclass
  * @author sgrekhov@unipro.ru
