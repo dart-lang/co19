ec981af8a6e299ee1cc2920b234632ad63be07d4
Sergey G. Grekhov <sgrekhov@unipro.ru>
2015-09-11 13:49:17 +0600

    More Getter Access and Method Extraction tests

---
+++ Language/Expressions/Property_Extraction/Getter_Access_and_Method_Extraction/class_object_member_t04.dart
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is a compile-time error if m is a member of class Object and
+ * e is either a prefix object or a constant type literal.
+ * @description Check that it is a compile-time error if in expression e.m
+ * e is a library prefix and m is Object's toString() method
+ * @issue 24332
+ * @compile-error
+ * @author sgrekhov@unipro.ru
+ */
+import 'instance_of_type_lib.dart' as lib;
+
+main() {
+  var x = lib.toString;
+}
