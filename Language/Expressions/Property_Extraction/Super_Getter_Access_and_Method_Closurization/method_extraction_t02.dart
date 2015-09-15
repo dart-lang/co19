022055041e1000bf8c6aa945946104d28700eb36
Sergey G. Grekhov <sgrekhov@unipro.ru>
2015-09-15 17:31:02 +0600

    Super Getter Access and Method Closurization tests

---
+++ Language/Expressions/Property_Extraction/Super_Getter_Access_and_Method_Closurization/method_extraction_t02.dart
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Evaluation of a property extraction i of the form super.m
+ * proceeds as follows:
+ * Let S be the superclass of the immediately enclosing class. Let f be the
+ * result of looking up method m in S with respect to the current library L. If
+ * method lookup succeeds then i evaluates to the closurization of method f
+ * with respect to superclass S
+ * @description Check that if method lookup succeeds then result of the
+ * property extraction is method that was found in superclass defined in the
+ * library
+ * @author sgrekhov@unipro.ru
+ */
+import '../../../../Utils/expect.dart';
+import 'method_extraction_lib.dart';
+
+class C extends A {
+  int m() => 2;
+  void test() {
+    var f = super.m;
+    Expect.equals(1, f());
+  }
+}
+
+main() {
+  C c = new C();
+  c.test();
+}
