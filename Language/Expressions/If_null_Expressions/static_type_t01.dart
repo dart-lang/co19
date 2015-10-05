63fd10db664b14eb2fb1ee0fbf022989aa24e8c4
Aleksei Semenov <alsemenov@users.noreply.github.com>
2015-10-05 17:19:00 +0600

    tests for chapter 'If-null Expressions'

--- Language/Expressions/If_null_Expressions/static_type_t01.dart
+++ Language/Expressions/If_null_Expressions/static_type_t01.dart
@@ -8,33 +8,26 @@
  * equivalent ... The static type of e is least upper bound (19.8.2) of the
  * static type of e1 and the static type of e2 .
  *
- * @description Check static type of expression e1??e2
+ * @description Check that static type of expression e1??e2 match with
+ * static type (int) in assignment.
  *
+ * @static-clean
  * @author a.semenov@unipro.ru
  */
-import '../../../Utils/expect.dart';
+
 import '../../../Utils/dynamic_check.dart';
 
-void static_int(int a){
-}
-
-void static_string(String a){
-}
 
 main() {
-  static_int( 1 ?? 2 ); /// 01: Ok
-  static_int( 1 ?? 2.0 ); /// 02: Ok
-  checkTypeError(() => static_int( 1.0 ?? 2.0 )); /// 03: static type warning
-  static_int( null ?? null );  /// 04: Ok
-  static_int( null ?? 1 );  /// 05: Ok
-  checkTypeError(() => static_int( null ?? 2.0 )); /// 06: static type warning
-  checkTypeError(() => static_int( true ?? false )); /// 07: static type warning
-
-  checkTypeError(() => static_string( 1 ?? 2 )); /// 07: static type warning
-  static_string( 'aaa' ?? 'bbb' ); /// 08: Ok
-  static_string( 'bbb' ?? 1 ); /// 09: Ok
-  static_string( 'bbb' ?? null ); /// 10: Ok
-  static_string( null ?? 'bbb' ); /// 11: Ok
-  checkTypeError(() => static_string( 2.0 ?? 1 )); /// 12: static type warning
-
+  int x = 1 ?? 2;
+  x = 1 ?? 2.0;
+  checkTypeError(() { int y = 2.0 ?? 1; return y; });
+  x = null ?? 1;
+  x = null ?? null;
+  x = 1 ?? null;
+  x = 1 ?? 'aaa';
+  checkTypeError(() { int y = 'aaa' ?? 1; return y; });
+  checkTypeError(() { int y = 'aaa' ?? true; return y; });
+  return x; // use x, so dart analyzer is happy
 }
+
---
+++ Language/Expressions/If_null_Expressions/static_type_t02.dart
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Evaluation of an if-null expression e of the form e1??e2 is
+ * equivalent ... The static type of e is least upper bound (19.8.2) of the
+ * static type of e1 and the static type of e2 .
+ *
+ * @description Check that static type of expression e1??e2 do not match with
+ * static type int in assignment
+ *
+ * @static-warning
+ * @author a.semenov@unipro.ru
+ *
+ */
+import 'dart:collection';
+import '../../../Utils/dynamic_check.dart';
+
+main() {
+  checkTypeError((){int x = 1.0 ?? 2.0; return x;}); /// static type warning
+  checkTypeError((){int x = null ?? 2.0; return x;}); /// static type warning
+  checkTypeError((){int x = 'aaa' ?? 'bbb'; return x;}); /// static type warning
+  checkTypeError((){int x = true ?? false; return x;}); /// static type warning
+  checkTypeError((){
+    int x = new LinkedHashMap() ?? new SplayTreeMap(); /// static type warning
+    return x;
+  });
+  checkTypeError((){int x = 'aaa' ?? null; return x;}); /// static type warning
+}
+
