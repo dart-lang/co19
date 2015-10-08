7fba1203a705c9026adfd1234bbfdef58c1f653f
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2015-10-08 18:02:44 +0600

    tests for chapter 'Await Expressions'

---
+++ Language/Expressions/Await_Expressions/syntax_t03.dart
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion
+ *  awaitExpression:
+ *      await unaryExpression
+ *
+ * @description Check that it is a compile error if unaryExpression is missing
+ * @compile-error
+ * @author a.semenov@unipro.ru
+ */
+import 'dart:async';
+
+f() {
+  return new Future<int>.value(1);
+}
+
+test() async {
+  await;
+}
+
+main() {
+  test();
+}
