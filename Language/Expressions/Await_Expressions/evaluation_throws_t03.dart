a9ecb260b6a7a1891ac63bc7643ce14e11479f84
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2015-10-15 09:23:17 +0600

    tests for chapter 'Await Expressions'

---
+++ Language/Expressions/Await_Expressions/evaluation_throws_t03.dart
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Evaluation of an await expression a of the form await e
+ * proceeds as follows: First, the expression e is evaluated. Next:
+ * If e raises an exception x, then an instance f of class Future is allocated
+ * and later completed with x. ... If f has completed with an exception x,
+ * a raises x.
+ *
+ * @description Check that if evaluation of e produces
+ * IntegerDivisionByZeroException, then await expression throws the same
+ * exception.
+ *
+ * @author a.semenov@unipro.ru
+ */
+import '../../../Utils/expect.dart';
+
+main() async {
+  try {
+    await (1 ~/ 0);
+    Expect.fail('await expression should throw  IntegerDivisionByZeroException');
+  } catch (x){
+    Expect.isTrue(x is IntegerDivisionByZeroException);
+  }
+}
