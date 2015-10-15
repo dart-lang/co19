d94d4fd7d197a4ce5cb956a9bf82f53ec4dc9ead
Sergey G. Grekhov <sgrekhov@unipro.ru>
2015-10-15 13:40:38 +0600

    Ordinary member Closurization tests. Getters and setters added

---
+++ Language/Expressions/Property_Extraction/Ordinary_Member_Closurization/getter_closurization_t01.dart
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion The closurization of getter f on object o is defined to be
+ * equivalent to (){return u.m;} if f is named m, except that iff
+ * identical(o1, o2) then o1#m == o2#m.
+ *
+ * @description Check that closurization of getter is equivalent to
+ * (){return u.m;}
+ *
+ * @author sgrekhov@unipro.ru
+ */
+import '../../../../Utils/expect.dart';
+
+class C {
+  int get m => 1;
+}
+
+main() {
+  C o = new C();
+  var f = o#m;
+
+  var f1 = () {return o.m;};
+
+  Expect.equals(f(), f1());
+}
