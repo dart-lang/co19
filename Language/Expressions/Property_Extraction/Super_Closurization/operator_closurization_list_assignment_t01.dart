fd0a20920aa2232656bad05cf80058dc42dc7fd3
Sergey G. Grekhov <sgrekhov@yandex.ru>
2015-10-20 13:41:54 +0600

    Super closurization. Test for operators added

---
+++ Language/Expressions/Property_Extraction/Super_Closurization/operator_closurization_list_assignment_t01.dart
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion The closurization of method f with respect to superclass S is
+ *  defined to be equivalent to:
+ * ...
+ * • (a, b){return super[a]= b;} if f is named []=
+ *
+ * @description Check that closurization of operator []= on the superclass is
+ * equivalent to (a, b) {return super[a]= b;}
+ *
+ * @author sgrekhov@unipro.ru
+ */
+import '../../../../Utils/expect.dart';
+
+class C {
+  var values = [1, 2, 3];
+  void operator []= (var a, b) {
+    return values[a] = b;
+  }
+}
+
+main() {
+  C o = new C();
+  var f = o#[]=;
+
+  var f1 = (a, b) {return o[a] = b;};
+
+  f(0, 10);
+  Expect.equals(10, o.values[0]);
+  o.values[0] = 1;
+  f1(0, 10);
+  Expect.equals(10, o.values[0]);
+
+  f(1, 20);
+  Expect.equals(20, o.values[1]);
+  o.values[1] = 2;
+  f1(1, 20);
+  Expect.equals(20, o.values[1]);
+
+  f(2, 30);
+  Expect.equals(30, o.values[2]);
+  o.values[2] = 3;
+  f1(2, 30);
+  Expect.equals(30, o.values[2]);
+}
---
+++ Language/Expressions/Property_Extraction/Super_Closurization/operator_closurization_t13.dart
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertionThe closurization of method f with respect to superclass S is
+ * defined to be equivalent to:
+ * • (a){return super op a;} if f is named op and op is one of <, >, <=, >=,
+ * ==, -, +, /, ˜/, *, %, |, ˆ, &, <<, >>
+ *
+ * @description Check that closurization of operator << of the superclass is
+ * equivalent to (a) {return super << a;}
+ *
+ * @author sgrekhov@unipro.ru
+ */
+import '../../../../Utils/expect.dart';
+
+class A {
+  int operator << (var v) {
+    return v << 2;
+  }
+}
+
+class C extends A {
+  void test() {
+    var f1 = super#<<;
+    var f2 = (a) {return super << a;};
+
+    Expect.equals(f1(2), f2(2));
+    Expect.equals(f1(-4), f2(-4));
+    Expect.notEquals(f1(3), f2(2));
+  }
+}
+
+main() {
+  C o = new C();
+  o.test();
+}
