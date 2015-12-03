1fc341fd83137b1097c17197076d50357ddf959b
Natalya Golovleva <ngl@unipro.ru>
2015-12-03 16:32:24 +0600

    Fixed assertion text.

--- Language/Expressions/This/placement_t06.dart
+++ Language/Expressions/This/placement_t06.dart
@@ -4,10 +4,10 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion It is a compile-time error if this appears in a top-level
- * function or variable initializer, in a factory constructor, or in a static
- * method or variable initializer, or in the initializer of an instance
- * variable.
+ * @assertion It is a compile-time error if this appears, implicitly or
+ * explicitly, in a top-level function or variable initializer, in a factory
+ * constructor, or in a static method or variable initializer, or in the
+ * initializer of an instance variable.
  * @description Checks that it is a compile-error if 'this' appears in a static
  * method.
  * @compile-error
