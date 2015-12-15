90a89a7497ef2a6733323e3af30eaf06704b7403
Natalya Golovleva <ngl@unipro.ru>
2015-12-15 22:45:37 +0600

    Expressions/Function_Invocation/Unqualified_Invocation test refactored

--- Language/Expressions/Function_Invocation/Unqualified_Invocation/invocation_t04.dart
+++ Language/Expressions/Function_Invocation/Unqualified_Invocation/invocation_t04.dart
@@ -4,22 +4,22 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion An unqualified function invocation i has the form id(a1, ... , an, xn+1 : an+1, ... , xn+k :
- * an+k), where id is an identifier.
- * If there exists a lexically visible declaration named id, let fid be the inner-
- * most such declaration. Then:
- *  - If fid is a local function, a library function, a library or static getter or a
- * variable then i is interpreted as a function expression invocation.
- *  - Otherwise, if fid is a static method of the enclosing class C, i is equivalent to
- * C.id(a1, ... , an, xn+1 : an+1, ... , xn+k : an+k).
- *  - Otherwise, fid is necessarily an instance method or getter of the enclosing
- * class C, and i is equivalent to the ordinary method invocation
- * this.id(a1, ... , an, xn+1 : an+1, ... , xn+k : an+k).
+ * @assertion An unqualified function invocation i has the form
+ * id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k), where id is an identifier.
+ * If there exists a lexically visible declaration named id, let fid be the
+ * innermost such declaration. Then:
+ *  - If fid is a local function, a library function, a library or static getter
+ *    or a variable then i is interpreted as a function expression invocation.
+ *  - Otherwise, if fid is a static method of the enclosing class C, i is
+ *    equivalent to C.id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k).
+ *  - Otherwise, fid is necessarily an instance method or getter of the
+ *    enclosing class C, and i is equivalent to the ordinary method invocation
+ *    this.id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k).
  * Otherwise, if i occurs inside a top level or static function (be it function,
  * method, getter, or setter) or variable initializer, evaluation of i causes a
  * NoSuchMethodError to be thrown.
  * If i does not occur inside a top level or static function, i is equivalent to
- * this.id(a1, ... , an, xn+1 : an+1, ... , xn+k : an+k).
+ * this.id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k).
  * @description Checks that if fid is a static getter, it is evaluated as
  * a function expression invocation.
  * @author msyabro
@@ -35,7 +35,7 @@ class A {
       return "";
     };
   }
-  
+
   static get n {
     return "";
   }
@@ -47,7 +47,7 @@ class A {
     try {
       n("a", 1);
       Expect.fail("NoSuchMethodError expected");
-    } on NoSuchMethodError catch(ok) {}
+    } on NoSuchMethodError catch (ok) {}
   }
 }
 
--- Language/Expressions/Function_Invocation/Unqualified_Invocation/invocation_t08.dart
+++ Language/Expressions/Function_Invocation/Unqualified_Invocation/invocation_t08.dart
@@ -4,24 +4,25 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion An unqualified function invocation i has the form id(a1, ... , an, xn+1 : an+1, ... , xn+k :
- * an+k), where id is an identifier.
- * If there exists a lexically visible declaration named id, let fid be the inner-
- * most such declaration. Then:
- *  - If fid is a local function, a library function, a library or static getter or a
- * variable then i is interpreted as a function expression invocation.
- *  - Otherwise, if fid is a static method of the enclosing class C, i is equivalent to
- * C.id(a1, ... , an, xn+1 : an+1, ... , xn+k : an+k).
- *  - Otherwise, fid is necessarily an instance method or getter of the enclosing
- * class C, and i is equivalent to the ordinary method invocation
- * this.id(a1, ... , an, xn+1 : an+1, ... , xn+k : an+k).
+ * @assertion An unqualified function invocation i has the form
+ * id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k), where id is an identifier.
+ * If there exists a lexically visible declaration named id, let fid be the
+ * innermost such declaration. Then:
+ *  - If fid is a local function, a library function, a library or static getter
+ *    or a variable then i is interpreted as a function expression invocation.
+ *  - Otherwise, if fid is a static method of the enclosing class C, i is
+ *    equivalent to C.id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k).
+ *  - Otherwise, fid is necessarily an instance method or getter of the
+ *    enclosing class C, and i is equivalent to the ordinary method invocation
+ *    this.id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k).
  * Otherwise, if i occurs inside a top level or static function (be it function,
  * method, getter, or setter) or variable initializer, evaluation of i causes a
  * NoSuchMethodError to be thrown.
  * If i does not occur inside a top level or static function, i is equivalent to
- * this.id(a1, ... , an, xn+1 : an+1, ... , xn+k : an+k).
- * @description Checks that if fid is an instance method of a class, unqualified invocation is
- * equivalent to a method invocation this.id(a1, ... , an, xn+1 : an+1, ... , xn+k : an+k).
+ * this.id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k).
+ * @description Checks that if fid is an instance method of a class,
+ * unqualified invocation is equivalent to a method invocation
+ * this.id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k).
  * @author msyabro
  * @reviewer rodionov
  */
@@ -37,7 +38,7 @@ class C extends S {
   m() {
     return "instance method";
   }
-  
+
   test() {
     Expect.equals("instance method", m());
     Expect.equals(this.m(), m());
