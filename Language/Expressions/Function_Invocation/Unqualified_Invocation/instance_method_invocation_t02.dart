a2fddb195281cb5a6fd4075d3834a935594faa21
Natalya Golovleva <ngl@unipro.ru>
2015-12-23 15:25:48 +0600

    Expressions/Function_Invocation/Unqualified_Invocation tests renamed

--- Language/Expressions/Function_Invocation/Unqualified_Invocation/instance_method_invocation_t02.dart
+++ Language/Expressions/Function_Invocation/Unqualified_Invocation/instance_method_invocation_t02.dart
@@ -8,18 +8,11 @@
  * id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k), where id is an identifier.
  * If there exists a lexically visible declaration named id, let fid be the
  * innermost such declaration. Then:
- *  - If fid is a local function, a library function, a library or static getter
- *    or a variable then i is interpreted as a function expression invocation.
- *  - Otherwise, if fid is a static method of the enclosing class C, i is
- *    equivalent to C.id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k).
- *  - Otherwise, fid is necessarily an instance method or getter of the
+ * . . .
+ *  • Otherwise, fid is necessarily an instance method or getter of the
  *    enclosing class C, and i is equivalent to the ordinary method invocation
  *    this.id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k).
- * Otherwise, if i occurs inside a top level or static function (be it function,
- * method, getter, or setter) or variable initializer, evaluation of i causes a
- * NoSuchMethodError to be thrown.
- * If i does not occur inside a top level or static function, i is equivalent to
- * this.id(a1, ..., an, xn+1: an+1, ..., xn+k: an+k).
+ * . . .
  * @description Checks that if fid is a instance variable declaration,
  * then i is equivalent to this.id().
  * @author ilya
