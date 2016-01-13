4765b5cc82ac7dcbdae73cb88b4a446ac9429cd6
Sergey G. Grekhov <sgrekhov@unipro.ru>
2016-01-13 18:06:52 +0600

    Debug code removed

--- LayoutTests/fast/css/css-selector-text_t01.dart
+++ LayoutTests/fast/css/css-selector-text_t01.dart
@@ -12,8 +12,7 @@ import "../../testcommon.dart";
 main() {
   document.head.id = "head";
 
-  parseThenSerializeRule(rule)
-  {
+  parseThenSerializeRule(rule) {
     var styleElement = document.getElementById("style");
     var head = document.getElementById("head");
     if (styleElement != null)
@@ -25,7 +24,6 @@ main() {
     head.append(styleElement);
 
     styleElement.append(new Text(rule));
-    print('rule=' + rule + ', css=' + styleElement.sheet.cssRules[0].cssText);
     return styleElement.sheet.cssRules[0].cssText;
   }
 
@@ -38,7 +36,6 @@ main() {
   }
 
   testSelectorRoundTrip(selector) {
-    //shouldBe(parseThenSerializeRule("$selector { }"), "$selector { }");
     shouldBeIgnoreSpaces(parseThenSerializeRule("$selector {}"), "$selector {}");
   }
 
@@ -55,24 +52,11 @@ main() {
   testSelectorRoundTrip(r'[a$="b"]');
   testSelectorRoundTrip('[a*="b"]');
 
-  debug('');
-
-  //testSelectorRoundTrip('*|a');
-  //testSelectorRoundTrip('n|a');
-  //testSelectorRoundTrip('*|*');
-  //testSelectorRoundTrip('n|*');
-  //testSelectorRoundTrip('[*|a]');
-  //testSelectorRoundTrip('[n|a]');
-
-  //debug('');
-
   testSelectorRoundTrip('a:active');
   testSelectorRoundTrip('a b');
   testSelectorRoundTrip('a + b');
   testSelectorRoundTrip('a ~ b');
   testSelectorRoundTrip('a > b');
-
-  debug('');
 
   testSelectorRoundTrip(":active");
   testSelectorRoundTrip(":checked");
@@ -89,88 +73,13 @@ main() {
   testSelectorRoundTrip(":target");
   testSelectorRoundTrip(":visited");
 
-  debug('');
-
   testSelectorRoundTrip(":lang(a)");
   testSelectorRoundTrip(":not(a)");
-  //testSelectorRoundTrip(":-webkit-any(a,b,p)");
-
-  debug('');
-
-  //testSelectorRoundTrip("::after");
-  //testSelectorRoundTrip("::before");
-  //testSelectorRoundTrip("::first-letter");
-  //testSelectorRoundTrip("::first-line");
-  //testSelectorRoundTrip("::selection");
-/*
-  debug('');
-
-  testSelectorRoundTrip(":-webkit-any-link");
-  testSelectorRoundTrip(":-webkit-autofill");
-  testSelectorRoundTrip(":-webkit-drag");
-
-  debug('');
-
-  shouldBe(parseThenSerializeRule('::-webkit-file-upload-button { }'), '*::-webkit-file-upload-button { }');
-  shouldBe(parseThenSerializeRule('::-webkit-search-cancel-button { }'), '*::-webkit-search-cancel-button { }');
-  shouldBe(parseThenSerializeRule('::-webkit-search-decoration { }'), '*::-webkit-search-decoration { }');
-  shouldBe(parseThenSerializeRule('::-webkit-search-results-button { }'), '*::-webkit-search-results-button { }');
-  shouldBe(parseThenSerializeRule('::-webkit-search-results-decoration { }'), '*::-webkit-search-results-decoration { }');
-  shouldBe(parseThenSerializeRule('::-webkit-slider-thumb { }'), '*::-webkit-slider-thumb { }');
-
-  debug('');
-
-  testSelectorRoundTrip("a::-webkit-slider-thumb");
-  shouldBe(parseThenSerializeRule('a ::-webkit-slider-thumb { }'), 'a *::-webkit-slider-thumb { }');
-  testSelectorRoundTrip("[a]::-webkit-slider-thumb");
-  shouldBe(parseThenSerializeRule('[a] ::-webkit-slider-thumb { }'), '[a] *::-webkit-slider-thumb { }');
-  testSelectorRoundTrip(".a::-webkit-slider-thumb");
-  shouldBe(parseThenSerializeRule('.a ::-webkit-slider-thumb { }'), '.a *::-webkit-slider-thumb { }');
-  testSelectorRoundTrip("#a::-webkit-slider-thumb");
-  shouldBe(parseThenSerializeRule('#a ::-webkit-slider-thumb { }'), '#a *::-webkit-slider-thumb { }');
-  shouldBe(parseThenSerializeRule('* ::-webkit-slider-thumb { }'), '* *::-webkit-slider-thumb { }');
-
-  debug('');
-
-  testSelectorRoundTrip("a[b]::-webkit-slider-thumb");
-  testSelectorRoundTrip("a.b::-webkit-slider-thumb");
-  testSelectorRoundTrip("a#b::-webkit-slider-thumb");
-  testSelectorRoundTrip("a[b].c#d::-webkit-slider-thumb");
-
-  debug('');
-
-  testSelectorRoundTrip('input:not([type="file"]):focus');
-  testSelectorRoundTrip(':-webkit-any([type="file"])');
-  testSelectorRoundTrip(':-webkit-any(:hover)');
-  testSelectorRoundTrip('input:-webkit-any([type="file"],:hover,:focus):enabled');
-  testSelectorRoundTrip(':-webkit-any(input[type="file"],a:hover,button:focus)');
-  testSelectorRoundTrip(':-webkit-any(.class1.class2.class3)');
-  testSelectorRoundTrip(':-webkit-any(.class1:hover)');
-  testSelectorRoundTrip(':-webkit-any(a.class1.class2.class3:hover)');
-*/
-  debug('');
 
   shouldBeIgnoreSpaces(parseThenSerializeRule('*:active { }'), ':active { }');
-  //shouldBe(parseThenSerializeRule('|a { }'), 'a { }');
   shouldBeIgnoreSpaces(parseThenSerializeRule('|a { }'), '|a { }');
-
-  debug('');
-
-  //shouldBe(parseThenSerializeRule('input[type=file]:focus { }'), 'input[type="file"]:focus { }');
-
-  debug('');
 
   shouldBeIgnoreSpaces(parseThenSerializeRule('a+b { }'), 'a + b { }');
   shouldBeIgnoreSpaces(parseThenSerializeRule('a~b { }'), 'a ~ b { }');
   shouldBeIgnoreSpaces(parseThenSerializeRule('a>b { }'), 'a > b { }');
-
-  debug('');
-/*
-  shouldBeIgnoreSpaces(parseThenSerializeRule(':after { }'), '::after { }');
-  shouldBeIgnoreSpaces(parseThenSerializeRule(':before { }'), '::before { }');
-  shouldBeIgnoreSpaces(parseThenSerializeRule(':first-letter { }'), '::first-letter { }');
-  shouldBeIgnoreSpaces(parseThenSerializeRule(':first-line { }'), '::first-line { }');
-  //shouldBe(parseThenSerializeRule(':-webkit-any(    a.class1  ,  	#id,[attr]  ) { }'),':-webkit-any(a.class1,#id,[attr]) { }');
-
-  debug('');*/
 }
