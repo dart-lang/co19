f9056e5eafea4ede97d9d10257096f624cbe2314
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2016-01-28 16:30:52 +0600

    tests for chapter 'Yield'

---
+++ Language/Statements/Yield_and_Yield_Each/Yield/execution_sync_t01.dart
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Execution of a statement s of the form yield e; proceeds as
+ * follows:
+ *    First, the expression e is evaluated to an object o. If the enclosing
+ *  function m is marked async* and the stream u associated with m has been
+ *  paused, then execution of m is suspended until u is resumed or canceled.
+ *    Next, o is added to the iterable or stream associated with the immediately
+ *  enclosing function.
+ * ...
+ * If the enclosing function m is marked sync*  then:
+ *   - Execution of the function m immediately enclosing s is suspended until
+ *   the nullary method moveNext() is invoked upon the iterator used to initiate
+ *   the current invocation of m.
+ *   - The current call to moveNext() returns true.
+ *
+ * @description Check that object o is added to iterable associated with the
+ * immediately enclosing function.
+ *
+ * @author a.semenov@unipro.ru
+ */
+import 'dart:async';
+import '../../../../Utils/async_utils.dart';
+import '../../../../Utils/expect.dart';
+
+Iterable generator(Iterable iterable) sync* {
+  for (var o in iterable) {
+    yield o;
+  }
+}
+
+main() {
+  List data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  List log = [];
+  Iterable iterable = generator(data);
+  Iterator<int> it = iterable.iterator;
+  while(it.moveNext()){
+    log.add(it.current);
+  }
+  Expect.listEquals(data, log);
+}
---
+++ Language/Statements/Yield_and_Yield_Each/Yield/static_type_t01.dart
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Let T be the static type of e and let f be the immediately
+ * enclosing function. It is a static type warning if either:
+ *  • the body of f is marked async* and the type Stream<T> may not be
+ *  assigned to the declared return type of f.
+ *  • the body of f is marked sync* and the type Iterable<T> may not be
+ *  assigned to the declared return type of f.
+ *
+ * @description Check that static warning is issued, if the body of f is marked
+ * sync* and the type Iterable<T> may not be assigned to the declared return
+ * type of f.
+ *
+ * @author a.semenov@unipro.ru
+ */
+import '../../../../Utils/expect.dart';
+
+Iterable<String> generator() sync* {
+  yield 1; /// static type warning
+}
+
+main() {
+
+  Expect.equals(1, generator().first);
+}
