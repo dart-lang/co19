8a25b46d591b7fa7e0a3bfff854ea0c4416ba25e
Natalya Golovleva <ngl@unipro.ru>
2016-02-25 20:53:15 +0600

    Language/Statements/For/For_Loop tests refactored.

--- Language/Statements/For/For_Loop/execution_t07.dart
+++ Language/Statements/For/For_Loop/execution_t07.dart
@@ -4,13 +4,12 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion Execution of a for statement of the form for (var v = e' ; c; e) s 
+ * @assertion Execution of a for statement of the form for (var v = e0; c; e) s
  * proceeds as follows:
  * If c is empty then let c' be true otherwise let c' be c.
- * First the variable declaration statement var v = e' is executed. Then:
- * 
- * 1. If this is the first iteration of the for loop, let v' be v. Otherwise, let v' be
- *    the variable v'' created in the previous execution of step 4.
+ * First the variable declaration statement var v = e0 is executed. Then:
+ * 1. If this is the first iteration of the for loop, let v' be v. Otherwise,
+ *    let v' be the variable v'' created in the previous execution of step 4.
  * 2. The expression [v'/v]c is evaluated and subjected to boolean conversion.
  *    If the result is false, the for loop completes. Otherwise, execution
  *    continues at step 3.
@@ -18,7 +17,7 @@
  * 4. Let v'' be a fresh variable. v'' is bound to the value of v'.
  * 5. The expression [v''/v]e is evaluated, and the process recurses at step 1.
  * @description Checks that it is a dynamic type error if c is not of type bool
- * in checked mode. 
+ * in checked mode.
  * @static-warning
  * @author vasya
  * @reviewer rodionov
