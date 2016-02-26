baddb80cde77fdd9157dbe6bd0a15bdcd00de361
Natalya Golovleva <ngl@unipro.ru>
2016-02-26 16:27:32 +0600

    Language/Statements/While tests refactored.

--- Language/Statements/While/execution_t02.dart
+++ Language/Statements/While/execution_t02.dart
@@ -4,10 +4,11 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion The expression e is evaluated to an object o. Then, o is
- * subjected to boolean conversion, producing an object r.
- * If r is true, then the statement {s} is executed and then the while
- * statement is re-executed recursively.
+ * @assertion Execution of a while statement of the form while (e) s; proceeds
+ * as follows:
+ *   The expression e is evaluated to an object o. Then, o is subjected to
+ * boolean conversion, producing an object r. If r is true, then the statement
+ * {s} is executed and then the while statement is re-executed recursively.
  * If r is false, execution of the while statement is complete.
  * @description Checks that no dynamic type error is produced in checked mode
  * if o is not null and not of type bool.
@@ -22,38 +23,38 @@ import '../../../Utils/expect.dart';
 import '../../../Utils/dynamic_check.dart';
 
 main() {
-  while(true) {
+  while (true) {
     break;
   }
 
-  while(false) {
+  while (false) {
     break;
   }
 
   checkTypeError(() {
-    while("true") { /// static type warning
+    while ("true") { /// static type warning
       break;
     }
   });
 
   checkTypeError(() {
-    while(1) { /// static type warning
+    while (1) { /// static type warning
       break;
     }
   });
 
-  if(isCheckedMode()) {
+  if (isCheckedMode()) {
     bool fail = false;
     try {
-      while(null) {
+      while (null) {
         break;
       }
       fail = true;
-    } on AssertionError catch(ok) {
+    } on AssertionError catch (ok) {
     }
     Expect.isFalse(fail, "AssertionError expected in checking mode");
   } else {
-    while(null) {
+    while (null) {
       break;
     }
   }
--- Language/Statements/While/execution_t03.dart
+++ Language/Statements/While/execution_t03.dart
@@ -4,22 +4,22 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion The expression e is evaluated to an object o. Then, o is
- * subjected to boolean conversion, producing an object r.
- * If r is true, then the statement {s} is executed and then the while
- * statement is re-executed recursively.
+ * @assertion Execution of a while statement of the form while (e) s; proceeds
+ * as follows:
+ *   The expression e is evaluated to an object o. Then, o is subjected to
+ * boolean conversion, producing an object r. If r is true, then the statement
+ * {s} is executed and then the while statement is re-executed recursively.
  * If r is false, execution of the while statement is complete.
- * @description Checks that no dynamic type error is produced in checked mode
- * if o is not null and not of type bool.
- * Checks that while body statement is enclosed in implicit extra block.
+ * @description Checks that while body statement is enclosed in implicit extra
+ * block.
  * @author ilya
  */
 import '../../../Utils/expect.dart';
 
 main() {
-  var x, i=0;
+  var x, i = 0;
 
-  while (i++ < 5) var x=i;
+  while (i++ < 5) var x = i;
 
   Expect.isNull(x);
 }
