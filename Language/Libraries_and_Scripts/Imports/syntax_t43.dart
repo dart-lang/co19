788d5fb83ffe9bd6481ccb2e3c230b0f96d07732
Natalya Golovleva <ngl@unipro.ru>
2016-03-22 22:17:47 +0600

    Language/Libraries_and_Scripts/Imports new tests added.

---
+++ Language/Libraries_and_Scripts/Imports/syntax_t43.dart
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion An import specifies a library to be used in the scope of another
+ * library.
+ * libraryImport:
+ *   metadata importSpecification
+ * ;
+ * importSpecification:
+ *   import uri (as identifier)? combinator* ‘;’ |
+ *   import uri deferred as identifier combinator* ‘;’
+ * ;
+ * combinator:
+ *   show identifierList |
+ *   hide identifierList
+ * ;
+ * identifierList:
+ *   identifier (, identifier)*
+ * ;
+ * @description Checks that it is a compile-time error if as clause precedes
+ * deferred clause in a deferred import declaration.
+ * @compile-error
+ * @author ngl@unipro.ru
+ */
+
+import "syntax_lib.dart" as p deferred;
+
+main() {
+  try {
+    var x = p.foo;
+  } catch (ok) {}
+}
