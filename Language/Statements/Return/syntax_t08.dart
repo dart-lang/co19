db37d59fc9fbecc313f1fa08eeb176bb01709c03
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2016-03-28 17:46:43 +0600

    tests for chapter 'Return'

---
+++ Language/Statements/Return/syntax_t08.dart
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+    The return statement returns a result to the caller of a
+ * synchronous function, completes the future associated with an asynchronous
+ * function or terminates the stream or iterable associated with a generator.
+ * returnStatement:
+ *   return expression? ';'
+ * ;
+ * @description Check that statement 'return e;' is accepted as valid in
+ * asynchronous static method.
+ *
+ * @author a.semenov@unipro.ru
+ */
+import 'dart:async';
+import '../../../Utils/expect.dart';
+import '../../../Utils/async_utils.dart';
+
+class A {
+  static Future test() async {
+    return 1;
+  }
+}
+
+main() {
+  asyncStart();
+  A.test().then((value) {
+    Expect.equals(1, value);
+    asyncEnd();
+  });
+}
