a0192d6ba524da13705e0f45bb3ecd7fc23ba14a
Natalya Golovleva <ngl@unipro.ru>
2016-04-14 15:48:51 +0600

    Language/Reference/Lexical_Rules/Reserved_Words/status* tests refactored.

--- Language/Reference/Lexical_Rules/Reserved_Words/super_t04.dart
+++ Language/Reference/Lexical_Rules/Reserved_Words/super_t04.dart
@@ -5,10 +5,10 @@
  */
 /**
  * @assertion "super" is a reserved word.
- * A reserved word may not be used as an identifier; 
- * it is a compile-time error if a reserved word is used where an identifier is expected.
- * @description Checks that it is a compile-time error when a reserved word "super"
- * is used as a class member (field) name.
+ * A reserved word may not be used as an identifier; it is a compile-time error
+ * if a reserved word is used where an identifier is expected.
+ * @description Checks that it is a compile-time error when a reserved word
+ * "super" is used as a class member (field) name.
  * @compile-error
  * @author rodionov
  * @reviewer iefremov
