2ffabf708c6cbf74e219d92e9aeb9ee40e3f2757
Natalya Golovleva <ngl@unipro.ru>
2016-04-14 22:41:28 +0600

    Language/Reference/Lexical_Rules/Reserved_Words/enum* tests added.

---
+++ Language/Reference/Lexical_Rules/Reserved_Words/enum_t02.dart
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion "enum" is a reserved word.
+ * A reserved word may not be used as an identifier; it is a compile-time error
+ * if a reserved word is used where an identifier is expected.
+ * @description Checks that it is a compile-time error when a reserved word
+ * "enum" is used as a type parameter name.
+ * @compile-error
+ * @author ngl@unipro.ru
+ */
+
+class A<enum> {
+  bool check(x) => x is enum;
+}
+
+main() {
+  new A().check(null);
+}
---
+++ Language/Reference/Lexical_Rules/Reserved_Words/enum_t03.dart
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion "enum" is a reserved word.
+ * A reserved word may not be used as an identifier; it is a compile-time error
+ * if a reserved word is used where an identifier is expected.
+ * @description Checks that it is a compile-time error when a reserved word
+ * "enum" is used as a function type alias name.
+ * @compile-error
+ * @author ngl@unipro.ru
+ */
+
+typedef void enum();
+
+main() {
+  try {
+    null is enum;
+  } catch (anything) {
+  }
+}
