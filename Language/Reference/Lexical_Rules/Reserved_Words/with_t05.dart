c30c8d6790640f92ddf7014fe6d584f6e8c23e74
Natalya Golovleva <ngl@unipro.ru>
2016-04-14 21:52:46 +0600

    Language/Reference/Lexical_Rules/Reserved_Words/with* tests refactored.

--- Language/Reference/Lexical_Rules/Reserved_Words/with_t05.dart
+++ Language/Reference/Lexical_Rules/Reserved_Words/with_t05.dart
@@ -5,10 +5,10 @@
  */
 /**
  * @assertion "with" is a reserved word.
- * A reserved word may not be used as an identifier; 
- * it is a compile-time error if a reserved word is used where an identifier is expected.
- * @description Checks that it is a compile-time error when a reserved word "with"
- * is used as a local function name.
+ * A reserved word may not be used as an identifier; it is a compile-time error
+ * if a reserved word is used where an identifier is expected.
+ * @description Checks that it is a compile-time error when a reserved word
+ * "with" is used as a local function name.
  * @compile-error
  * @author rodionov
  * @reviewer kaigorodov
