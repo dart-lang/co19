2b3bb2599ab058ea452a41dc8ccd3b8d96a7384c
Natalya Golovleva <ngl@unipro.ru>
2016-04-14 19:33:58 +0600

    Language/Reference/Lexical_Rules/Reserved_Words/var* tests refactored.

--- Language/Reference/Lexical_Rules/Reserved_Words/var_t02.dart
+++ Language/Reference/Lexical_Rules/Reserved_Words/var_t02.dart
@@ -5,10 +5,10 @@
  */
 /**
  * @assertion "var" is a reserved word.
- * A reserved word may not be used as an identifier; 
- * it is a compile-time error if a reserved word is used where an identifier is expected.
- * @description Checks that it is a compile-time error when a reserved word "var"
- * is used as a type parameter name.
+ * A reserved word may not be used as an identifier; it is a compile-time error
+ * if a reserved word is used where an identifier is expected.
+ * @description Checks that it is a compile-time error when a reserved word
+ * "var" is used as a type parameter name.
  * @compile-error
  * @author rodionov
  * @reviewer iefremov
