c66ef0d9d1ec91b328b072650efb41c16412ed24
Natalya Golovleva <ngl@unipro.ru>
2016-04-14 16:37:57 +0600

    Language/Reference/Lexical_Rules/Reserved_Words/throw* tests refactored.

--- Language/Reference/Lexical_Rules/Reserved_Words/throw_t06.dart
+++ Language/Reference/Lexical_Rules/Reserved_Words/throw_t06.dart
@@ -5,10 +5,10 @@
  */
 /**
  * @assertion "throw" is a reserved word.
- * A reserved word may not be used as an identifier; 
- * it is a compile-time error if a reserved word is used where an identifier is expected.
- * @description Checks that it is a compile-time error when a reserved word "throw"
- * is used as a label name.
+ * A reserved word may not be used as an identifier; it is a compile-time error
+ * if a reserved word is used where an identifier is expected.
+ * @description Checks that it is a compile-time error when a reserved word
+ * "throw" is used as a label name.
  * @compile-error
  * @author rodionov
  * @reviewer kaigorodov
@@ -19,6 +19,6 @@ main() {
     throw:
     do {
       break throw;
-    } while(false);
-  } catch(x) {}
+    } while (false);
+  } catch (x) {}
 }
