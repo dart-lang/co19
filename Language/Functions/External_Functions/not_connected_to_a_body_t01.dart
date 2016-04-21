df399b5b6c44a7390f4cb8d524d14c2fe4919fb2
Natalya Golovleva <ngl@unipro.ru>
2016-04-21 17:19:08 +0600

    Language/Functions/External_Functions tests refactored.

--- Language/Functions/External_Functions/not_connected_to_a_body_t01.dart
+++ Language/Functions/External_Functions/not_connected_to_a_body_t01.dart
@@ -4,15 +4,18 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion An external function is a function whose body is provided separately from its declaration. An external function may be a 
- * top-level function, a method, a getter, a setter or a non-redirecting constructor.
- * External functions are introduced via the built-in identifier external followed by 
- * the function signature.
- * An external function is connected to its body by an implementation specific mechanism.
- * Attempting to invoke an external function that has not been connected to its body 
- * will raise a NoSuchMethodError or some subclass thereof.
- * @description Checks that invoking an external function that is not connected to
- * its body indeed results in a NoSuchMethodError or its subclass being thrown.
+ * @assertion An external function is a function whose body is provided
+ * separately from its declaration. An external function may be a top-level
+ * function, a method, a getter, a setter or a non-redirecting constructor.
+ * External functions are introduced via the built-in identifier external
+ * followed by the function signature.
+ * An external function is connected to its body by an implementation specific
+ * mechanism. Attempting to invoke an external function that has not been
+ * connected to its body will raise a NoSuchMethodError or some subclass
+ * thereof.
+ * @description Checks that invoking an external function that is not connected
+ * to its body indeed results in a NoSuchMethodError or its subclass being
+ * thrown.
  * @author rodionov
  * @reviewer kaigorodov
  */
@@ -23,20 +26,20 @@ external g([var x]);
 external h({var x});
 
 main() {
-var s="";
+  var s = "";
   try {
     f();
     Expect.fail("NoSuchMethodError expected");
-  } on NoSuchMethodError catch(e) {
-  } catch(e) {print("unexpected exception of type ${e.runtimeType}: $e");}
+  } on NoSuchMethodError catch (e) {
+  } catch (e) {print("unexpected exception of type ${e.runtimeType}: $e");}
 
   try {
     g();
     Expect.fail("NoSuchMethodError expected");
-  } on NoSuchMethodError catch(e) {}
+  } on NoSuchMethodError catch (e) {}
 
   try {
     h();
     Expect.fail("NoSuchMethodError expected");
-  } on NoSuchMethodError catch(e) {}
+  } on NoSuchMethodError catch (e) {}
 }
