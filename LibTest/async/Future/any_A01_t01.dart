2936069e60fd49926715a3e6c030d80c71fe9062
Natalya Golovleva <ngl@unipro.ru>
2016-05-17 19:25:39 +0600

    Future/any* tests added.

---
+++ LibTest/async/Future/any_A01_t01.dart
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion  Future any(Iterable<Future> futures)
+ * Returns the result of the first future in futures to complete.
+ *
+ * The returned future is completed with the result of the first future in
+ * futures to report that it is complete. The results of all the other futures
+ * are discarded.
+ *
+ * If futures is empty, or if none of its futures complete, the returned future
+ * never completes.
+ *
+ * @description Checks that returned future is completed with the result of the
+ * first future in the futures that is completed. The first future in futures
+ * completed with 6 is element 0.
+ * @author ngl@unipro.ru
+ */
+import "../../../Utils/async_utils.dart";
+import "../../../Utils/expect.dart";
+
+import "dart:async";
+
+const N = 6;
+
+main() {
+  List<Future> futures = new List<Future>(N);
+
+  for (int k = 0; k < N; k++) {
+    Completer c = new Completer();
+    futures[k] = c.future;
+    if (k == 0) {
+      c.complete(6);
+    }
+    if (k == 1) {
+      c.complete(true);
+    }
+    if (k == 2) {
+      c.complete(false);
+    }
+    if (k == 3) {
+      c.complete(null);
+    }
+  }
+
+  Future f = Future.any(futures);
+
+  asyncStart();
+  f.then((fValue) {
+    Expect.equals(fValue, 6);
+    asyncEnd();
+  });
+}
---
+++ LibTest/async/Future/any_A01_t04.dart
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion  Future any(Iterable<Future> futures)
+ * Returns the result of the first future in futures to complete.
+ *
+ * The returned future is completed with the result of the first future in
+ * futures to report that it is complete. The results of all the other futures
+ * are discarded.
+ *
+ * If futures is empty, or if none of its futures complete, the returned future
+ * never completes.
+ *
+ * @description Checks that returned future is completed with the result of the
+ * first future in the futures that is completed. The first future in futures
+ * completed with null is element 3.
+ * @author ngl@unipro.ru
+ */
+import "../../../Utils/async_utils.dart";
+import "../../../Utils/expect.dart";
+
+import "dart:async";
+
+const N = 6;
+
+main() {
+  List<Future> futures = new List<Future>(N);
+
+  for (int k = 0; k < N; k++) {
+    Completer c = new Completer();
+    futures[k] = c.future;
+    if (k == 3) {
+      c.complete(null);
+    }
+    if (k == 4) {
+      c.complete(6);
+    }
+    if (k == 5) {
+      c.complete(true);
+    }
+  }
+
+  Future f = Future.any(futures);
+
+  asyncStart();
+  f.then((fValue) {
+    Expect.equals(fValue, null);
+    asyncEnd();
+  });
+}
