284edd14ab3a4889d69e4e927b28bee22e0fef71
Natalya Golovleva <ngl@unipro.ru>
2016-05-20 20:33:09 +0600

    Stream/length* tests refactored.

--- LibTest/async/Stream/length_A01_t01.dart
+++ LibTest/async/Stream/length_A01_t01.dart
@@ -4,8 +4,8 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion final Future<int> length
- * Counts the elements in the stream. 
+ * @assertion Future<int> length
+ * Counts the elements in the stream.
  * @description Checks that correct number of elements is returned.
  * @author kaigorodov
  */
@@ -15,9 +15,9 @@ import "../../../Utils/async_utils.dart";
 import "../../../Utils/expect.dart";
 
 check(Iterable<int> data, int expected) {
-  Stream s=new Stream.fromIterable(data);
+  Stream s = new Stream.fromIterable(data);
   asyncStart();
-  s.length.then((value){
+  s.length.then((value) {
     Expect.equals(expected, value);
     asyncEnd();
   });
@@ -25,7 +25,7 @@ check(Iterable<int> data, int expected) {
 
 main() {
   check([], 0);
-  check([1,2,3,null], 4);
-  check(new Iterable.generate(0, (int index)=>index*2), 0);
-  check(new Iterable.generate(10, (int index)=>index*2), 10);
+  check([1, 2, 3, null], 4);
+  check(new Iterable.generate(0, (int index) => index * 2), 0);
+  check(new Iterable.generate(10, (int index) => index * 2), 10);
 }
