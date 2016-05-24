e1e55952e604c2cbb9a0d0a12b06da7120f4be3b
Natalya Golovleva <ngl@unipro.ru>
2016-05-24 21:18:28 +0600

    Stream/skip* tests refactored.

--- LibTest/async/Stream/skip_A01_t01.dart
+++ LibTest/async/Stream/skip_A01_t01.dart
@@ -15,32 +15,32 @@ import "../../../Utils/async_utils.dart";
 import "../../../Utils/expect.dart";
 
 void check(List data, int count) {
-  Stream s=new Stream.fromIterable(data);
-  Stream t=s.skip(count);
-  int seen=0;
+  Stream s = new Stream.fromIterable(data);
+  Stream t = s.skip(count);
+  int seen = 0;
   asyncStart();
-  t.listen((value){
-      Expect.isTrue(seen+count<data.length, "seen=$seen, count=$count");
-      Expect.equals(data[count+seen], value);
+  t.listen((value) {
+      Expect.isTrue(seen + count < data.length, "seen=$seen, count=$count");
+      Expect.equals(data[count + seen], value);
       seen++;
     },
-    onDone: (){
+    onDone: () {
       asyncEnd();
     }
   );
 }
 
 main() {
-  check([],0);
-  check([],1);
-  check([],10);
+  check([], 0);
+  check([], 1);
+  check([], 10);
   check([null], 0);
   check([null], 1);
   check([null], 2);
-  check([1,2,3], 0);
-  check([1,2,3], 1);
-  check([1,2,3], 2);
-  check([1,2,3], 12);
-  check([[],[[]],[[[]]]], 2);
+  check([1, 2, 3], 0);
+  check([1, 2, 3], 1);
+  check([1, 2, 3], 2);
+  check([1, 2, 3], 12);
+  check([[], [[]], [[[]]]], 2);
 }
 
