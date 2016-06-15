70a14b96530b6e123b8b3d79b22eaca875c7d674
Natalya Golovleva <ngl@unipro.ru>
2016-06-15 19:42:29 +0600

    New async/StreamController/addError test added.

---
+++ LibTest/async/StreamController/addError_A02_t01.dart
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion abstract void addError(Object error, [Object stackTrace])
+ * Send or enqueue an error event.
+ *
+ * If error is null, it is replaced by a NullThrownError.
+ *
+ * @description Checks that if an error is null, it is replaced by a
+ * NullThrownError.
+ * @author ngl@unipro.ru
+ */
+
+import "dart:async";
+import "../../../Utils/async_utils.dart";
+import "../../../Utils/expect.dart";
+
+main() {
+  StreamController controller = new StreamController();
+  controller.addError(null);
+  asyncStart();
+  controller.stream.listen((value) {
+    Expect.fail("unexpected onData call");
+  },
+  onError: (error1) {
+    Expect.isTrue(error1 is NullThrownError);
+  },
+  onDone: () {
+    asyncEnd();
+  });
+  controller.close();
+}
