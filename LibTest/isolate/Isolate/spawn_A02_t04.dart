c74abe2bcb0db6bf8d28d4f421b0b4a5baa65cc2
iarkh <irina.arkhipets@gmail.com>
2016-09-15 14:22:35 +0600

    LibTest/isolate adopted for strong mode.

---
+++ LibTest/isolate/Isolate/spawn_A02_t04.dart
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS
+ * file for details. All rights reserved. Use of this source code is governed
+ * by a BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Future<Isolate> spawn(void entryPoint(message), message,
+ * {bool paused: false, bool errorsAreFatal, SendPort onExit, SendPort onError})
+ *
+ *    The argument entryPoint specifies the entry point of the spawned isolate.
+ * It must be a top-level function or a static method that takes one argument -
+ * that is, one-parameter functions that can be compile-time constant function
+ * values. It is not allowed to pass the value of function expressions or
+ * an instance method extracted from an object.
+ *
+ *    The entry-point function is invoked with the initial message. Usually
+ * the initial message contains a SendPort so that the spawner and spawnee can
+ * communicate with each other.
+ *  ...
+ *    Returns a future that will complete with an Isolate instance if
+ * the spawning succeeded. It will complete with an error otherwise.
+ *
+ * @description Checks that if entryPoint is top level function with more
+ * than one argument, then type error is arisen in checked mode or returned
+ * Future instance completes with error otherwise. In strong mode compile
+ * error arises.
+ * @compile-error
+ * @issue #26588
+ * @author a.semenov@unipro.ru
+ */
+import "dart:isolate";
+import "../../../Utils/expect.dart";
+import "../../../Utils/async_utils.dart";
+
+entryPoint2Args(message1, message2) {
+  print("message1: $message1 message2: $message2");
+}
+
+main() {
+  asyncStart();
+  try {
+    Isolate.spawn(entryPoint2Args, "hello").then( /// compile error
+        (v) {
+          Expect.fail("Isolate.spawn(entryPoint2Args, 'hello') is expected to fail");
+        },
+        onError: (e) {
+          Expect.fail("Future completed with error: $e");
+          asyncEnd();
+        }
+    );
+  } catch (e) {
+    print("Caught an error: $e");
+    asyncEnd();
+  }
+}
---
+++ LibTest/isolate/Isolate/spawn_A05_t03.dart
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS
+ * file for details. All rights reserved. Use of this source code is governed
+ * by a BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion If the errorAreFatal, onExit and/or onError parameters are
+ * provided, the isolate will act as if, respectively, setErrorsFatal,
+ * addOnExitListener and addErrorListener were called with the corresponding
+ * parameter and was processed before the isolate starts running.
+ *
+ * @description Check that if onExit parameter is supplied, the isolate sends
+ * null value on given send port. The isolate is terminated with error.
+ * @author a.semenov@unipro.ru
+ */
+import "dart:isolate";
+import "../../../Utils/async_utils.dart";
+import "../../../Utils/expect.dart";
+
+void entryPoint(message) {
+  dynamic i = 1;
+  var x = "a" + i;
+}
+
+main() {
+  asyncStart();
+  ReceivePort exitPort = new ReceivePort();
+  exitPort.listen(
+     (value) {
+        exitPort.close();
+        Expect.isNull(value);
+        asyncEnd();
+     }
+  );
+
+  Isolate.spawn(
+      entryPoint,
+      "hello",
+      errorsAreFatal:true,
+      onExit:exitPort.sendPort
+  );
+}
