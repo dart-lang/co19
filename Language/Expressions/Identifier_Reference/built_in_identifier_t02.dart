c2124e5c07d53bb1b332208d72bbf57e31ddec27
Sergey G. Grekhov <sgrekhov@unipro.ru>
2016-09-21 15:20:23 +0700

    Language\Expressions\Identifier_Reference\ deleted tests returned back and adapted for strong mode

---
+++ Language/Expressions/Identifier_Reference/built_in_identifier_t02.dart
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is a compile-time error if a built-in identifier is used as
+ * the declared name of a prefix, class, type parameter or type alias.
+ * @description  Checks that when a built-in identifier is used in an identifier
+ * reference, but since no declaration with that name can exist, lookup fails
+ * eventually with the type of error depending on context. This test checks that
+ * in a class instance scope, referencing a built-in identifier results in
+ * NoSuchMethodError as long as the identifier is placed in such a way that it
+ * cannot be mistaken.
+ * @author rodionov
+ * @reviewer iefremov
+ */
+import '../../../Utils/expect.dart';
+
+@proxy
+class A {
+  test() {
+    Expect.throws(() {abstract();}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {1 + as;}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {1 << export;}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {external ^ 0;}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {1 * factory;}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {1 >= get;}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {var x = implements;}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {1 == import;}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {1 == library;}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {operator.x;}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {1 == part;}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {--set;}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {true && static;}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {++typedef;}, (e) => e is NoSuchMethodError);
+  }
+}
+
+main() {
+  new A().test();
+}
