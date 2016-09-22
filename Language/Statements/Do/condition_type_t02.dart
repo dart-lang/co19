8609b92098a28a80e06e023e81611ece1e669301
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2016-09-22 16:27:52 +0700

    adapt tests in Language\Statements\Do for strong mode

--- Language/Statements/Do/condition_type_t02.dart
+++ Language/Statements/Do/condition_type_t02.dart
@@ -6,9 +6,9 @@
 /**
  * @assertion It is a static type warning if the type of e can not be assigned
  * to bool.
- * @description Checks that it is a static type warning if the type of e may not
+ * @description Checks that it is a compile error if the type of e may not
  * be assigned to bool.
- * @static-warning
+ * @compile-error
  * @author rodionov
  * @reviewer iefremov
  */
@@ -18,6 +18,6 @@ void f() {}
 main() {
   do {
     break;
-  } while (f()); /// static type warning
+  } while (f());
 }
 
--- Language/Statements/Do/execution_t05.dart
+++ Language/Statements/Do/execution_t05.dart
@@ -20,6 +20,6 @@
 main() {
   int x = 0;
   do {
-    x = 1;
-  } while (1);
+    x = 2;
+  } while ("true");
 }
