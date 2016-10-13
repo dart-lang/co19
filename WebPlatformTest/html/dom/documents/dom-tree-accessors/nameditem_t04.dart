17f94faa04c2031fb43698d2009e902f8b040589
Sergey G. Grekhov <sgrekhov@unipro.ru>
2016-10-13 13:10:48 +0700

    WebPlatformTest/html/dom/documents/dom-tree-accessors/ tests adapted for strong mode

--- WebPlatformTest/html/dom/documents/dom-tree-accessors/nameditem_t04.dart
+++ WebPlatformTest/html/dom/documents/dom-tree-accessors/nameditem_t04.dart
@@ -45,13 +45,13 @@ void main() {
   document.body.appendHtml(htmlEL);
 
 test(() {
-  var form = document.getElementsByTagName("form")[0];
+  var form = document.getElementsByTagName("form")[0] as Element;
   assert_equals(form.attributes["name"], "test1");
 }, "If there is one form, it should be returned (name)");
 
 test(() {
-  var form1 = document.getElementsByTagName("form")[1];
-  var form2 = document.getElementsByTagName("form")[2];
+  var form1 = document.getElementsByTagName("form")[1] as Element;
+  var form2 = document.getElementsByTagName("form")[2] as Element;
   assert_equals(form1.attributes["name"], "test2");
   assert_equals(form2.attributes["name"], "test2");
 
@@ -61,53 +61,53 @@ test(() {
 
 test(() {
   var collection = document.getElementsByTagName("form");
-  var form = collection[3];
+  var form = collection[3] as Element;
   assert_equals(form.id, "test3");
 
   Expect.equals(0, document.getElementsByName("test3").length);
 }, "If there is one form, it should not be returned (id)");
 
 test(() {
-  var form1 = document.getElementsByTagName("form")[4];
+  var form1 = document.getElementsByTagName("form")[4] as Element;
   assert_equals(form1.id, "test4");
-  var form2 = document.getElementsByTagName("form")[5];
+  var form2 = document.getElementsByTagName("form")[5] as Element;
   assert_equals(form2.id, "test4");
 
   Expect.equals(0, document.getElementsByName("test4").length, '"test4" in document should be false');
 }, "If there are two forms, nothing should be returned. (id)");
 
 test(() {
-  var form1 = document.getElementsByTagName("form")[6];
+  var form1 = document.getElementsByTagName("form")[6] as FormElement;
   assert_equals(form1.name, "test5");
-  var form2 = document.getElementsByTagName("form")[7];
+  var form2 = document.getElementsByTagName("form")[7] as Element;
   assert_equals(form2.id, "test5");
 
-  var test5=document.getElementsByName("test5")[0];
+  var test5 = document.getElementsByName("test5")[0];
   assert_equals(test5, form1, '"test5" in document should be true');
 }, "If there are two forms, a collection should be returned. (name and id)");
 
 test(() {
-  var form1 = document.getElementsByTagName("form")[8];
+  var form1 = document.getElementsByTagName("form")[8] as Element;
   assert_equals(form1.id, "test6");
-  var form2 = document.getElementsByTagName("form")[9];
+  var form2 = document.getElementsByTagName("form")[9] as FormElement;
   assert_equals(form2.name, "test6");
 
-  var test6=document.getElementById("test6");
+  var test6 = document.getElementById("test6");
   assert_equals(test6, form1, '"test6" in document should be true');
 }, "If there are two forms, a collection should be returned. (id and name)");
 
 test(() {
-  var form = document.getElementsByTagName("form")[10];
+  var form = document.getElementsByTagName("form")[10] as Element;
   assert_equals(form.id, "test7");
 
   Expect.equals(0, document.getElementsByName("test7").length, '"test7" in document should be false');
 }, "A name shouldn't affect getting an form by id");
 
 test(() {
-  var form = document.getElementsByTagName("form")[11];
+  var form = document.getElementsByTagName("form")[11] as FormElement;
   assert_equals(form.name, "test8");
 
-  var test8=document.getElementsByName("test8")[0];
+  var test8 = document.getElementsByName("test8")[0];
   assert_equals(test8, form, '"test8" in document should be true');
 }, "An id shouldn't affect getting an form by name");
 
