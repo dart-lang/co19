43241784bcf5b820c960a2df0e596b294f90f823
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2016-11-08 17:31:55 +0700

    adapt tests in LayoutTests\fast\forms\datetimelocal-multiple-fields for strong mode

--- LayoutTests/fast/forms/datetimelocal-multiple-fields/datetimelocal-multiple-fields-change-layout-by-value_t01.dart
+++ LayoutTests/fast/forms/datetimelocal-multiple-fields/datetimelocal-multiple-fields-change-layout-by-value_t01.dart
@@ -8,29 +8,28 @@
  */
 import "dart:html";
 import "../../../testcommon.dart";
-import "../../../../Utils/async_utils.dart";
 
 main() {
-  var testInput = document.createElement('input');
+  InputElement testInput = document.createElement('input') as InputElement;
   testInput.type = 'datetime-local';
   testInput.max = '9999-12-31T23:59';
   document.body.append(testInput);
-  var widthOfEmptyValue = testInput.offsetWidth;
+  int widthOfEmptyValue = testInput.offsetWidth;
 
   testInput.value = '10000-01-01T00:00';
-  var widthOfOverflowValue = testInput.offsetWidth;
+  int widthOfOverflowValue = testInput.offsetWidth;
   shouldBeTrue(widthOfEmptyValue < widthOfOverflowValue);
 
   testInput.value = '2012-10-05T12:00';
-  var widthOfValidValue = testInput.offsetWidth;
+  int widthOfValidValue = testInput.offsetWidth;
   shouldBe(widthOfEmptyValue, widthOfValidValue);
 
   testInput.value = '2012-10-05T12:00:01';
-  var widthWithSecond = testInput.offsetWidth;
+  int widthWithSecond = testInput.offsetWidth;
   shouldBeTrue(widthOfEmptyValue < widthWithSecond);
 
   testInput.value = '2012-10-05T12:00:01.234';
-  var widthWithMillisecond = testInput.offsetWidth;
+  int widthWithMillisecond = testInput.offsetWidth;
   shouldBeTrue(widthWithSecond < widthWithMillisecond);
 
   testInput.remove();
