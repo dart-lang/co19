bf020e4b4a4d022ff39ee7bcb3b536e6572d15e5
Sergey G. Grekhov <sgrekhov@unipro.ru>
2016-11-29 19:00:32 +0700

    Copyright year updated

--- LibTest/core/ConcurrentModificationError/ConcurrentModificationError_A01_t02.dart
+++ LibTest/core/ConcurrentModificationError/ConcurrentModificationError_A01_t02.dart
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
  * for details. All rights reserved. Use of this source code is governed by a
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
  * @assertion ConcurrentModificationError([Object modifiedObject ])
- * @description Checks that this constructor can be called цшер зфкфьутук and
+ * @description Checks that this constructor can be called with parameter and
  * returns instance of ConcurrentModificationError
  * @author sgrekhov@unipro.ru
  */
