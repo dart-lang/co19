380e3cfaca335fbf49d752085b290d43dfb228ed
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2017-01-27 12:27:03 +0700

    add new tests for Isolate.setErrorsFatal()

---
+++ LibTest/isolate/Isolate/setErrorsFatal_A02_t02.dart
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS
+ * file for details. All rights reserved. Use of this source code is governed
+ * by a BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion void setErrorsFatal(
+ *                                bool errorsAreFatal
+ *                               )
+ * ...
+ *    This call requires the terminateCapability for the isolate. If the
+ * capability is not correct, no change is made.
+ *
+ * @description Check that call to setErrorsFatal(true) on isolate without
+ * terminateCapability have no effect
+ *
+ * @author a.semenov@unipro.ru
+ */
+import "dart:isolate";
+import "dart:async";
+import "../../../Utils/async_utils.dart";
+import "../../../Utils/expect.dart";
+import "IsolateUtil.dart";
+
+test() async {
+  // setup
+  ReceivePort errorPort = new ReceivePort();
+  StreamIterator errors = new StreamIterator(errorPort);
+  ErrorServer server = await ErrorServer.spawn(
+                                                errorsAreFatal:false,
+                                                onError: errorPort.sendPort
+                                             );
+  Isolate isolate = new Isolate(
+      server.isolate.controlPort,
+      pauseCapability:null,
+      terminateCapability:null
+  );
+
+  isolate.setErrorsFatal(true);
+  // verify that errors are still not fatal
+
+  for (int i=0; i<3; i++) {
+    server.generateError();
+    Expect.isTrue(await errors.moveNext());
+    Expect.equals(i.toString(), errors.current[0]);
+  }
+
+  // clean up
+  await server.stop();
+  errorPort.close();
+  asyncEnd();
+}
+
+main() {
+  asyncStart();
+  test();
+}
