5fe8b65b4da57502bf1e840caa892115584a9df7
iarkh <irina.arkhipets@gmail.com>
2017-02-27 15:14:21 +0700

    Correction for inherited tests for DoubleLinkedQueue.

---
+++ LibTest/collection/DoubleLinkedQueue/DoubleLinkedQueue_A01_t01_class.dart
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Queue<E> abstract class
+ * Implements Queue<E>
+ * @description Checks that all members of [Queue] are implemented.
+ * @issue 26587
+ * @author kaigorodov
+ */
+import "dart:collection";
+import "../Queue/all_tests.lib.dart";
+
+Iterable create([Iterable content]) {
+  DoubleLinkedQueue res = new DoubleLinkedQueue();
+  if (content != null) {
+    res.addAll(content);
+  }
+  return res;
+}
+
+main() {
+  test(create);
+}
