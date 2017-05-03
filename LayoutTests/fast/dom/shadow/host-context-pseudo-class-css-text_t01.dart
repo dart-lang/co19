902d13ff018c4b22816b947d652e6b352a2662ac
Sergey G. Grekhov <sgrekhov@unipro.ru>
2017-05-03 13:15:01 +0700

    Fix for issue #100. Expected result fixed

--- LayoutTests/fast/dom/shadow/host-context-pseudo-class-css-text_t01.dart
+++ LayoutTests/fast/dom/shadow/host-context-pseudo-class-css-text_t01.dart
@@ -30,8 +30,6 @@ main() {
   CssStyleSheet sh1 = se1.sheet;
 
   shouldBeLikeString(sh1.cssRules[0].cssText,
-      ".foo:host-context(div,body.mytheme,p#myid,div::before,span:hover) > div { display: block; }");
-  shouldBeLikeString(sh1.cssRules[1].cssText,
       ":host-context(*) { display: block; }");
 
   StyleElement se2 = document.getElementById('style-invalid');
