bc7fe39c8448463042d09ca28553231676b38a73
Sergey G. Grekhov <sgrekhov@unipro.ru>
2017-05-26 16:08:07 +0700

    More Directory tests

---
+++ LibTest/io/Directory/list_A01_t03.dart
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Stream<FileSystemEntity> list({
+ *  bool recursive: false,
+ *  bool followLinks: true
+ *  })
+ * Lists the sub-directories and files of this Directory. Optionally recurses
+ * into sub-directories.
+ *
+ * If followLinks is false, then any symbolic links found are reported as Link
+ * objects, rather than as directories or files, and are not recursed into.
+ *
+ * If followLinks is true, then working links are reported as directories or
+ * files, depending on their type, and links to directories are recursed into.
+ * Broken links are reported as Link objects. If a symbolic link makes a loop in
+ * the file system, then a recursive listing will not follow a link twice in the
+ * same recursive descent, but will report it as a Link the second time it is
+ * seen.
+ *
+ * The result is a stream of FileSystemEntity objects for the directories,
+ * files, and links.
+ * @description Checks that this method lists the sub-directories of this
+ * Directory. Test recursive parameter
+ * @author sgrekhov@unipro.ru
+ */
+import "dart:io";
+import "../../../Utils/expect.dart";
+import "../../../Utils/async_utils.dart";
+
+List<String> setUp(Directory parent, Map directories) {
+  List<String> created = new List<String>();
+  directories.forEach((key, value) {
+    Directory dir = new Directory(parent.path + Platform.pathSeparator + key);
+    dir.createSync();
+    created.add(dir.path);
+    if (value != null) {
+      created.addAll(setUp(dir, value));
+    }
+  });
+  return created;
+}
+
+Map getContent(Directory dir) {
+  Map found = new Map();
+  dir.list(recursive: true).forEach((entity) {
+
+      found.addAll({entity.path: null});
+  });
+  return found;
+}
+
+test(Directory dir) async {
+  Map struct = {"a": null, "b": null, "c": {"c1": null, "c2": null}, "d": null};
+  List<String> created = setUp(dir, struct);
+
+  List<String> found = new List<String>();
+  dir.list(recursive: true).forEach((entity) {
+    found.add(entity.path);
+  }).then((_) {
+    try {
+      Expect.listEquals(created, found);
+    } finally {
+      dir.delete(recursive: true);
+      asyncEnd();
+    }
+  });
+}
+
+main() {
+  Directory dir = new Directory("TestDir").createTempSync();
+  asyncStart();
+  test(dir);
+}
