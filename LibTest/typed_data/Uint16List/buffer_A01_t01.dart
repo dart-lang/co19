f3c74cbc97cc1e0735107cd2b66c6bd055f334af
Natalya Golovleva <ngl@unipro.ru>
2017-07-04 16:01:51 +0700

    Code style fixed.

--- LibTest/typed_data/Uint16List/buffer_A01_t01.dart
+++ LibTest/typed_data/Uint16List/buffer_A01_t01.dart
@@ -20,7 +20,7 @@ main() {
   var byteBuffer = tmp.buffer;
   var elemSize = Uint16List.BYTES_PER_ELEMENT;
 
-  for (int i= 0; i <= 10; ++i) {
+  for (int i = 0; i <= 10; ++i) {
     var l = new Uint16List.view(byteBuffer, i * elemSize, 10 - i) ;
     Expect.equals(byteBuffer, l.buffer);
   }
