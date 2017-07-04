630349f9b02a7f63289437eb2bda762229700f12
Natalya Golovleva <ngl@unipro.ru>
2017-07-04 21:08:32 +0700

    Code style fixed.

--- LibTest/typed_data/Int32x4/y_A01_t01.dart
+++ LibTest/typed_data/Int32x4/y_A01_t01.dart
@@ -14,7 +14,7 @@ import "dart:typed_data";
 import "../../../Utils/expect.dart";
 
 check(y) {
-  var obj = new Int32x4(0 ,y, 0, 0);
+  var obj = new Int32x4(0, y, 0, 0);
   Expect.equals(y, obj.y);
 }
 
