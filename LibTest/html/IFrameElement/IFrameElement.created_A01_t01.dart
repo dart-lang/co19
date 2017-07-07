329ae338b8757dd7596c05dcf66547cbd1ef8f39
Sergey G. Grekhov <sgrekhov@unipro.ru>
2017-07-07 15:12:38 +0700

    Fix for issue #117. The way of element creation fixed

--- LibTest/html/IFrameElement/IFrameElement.created_A01_t01.dart
+++ LibTest/html/IFrameElement/IFrameElement.created_A01_t01.dart
@@ -10,23 +10,29 @@
  * @description Checks that this constructor can be called only by subclasses
  */
 import "dart:html";
-import "../../../Utils/async_utils.dart";
 import "../../../UtilsHtml/expect.dart";
 
 class MyIFrameElement extends IFrameElement {
+  factory MyIFrameElement() {
+    return new Element.tag('my-iframe');
+  }
+
   MyIFrameElement.created() : super.created();
+
+  String testit() => "In testit";
 }
 
 main() {
   var ife;
   Expect.throws((){
-    ife=new IFrameElement.created();
+    ife = new IFrameElement.created();
   });
 
   var tag = 'my-iframe';
   document.register(tag, MyIFrameElement, extendsTag:"iframe");
 
-  ife = new Element.tag(tag);
+  ife = document.createElement('iframe', 'my-iframe');
   document.body.append(ife);
   Expect.isTrue(ife is MyIFrameElement);
+  Expect.equals("In testit", ife.testit());
 }
