bfa867ab5e774f637fcbabd41698142d53615b33
Sergey G. Grekhov <sgrekhov@unipro.ru>
2017-07-11 18:50:07 +0700

    More File tests added

---
+++ LibTest/io/File/createSync_A01_t01.dart
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion void createSync({bool recursive: false})
+ * Synchronously create the file. Existing files are left untouched by
+ * createSync. Calling createSync on an existing file might fail if there are
+ * restrictive permissions on the file.
+ *
+ * If recursive is false, the default, the file is created only if all
+ * directories in the path exist. If recursive is true, all non-existing path
+ * components are created.
+ *
+ * Throws a FileSystemException if the operation fails.
+ * @description Checks that this method creates the file
+ * @author sgrekhov@unipro.ru
+ */
+import "dart:io";
+import "../../../Utils/expect.dart";
+import "../../../Utils/file_utils.dart";
+
+main() {
+  File file = new File(getTempFilePath());
+  file.createSync();
+  try {
+    Expect.isTrue(file.existsSync());
+  } finally {
+    file.delete();
+  }
+}
