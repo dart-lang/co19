c8c9cdc9207dee925738070cdcef92c159560ac6
Sergey G. Grekhov <sgrekhov@unipro.ru>
2017-07-17 15:45:17 +0700

    File.openRead() tests added

---
+++ LibTest/io/File/openRead_A01_t01.dart
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Stream<List<int>> openRead([int start, int end])
+ * Create a new independent Stream for the contents of this file.
+ *
+ * If start is present, the file will be read from byte-offset start. Otherwise
+ * from the beginning (index 0).
+ *
+ * If end is present, only up to byte-index end will be read. Otherwise, until
+ * end of file.
+ *
+ * In order to make sure that system resources are freed, the stream must be
+ * read to completion or the subscription on the stream must be cancelled.
+ * @description Checks that this method creates a new independent Stream for the
+ * contents of this file
+ * @author sgrekhov@unipro.ru
+ */
+import "dart:io";
+import "../../../Utils/expect.dart";
+import "../../../Utils/file_utils.dart";
+import "../../../Utils/async_utils.dart";
+
+main() {
+  File file = getTempFileSync();
+  file.writeAsBytesSync([1, 2, 3]);
+  asyncStart();
+  file.openRead().toList().then((data) {
+    try {
+      Expect.listEquals(data, [[1, 2, 3]]);
+      asyncEnd();
+    } finally {
+      file.delete();
+    }
+  });
+}
