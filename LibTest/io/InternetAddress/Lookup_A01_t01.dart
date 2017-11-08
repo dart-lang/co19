b9eb5228c827b2c0c4b12da6fa804e6ff1ebd42c
iarkh <irina.arkhipets@gmail.com>
2017-11-08 13:16:15 +0700

    InternetAddress tests added

---
+++ LibTest/io/InternetAddress/Lookup_A01_t01.dart
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion  Future<List<InternetAddress>> lookup(String host,
+ *   { InternetAddressType type: InternetAddressType.ANY })
+ * Lookup a host, returning a [Future] of a list of [InternetAddresss].
+ * @description Checks that result [internetAddress] list contains addresses for
+ * the given host only.
+ * @author iarkh@unipro.ru
+ */
+
+import "../../../Utils/expect.dart";
+import "dart:io";
+import "dart:async";
+
+main() {
+  Future<List<InternetAddress>>  list = InternetAddress.lookup("localhost");
+
+  list.then((addresses) {
+    addresses.forEach((InternetAddress addr) {
+      Expect.equals("localhost", addr.host);
+    });
+  }, onError: (e) { Expect.fail("Unexpected error appeared: " + e); });
+}
