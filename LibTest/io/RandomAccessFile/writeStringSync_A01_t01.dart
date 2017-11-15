a12eba6bd4e67551b5f191d71b4d8491a13b3974
Natalya Golovleva <ngl@unipro.ru>
2017-11-15 20:42:09 +0700

    New tests added.

---
+++ LibTest/io/RandomAccessFile/writeStringSync_A01_t01.dart
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion void writeStringSync(String string, {Encoding encoding: UTF8})
+ * Synchronously writes a single string to the file using the given Encoding.
+ *
+ * @description Checks that method writeStringSync synchronously writes a single
+ * string to the file using the given default Encoding UTF8.
+ * @author ngl@unipro.ru
+ */
+import "dart:async";
+import "dart:io";
+import "dart:convert";
+import "../../../Utils/expect.dart";
+import "../../../Utils/async_utils.dart";
+import "../file_utils.dart";
+
+check(String s) {
+  File file = getTempFileSync();
+  asyncStart();
+  Future<RandomAccessFile> raFile = file.open(mode: FileMode.WRITE);
+  raFile.then((RandomAccessFile rf) {
+    rf.writeStringSync(s);
+
+    var flen = rf.lengthSync();
+    rf.setPositionSync(0);
+    List<int> l = rf.readSync(flen);
+    var ed = new Utf8Codec();
+    String s1 = ed.decode(l);
+    Expect.equals(s, s1);
+
+    asyncEnd();
+  }).whenComplete(() {
+    file.deleteSync();
+  });
+}
+
+main() {
+  check("");
+  check("abc");
+  check("алфавит");
+}
---
+++ LibTest/io/RandomAccessFile/writeString_A02_t01.dart
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion
+ * Future<RandomAccessFile> writeString(
+ *     String string, {
+ *     Encoding encoding: UTF8
+ * })
+ * . . .
+ * Returns a Future<RandomAccessFile> that completes with this RandomAccessFile
+ * when the write completes.
+ *
+ * @description Checks that method writeString returns a
+ * Future<RandomAccessFile> that completes with this RandomAccessFile when the
+ * write completes.
+ * @author ngl@unipro.ru
+ */
+import "dart:async";
+import "dart:io";
+import "../../../Utils/expect.dart";
+import "../../../Utils/async_utils.dart";
+import "../file_utils.dart";
+
+check(String s) {
+  File file = getTempFileSync();
+  asyncStart();
+  Future<RandomAccessFile> raFile = file.open(mode: FileMode.WRITE);
+  raFile.then((RandomAccessFile rf) {
+    var f = rf.writeString(s);
+    Expect.isTrue(f is Future<RandomAccessFile>);
+
+    f.then((RandomAccessFile file) {
+      Expect.isTrue(file is RandomAccessFile);
+      Expect.equals(rf, file);
+    });
+
+    asyncEnd();
+  }).whenComplete(() {
+    file.deleteSync();
+  });
+}
+
+main() {
+  check("");
+  check("abc");
+  check("алфавит");
+}
