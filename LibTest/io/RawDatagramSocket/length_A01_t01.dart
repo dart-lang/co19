4acb1372cc157b5e5f511c95d2cd9f0831e872bd
Natalya Golovleva <ngl@unipro.ru>
2018-01-09 20:08:48 +0700

    Tests added.

---
+++ LibTest/io/RawDatagramSocket/length_A01_t01.dart
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Future<int> length
+ * Counts the elements in the stream.
+ *
+ * @description Checks that property length returns the number of elements
+ * in the socket when this RawDatagramSocket is closed (returns 1).
+ * @author ngl@unipro.ru
+ */
+import "dart:io";
+import "../../../Utils/expect.dart";
+import "../../../Utils/async_utils.dart";
+
+check([bool no_write_events = false]) {
+  asyncStart();
+  var address = InternetAddress.LOOPBACK_IP_V4;
+  RawDatagramSocket.bind(address, 0).then((producer) {
+    RawDatagramSocket.bind(address, 0).then((receiver) {
+      if (no_write_events) {
+        receiver.writeEventsEnabled = false;
+      }
+      int sent = 0;
+      var rValue;
+
+      producer.send([sent++], address, receiver.port);
+      producer.send([sent++], address, receiver.port);
+      producer.send([sent], address, receiver.port);
+      producer.close();
+      receiver.close();
+
+      void action() {
+        Expect.equals(1, rValue);
+        asyncEnd();
+      }
+
+      receiver.length.then((value) {
+        rValue = value;
+      }).whenComplete (action);
+    });
+  });
+}
+
+main() {
+  check();
+  check(true);
+}
