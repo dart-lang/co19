8ae733cfd62480eb3eb82ee3511c49d9690d1ca4
Sergey G. Grekhov <sgrekhov@unipro.ru>
2018-01-10 10:56:02 +0700

    Fix for issue #129.  Added test with expects compile time error if class C is superinterface of C

---
+++ Language/Classes/definition_t24.dart
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion classDefinition:
+ *   metadata abstract? class identifier typeParameters? (superclass mixins?)?
+ * interfaces? ‘{’ (metadata classMemberDefinition)* ‘}’ |
+ *   metadata abstract? class mixinApplicationClass
+ * ;
+ * ...
+ * It is a compile-time error if the superclass of a class C is specified as a
+ * superinterface of C.
+ * ...
+ * @description Checks that it is a compile-time error if the superclass of a
+ * class C is specified as a superinterface of C.
+ * @compile-error
+ * @author sgrekhov@unipro.ru
+ * @issue dart-lang/co19#129
+ * @issue 31624
+ */
+class X = A with B implements A;
+
+abstract class A {}
+class B {}
+
+main() {
+  new X();
+}
