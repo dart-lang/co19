11d1618765ce6c2f11b08aa5615a999bc7cfcc1b
Sergey G. Grekhov <sgrekhov@unipro.ru>
2018-01-10 13:16:34 +0700

    HttpClientRequest.flush() tests added

---
+++ LibTest/io/HttpClientRequest/flush_A01_t01.dart
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Future flush()
+ * Returns a Future that completes once all buffered data is accepted by the
+ * underlying StreamConsumer.
+ *
+ * This method must not be called while an addStream is incomplete.
+ *
+ * NOTE: This is not necessarily the same as the data being flushed by the
+ * operating system.
+ * @description Checks that this method returns a Future that completes once all
+ * buffered data is accepted by the underlying StreamConsumer.
+ * @author sgrekhov@unipro.ru
+ */
+import "dart:io";
+import "dart:convert";
+import "../../../Utils/expect.dart";
+import "../../../Utils/async_utils.dart";
+
+var localhost = InternetAddress.LOOPBACK_IP_V4.address;
+
+test(String method) async {
+  asyncStart();
+  String helloWorld = "Hello test world!";
+  HttpServer server = await HttpServer.bind(localhost, 0);
+  server.listen((HttpRequest request) {
+    request.response.write(helloWorld);
+    request.response.close();
+    server.close();
+  });
+
+  HttpClient client = new HttpClient();
+  client.open(method, localhost, server.port, "")
+      .then((HttpClientRequest request) {
+    request.flush().then((r) {
+      Expect.equals(r, request);
+      asyncEnd();
+    });
+    return request.close();
+  }).then((HttpClientResponse response) {
+    response.transform(UTF8.decoder).listen((content) {
+    });
+  });
+}
+
+main() {
+  test("get");
+  test("head");
+  test("delete");
+  test("put");
+  test("post");
+  test("patch");
+}
