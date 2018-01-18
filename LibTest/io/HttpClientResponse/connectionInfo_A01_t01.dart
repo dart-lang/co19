e2fbc0acfa9c44c53e46a892bb480c93dd44818b
Sergey G. Grekhov <sgrekhov@unipro.ru>
2018-01-18 14:15:59 +0700

    HttpClientResponse tests added

---
+++ LibTest/io/HttpClientResponse/connectionInfo_A01_t01.dart
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion HttpConnectionInfo connectionInfo
+ * Gets information about the client connection. Returns null if the socket is
+ * not available.
+ * @description Checks that this getter returns information about the client
+ * connection
+ * @author sgrekhov@unipro.ru
+ */
+import "dart:io";
+import "dart:convert";
+import "../../../Utils/expect.dart";
+import "../../../Utils/async_utils.dart";
+
+var localhost = InternetAddress.LOOPBACK_IP_V4.address;
+
+test(String method) async {
+  asyncStart();
+  String helloWorld = "Hello test world!";
+  HttpServer server = await HttpServer.bind(localhost, 0);
+  int port = server.port;
+  server.listen((HttpRequest request) {
+    request.response.write(helloWorld);
+    request.response.close();
+    server.close();
+  });
+
+  HttpClient client = new HttpClient();
+  client.open(method, localhost, server.port, "")
+      .then((HttpClientRequest request) {
+    return request.close();
+  }).then((HttpClientResponse response) {
+    Expect.isNotNull(response.connectionInfo);
+    Expect.equals(localhost, response.connectionInfo.remoteAddress.address);
+    Expect.equals(port, response.connectionInfo.remotePort);
+    response.transform(UTF8.decoder).listen((content) {});
+    asyncEnd();
+  });
+}
+
+main() {
+  test("get");
+  test("head");
+  test("delete");
+  test("put");
+  test("post");
+  test("patch");
+}
---
+++ LibTest/io/HttpClientResponse/contentLength_A01_t02.dart
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion int contentLength
+ * Returns the content length of the response body. Returns -1 if the size of
+ * the response body is not known in advance.
+ *
+ * If the content length needs to be set, it must be set before the body is
+ * written to. Setting the reason phrase after writing to the body will throw a
+ * StateError.
+ * @description Checks that this getter returns the content length of the
+ * response body. Test zero length response
+ * @author sgrekhov@unipro.ru
+ */
+import "dart:io";
+import "dart:convert";
+import "../../../Utils/expect.dart";
+import "../../../Utils/async_utils.dart";
+
+var localhost = InternetAddress.LOOPBACK_IP_V4.address;
+
+test(String method) async {
+  asyncStart();
+  String helloWorld = "Hello test world!";
+  HttpServer server = await HttpServer.bind(localhost, 0);
+  server.listen((HttpRequest request) {
+    request.response.contentLength = 0;
+    request.response.close();
+    server.close();
+  });
+
+  HttpClient client = new HttpClient();
+  client.open(method, localhost, server.port, "")
+      .then((HttpClientRequest request) {
+    return request.close();
+  }).then((HttpClientResponse response) {
+    Expect.equals(0, response.contentLength);
+    response.transform(UTF8.decoder).listen((content) {});
+    asyncEnd();
+  });
+}
+
+main() {
+  test("get");
+  test("head");
+  test("delete");
+  test("put");
+  test("post");
+  test("patch");
+}
