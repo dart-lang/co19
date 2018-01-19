189220b43e6d291d4d2c86d1496840c1814e1d9e
Natalya Golovleva <ngl@unipro.ru>
2018-01-19 23:07:32 +0700

    Tests added.

---
+++ LibTest/io/RawDatagramSocket/any_A01_t01.dart
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Future<bool> any(bool test(T element))
+ * Checks whether test accepts any element provided by this stream.
+ *
+ * Completes the Future when the answer is known.
+ *
+ * If this stream reports an error, the Future reports that error.
+ *
+ * Stops listening to the stream after the first matching element has been
+ * found.
+ *
+ * @description Checks whether test accepts any element provided by this stream
+ * and stops listening to the stream after the first matching element has been
+ * found.
+ * @author ngl@unipro.ru
+ */
+import "dart:async";
+import "dart:io";
+import "../../../Utils/expect.dart";
+import "../../../Utils/async_utils.dart";
+
+check(int expectedValue, [bool no_write_events = false]) {
+  asyncStart();
+  var address = InternetAddress.LOOPBACK_IP_V4;
+  RawDatagramSocket.bind(address, 0).then((producer) {
+    RawDatagramSocket.bind(address, 0).then((receiver) {
+      if (no_write_events) {
+        receiver.writeEventsEnabled = false;
+      }
+      int sent = 0;
+      int count = 0;
+
+      new Timer.periodic(const Duration(microseconds: 1), (timer) {
+        producer.send([sent], address, receiver.port);
+        sent++;
+        if (sent > 3) {
+          timer.cancel();
+          producer.close();
+          receiver.close();
+        }
+      });
+
+      bool test(RawSocketEvent x) {
+        count++;
+        var d = receiver == null ? null : receiver.receive();
+        if (d != null) {
+          return d.data[0] == expectedValue;
+        } else {
+          return false;
+        }
+      }
+
+      receiver.any((event) => test(event)).then((value) {
+        Expect.equals(true, value);
+        Expect.equals(expectedValue + 1, count);
+      }).whenComplete(() {
+        asyncEnd();
+      });
+    });
+  });
+}
+
+main() {
+  for (int i = 0; i < 3; i++) {
+    check(i);
+    check(i, true);
+  }
+}
---
+++ LibTest/io/RawDatagramSocket/any_A01_t06.dart
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Future<bool> any(bool test(T element))
+ * Checks whether test accepts any element provided by this stream.
+ *
+ * Completes the Future when the answer is known.
+ *
+ * If this stream reports an error, the Future reports that error.
+ *
+ * Stops listening to the stream after the first matching element has been
+ * found.
+ *
+ * @description Checks that method any returns true when RawSocketEvent.CLOSED
+ * is searched and writeEventsEnabled is false. In this case the listening to
+ * the stream is stopped after the last received event.
+ * @issue 31881
+ * @author ngl@unipro.ru
+ */
+import "dart:async";
+import "dart:io";
+import "../../../Utils/expect.dart";
+import "../../../Utils/async_utils.dart";
+
+main() {
+  var expectedEvent = RawSocketEvent.CLOSED;
+  asyncStart();
+  var address = InternetAddress.LOOPBACK_IP_V4;
+  RawDatagramSocket.bind(address, 0).then((producer) {
+    RawDatagramSocket.bind(address, 0).then((receiver) {
+      receiver.writeEventsEnabled = false;
+      int sent = 0;
+      int count = 0;
+
+      new Timer.periodic(const Duration(microseconds: 1), (timer) {
+        producer.send([sent], address, receiver.port);
+        sent++;
+        if (sent > 3) {
+          timer.cancel();
+          producer.close();
+          receiver.close();
+        }
+      });
+
+      bool test(x) {
+        count++;
+        return x == expectedEvent;
+      }
+
+      receiver.any((event) => test(event)).then((value) {
+        Expect.equals(true, value);
+        Expect.equals(4, count);
+      }).whenComplete(() {
+        asyncEnd();
+      });
+    });
+  });
+}
