1fc198a8cbcc430357a9de691c15cdcd50745a3f
Natalya Golovleva <ngl@unipro.ru>
2018-03-07 18:46:37 +0700

    Description fixed. New test added.

--- LibTest/io/RawDatagramSocket/length_A02_t01.dart
+++ LibTest/io/RawDatagramSocket/length_A02_t01.dart
@@ -5,27 +5,23 @@
  */
 /**
  * @assertion Future<int> length
- * The number of elements in this stream.
+ * . . .
+ * If the stream contains an error, the returned future is completed with that
+ * error, and processing stops.
  *
- * Waits for all elements of this stream. When the stream ends, the returned
- * future is completed with the number of elements.
- *
- * @description Checks that property length returns the number of elements in
- * the socket when this RawDatagramSocket is regarded as a broadcast stream.
+ * @description Checks that if this stream contains an error, the returned
+ * future is completed with that error, and processing stops.
  * @author ngl@unipro.ru
  */
 import "dart:io";
 import "../../../Utils/expect.dart";
 import "../../../Utils/async_utils.dart";
 
-check([bool no_write_events = false]) {
+check(convert, expected) {
   asyncStart();
   var address = InternetAddress.LOOPBACK_IP_V4;
   RawDatagramSocket.bind(address, 0).then((producer) {
     RawDatagramSocket.bind(address, 0).then((receiver) {
-      if (no_write_events) {
-        receiver.writeEventsEnabled = false;
-      }
       int sent = 0;
       int counter = 0;
       producer.send([sent++], address, receiver.port);
@@ -34,8 +30,10 @@ check([bool no_write_events = false]) {
       producer.close();
 
       Stream<RawSocketEvent> stream = receiver.asBroadcastStream();
-      stream.length.then((value) {
-        Expect.equals(4, value);
+      stream.expand(convert).length.then((value) {
+        Expect.fail("value == $value. Future should be completed with error.");
+      }, onError: (error) {
+        Expect.equals(expected, error);
       });
 
       new Timer(const Duration(milliseconds: 200), () {
@@ -55,6 +53,7 @@ check([bool no_write_events = false]) {
 }
 
 main() {
-  check();
-  check(true);
+  check((e) => e == RawSocketEvent.WRITE ? throw 11 : [e], 11);
+  check((e) => e == RawSocketEvent.READ ? throw 12 : [e], 12);
+  check((e) => e == RawSocketEvent.CLOSED ? throw 13 : [e], 13);
 }
