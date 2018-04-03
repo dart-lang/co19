fa0d2d46045d9b1b590d7db4891cc37023ab6ea1
iarkh <irina.arkhipets@gmail.com>
2018-04-03 15:15:41 +0700

    Nw tests for io:Stdin added.

---
+++ LibTest/io/Stdin/any_A01_t01.dart
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Future<bool> any(bool test(T element))
+ * Checks whether [test] accepts any element provided by this stream.
+ * @description Checks that [any] returns [true] if [test] returns [true]
+ * @author iarkh@unipro.ru
+ */
+import "../../../Utils/expect.dart";
+import "dart:io";
+
+run_process() async {
+  await stdin.any((x) => true).then((x) { exit(x ? 0 : 99); });
+}
+
+run_main() async {
+  String executable = Platform.resolvedExecutable;
+  String eScript = Platform.script.toString();
+  int called = 0;
+
+  await Process.start(executable, [eScript, "test"], runInShell: true).then(
+      (Process process) async {
+    process.stdin.writeln("Testme");
+    await process.exitCode.then((int code) async {
+      Expect.equals(0, code);
+      called++;
+    });
+  });
+  Expect.equals(1, called);
+}
+
+main(List<String> args) { args.length > 0 ? run_process() : run_main(); }
