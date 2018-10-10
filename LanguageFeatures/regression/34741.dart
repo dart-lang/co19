1988227b93e863e92aab5e48490ff2f04466eeb3
iarkh <irina.arkhipets@gmail.com>
2018-10-10 13:56:30 +0700

    Regression test for 34741 has been added.

---
+++ LanguageFeatures/regression/34741.dart
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @description Regression test for the Issue 34741: Analyzer doesn't allow
+ * function to use type parameter [<Instance of 'TypeParameterMember'>] as a
+ * type argument whereas dart does allow this.
+ * @Issue 34741
+ * @author iarkh@unipro.ru
+ */
+Type typeOf<X>() => X;
+
+typedef G = Function<X extends num>();
+typedef G1<X> = Function();
+
+main() {
+  typeOf<G>();
+  typeOf<G1>();
+}
