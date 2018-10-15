fa78f52362a38c173f63ea166f1624eb7eacabcd
Sergey G. Grekhov <sgrekhov@unipro.ru>
2018-10-15 16:35:09 +0700

    Additional fix for #164. Double quote fixed as well

--- Language/Expressions/Strings/String_Interpolation/double_quote_t02.dart
+++ Language/Expressions/Strings/String_Interpolation/double_quote_t02.dart
@@ -15,10 +15,11 @@ import '../../../../Utils/expect.dart';
 
 class C {
   var id;
+  dynamic x = null;
   test() {
     Expect.throws(() {"${[][10]}";}, (e) => e is RangeError);
-    Expect.throws(() {"${(const []).add(1)}";}, (e) => e is UnsupportedError);
-    Expect.throws(() {"${null.someMethod()}";}, (e) => e is NoSuchMethodError);
+    Expect.throws(() {"${(const [0]).removeLast()}";}, (e) => e is UnsupportedError);
+    Expect.throws(() {"${x.someMethod()}";}, (e) => e is NoSuchMethodError);
     Expect.throws(() {"${id()}";}, (e) => e is NoSuchMethodError);
   }
 }
