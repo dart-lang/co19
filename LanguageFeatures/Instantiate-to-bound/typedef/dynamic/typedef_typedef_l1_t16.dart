2a2cf3b41ce31d4709b472dc771cecd0ae329a97
iarkh <irina.arkhipets@gmail.com>
2018-12-18 10:54:38 +0700

    Instantiate-to-bounds tests added.

--- LanguageFeatures/Instantiate-to-bound/typedef/dynamic/typedef_typedef_l1_t16.dart
+++ LanguageFeatures/Instantiate-to-bound/typedef/dynamic/typedef_typedef_l1_t16.dart
@@ -1,4 +1,4 @@
- /*
+/*
  * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
  * for details. All rights reserved. Use of this source code is governed by a
  * BSD-style license that can be found in the LICENSE file.
@@ -42,19 +42,18 @@
  *
  *   3. Otherwise, (when no dependencies exist) terminate with the result
  *   [<U1,m ..., Uk,m>].
- * @description Checks that instantiate-to-bounds works correctly for [typedef
- *  G<X extends A<X>> = Function(X)] (contravariant)
- * @Issue 34689
+ * @description Checks that instantiate-to-bounds works correctly for
+ * [typedef A<X> = void Function(); typedef G<X extends A<X>> = void Function()]
  * @author iarkh@unipro.ru
  */
 import "../../../../Utils/expect.dart";
 
-class A<X> {}
-typedef G<X extends A<X>> = Function(X);
+typedef A<X> = void Function();
+typedef G<X extends A<X>> = void Function();
 
 main() {
   Expect.equals(
-    typeOf<G<A<Null>>>(),
+    typeOf<G<A<dynamic>>>(),
     typeOf<G>()
   );
 }
