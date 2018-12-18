80bef95b1976abe2e1f37d9405e59a3a3bbb72ba
Sergey G. Grekhov <sgrekhov@unipro.ru>
2018-12-18 13:17:47 +0700

    Null-aware expression test added for set literals

---
+++ LanguageFeatures/Set-literals/syntax_compatibility_A01_t02.dart
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion The token following a question mark dictates whether it's part of
+ * a null-aware operation or the beginning of a new expression.
+ *
+ * @description Checks usage of a null-aware expression when a set literal is
+ * declared.
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=set-literals
+import "../../Utils/expect.dart";
+
+class C {
+  int n1 = 1;
+  static int n2 = 2;
+  int x = null;
+}
+main() {
+  C c = null;
+
+  var v1 = {c?.x ?? 3, C?.n2, c?.n1};
+  Expect.isTrue(v1 is Set);
+  Expect.iterableEquals({3, 2, 1}, v1));
+
+  c = new C();
+  print(c.n1 ??= c.x);
+  print(c.x ??= 3);
+
+  var v2 = {c?.x ?? 3, C?.n2, c?.n1};
+  Expect.isTrue(v2 is Set);
+  Expect.iterableEquals({3, 2, 1}, v2));
+
+  const v3 = const <int>{c?.x ?? 3, C?.n2, c?.n1};
+  Expect.isTrue(v3 is Set);
+  Expect.iterableEquals({3, 2, 1}, v3));
+
+  var v4 = <int>{c.n1 ??= c.x, c.x ??= 4, C?.n2, c?.x ?? 3};
+  Expect.isTrue(v4 is Set);
+  Expect.iterableEquals({1, 4, 2, 3}, v4));
+}
