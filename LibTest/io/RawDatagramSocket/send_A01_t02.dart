9feec9dda8f17d70b4c34614d21e3f3cc6438d0a
Sergey G. Grekhov <sgrekhov@unipro.ru>
2018-12-21 13:20:23 +0700

    RawDatagramSocket.send() tests rewritten

--- LibTest/io/RawDatagramSocket/send_A01_t02.dart
+++ LibTest/io/RawDatagramSocket/send_A01_t02.dart
@@ -10,73 +10,23 @@
  * Returns the number of bytes written. This will always be either the size of
  * buffer or 0.
  *
- * @description Checks that method send returns 0 if buffer length is greater
- * then a maximum length of a received datagram.
- * @author ngl@unipro.ru
- * @issue 31873
+ * @description Checks that method send returns the number of bytes that were
+ * sent.
+ * @author sgrekhov@unipro.ru
  */
-import "dart:async";
 import "dart:io";
+import "../http_utils.dart";
 import "../../../Utils/expect.dart";
 
-int maxSentLength = 65503;
-int sentLength = 66000;
+var localhost = InternetAddress.loopbackIPv4;
 
-check([bool no_write_events = false]) {
-  asyncStart();
-  var address = InternetAddress.loopbackIPv4;
-  RawDatagramSocket.bind(address, 0).then((producer) {
-    RawDatagramSocket.bind(address, 0).then((receiver) {
-      if (no_write_events) {
-        receiver.writeEventsEnabled = false;
-      }
-      Timer timer2;
-      int received = 0;
-      int sent = 0;
+main() async {
+  RawDatagramSocket producer = await RawDatagramSocket.bind(localhost, 0);
+  RawDatagramSocket receiver = await RawDatagramSocket.bind(localhost, 0);
+  List<List<int>> toSent = [[1, 1], bigData, [2]];
+  bool sent = await sendDatagram(producer, toSent, localhost, receiver.port);
+  Expect.isTrue(sent);
 
-      List<int> sList1 = new List<int>(maxSentLength);
-      for (int i = 0; i < maxSentLength; i++) {
-        sList1[i] = i & 0xff;
-      }
-
-      List<int> sList2 = new List<int>(sentLength);
-      for (int i = 0; i < sentLength; i++) {
-        sList2[i] = i & 0xff;
-      }
-      List<List<int>> list = [[1], sList1, sList2];
-      new Timer.periodic(const Duration(microseconds: 1), (timer) {
-        int count = producer.send(list[sent], address, receiver.port);
-        Expect.isTrue(count == 0 || count == list[sent].length);
-        sent++;
-        if (sent > 2) {
-          timer.cancel();
-          producer.close();
-        }
-      });
-
-      void action() {
-        asyncEnd();
-      }
-
-      receiver.listen((event) {
-        received++;
-        var datagram = receiver.receive();
-        if (datagram != null) {
-          Expect.listEquals(list[received - 1], datagram.data);
-        }
-        if (timer2 != null) timer2.cancel();
-        timer2 = new Timer(const Duration(milliseconds: 200), () {
-          if (event != RawSocketEvent.closed) {
-            Expect.equals(received, sent - 1);
-          }
-          receiver.close();
-        });
-      }).onDone(action);
-    });
-  });
-}
-
-main() {
-  check();
-  check(true);
+  List<List<int>> received = await receiveDatagram(receiver);
+  Expect.isTrue(received.length > 0);
 }
--- LibTest/io/http_utils.dart
+++ LibTest/io/http_utils.dart
@@ -11,6 +11,7 @@ library http_utils;
 
 import "dart:io";
 import "dart:async";
+import "../../Utils/expect.dart";
 
 /**
  * Spawns a HttpServer instance
@@ -51,4 +52,104 @@ Future<HttpServer> spawnEchoWebSocketServer() {
     webSocket.add(await webSocket.first);
     webSocket.close();
   });
+}
+
+List<int> _bigData = null;
+
+List<int> get bigData  {
+ if (_bigData == null) {
+   _bigData = getList(8187);
+ }
+ return _bigData;
+}
+
+List<int> getList(int size) {
+  List<int> l = [];
+  for (int j = 0; j < size; j++) {
+    l.add(j);
+  }
+  return l;
+}
+
+Future<List<int>> sendDatagramOnce(RawDatagramSocket producer,
+    List<List<int>> data, InternetAddress address, int port,
+    {Duration period = const Duration(milliseconds: 1)}) {
+  List<int> sent = [];
+  Completer<List<int>> completer = new Completer<List<int>>();
+  Future<List<int>> f = completer.future;
+  if (data != null) {
+    int i = 0;
+    new Timer.periodic(period, (timer) {
+      if (i < data.length) {
+        sent.add(producer.send(data[i++], address, port));
+      } else {
+        timer.cancel();
+        producer.close();
+        completer.complete(sent);
+      }
+    });
+  } else {
+    completer.complete(sent);
+  }
+  return f;
+}
+
+Future<bool> sendDatagram(RawDatagramSocket producer, List<List<int>> data,
+    InternetAddress address, int port,
+    {Duration period = const Duration(milliseconds: 1), int attempts = 5}) async {
+  for (int attempt = 0; attempt < attempts; attempt++) {
+    List<int> bytesWritten = await sendDatagramOnce(producer, data, address, port,
+        period: period);
+    if (wasSent(bytesWritten)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool wasSent(List<int> bytesWritten) {
+  for (int i = 0; i < bytesWritten.length; i++) {
+    if (bytesWritten[i] > 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+Future<List<List<int>>> receiveDatagram(RawDatagramSocket receiver,
+    {Duration delay = const Duration(seconds: 2), RawSocketEvent event}) {
+  List<List<int>> received = [];
+  Completer<List<List<int>>> completer = new Completer<List<List<int>>>();
+  Future<List<List<int>>> f = completer.future;
+  receiver.listen((_event) {
+    if (event == null || _event == event) {
+      var datagram = receiver.receive();
+      if (datagram != null) {
+        print("Received: ${datagram.data}");
+        received.add(datagram.data);
+      }
+    }
+  });
+  new Future.delayed(delay, () {
+    receiver.close();
+    if(!completer.isCompleted) {
+      completer.complete(received);
+    }
+  });
+  return f;
+}
+
+compareDatagrams(List<List<int>> sent, List<List<int>> received, List<int> sentStatus) {
+  Expect.equals(sentStatus.length, sent.length, "Wrong sizes of sent and sentRezult");
+  for (int i = 0, k = 0; i < sent.length; i++) {
+    if (sentStatus[i] == 0) {
+      continue;
+    }
+    Expect.equals(sent.length, received.length);
+    Expect.equals(sent[i].length, received[k].length, "Wrong sizes of sent and received");
+    for (int j = 0; j < sent[i].length; j++) {
+      Expect.equals(sent[i][j] & 0xff, received[k][j], "Wrong values: i=$i, k=$k, j=$j");
+    }
+    k++;
+  }
 }
