71ed9977c329c149581a8c8f9e9742dc1950c2fd
iarkh <irina.arkhipets@gmail.com>
2019-02-07 00:00:02 +0700

    New tests for spread collections added.

---
+++ LanguageFeatures/Spread-collections/DynamicSemantics_List_A01_t01.dart
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion A list literal <E>[elem_1 ... elem_n] is evaluated as follows:
+ *
+ *  1.  Create a fresh instance list of a class that implements [List<E>].
+ * @description Checks that instance of [List<E>] is created for a list literal
+ * @author iarkh@unipro.ru
+ */
+// SharedOptions=--enable-experiment=spread-collections
+
+import "../../Utils/expect.dart";
+
+main() {
+  List list1 = [1, 2, 3, 4, 5];
+  List list2 = [];
+  List list3 = null;
+  List list4 = [1 ,2, 3, "checkme"];
+
+  Expect.isTrue(<int>[...list1] is List<int>);
+  Expect.isTrue(<int>[1, 2, ...list1] is List<int>);
+  Expect.isTrue(<int>[1, ...?list1, 14, 9] is List<int>);
+  Expect.isTrue(<String>[...list2, "123"] is List<String>);
+  Expect.isTrue(<String>[...?list3] is List<String>);
+  Expect.isTrue(<int>[...?list3] is List<int>);
+  Expect.isTrue([...list4] is List<Object>);
+}
---
+++ LanguageFeatures/Spread-collections/DynamicSemantics_List_A02_t03.dart
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion A list literal <E>[elem_1 ... elem_n] is evaluated as follows:
+ *
+ * 2. For each element in the list literal:
+ *   i. Evaluate the element's expression to a value value.
+ *   ii. If element is a spread element:
+ *     a. If element is null-aware and value is [null], continue to the next
+ *        element in the literal.
+ *     b. Evaluate [value.iterator] to a [value] iterator.
+ *     c. Loop:
+ *        a. If [iterator.moveNext()] returns [false], exit the loop.
+ *        b. Evaluate [iterator.current] and append the result to list.
+ *   iii. Else:
+ *     a. Append value to list.
+ *
+ * @description Checks that elements in the null-aware spread element are added
+ * to the result list in correct order if this spread element is not [null].
+ * @author iarkh@unipro.ru
+ */
+// SharedOptions=--enable-experiment=spread-collections
+
+import "dart:collection";
+import "../../Utils/expect.dart";
+
+class MyIterable extends IterableBase {
+  MyIterator iterator;
+  MyIterable(List list) { iterator = new MyIterator(list); }
+  Iterator getIterator() { return iterator; }
+}
+
+class MyIterator extends Iterator {
+  int i = 0;
+  List list;
+
+  MyIterator(List aList) { list = aList; }
+
+  @override
+  bool moveNext() {
+    if (i < list.length) {
+      i++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  int get current { return list[i]; }
+}
+
+List myLists = [[1, 2, 3, 4, 5],
+  ["a", "14", 1, 2, null, 143, [11]],
+  [1, 2, {}, [123,4], 14],
+  [[], [], 12345],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  []];
+
+main() {
+  myLists.forEach((var list) {
+    MyIterable it = new MyIterable(list);
+    Expect.listEquals(list, [...?it]);
+    Expect.isFalse(it.getIterator().moveNext());
+  });
+}
