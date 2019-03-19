498c3098e963c2f90537864b08906afc6e5cb29d
Sergey G. Grekhov <sgrekhov@unipro.ru>
2019-03-19 16:56:22 +0700

    Additional fix for #309. Remove tests with non-primitive '=='

--- Language/Expressions/Maps/equal_keys_t01.dart
+++ Language/Expressions/Maps/equal_keys_t01.dart
@@ -13,18 +13,7 @@
  * @author sgrekhov@unipro.ru
  */
 
-class C {
-  final a;
-  final b;
-
-  const C(this.a, this.b);
-  int get hashCode => a;
-  bool operator== (other) => this.a == other.a;
-}
-
 main() {
   const m1 = <int, String>{1: "val1", 1: "val2"};     //# 01: compile-time error
   var m2 = const <String, int>{"key1": 1, "key1": 2}; //# 02: compile-time error
-  const m3 = <C, int>{const C(1, 2): 1, const C(1, 3): 2};      //# 03: compile-time error
-  var m4 = const <C, int>{const C(1, 2): 1, const C(1, 3): 2};  //# 04: compile-time error
 }
