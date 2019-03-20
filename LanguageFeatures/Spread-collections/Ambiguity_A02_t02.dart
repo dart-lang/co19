2c399148f14474691dbc58e0151be8bcbe97fc59
iarkh <irina.arkhipets@gmail.com>
2019-03-20 12:22:13 +0700

    Additional fix for the issue #304

--- LanguageFeatures/Spread-collections/Ambiguity_A02_t02.dart
+++ LanguageFeatures/Spread-collections/Ambiguity_A02_t02.dart
@@ -6,17 +6,15 @@
 /**
  * @assertion In cases where the context type is not specific enough to
  * disambiguate, we could make it an error instead of defaulting to map.
- * @description Checks that {...?null} collection declaring causes a compile
- * time error.
+ * @description Checks that {...?null} collection type is detected correctly
  * @author iarkh@unipro.ru
  */
 // SharedOptions=--enable-experiment=spread-collections
 
 main() {
-  Set res1 = {...?null}; //# 01: compile-time error
-  Map res2 = {...?null}; //# 02: compile-time error
-  var res3 = {...?null}; //# 03: compile-time error
+  Set res1 = {...?null};
+  Map res2 = {...?null};
 
-  Set res4 = <int>{...?null};
-  Map res5 = <int, int>{...?null};
+  var res3 = {...?null};     //# 01: compile-time error
+  dynamic res4 = {...?null}; //# 02: compile-time error
 }
