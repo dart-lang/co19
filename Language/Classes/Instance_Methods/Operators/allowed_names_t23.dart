0b01c5d544c62f8581f6a67125df72e321d1ae7b
iarkh <irina.arkhipets@gmail.com>
2019-03-26 23:24:17 +0700

    Issue #325: tests for >>> operator added.

--- Language/Classes/Instance_Methods/Operators/allowed_names_t23.dart
+++ Language/Classes/Instance_Methods/Operators/allowed_names_t23.dart
@@ -1,22 +1,23 @@
 /*
- * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
  * for details. All rights reserved. Use of this source code is governed by a
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
  * @assertion The following names are allowed for user-defined operators: 
  * <, >, <=, >=, ==, -, +, /, ˜/, *, %, |, ˆ, &, <<, >>, >>>, []=, [], ˜.
- * @description Checks that operator = cannot be defined in a user class.
+ * @description Checks that operator <<< cannot be defined in a user class.
  * @compile-error
+ * @author iefremov
  */
 
 class C {
-  operator =(x) {}
+  operator <<<(x) {}
 }
 
 main() {
   try {
     var b = (new C());
-    b = null;
+    (b <<< 1);
   } catch (x) {}
 }
