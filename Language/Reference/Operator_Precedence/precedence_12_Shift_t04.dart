a57c0ae1a3df3f7905bfb3000290273e1bac3db6
iarkh <irina.arkhipets@gmail.com>
2019-03-27 13:31:02 +0700

    Fixed issue #325: nests for >>>= operator added.

---
+++ Language/Reference/Operator_Precedence/precedence_12_Shift_t04.dart
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion
+ * Description    Operator                          Associativity Precedence
+ * Shift          <<, >>, >>>                         Left           12
+ * @description Test that '>>>' shift operator has precedence (12).
+ * Compare with Bitwise AND operator (precedence 11). Test that '>>>' has
+ * priority higher than '&'
+ * @author iarkh@unipro.ru
+ */
+import "../../../Utils/expect.dart";
+
+main() {
+  Expect.equals(2 & (8 >>> 2) & 3, 2 & 8 >>> 2 & 3);
+  Expect.notEquals((2 & 8) >>> (2 & 3), 2 & 8 >>> 2 & 3);
+  Expect.notEquals(((2 & 8) >>> 2) & 3, 2 & 8 >>> 2 & 3);
+  Expect.notEquals(23 & (12 >>> (2 & 1)), 23 & 12 >>> 2 & 1);
+}
--- Language/Reference/Operator_Precedence/precedence_t05.dart
+++ Language/Reference/Operator_Precedence/precedence_t05.dart
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
  * for details. All rights reserved. Use of this source code is governed by a
  * BSD-style license that can be found in the LICENSE file.
  */
@@ -7,8 +7,7 @@
  * @assertion Operator precedence is given implicitly by the grammar.
  * @description Checks that operator precedence given implicitly by the grammar
  * holds true (additive > shift > bitwise AND > XOR > OR). 
- * @author vasya
- * @reviewer rodionov
+ * @author vasya, iarkh@unipro.ru
  */
 import "../../../Utils/expect.dart";
 
@@ -16,6 +15,7 @@ main() {
   Expect.equals(1024 >> (1 + 3), 1024 >> 1 + 3);
   Expect.equals((1 | (((4 + 16) << 3) & 7)), 1 | 4 + 16 << 2 & 7);
   Expect.equals((3 | (((4 + 1) >> 2) & 7)), 3 | 4 + 1 >> 2 & 7);
+  Expect.equals((3 | (((4 + 1) >>> 2) & 7)), 3 | 4 + 1 >>> 2 & 7);
   Expect.equals((2 ^ 3) ^ 4, 2 ^ 3 ^ 4);
   Expect.equals((2 ^ 3) | 4, 2 ^ 3 | 4);
   Expect.equals(( 1 | 2 ) & 3, 1 | 2 & 3);
