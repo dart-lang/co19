c038923de3bc74c28b306e946b4d4c9ba60cb6b2
iarkh <irina.arkhipets@gmail.com>
2019-04-04 18:53:41 +0700

    New spread collections tests added

---
+++ LanguageFeatures/Spread-collections/StaticSemantic_A09_t04.dart
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is a compile-time error if:
+ * A non-null-aware spread element has static type Null.
+ * @description Checks that compile error is thrown if spread element is
+ * statically [Null].
+ * @description Check that trying to use [null] element as a non-null-aware
+ * spread element in constant causes compile error
+ * @author iarkh@unipro.ru
+ */
+// SharedOptions=--enable-experiment=spread-collections,constant-update-2018
+
+const Null n = null;
+
+main() {
+  const res1 = [...n];           //# 01: compile-time error
+  const Map res2 = {...n};       //# 02: compile-time error
+  const Set res3 = {...n};       //# 03: compile-time error
+
+  const res4 = <int>[...n];      //# 04: compile-time error
+  const res5 = <int>{...n};      //# 05: compile-time error
+  const res6 = <int, int>{...n}; //# 06: compile-time error
+
+  const res7 = [...?n];
+  const Map res8 = {...?n};
+  const Set res9 = {...?n};
+
+  const res10 = <int>[...?n];
+  const res11 = <int>{...?n};
+  const res12 = <int, int>{...?n};
+}
---
+++ LanguageFeatures/Spread-collections/StaticSemantic_A11_t03.dart
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is a compile-time error if:
+ * A spread element in a map literal has a static type that is not [dynamic] and
+ * not a subtype of [Map<Object, Object>].
+ * @description Checks that compile error is thrown if spread element in the
+ * constant map is not assignable to [Map]
+ * @author iarkh@unipro.ru
+ */
+// SharedOptions=--enable-experiment=spread-collections,constant-update-2018
+
+main() {
+  const x1 = [];
+  const Set x2 = {1};
+  const Map x3 = {1: 1};
+  const x4 = 100;
+  const x5 = "check";
+  const x6 = null;
+
+  const Map m1  = {...x1}; //# 01: compile-time error
+  const Map m2  = {...x2}; //# 02: compile-time error
+  const Map m3  = {...x3};
+  const Map m4  = {...x4}; //# 03: compile-time error
+  const Map m5  = {...x5}; //# 04: compile-time error
+  const Map m6  = {...x6}; //# 05: compile-time error
+}
