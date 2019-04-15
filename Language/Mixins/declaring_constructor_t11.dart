438603ef4cb1884077d354a63e0a47b20bae6af2
Sergey G. Grekhov <sgrekhov@unipro.ru>
2019-04-15 17:09:06 +0700

    More type alias tests for mixin

---
+++ Language/Mixins/declaring_constructor_t11.dart
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is a compile-time error if a declared or derived mixin
+ * explicitly declares a constructor.
+ * @description Checks that it is a compile-time error if a derived mixin
+ * explicitly declares a constructor.
+ * @compile-error
+ * @author sgrekhov@unipro.ru
+ */
+
+class A {
+}
+
+class M {
+  M() {}
+}
+
+typedef MAlias = M;
+
+class C extends A with MAlias {
+}
+
+main() {
+  new C();
+}
