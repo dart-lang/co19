21b491e86cec7cce088e36fbfd66871690215ded
Sergey G. Grekhov <sgrekhov@unipro.ru>
2019-05-28 17:04:43 +0700

    Fixes #363. More type alias tests added. Test self reference via type alias

---
+++ Language/Types/Type_Aliases/self_reference_t23.dart
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Any self reference in a type alias, either directly or recursively
+ * via another type declaration, is a compile-time error
+ * @description Checks that any self reference in a type alias is a compile-time
+ * error.
+ * @compile-error
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=nonfunction-type-aliases
+class C {}
+typedef CAlias = C;
+
+class CM = C with CAlias;
+
+main() {
+}
