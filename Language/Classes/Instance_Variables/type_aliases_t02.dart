6f42a7271aeb26c013ba0aa3291d61f200abf549
Sergey G. Grekhov <sgrekhov@unipro.ru>
2019-05-28 17:03:27 +0700

    Issue #363. More type alias tests added Test that alias can be used as a class member

---
+++ Language/Classes/Instance_Variables/type_aliases_t02.dart
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Instance variables are variables whose declarations are
+ * immediately contained within a class declaration and that are not declared
+ * static. The instance variables of a class C are those instance variables
+ * declared by C and the instance variables inherited by C from its superclass.
+ * @description Checks that class may contain type alias as a member
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=nonfunction-type-aliases
+import "../../../Utils/expect.dart";
+class A {
+  CAlias c = new CAlias();
+}
+
+class C extends A {
+}
+typedef CAlias = C;
+
+main() {
+  C c = new C();
+  Expect.isNotNull(c.c);
+}
