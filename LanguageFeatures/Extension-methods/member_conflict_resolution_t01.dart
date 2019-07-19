8c2d9a52f88acc183f5ea0aa987b94a1a5f87141
Sergey G. Grekhov <sgrekhov@unipro.ru>
2019-07-19 16:34:57 +0700

    Static extension methods. Member conflict resolution test added

---
+++ LanguageFeatures/Extension-methods/member_conflict_resolution_t01.dart
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion An extension can declare a member with the same (base-)name as a
+ * member of the type it is declared on. This does not cause a compile-time
+ * conflict, even if the member does not have a compatible signature.
+ *
+ * @description Check that an extension can declare a member with the same
+ * (base-)name as a member of the type it is declared on
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=extension-methods
+
+import "../../Utils/expect.dart";
+
+extension ExtendedList<T> on List<T> {
+  static boolean called = false;
+  void add(T val) {
+    called = true;
+  }
+}
+
+extension ExtendedList2<T> on List<T> {
+  static boolean called = false;
+  void add(T val, {int count = 1}) {
+    called = true;
+  }
+}
+
+main() {
+  List<String> list = ["Lily", "was"];
+  list.add("here");
+  Expect.listEquals(["Lily", "was", "here"], list);
+  Expect.isFalse(ExtendedList.called);
+  Expect.isFalse(ExtendedList2.called);
+}
