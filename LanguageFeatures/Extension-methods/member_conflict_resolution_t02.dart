cd378d01131eef178faa67d4abe220969b584b46
Sergey G. Grekhov <sgrekhov@unipro.ru>
2019-07-22 12:14:44 +0700

    Static extension methods. Member conflict resolution tests added

---
+++ LanguageFeatures/Extension-methods/member_conflict_resolution_t02.dart
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion An extension can declare a member with the same (base-)name as a
+ * member of the type it is declared on. This does not cause a compile-time
+ * conflict, even if the member does not have a compatible signature.
+ *
+ * @description Check that it is a compile error if an extension declares a
+ * member with the same (base-)name as a member of the type it is declared on
+ * but with the different arguments
+ * @compile-error
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=extension-methods
+
+extension ExtendedList<T> on List<T> {
+  static boolean called = false;
+  void add(T val, {int count = 1}) {
+    called = true;
+  }
+}
+
+main() {
+  List<String> list = ["Lily", "was"];
+  list.add("here", count: 2);
+}
---
+++ LanguageFeatures/Extension-methods/member_conflict_resolution_t03.dart
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion An extension can declare a member with the same (base-)name as a
+ * member of the type it is declared on. This does not cause a compile-time
+ * conflict, even if the member does not have a compatible signature.
+ *
+ * @description Check that it is a NoSuchMethodError if an extension declares a
+ * member with the same (base-)name as a member of the type it is declared on
+ * but with the different arguments
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=extension-methods
+
+import "../../Utils/expect.dart";
+
+extension ExtendedList<T> on List<T> {
+  static boolean called = false;
+  void add(T val, {int count = 1}) {
+    called = true;
+  }
+}
+
+main() {
+  dynamic list = ["Lily", "was"];
+  Expect.throws(() {
+    list.add("here", count: 2);
+  }, (e) {return e is NoSuchMethodError;});
+}
