09b3ab9ce241417b6d6f1fbf87bb2385bcd57cd0
Sergey G. Grekhov <sgrekhov@unipro.ru>
2019-08-14 16:50:24 +0700

    #389. More NNBD static errors tests added

---
+++ LanguageFeatures/nnbd/static_errors_A20_t01.dart
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is a warning to use a null aware operator (?., ?.., ??, ??=,
+ * or ...?) on a non-nullable receiver.
+ *
+ * @description Check it is a warning to use a null aware operator (?., ?.., ??,
+ * ??=, or ...?) on a non-nullable receiver.
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=non-nullable
+class A {
+  test() {}
+}
+
+class C extends A {
+
+}
+
+main() {
+  A a = A();
+  C c = C();
+  c?.test();                              //# 01: static-type warning
+  c?..test();                             //# 02: static-type warning
+  c ?? a;                                 //# 03: static-type warning
+  a ??= c;                                //# 04: static-type warning
+  List<C> clist = [C(), C()];
+  List<A> alist = [A(), C(), ...? clist]; //# 05: static-type warning
+}
