bdcd11fdac09abc666ed9baf324dfefdfc4826de
Sergey G. Grekhov <sgrekhov@unipro.ru>
2019-08-21 17:54:53 +0700

    #389. NNBD static errors tests for variable initialization

---
+++ LanguageFeatures/nnbd/static_errors_A04_t09.dart
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is an error if a top level variable, static variable, or
+ * instance field with potentially non-nullable type has no initializer
+ * expression and is not initialized in a constructor via an initializing formal
+ * or an initializer list entry, unless the variable or field is marked with the
+ * 'late' modifier.
+ *
+ * @description Check that it is no compile-time error if an instance field
+ * with potentially non-nullable type has no initializer expression but
+ * initialized in a constructor via an initializer list entry
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=non-nullable
+
+class A {
+}
+
+class C<X extends A?> {
+  X x;
+
+  C(X x): this.x = x;
+}
+
+main() {
+  new C<A?>(new A());
+}
--- LanguageFeatures/nnbd/static_errors_A13_t01.dart
+++ LanguageFeatures/nnbd/static_errors_A13_t01.dart
@@ -37,5 +37,5 @@ void test(var x) {
 
 main() {
   A? a = null;
-  test(a);
+  test(a!);
 }
