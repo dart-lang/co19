d4031d2ccf9fbe5af24e1b253eb0926d08864637
Sergey G. Grekhov <sgrekhov@unipro.ru>
2019-08-27 18:23:11 +0700

    #458. Null-aware operators tests added

---
+++ LanguageFeatures/nnbd/null_aware_operator_A01_t02.dart
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion A property access e?.f translates to:
+ *  SHORT[EXP(e), fn[x] => x.f]
+ *
+ * @description Check that a property access e?.f translates to:
+ *  SHORT[EXP(e), fn[x] => x.f]
+ *  @static-warning
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=non-nullable
+import "../../Utils/expect.dart";
+
+class A {
+  String test() => "Lily was here";
+}
+
+main() {
+  A a = Never;
+  Expect.isNotNull(a?.toString());    /// static type warning
+  a = new A();
+  Expect.equals("Lily was here", a?.test());    /// static type warning
+
+  String s = "Let it be";
+  Expect.equals("Let it be", s?.toString());    /// static type warning
+}
