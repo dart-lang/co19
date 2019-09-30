a5b14188fe92a4a7ee542fd2f53f4525f530bca5
iarkh <irina.arkhipets@gmail.com>
2019-09-30 14:44:27 +0700

    Issue #477: More tests for constant short circuit operations added.

---
+++ LanguageFeatures/Constant-update-2018/ShortCircuitOperators_A04_t31.dart
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Finally, the conditional [?]/[:] operator only evaluates one of
+ * its branches, depending on whether the condition expression evaluates to
+ * [true] or [false]. The other branch must also be a potentially constant
+ * expression.
+ * @description Checks that conditional operator [?]/[:] in constant expression
+ * throws a compile error if condition is of incorrect type.
+ * @author iarkh@unipro.ru
+ */
+// SharedOptions=--enable-experiment=constant-update-2018
+
+const s = "wrong condition";
+const dynamic b = true;
+const dynamic d = "wrong dynamic";
+const dynamic nil = null;
+
+class MyClass {
+  final String res;
+  const MyClass(dynamic cond) : res = cond ? "yes" : "no";
+}
+
+
+main() {
+  const res1 = MyClass(s);
+//             ^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  const res2 = MyClass(b);
+
+  const res3 = MyClass(d);
+//             ^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  const res4 = MyClass(null);
+//             ^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  const res5 = MyClass(nil);
+//             ^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  const res6 = MyClass(10);
+//             ^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+}
