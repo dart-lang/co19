07d821d0f8923ffe9330327e2ced25cdaa661ba3
Sergey G. Grekhov <sgrekhov@unipro.ru>
2019-12-11 11:25:41 +0700

    Tests for calling of 'call()' method via 'this()' and 'super()' added

---
+++ LanguageFeatures/Extension-methods/call_member_t10.dart
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Any expression of the form e1(args) or e1<types>(args) where e1
+ * does not denote a method, and where the static type of e1 is not a function
+ * type, an interface type declaring a call method, or dynamic, will currently
+ * be a compile-time error. If the static type of e1 is an interface type
+ * declaring a call getter, then this stays a compile-time error. Otherwise we
+ * check for extensions applying to the static type of e1 and declaring a call
+ * member. If one such most specific extension exists, and it declares a call
+ * extension method, then the expression is equivalent to e1.call(args) or
+ * e1.call<typeS>(args). Otherwise it is still a compile-time error.
+ *
+ * @description Check the case when 'call()' method is called via 'this()'
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=extension-methods
+import "../../Utils/expect.dart";
+class C {}
+
+extension on C {
+  test() {
+    Expect.equals("Lily was here: 42", this(42));
+  }
+  String call(int v) => "Lily was here: $v";
+}
+
+main() {
+  C c = new C();
+  c.test();
+}
