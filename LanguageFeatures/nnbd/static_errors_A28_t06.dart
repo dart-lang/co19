f55ec7b3f9cc201e1aab70f68b2ce55959821e8c
sgrekhov <sgrekhov@unipro.ru>
2020-01-21 16:25:57 +0700

    More static errors tests added

---
+++ LanguageFeatures/nnbd/static_errors_A28_t06.dart
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is a compile time error to assign a value to a local variable
+ * marked late and final when the variable is definitely assigned. This includes
+ * all forms of assignments, including assignments via the composite assignment
+ * operators as well as pre and post-fix operators.
+ *
+ * @description Check that it is a compile time error to assign a value to a
+ * local variable marked late and final when the variable is definitely assigned
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=non-nullable
+// Requirements=nnbd-strong
+
+class C {
+  C operator +(int val) {
+    return this;
+  }
+
+  C operator -(int val) {
+    return this;
+  }
+
+  C operator *(int val) {
+    return this;
+  }
+
+  C operator /(int val) {
+    return this;
+  }
+
+  C operator %(int val) {
+    return this;
+  }
+
+  C operator <<(int val) {
+    return this;
+  }
+
+  C operator >>(int val) {
+    return this;
+  }
+
+  C operator ~/(int val) {
+    return this;
+  }
+
+  C operator &(int val) {
+    return this;
+  }
+
+  C operator |(int val) {
+    return this;
+  }
+
+  C operator ^(int val) {
+    return this;
+  }
+
+}
+
+main() {
+  late final C x = new C();
+
+  x += 1;
+//  ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  x -= 1;
+//  ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  x *= 1;
+//  ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  x /= 1;
+//  ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  x %= 1;
+//  ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  x ~/= 1;
+//  ^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  x <<= 1;
+//  ^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  x >>= 1;
+//  ^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  x &= 1;
+//  ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  x ^= 1;
+//  ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  x |= 1;
+//  ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
