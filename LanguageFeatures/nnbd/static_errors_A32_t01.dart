f8da83ecc0e422374bd207a84349089e471fe783
sgrekhov <sgrekhov@unipro.ru>
2020-01-27 15:35:05 +0700

    More static errors tests

---
+++ LanguageFeatures/nnbd/static_errors_A32_t01.dart
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is an error if a class has a setter and a getter with the same
+ * basename where the return type of the getter is not a subtype of the argument
+ * type of the setter. Note that this error specifically requires subtyping and
+ * not assignability and hence makes no exception for dynamic.
+ *
+ * @description Check that it is an error if a class has a setter and a getter
+ * with the same basename where the return type of the getter is not a subtype
+ * of the argument type of the setter
+ * @author sgrekhov@unipro.ru
+ * @issue 40333
+ */
+// SharedOptions=--enable-experiment=non-nullable
+// Requirements=nnbd-strong
+class C {
+  void set test(int v) {}
+  num get test => 3.14;
+//^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  C c = new C();
+  c.test = 1;
+  c.test;
+}
