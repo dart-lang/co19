5cb59c6c91796bf093eb2a506eaa18c0f4370065
sgrekhov <sgrekhov@unipro.ru>
2020-02-25 12:08:26 +0700

    Static errors tests renamed and reordered according to the spec text

--- LanguageFeatures/nnbd/static_errors_A12_t01.dart
+++ LanguageFeatures/nnbd/static_errors_A12_t01.dart
@@ -4,51 +4,43 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion It is an error for a class to extend, implement, or mixin a type
- * of the form T? for any T.
+ * @assertion It is an error if a named parameter that is part of a required
+ * group is not bound to an argument at a call site
  *
- * @description Check that it is an error for a class to extend, implement,
- * or mixin a type of the form T? for any T.
+ * @description Check that it is an error if a named parameter that is part of a
+ * required group is not bound to an argument at a call site
  * @author sgrekhov@unipro.ru
  */
 // SharedOptions=--enable-experiment=non-nullable
 // Requirements=nnbd-strong
-class A {}
-class B {}
-class C {}
+class C {
+  static void test1({required int x}) {}
+  void test2({required int x}) {}
+}
 
-class C1 extends A? {}
-//               ^
-// [analyzer] unspecified
-// [cfe] unspecified
-class C2 implements A? {}
-//                  ^^
-// [analyzer] unspecified
-// [cfe] unspecified
-class C3 = A with B?;
-//                ^^
-// [analyzer] unspecified
-// [cfe] unspecified
-class C4 = A with B implements C?;
-//                             ^^
-// [analyzer] unspecified
-// [cfe] unspecified
-class C5 extends A with B? {}
-//                      ^^
-// [analyzer] unspecified
-// [cfe] unspecified
-mixin M1 on A? {}
-//          ^^
-// [analyzer] unspecified
-// [cfe] unspecified
-mixin M2 on A, B? {}
-//             ^^
-// [analyzer] unspecified
-// [cfe] unspecified
-mixin M3 on A implements B? {}
-//                       ^^
-// [analyzer] unspecified
-// [cfe] unspecified
+void test3({required int x}) {}
+
+typedef void Foo({required int x});
 
 main() {
+  C.test1();
+//  ^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  C().test2();
+//    ^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  test3();
+//^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  Foo foo = ({required int x}) {};
+  foo();
+//^^^
+// [analyzer] unspecified
+// [cfe] unspecified
 }
--- LanguageFeatures/nnbd/static_errors_A15_t01.dart
+++ LanguageFeatures/nnbd/static_errors_A15_t01.dart
@@ -4,59 +4,51 @@
  * BSD-style license that can be found in the LICENSE file.
  */
 /**
- * @assertion It is an error if the static type of e in the expression 'throw e'
- * is not assignable to Object
+ * @assertion It is an error for a class to extend, implement, or mixin a type
+ * of the form T? for any T.
  *
- * @description Check that it is an error if the static type of e in the
- * expression throw e is not assignable to Object
+ * @description Check that it is an error for a class to extend, implement,
+ * or mixin a type of the form T? for any T.
  * @author sgrekhov@unipro.ru
  */
 // SharedOptions=--enable-experiment=non-nullable
 // Requirements=nnbd-strong
-class A {
-}
+class A {}
+class B {}
+class C {}
 
-void test1(Null x) {
-  throw x;
-//      ^
+class C1 extends A? {}
+//               ^
 // [analyzer] unspecified
 // [cfe] unspecified
-}
-
-void test2(A? x) {
-  throw x;
-//      ^
+class C2 implements A? {}
+//                  ^^
 // [analyzer] unspecified
 // [cfe] unspecified
-}
-
-void test3(Function? x) {
-  throw x;
-//      ^
+class C3 = A with B?;
+//                ^^
 // [analyzer] unspecified
 // [cfe] unspecified
-}
-
-void test4<T extends Null>(T x) {
-  throw x;
-//      ^
+class C4 = A with B implements C?;
+//                             ^^
 // [analyzer] unspecified
 // [cfe] unspecified
-}
-
-void test5<T extends Object?>(T x) {
-  throw x;
-//      ^
+class C5 extends A with B? {}
+//                      ^^
 // [analyzer] unspecified
 // [cfe] unspecified
-}
-
-void test6<T extends Function?>(T x) {
-  throw x;
-//      ^
+mixin M1 on A? {}
+//          ^^
 // [analyzer] unspecified
 // [cfe] unspecified
-}
+mixin M2 on A, B? {}
+//             ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+mixin M3 on A implements B? {}
+//                       ^^
+// [analyzer] unspecified
+// [cfe] unspecified
 
 main() {
 }
