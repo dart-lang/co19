b439acf5d2806dda89bf89cd88a93c786903b336
sgrekhov <sgrekhov@unipro.ru>
2020-03-04 16:00:03 +0700

    More static errors tests added

---
+++ LanguageFeatures/nnbd/static_errors_A08_t22.dart
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is an error if a potentially non-nullable local variable which
+ * has no initializer expression and is not marked late is used before it is
+ * definitely assigned
+ *
+ * @description Check that it is not an error if a potentially non-nullable
+ * local variable which has no initializer expression but is marked late is used
+ * before it is definitely assigned. Test some type <X extends Object>
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=non-nullable
+// Requirements=nnbd-strong
+import "../../Utils/expect.dart";
+
+class C<X extends Object> {
+  void test2() {
+    late X x;
+    Expect.throws(() {x.toString();}, (e) => e is LateInitializationError);
+  }
+}
+
+void test<X extends Object>() {
+  late X x;
+  Expect.throws(() {x.toString();}, (e) => e is LateInitializationError);
+}
+main() {
+  new C().test2();
+  test();
+}
---
+++ LanguageFeatures/nnbd/static_errors_A08_t31.dart
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion It is an error if a potentially non-nullable local variable which
+ * has no initializer expression and is not marked late is used before it is
+ * definitely assigned
+ *
+ * @description Check that it is not an error if a potentially non-nullable
+ * local variable which has no initializer expression but is marked late is used
+ * before it is definitely assigned. Test F*, where F is a function type
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=non-nullable
+// Requirements=nnbd-strong
+import "../../Utils/expect.dart";
+import "legacy_lib.dart";
+
+class C {
+  static void test() {
+    LegacyFoo f;
+    Expect.throws(() {f.toString();}, (e) => e is LateInitializationError);
+  }
+
+  void test2() {
+    LegacyFoo f;
+    Expect.throws(() {f.toString();}, (e) => e is LateInitializationError);
+  }
+}
+
+main() {
+  LegacyFoo f;
+  Expect.throws(() {f.toString();}, (e) => e is LateInitializationError);
+
+  Function foo = () {
+    LegacyFoo f;
+    Expect.throws(() {f.toString();}, (e) => e is LateInitializationError);
+  };
+  C.test();
+  new C().test2();
+  foo();
+}
