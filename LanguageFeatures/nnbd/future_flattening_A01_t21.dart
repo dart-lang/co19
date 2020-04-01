d9592a2f4e16417c197330c9e02163aceba68481
iarkh <irina.arkhipets@gmail.com>
2020-04-01 09:52:39 +0700

    Fixed issue #577: new tests for future flattening added.

---
+++ LanguageFeatures/nnbd/future_flattening_A01_t21.dart
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion The flatten function is modified as follows:
+ *
+ * [flatten(T)] is defined by cases on [T]:
+ *
+ *   if [T] is [S?] then [flatten(T) = flatten(S)?]
+ *   otherwise if [T] is [S*] then [flatten(T) = flatten(S)*]
+ *   otherwise if [T] is [FutureOr<S>] then [flatten(T) = S]
+ *   otherwise [if T <: Future] then let [S] be a type such that [T <:
+ *       Future<S>] and for all [R], if [T <: Future<R>] then [S <: R]; then
+ *       [flatten(T) = S]
+ *   otherwise [flatten(T) = T]
+ *
+ * @description Check that type of await expression match with expected
+ * [void] type and the expression can be null.
+ *
+ * @author iarkh@unipro.ru
+ */
+// SharedOptions=--enable-experiment=non-nullable
+// Requirements=nnbd-strong
+
+import "dart:async";
+import "../../Utils/expect.dart";
+
+Future<void> test() async {
+  void d = await null;
+  return d;
+}
+
+main() {
+  asyncStart();
+  test().then((value) => asyncEnd());
+}
