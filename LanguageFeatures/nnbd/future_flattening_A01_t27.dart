1778ef6a02664f1175f05609b24378b87f2d5fe3
iarkh <irina.arkhipets@gmail.com>
2020-04-10 17:15:02 +0700

    Fixed issue #603: expected result upated according to the issue 41437 evaluation.

--- LanguageFeatures/nnbd/future_flattening_A01_t27.dart
+++ LanguageFeatures/nnbd/future_flattening_A01_t27.dart
@@ -16,22 +16,40 @@
  *       [flatten(T) = S]
  *   otherwise [flatten(T) = T]
  *
- * @description Check that type of await expression match with expected
- * [Never] type dynamically and the expression cannot be non-null.
+ * @description Check that compile error is thrown if synchronous function with
+ * [Future<Never>] return value type returns a value of the type [dynamic].
  *
+ * Here is a comment from the Issue #41266:
+ *
+ * Section 9 of the spec says that it is a compile time error if:
+ *
+ *   The function is asynchronous, [flatten(T)] is not [void], and it would
+ *   have been a compile-time error to declare the function with the body [async
+ *   { return e; }] rather than [async => e] and the relevant passage of section
+ *   17.12 is:
+ *
+ *   It is a compile-time error if [s] is [return e;], [flatten(S)] is not
+ *   [void], and [Future<flatten(S)>] is not assignable to [T]
+ *
+ * Here [flatten(S)} is [dynamic] and [Future<dynamic>] is not assignable to
+ * [Future<lbool>] when the nnbd flag is enabled.
+ *
+ * If any changes are forthcoming in the spec we will open issues for
+ * implementation when needed.
+ *
+ * @Issue 41266,41437
  * @author iarkh@unipro.ru
  */
 // SharedOptions=--enable-experiment=non-nullable
 // Requirements=nnbd-strong
 
 import "dart:async";
-import "../../Utils/expect.dart";
 
 dynamic getInt() => 1;
 
 Future<Never> test() async => await getInt();
+//            ^
+// [analyzer] unspecified
+// [cfe] unspecified
 
-main() {
-  asyncStart();
-  test().then((value) {}, onError:(e) => asyncEnd());
-}
+main() {}
