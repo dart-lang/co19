c3aaf50c2fed1def09e471ae874e84e2ee02871f
iarkh <irina.arkhipets@gmail.com>
2020-05-06 14:59:59 +0700

    Fixed Issue #633: Missing class declaration added to common utils.

--- Utils/lang_feature_check.dart
+++ Utils/lang_feature_check.dart
@@ -4,6 +4,9 @@ typedef F<X> = void Function<Y extends X>();
 
 typedef expected_target=Function<X>();
 
+// Function to check nnbd_top_merge features, etc.
+class A<T> { T Function(T) get f => (x) => x; }
+
 // Functions for correct type comparison in language feature tests
 F<X>? toF<X>(X x) => null;
 Type typeOf<X>() => X;
