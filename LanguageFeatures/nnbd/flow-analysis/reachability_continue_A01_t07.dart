95f2b200828ee2f804ffe094f7a432d051a224cc
sgrekhov <sgrekhov@unipro.ru>
2020-05-26 11:08:32 +0700

    #478. Reachability tests for continue statement added

---
+++ LanguageFeatures/nnbd/flow-analysis/reachability_continue_A01_t07.dart
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Continue statement: If N is a statement of the form continue [L];
+ * then:
+ *
+ *  Let S be the statement targeted by the continue. If L is not present, this
+ *  is the innermost do, for, or while statement. Otherwise it is the do, for,
+ *  or while statement with a label matching L, or the switch statement
+ *  containing a switch case with a label matching L.
+ *
+ *  Update continue(S) = join(continue(S), before(N)).
+ *
+ *  Let after(N) = unreachable(before(N)).
+ *
+ * @description Checks reachability after continue statement. Test for-in loop
+ * and a label. The outer loop is while
+ *
+ * @author sgrekhov@unipro.ru
+ * @issue 42045
+ */
+// SharedOptions=--enable-experiment=non-nullable
+// Requirements=nnbd-strong
+
+main() {
+  late int i;
+  int j = 0;
+  outerLoop:
+  while (j++ < 3) {
+    for (;;) {
+      continue outerLoop;
+      i = 42; // Variable is initialized in a dead code. This leaves it definitely unassigned
+    }
+  }
+  i; // It is an error to read a local late variable when it is definitely unassigned.
+//^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
---
+++ LanguageFeatures/nnbd/flow-analysis/reachability_continue_A03_t01.dart
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Continue statement: If N is a statement of the form continue [L];
+ * then:
+ *
+ *  Let S be the statement targeted by the continue. If L is not present, this
+ *  is the innermost do, for, or while statement. Otherwise it is the do, for,
+ *  or while statement with a label matching L, or the switch statement
+ *  containing a switch case with a label matching L.
+ *
+ *  Update continue(S) = join(continue(S), before(N)).
+ *
+ *  Let after(N) = unreachable(before(N)).
+ *
+ * @description Checks reachability after continue statement. Test do loop
+ *
+ * @author sgrekhov@unipro.ru
+ */
+// SharedOptions=--enable-experiment=non-nullable
+// Requirements=nnbd-strong
+
+main() {
+  late int i;
+  do {
+    continue;
+    i = 42; // Variable is initialized in a dead code. This leaves it definitely unassigned
+  } while (false);
+  i; // It is an error to read a local late variable when it is definitely unassigned.
+//^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
