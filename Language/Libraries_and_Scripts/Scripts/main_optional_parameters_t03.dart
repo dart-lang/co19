68277f0f9ae77e33446475d3d2fc8d4e58ac926c
sgrekhov <sgrekhov@unipro.ru>
2020-09-25 14:04:11 +0700

    #924. Added tests for the new static analysis of 'main'

---
+++ Language/Libraries_and_Scripts/Scripts/main_optional_parameters_t03.dart
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion An implementation is free to provide additional arguments allowed
+ * by the signature of main (the above rules ensure that the corresponding
+ * parameters are optional)
+ *
+ * @description Checks that function 'main' may have mora than two optional
+ * positional argument
+ * @author sgrekhov@unipro.ru
+ */
+main([List<String> args = const [], var i, String s = ""]) {
+}
