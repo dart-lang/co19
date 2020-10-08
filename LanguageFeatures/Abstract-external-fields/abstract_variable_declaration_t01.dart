5cb6428832700e0e5f74ba33ba0c2011e898e9f4
sgrekhov <sgrekhov@unipro.ru>
2020-10-08 11:26:32 +0700

    Fixes #912. Abstract variables tests added

---
+++ LanguageFeatures/Abstract-external-fields/abstract_variable_declaration_t01.dart
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion An abstract instance variable declaration is an instance variable
+ * declaration prefixed by the modifier abstract. The declaration must not be
+ * late, and it cannot have an initializer expression.
+ *
+ * @description Checks that it is a compile error if abstract variable
+ * declaration is late
+ * @author sgrekhov@unipro.ru
+ */
+
+abstract class C {
+  late abstract var x;
+//     ^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  abstract late var y;
+//^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+}
---
+++ LanguageFeatures/Abstract-external-fields/abstract_variable_declaration_t04.dart
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion An abstract instance variable declaration is an instance variable
+ * declaration prefixed by the modifier abstract. The declaration must not be
+ * late, and it cannot have an initializer expression.
+ *
+ * @description Checks that it is a compile error if abstract variable has an
+ * initializer expression
+ * @author sgrekhov@unipro.ru
+ */
+
+abstract class C {
+  final abstract x = 42;
+//      ^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  final abstract int y = 42;
+//      ^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+}
