d65b0fceacccf91f80f80d993b6d19d3b81e4ae1
sgrekhov <sgrekhov@unipro.ru>
2021-02-02 16:39:12 +0700

    Fixes #992. Boolean conditional evaluation tests added

---
+++ LanguageFeatures/nnbd/boolean_conditional_evaluation_A02_t02.dart
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion The requirement that the condition in a boolean conditional
+ * control expression (e.g. the a conditional statement, conditional element,
+ * while loop, etc) be assignable to bool is unchanged from pre null-safe Dart.
+ * The change in assignability means that the static type of the condition may
+ * only be dynamic, Never, or bool
+ *
+ * @description Checks that the static type of the condition (in assignability)
+ * may be dynamic, Never, or bool
+ *
+ * @author sgrekhov@unipro.ru
+ */
+void foo() {}
+
+main() {
+  bool x = 42;
+//         ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  x = "true";
+//    ^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  x = null;
+//    ^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  x = foo();
+//    ^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  Object? o = true;
+  x = o;
+//    ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
