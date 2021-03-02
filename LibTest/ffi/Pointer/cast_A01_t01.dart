ff1eb197c9a8d69efa220339d4583b43fec88de4
sgrekhov <sgrekhov@unipro.ru>
2021-03-02 15:51:51 +0700

    #993. Pointer.cast() tests added

---
+++ LibTest/ffi/Pointer/cast_A01_t01.dart
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Pointer<U> cast <U extends NativeType>()
+ *  Cast Pointer to a Pointer.
+ *
+ * @description Checks that this method casts Pointer to a Pointer
+ * @author sgrekhov@unipro.ru
+ */
+import "dart:ffi";
+import '../ffi_utils.dart';
+import '../../../Utils/expect.dart';
+
+void main() {
+  Pointer<Int16> p1 = calloc<Int16>();
+  try {
+    Pointer<Int8> p2 = new Pointer.fromAddress(p1.address);
+    p1.value = 256;
+    Expect.equals(0, p2.value);
+    var p3 = p2.cast<Int16>();
+    Expect.equals(256, p3.value);
+  } finally {
+    calloc.free(p1);
+  }
+}
---
+++ LibTest/ffi/Pointer/cast_A01_t04.dart
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+/**
+ * @assertion Pointer<U> cast <U extends NativeType>()
+ *  Cast Pointer to a Pointer.
+ *
+ * @description Checks that this method casts Pointer to a Pointer
+ * @author sgrekhov@unipro.ru
+ * @issue 45149
+ */
+import "dart:ffi";
+import '../ffi_utils.dart';
+import '../../../Utils/expect.dart';
+
+void main() {
+  Pointer<Int16> p1 = calloc<Int16>();
+  try {
+    Pointer<Opaque> p2 = new Pointer.fromAddress(p1.address);
+    p1.value = 256;
+    var p3 = p2.cast<Int8>();
+    Expect.equals(0, p3.value);
+  } finally {
+    calloc.free(p1);
+  }
+}
