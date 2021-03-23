347f19f378f9afe7aab128d6fbf9d70ce03c3777
sgrekhov <sgrekhov@unipro.ru>
2021-03-23 10:20:13 +0700

    #1028. Remove triple shift operator from co19_2 repository

--- Language/Classes/Instance_Methods/Operators/allowed_names_t01.dart
+++ Language/Classes/Instance_Methods/Operators/allowed_names_t01.dart
@@ -124,7 +124,6 @@ main() {
   Expect.equals(2, (c1 & c2));
   Expect.equals(28, (c1 << c2));
   Expect.equals(1, (c1 >> c2));
-  Expect.equals(1, (c1 >>> c2));
   Expect.equals(~7, ~c1);
   Expect.equals(-2, -c2);
   Expect.equals(true, c1 == c1);
