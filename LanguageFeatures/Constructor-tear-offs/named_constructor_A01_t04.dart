ac0b73af13eb6af4c2aec304455502f0ad36e453
iarkh <irina.arkhipets@gmail.com>
2021-06-02 15:12:09 +0700

    Issue #1087: New tests for unnamed constructor tear offs added

--- LanguageFeatures/Constructor-tear-offs/named_constructor_A01_t04.dart
+++ LanguageFeatures/Constructor-tear-offs/named_constructor_A01_t04.dart
@@ -3,15 +3,15 @@
 // BSD-style license that can be found in the LICENSE file.
 
 /// @assertion We allow tearing off named constructors.
-//
-// If [C] denotes a class declaration and [C.name] is the name of a constructor
-// of that class, we allow you to tear off that constructors as:
-//
-//  C.name, or
-//  C<typeArgs>.name
-//
-// just as you can currently invoke the constructor as [C.name(args)], or
-// [C<typeArgs>.name(args)].
+///
+/// If [C] denotes a class declaration and [C.name] is the name of a constructor
+/// of that class, we allow you to tear off that constructors as:
+///
+///  C.name, or
+///  C<typeArgs>.name
+///
+/// just as you can currently invoke the constructor as [C.name(args)], or
+/// [C<typeArgs>.name(args)].
 ///
 /// @description Checks that compile error appears for tearing off unexisting
 /// named constructor.
