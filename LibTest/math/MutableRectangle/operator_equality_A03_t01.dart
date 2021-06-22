7db8bbf3a3894ce83bae1f0476b0e26e99a78c5d
Sergey G. Grekhov <sgrekhov@unipro.ru>
2021-06-22 13:35:55 +0700

    Fixes #1102. Change test expected results according to the changed behavior

--- LibTest/math/MutableRectangle/operator_equality_A03_t01.dart
+++ LibTest/math/MutableRectangle/operator_equality_A03_t01.dart
@@ -21,11 +21,11 @@ main() {
   point = new MutableRectangle(double.nan, double.nan, 0, 0);
   Expect.isFalse(point == point);
   point = new MutableRectangle(double.infinity, 0, double.negativeInfinity, 0);
-  Expect.isFalse(point == point);
+  Expect.isTrue(point == point);
   point = new MutableRectangle(double.negativeInfinity, 0, double.infinity, 0);
   Expect.isFalse(point == point);
   point = new MutableRectangle(0, double.infinity, 0, double.negativeInfinity);
-  Expect.isFalse(point == point);
+  Expect.isTrue(point == point);
   point = new MutableRectangle(0, double.negativeInfinity, 0, double.infinity);
   Expect.isFalse(point == point);
 }
