a291f919c1f78a1883ecfa43989f516d91b1901f
iarkh <irina.arkhipets@gmail.com>
2021-08-04 15:14:42 +0700

    Issue #1094: changes from master moved to pre-nnbd for LibTests/io/ProcessSignal

--- LibTest/io/ProcessSignal/watch_A01_t02.dart
+++ LibTest/io/ProcessSignal/watch_A01_t02.dart
@@ -23,12 +23,16 @@
 /// @author ngl@unipro.ru
 
 import "dart:io";
+import "dart:async";
 import "../../../Utils/expect.dart";
 
-check(ProcessSignal signal, int ec) {
+check(ProcessSignal signal, int ec, [int ec2]) {
+  String executable = Platform.resolvedExecutable;
+  String eScript = Platform.script.toString();
   asyncStart();
-  Process.start(Platform.executable, [
-    Platform.script.resolve('watch_A01_t02_lib.dart').toFilePath(),
+  Process.start(executable, [
+    ...Platform.executableArguments,
+    eScript,
     signal.toString()
   ]).then((process) {
     process.stdin.close();
@@ -41,20 +45,90 @@ check(ProcessSignal signal, int ec) {
       }
     });
     process.exitCode.then((exitCode) {
+      if (ec2 == null) {
       Expect.equals(ec, exitCode);
+      } else {
+        Expect.isTrue(
+            exitCode == ec || exitCode == ec2, "exitCode is $exitCode");
+      }
       asyncEnd();
     });
   });
 }
 
-main() {
+runMain() {
   if (!Platform.isWindows) {
     check(ProcessSignal.sighup, -1);
     check(ProcessSignal.sigint, -2);
     check(ProcessSignal.sigterm, -15);
-    check(ProcessSignal.sigusr1, -10);
-    check(ProcessSignal.sigusr2, -12);
+    check(ProcessSignal.sigusr1, -10, -30);
+    check(ProcessSignal.sigusr2, -12, -31);
     // ProcessSignal.sigwinch does not kill process
     check(ProcessSignal.sigwinch, 0);
+  }
+}
+
+runProcess(List<String> args) {
+  // This process should die if it never receives a signal.
+  var timeout = new Timer(new Duration(seconds: 5), () => exit(1));
+
+  var sigCount = 0;
+  var li1;
+  bool sigw = false;
+
+  void check() {
+    if (sigCount > 1) {
+      exit(1);
+    }
+    if (sigw && (sigCount == 1)) {
+      exit(0);
+    }
+    if (sigCount == 1) {
+      li1.cancel();
+    }
+    print("ready");
+  }
+
+  var signal;
+  switch (args[0]) {
+    case 'SIGHUP':
+      signal = ProcessSignal.sighup;
+      break;
+    case 'SIGINT':
+      signal = ProcessSignal.sigint;
+      break;
+    case 'SIGTERM':
+      signal = ProcessSignal.sigterm;
+      break;
+    case 'SIGUSR1':
+      signal = ProcessSignal.sigusr1;
+      break;
+    case 'SIGUSR2':
+      signal = ProcessSignal.sigusr2;
+      break;
+    case 'SIGWINCH':
+      signal = ProcessSignal.sigwinch;
+      break;
+  }
+
+  li1 = signal.watch().listen((lsignal) {
+    if (lsignal != signal) {
+      exit(1);
+    }
+    sigCount++;
+    if (signal == ProcessSignal.sigwinch) {
+      sigw = true;
+    }
+    check();
+  });
+
+  check();
+}
+
+main(List<String> args) {
+  if (args.length > 0)
+    runProcess(args);
+  else {
+    runMain();
   }
 }
