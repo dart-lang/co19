3c6d12420ae8f6ffcc8bc44552abd861f4ead56f
iarkh <irina.arkhipets@gmail.com>
2021-08-10 18:45:21 +0700

    Issue #1094: Utils/tests/Expect tests corrected.

--- Utils/tests/Expect/identical_A01_t01.dart
+++ Utils/tests/Expect/identical_A01_t01.dart
@@ -6,11 +6,10 @@
 
 /// @assertion static void identical(var expected, var actual, [String reason = null]) 
 /// Checks whether the expected and actual values are identical.
-/// @description Checks that no exception is thrown when the arguments are identical,
-///              regardless of reason (the last argument). Null, numeric, String and
-///              Object values are tested.
+/// @description Checks that no exception is thrown when the arguments are
+/// identical, regardless of reason (the last argument). Null, numeric, String
+/// and Object values are tested.
 /// @author rodionov
-/// @reviewer varlax
 
 import "../../../Utils/expect.dart";
 
@@ -20,9 +19,11 @@ main()
   Expect.identical(null, null, "");
   Expect.identical(null, null, "not empty");
 
-  Expect.identical(double.nan, double.nan);
-  Expect.identical(double.nan, double.nan, "");
-  Expect.identical(double.nan, double.nan, "not empty");
+  if (!isJS) {
+    Expect.identical(double.nan, double.nan);
+    Expect.identical(double.nan, double.nan, "");
+    Expect.identical(double.nan, double.nan, "not empty");
+  }
 
   Expect.identical(1, 1);
   Expect.identical(1, 1, "");
--- Utils/tests/Expect/throws_A01_t04.dart
+++ Utils/tests/Expect/throws_A01_t04.dart
@@ -7,25 +7,25 @@
 /// @assertion static void throws(void f(), [_CheckExceptionFn check = null, String reason = null])
 /// typedef bool _CheckExceptionFn(exception)
 /// Calls the function [f] and verifies that it throws an exception.
-/// The optional [check] function can provide additional validation that the correct exception is being thrown.
-/// @description Checks that the optional check function is called with the thrown object.
+/// The optional [check] function can provide additional validation that the
+/// correct exception is being thrown.
+/// @description Checks that the optional check function is called with the
+/// thrown object.
 /// @author varlax
-/// @reviewer msyabro
 
 import "../../../Utils/expect.dart";
 
 typedef void test();
 
-
 main() {
-  var err = 0;
-  test t = (){throw err;};
-
-  Expect.throws(t, (e){return identical(e, err);});
+  var err1 = 0;
+  test t = () => throw err1;
+  Expect.throws(t, (e) => identical(e, err1));
   
   var err2 = "hello";
-  Expect.throws(t, (e){return identical(e, err2);});
+  t = () => throw err2;
+  Expect.throws(t, (e) => identical(e, err2));
 
   var err3 = new Object();
-  Expect.throws(t, (e){return identical(e, err3);});
+  Expect.throws(() { throw err3; }, (e) => identical(e, err3));
 }
