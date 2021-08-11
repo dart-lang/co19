ef018b98d489249b9f79d1d7ddbfefb8cf1580ff
sgrekhov <sgrekhov@unipro.ru>
2021-08-11 11:07:34 +0700

    Fixes #1136. Make LanguageFeatures/Constructor-tear-offs/tearing_off_from_typedef_A07_t01 test stronger

--- LanguageFeatures/Constructor-tear-offs/tearing_off_from_typedef_A07_t01.dart
+++ LanguageFeatures/Constructor-tear-offs/tearing_off_from_typedef_A07_t01.dart
@@ -22,10 +22,17 @@ typedef AAlias1 = A<int, String>;
 typedef AAlias2 = A;
 typedef AAlias3 = A<Never, Null>;
 typedef AAlias4<T> = A<T, String>;
+typedef AAlias5<T> = A<int, T>;
 
 main() {
   Expect.equals(A<int, String>.testme, AAlias1.testme);
+  Expect.identical(A<int, String>.testme, AAlias1.testme);
   Expect.equals(A<dynamic, dynamic>.testme, AAlias2.testme);
+  Expect.identical(A<dynamic, dynamic>.testme, AAlias2.testme);
   Expect.equals(A<Never, Null>.testme, AAlias3.testme);
+  Expect.identical(A<Never, Null>.testme, AAlias3.testme);
   Expect.equals(A<int, String>.testme, AAlias4<int>.testme);
+  Expect.identical(A<int, String>.testme, AAlias4<int>.testme);
+  Expect.equals(A<int, String>.testme, AAlias5<String>.testme);
+  Expect.identical(A<int, String>.testme, AAlias5<String>.testme);
 }
