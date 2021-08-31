a493a797f02e173c5b2f504af27eb02b1ae6e478
iarkh <irina.arkhipets@gmail.com>
2021-08-31 17:08:58 +0700

    Issue #1094: Test updated: microseconds are not supported in dart2js.

--- LibTest/core/DateTime/parse_A01_t03.dart
+++ LibTest/core/DateTime/parse_A01_t03.dart
@@ -19,7 +19,10 @@ check(String str, int year, int month, int day, int hours, int minutes,
   Expect.equals(minutes, d.minute);
   Expect.equals(seconds, d.second);
   Expect.equals(milliseconds, d.millisecond);
-  Expect.equals(microseconds, d.microsecond);
+  if (!isJS) {
+    // Microseconds are not supported in dart2js
+    Expect.equals(microseconds, d.microsecond);
+  }
 }
 
 main() {
