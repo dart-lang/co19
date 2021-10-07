9c787a4a55d3c94182701a63c92b0d028d59e7e1
sgrekhov <sgrekhov@unipro.ru>
2021-10-07 17:56:43 +0700

    #1207. Some grammar tests added

---
+++ LanguageFeatures/Enhanced-Enum/grammar_A02_t01.dart
@@ -0,0 +1,37 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion We propose the following to also be allowed:
+///
+/// enum Name<T> with Mixin1, Mixin2 implements Interface1, Interface2 {
+///   id1<int>(args1), id2<String>(args2), id3<bool>(args3);
+///   memberDeclaration*
+///   const Name(params) : initList;
+/// }
+/// where memberDeclaration* is any sequence of static and instance member
+/// declarations and/or an unnamed generative const constructor declaration.
+/// ...
+/// The superclass of the mixin applications is the Enum class (which has an
+/// abstract index getter, so the only valid super invocations are those valid
+/// on Object).
+///
+/// @description Check that enum can be declared with the mixins
+/// @author sgrekhov@unipro.ru
+
+import "../../Utils/expect.dart";
+
+mixin M on Enum {
+  int mixedInMethod() => 42;
+}
+
+enum E with M {
+  e1,
+  e2,
+  e3
+}
+
+main() {
+  E e = E.e1;
+  Expect.equals(42, e.mixedInMethod());
+}
