051d27f994e6b39265185fd7213c58fc18264ed9
sgrekhov <sgrekhov@unipro.ru>
2021-10-12 16:23:46 +0700

    #1207. More enhanced enums tests grammar tests added

---
+++ LanguageFeatures/Enhanced-Enum/grammar_A04_t03.dart
@@ -0,0 +1,34 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion The grammar of the enum declaration becomes:
+///
+/// <enumType> ::=
+///   `enum' <identifier> <typeParameters>? <mixins>? <interfaces>? `{'
+///      <enumEntry> (`,' <enumEntry>)* (`,')? (`;'
+///      (<metadata> <classMemberDefinition>)*
+///      )?
+///   `}'
+///
+/// <enumEntry> ::= <metadata> <identifier> <argumentPart>?
+///               | <metadata> <identifier> <typeArguments>? '.'
+///               | identifier> <arguments>
+///
+/// @description Check that old syntax can be used together with the new one
+/// @author sgrekhov@unipro.ru
+
+// SharedOptions=--enable-experiment=enhanced-enums
+
+enum E {
+  e1,
+  e2(),
+  e3;
+
+  const E();
+}
+
+main() {
+  E.e1;
+  E.e2;
+}
---
+++ LanguageFeatures/Enhanced-Enum/grammar_A05_t01.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if the enum declaration contains any
+/// generative constructor which is not const.
+//
+// We can allow omitting the const on constructors since it’s required, so we
+// can just assume it’s there. That’s a convenience we can also add at any later
+// point.
+///
+/// @description Check that it is a compile-time error if the enum declaration
+/// contains any generative constructor which is not const
+/// @author sgrekhov@unipro.ru
+
+// SharedOptions=--enable-experiment=enhanced-enums
+
+enum E {
+  e1,
+  e2,
+  e3;
+  E();
+//^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  E.e1;
+}
