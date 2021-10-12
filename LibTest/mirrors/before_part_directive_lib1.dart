d6845283a70b4b00187e239c253191c9df7fe39c
iarkh <irina.arkhipets@gmail.com>
2021-10-12 16:58:50 +0700

    ixes #1165: more Language/Metadata tests which use reflection moved to LibTests/mirrors tests.

---
+++ LibTest/mirrors/before_part_directive_lib1.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// @dart = 2.9
+
+
+library before_part_directive_lib1;
+
+@A() part 'before_part_directive_lib1_p1.dart';
+
+class A {
+  const A();
+}
