0afe5cb37e776dba6dd6681dc6d29295ba278e0f
Ahmed Ashour <asashour@yahoo.com>
2021-10-12 11:06:07 +0200

    Remove extra `be` (#1216)

--- LanguageFeatures/Subtyping/dynamic/test_cases/return_value_x01.dart
+++ LanguageFeatures/Subtyping/dynamic/test_cases/return_value_x01.dart
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 /// @description Check that if type T0 is a subtype of a type T1, then instance
-/// of T0 can be be used as a return value of type T1
+/// of T0 can be used as a return value of type T1
 /// @author sgrekhov@unipro.ru
 
 
