73eb89600bfbbd057140dbeb1e695f3c25a2da7a
iarkh <irina.arkhipets@gmail.com>
2021-10-12 17:10:31 +0700

    More fix for #523: one more Language/Metadata test which use reflection moved to LibTests/mirrors tests.

---
+++ LibTest/mirrors/syntax_lib1.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+
+library syntax_lib1;
+
+const X = 'No';
+
+class A {
+  const A();
+}
+
+class B<T> {
+  const B();
+  const B.b(T t);
+}
+
+class Foo {
+  const Foo(int x, int y);
+}
+
+class Y<T extends num> {
+  const Y();
+}
+
+class Z<T extends num> {
+  const Z(T t);
+}
+
+enum E {a, b, c}
