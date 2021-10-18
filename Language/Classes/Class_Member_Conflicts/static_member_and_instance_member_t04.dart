7257758533109de8c4e0346c6b6f1a952cc4ef6f
iarkh <irina.arkhipets@gmail.com>
2021-10-18 17:45:55 +0700

    More fix for #1094: Test updated to expect current dart behavior, Issue tag added.

--- Language/Classes/Class_Member_Conflicts/static_member_and_instance_member_t04.dart
+++ Language/Classes/Class_Member_Conflicts/static_member_and_instance_member_t04.dart
@@ -9,16 +9,31 @@
 /// @description Check that it is a compile-time error if C declares a static
 /// member with basename n and an instance member with basename n. Test instance
 /// variable
+///
+/// @Issue 47489
 /// @author sgrekhov@unipro.ru
 
 class C {
+
+  // For each 's1' and '_s1', two instance variables implicitly induce a getter
+  // and a setter, so there is a conflict specified in 'Class Member Conflicts'
+  // as well as a regular name clash. Dart produces two compile time errors here
+  // whereas analyzer reports the 'Class Member Conflicts' error, and it seems
+  // reasonable to omit the plain name clashes.
   int s1 = 1;
+//    ^^
+// [cfe] unspecified
+
+
   int s2 = 1;
   int s3 = 1;
   int s4 = 1;
   int s5 = 1;
 
   int _s1 = 1;
+//    ^^
+// [cfe] unspecified
+
   int _s2 = 1;
   int _s3 = 1;
   int _s4 = 1;
@@ -29,10 +44,11 @@ class C {
 // [analyzer] unspecified
 // [cfe] unspecified
 
-  static void s2() {}
+ static void s2() {}
 //            ^^
 // [analyzer] unspecified
 // [cfe] unspecified
+
 
   static int s3() => 1;
 //           ^^
