5fe618b9dbdd8fc8d0f460a76f3baa233528b137
iarkh <irina.arkhipets@gmail.com>
2021-10-19 12:41:06 +0700

    More fix for #1094: Test updated.

--- Language/Statements/Yield_and_Yield_Each/Yield_Each/execution_sync_t05.dart
+++ Language/Statements/Yield_and_Yield_Each/Yield_Each/execution_sync_t05.dart
@@ -56,7 +56,7 @@ Iterable generator(Iterable iterable) sync* {
 
 main() {
   IterableStub its = new IterableStub();
-  Expect.isTrue(generator(its).first);
+  Expect.isTrue(generator(its).isEmpty);
   Expect.isTrue(actualException is TestException);
 }
 
