213646c99b3e10749907e9c32b2feda2e454cad9
sgrekhov <sgrekhov@unipro.ru>
2021-11-17 16:07:05 +0700

    #1231. More super parameters tests added

---
+++ LanguageFeatures/Super-parameters/semantics_A06_t21.dart
@@ -0,0 +1,47 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Let C be a non-redirecting generative constructor with, implicit
+/// or explicit, super-constructor invocation s at the end of its initializer
+/// list. Let D be the superclass constructor targeted by s (which must exist).
+///
+/// We define the associated super-constructor parameter for each
+/// super-parameter p of C as follows:
+///
+/// If p is a positional parameter, let j be the number of positional
+/// super-parameters of C up to and including p in source order. The associated
+/// super-constructor parameter of p is the jth positional parameter of D
+/// (1-based), if D has that many positional parameters.
+/// If p is a named parameter with name n, the associated super-constructor
+/// parameter is the named parameter of D with name n, if D has a named
+/// parameter with that name.
+///
+/// It’s a compile-time error if a non-redirecting generative
+/// constructor has a super-parameter with no associated super-constructor
+/// parameter.
+///
+/// @description Check that it is a compile-time error if a non-redirecting
+/// generative constructor has a super-parameter with no associated
+/// super-constructor parameter.
+/// @author sgrekhov@unipro.ru
+
+// SharedOptions=--enable-experiment=super-parameters
+
+class S {
+  int s1;
+  S({int x1 = 0}) : s1 = x1;
+}
+
+class C extends S {
+  int i1;
+  int i2;
+  C(this.i1, int x, {super.s1}) : this.i2 = x;
+//                   ^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  C(1, 2, 3);
+}
---
+++ LanguageFeatures/Super-parameters/type_inference_A05_t04.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion We also copy the default value of the associated
+/// super-constructor if applicable:
+///
+/// If p is optional, does not declare a default value, the associated
+/// super-constructor parameter is also optional and has a default value d, and
+/// d is a subtype of the (declared or inferred above) type of p, then p gets
+/// the default value d.
+///
+/// @description Check that if p is optional, does not declare a default value,
+/// the associated super-constructor parameter is also optional and has a
+/// default value d, and d is a subtype of the (declared or inferred above) type
+/// of p, then p gets the default value d
+/// @author sgrekhov@unipro.ru
+
+// SharedOptions=--enable-experiment=super-parameters
+
+import "../../Utils/expect.dart";
+
+class S {
+  int s;
+  S([int x = 0]) : s = x - 1;
+}
+
+class C extends S {
+  int c;
+  C([super.x]) : c = x + 1;
+}
+
+main() {
+  Expect.equals(1, C().c);
+  Expect.equals(43, C(42).c);
+}
