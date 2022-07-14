7ce3da860dc1c56f1388936476a71ba1fa1eb4c4
sgrekhov <sgrekhov22@gmail.com>
2022-07-14 20:49:20 +0300

    #1363. Tests for yield updated to the current specification

---
+++ Language/Statements/Yield_and_Yield_Each/Yield/execution_async_A01_t01.dart
@@ -0,0 +1,61 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Execution of a statement s of the form yield e; proceeds as
+/// follows:
+///   First, the expression `e` is evaluated to an object `o`. If the enclosing
+/// function `m` is marked `async*` and the stream `u` associated with `m` has
+/// been paused, then the nearest enclosing asynchronous `for` loop if any, is
+/// paused and execution of `m` is suspended until `u` is resumed or canceled.
+///   Next, `o` is added to the iterable or stream associated with the
+/// immediately enclosing function.
+///   If the enclosing function `m` is marked `async*` and the stream `u`
+/// associated with `m` has been canceled, then the `yield` statement returns
+/// without an object, otherwise it completes normally.
+///   Otherwise, if the enclosing function `m` is marked `async*` then the
+/// enclosing function may suspend, in which case the nearest enclosing
+/// asynchronous `for` loop, if any, is paused first.
+///
+/// @description Check that if the enclosing function `m` is marked `async*` and
+/// the stream `u` associated with `m` has been paused, then execution of `m` is
+/// suspended until `u` is resumed.
+///
+/// @author a.semenov@unipro.ru
+/// @author sgrekhov22@gmail.com
+
+import 'dart:async';
+import '../../../../Utils/expect.dart';
+
+List<int> readyToSent = [];
+List<int> sent = [];
+
+Stream<int> generator() async* {
+  for (int i = 1; i <= 3; i++) {
+    readyToSent.add(i);
+    yield i;
+    sent.add(i);
+  }
+}
+
+main() {
+  asyncStart();
+  List received = [];
+  Stream<int> s = generator();
+  late StreamSubscription<int> ss;
+  ss = s.listen((int i) async {
+    received.add(i);
+    if (i == 1) {
+      ss.pause();
+      await Future.delayed(Duration(milliseconds: 100));
+      Expect.listEquals([], sent);
+      Expect.listEquals([1], readyToSent);
+      ss.resume();
+    }
+  }, onDone: () {
+    Expect.listEquals([1, 2, 3], received);
+    Expect.listEquals(sent, received);
+    Expect.listEquals(readyToSent, received);
+    asyncEnd();
+  });
+}
---
+++ Language/Statements/Yield_and_Yield_Each/Yield/execution_async_A01_t04.dart
@@ -0,0 +1,60 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Execution of a statement s of the form yield e; proceeds as
+/// follows:
+///   First, the expression `e` is evaluated to an object `o`. If the enclosing
+/// function `m` is marked `async*` and the stream `u` associated with `m` has
+/// been paused, then the nearest enclosing asynchronous `for` loop if any, is
+/// paused and execution of `m` is suspended until `u` is resumed or canceled.
+///   Next, `o` is added to the iterable or stream associated with the
+/// immediately enclosing function.
+///   If the enclosing function `m` is marked `async*` and the stream `u`
+/// associated with `m` has been canceled, then the `yield` statement returns
+/// without an object, otherwise it completes normally.
+///   Otherwise, if the enclosing function `m` is marked `async*` then the
+/// enclosing function may suspend, in which case the nearest enclosing
+/// asynchronous `for` loop, if any, is paused first.
+///
+/// @description Check that if the enclosing function `m` is marked `async*` and
+/// the stream `u` associated with `m` has been paused, then the nearest
+/// enclosing asynchronous `for` loop is paused and execution of `m` is
+/// suspended until `u` is canceled
+///
+/// @author sgrekhov22@gmail.com
+/// @issue 49451
+
+import 'dart:async';
+import '../../../../Utils/expect.dart';
+
+List<int> readyToSent = [];
+List<int> sent = [];
+
+Stream<int> generator() async* {
+  for (int i = 1; i <= 5; i++) {
+    readyToSent.add(i);
+    yield i;
+    sent.add(i);
+  }
+}
+
+main() async {
+  List<int> received = [];
+  Stream<int> stream = generator();
+  await for (int i in stream) {
+    received.add(i);
+    if (i == 3) {
+      await Future.delayed(Duration(milliseconds: 100));
+      Expect.listEquals([1, 2, 3], received);
+      Expect.listEquals([1, 2], sent);
+      Expect.listEquals([1, 2, 3], readyToSent);
+      // stream is paused now. Let's cancel it
+      break;
+    }
+  }
+  await Future.delayed(Duration(seconds: 1));
+  Expect.listEquals([1, 2, 3], received);
+  Expect.listEquals([1, 2], sent);
+  Expect.listEquals([1, 2, 3], readyToSent);
+}
