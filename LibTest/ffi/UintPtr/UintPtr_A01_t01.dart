aace5842e91a29bdc938b1d579fc41f3d980e1f0
sgrekhov <sgrekhov22@gmail.com>
2022-08-03 14:59:46 +0300

    #993. Excessive test deleted, UintPtr fixed for 32-bit platforms

--- LibTest/ffi/UintPtr/UintPtr_A01_t01.dart
+++ LibTest/ffi/UintPtr/UintPtr_A01_t01.dart
@@ -20,23 +20,30 @@ void main() {
   try {
     p1.value = 42;
     Expect.equals(42, p1.value);
-    p1.value = -42;
-    Expect.equals(-42, p1.value);
     p1.value = 256;
     Expect.equals(256, p1.value);
     p1.value = 32767;
     Expect.equals(32767, p1.value);
     p1.value = 32768;
     Expect.equals(32768, p1.value);
-    p1.value = -32769;
-    Expect.equals(-32769, p1.value);
     p1.value = 0x7FFFFFFF;
     Expect.equals(2147483647, p1.value);
+    p1.value = 0x80000000; // 2147483648
+    Expect.equals(2147483648, p1.value);
     if (sizeOf<UintPtr>() == 4) {
-      p1.value = 0x80000000;
-      Expect.equals(-2147483648, p1.value);
+      p1.value = -42;
+      Expect.equals(4294967254, p1.value);
+      p1.value = -32769;
+      Expect.equals(4294934527, p1.value);
       p1.value = -2147483649;
       Expect.equals(2147483647, p1.value);
+    } else {
+      p1.value = -42;
+      Expect.equals(-42, p1.value);
+      p1.value = -32769;
+      Expect.equals(-32769, p1.value);
+      p1.value = -2147483649; // 0xFFFFFFFF7FFFFFFF
+      Expect.equals(-2147483649, p1.value);
     }
   } finally {
     calloc.free(p1);
