8de0f50755569a50c4dadc1c93670b5ef1e05443
sgrekhov <sgrekhov22@gmail.com>
2022-09-20 11:13:25 +0300

    Fixes #1454. Typo in a records_A02.dart fixed

--- LanguageFeatures/Subtyping/dynamic/generated/records_arguments_binding_A02_t01.dart
+++ LanguageFeatures/Subtyping/dynamic/generated/records_arguments_binding_A02_t01.dart
@@ -32,7 +32,7 @@ import '../../utils/common.dart';
 (num, num z, String?, {num n1, String? n2}) t1Instance =
   (0, n1: 0, 0, n2: null, null);
 
-const t1Default = (0, 0, null, {n1: 0.1, n2: ""});
+const t1Default = (0, 0, null, n1: 0.1, n2: "");
 
 
 
