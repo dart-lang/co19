bc03048963c38e9547fb0b3a6cd54e8e1d832a52
sgrekhov <sgrekhov22@gmail.com>
2022-09-21 21:39:41 +0300

    #195. Added missed @dart = 2.9 flag

--- LibTest/io/RawDatagramSocket/writeEventsEnable_A01_t02.dart
+++ LibTest/io/RawDatagramSocket/writeEventsEnable_A01_t02.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// @dart = 2.9
+
 /// @assertion bool writeEventsEnabled
 /// Set or get, if the [RawDatagramSocket] should listen for
 /// [RawSocketEvent.write] events. Default is true. This is a one-shot listener,
