f691647f1c2cc4f1019fb619932c52d0a1adbf2c
Sergey G. Grekhov <sgrekhov22@gmail.com>
2022-12-15 11:54:56 +0200

    Fixes #1595. Expect TypeError in place of NullThrownError in co19_2 tests (#1597)
    Authored by @sgrekhov.

--- Language/Expressions/Throw/evaluation_t04.dart
+++ Language/Expressions/Throw/evaluation_t04.dart
@@ -7,12 +7,12 @@
 /// @assertion Evaluation of a throw expression of the form throw e; proceeds as
 /// follows:
 /// The expression e is evaluated yielding a value v.
-/// If v evaluates to null, then a NullThrownError is thrown. Otherwise, control
+/// If v evaluates to null, then a TypeError is thrown. Otherwise, control
 /// is transferred to the nearest dynamically enclosing exception handler, with
 /// the current exception set to v and the current return value becomes
 /// undefined.
 /// @description Checks that attempting to throw null in any manner results in
-/// NullThrownError being thrown instead.
+/// TypeError being thrown instead.
 /// @author rodionov
 /// @reviewer kaigorodov
 
@@ -27,21 +27,21 @@ n2() {}
 main() {
   try {
     throw null;
-    Expect.fail("NullThrownError expected.");
-  } on NullThrownError catch (ok) {}
+    Expect.fail("TypeError expected.");
+  } on TypeError catch (ok) {}
 
   try {
     throw n();
-    Expect.fail("NullThrownError expected.");
-  } on NullThrownError catch (ok) {}
+    Expect.fail("TypeError expected.");
+  } on TypeError catch (ok) {}
 
   try {
     throw n2();
-    Expect.fail("NullThrownError expected.");
-  } on NullThrownError catch (ok) {}
+    Expect.fail("TypeError expected.");
+  } on TypeError catch (ok) {}
 
   try {
     throw (true ? null : null);
-    Expect.fail("NullThrownError expected.");
-  } on NullThrownError catch (ok) {}
+    Expect.fail("TypeError expected.");
+  } on TypeError catch (ok) {}
 }
