fd6b0c9ffc27022594b9e4b7b717449d01bf3aa9
sgrekhov <sgrekhov22@gmail.com>
2022-12-20 12:42:10 +0200

    #1401. [Patterns] Pattern variable declaration tests

---
+++ LanguageFeatures/Patterns/variable_declaration_A01_t05.dart
@@ -0,0 +1,81 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Places in the language where a local variable can be declared are
+/// extended to allow a pattern, like:
+///
+/// var (a, [b, c]) = ("str", [1, 2]);
+/// Dart's existing C-style variable declaration syntax makes it harder to
+/// incorporate patterns. Variables can be declared just by writing their type,
+/// and a single declaration might declare multiple variables. Fully
+/// incorporating patterns into that could lead to confusing syntax like:
+///
+/// // Not allowed:
+/// (int, String) (n, s) = (1, "str");
+/// final (a, b) = (1, 2), c = 3, (d, e);
+/// To avoid this weirdness, patterns only occur in variable declarations that
+/// begin with a var or final keyword. Also, a variable declaration using a
+/// pattern can only have a single declaration "section". No comma-separated
+/// multiple declarations like:
+///
+/// // Not allowed:
+/// var [a] = [1], (b, c) = (2, 3);
+/// Declarations with patterns must have an initializer. This is not a
+/// limitation since the point of using a pattern in a variable declaration is
+/// to match it against the initializer's value.
+///
+/// @description Check that it is a compile-time error if patterns variable
+/// declarations have no initializer
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=patterns,records
+
+import "patterns_lib.dart";
+
+main() {
+  final ((n0,) && r0);
+//      ^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  var [a1, _];
+//    ^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  final [a2, b2, ...];
+//      ^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  var {"pi": a3};
+//    ^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  final {"pi": a4, ...};
+//    ^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  var (_, name: s5);
+//    ^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  final (n6,);
+//      ^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  var (name: s7);
+//    ^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  final Square(areaAsInt: n8);
+//      ^^^^^^^^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
---
+++ LanguageFeatures/Patterns/variable_declaration_A02_t01.dart
@@ -0,0 +1,76 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion
+/// patternVariableDeclaration ::= ('final' | 'var') outerPattern '=' expression
+///
+/// outerPattern                ::= parenthesizedPattern
+///                               | listPattern
+///                               | mapPattern
+///                               | recordPattern
+///                               | objectPattern
+///
+/// @description Check that it is a compile-time error if outerPattern is not
+/// parenthesizedPattern, listPattern, mapPattern, recordPattern or
+/// objectPattern
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=patterns
+
+main() {
+  var [a1] || [_] = ["Logical-or pattern"];
+//         ^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  final [a1] && [_] = ["Logical-and pattern"];
+//           ^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  var >= 0 = 1;
+//    ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  final str as String = "Cast pattern";
+//      ^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  var n1? = "Null-check pattern";
+//    ^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  final n2! = "Null-assert pattern";
+//      ^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  var null = "Constant pattern";
+//    ^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  final "null" = "Constant pattern";
+//      ^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  var String s1 = "Variable pattern";
+//    ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  final String s2 = "Variable pattern";
+//      ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  var String _ = "Variable pattern";
+//    ^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
