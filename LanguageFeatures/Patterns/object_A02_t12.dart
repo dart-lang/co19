6d9aac2234f8643623a01d67d0701fb43c9c757c
sgrekhov <sgrekhov22@gmail.com>
2023-01-12 11:37:44 +0200

    Implement review suggestions

---
+++ LanguageFeatures/Patterns/object_A02_t12.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion objectPattern ::= typeName typeArguments? '(' patternFields? ')'
+/// ...
+/// As with record patterns, the getter name can be omitted and inferred from
+/// the variable pattern in the field subpattern which may be wrapped in a unary
+/// pattern
+///
+/// @description Check that it is a compile-time error if an object pattern with
+/// the omitted getter name in a declaration context declares a variable that
+/// already exists
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=patterns,records
+
+import "patterns_lib.dart";
+
+main() {
+  int areaAsInt = 1, sizeAsInt = 1;
+  var Square(:areaAsInt) = Square(1);
+//            ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  var Square(: int sizeAsInt) = Square(2);
+//                 ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified}
+}
