14a71285bc9405c96c830a4ff3882d8fd5e87b61
sgrekhov <sgrekhov22@gmail.com>
2023-02-02 11:59:02 +0200

    Test updated according to the spec

--- LanguageFeatures/Patterns/pattern_context_A02_t01.dart
+++ LanguageFeatures/Patterns/pattern_context_A02_t01.dart
@@ -3,55 +3,66 @@
 // BSD-style license that can be found in the LICENSE file.
 
 /// @assertion
-/// It is a compile-time error if a variable pattern in a declaration context is
-/// marked with var or final
+/// In a declaration context, an identifier pattern declares a new variable with
+/// that name.
 ///
-/// @description Checks that it is a compile-time error if a variable pattern in
-/// a declaration context is marked with var or final
+/// @description Checks that in a declaration context, an identifier pattern
+/// declares a new variable with that name.
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=patterns,records
 
+import "../../Utils/expect.dart";
 import "patterns_lib.dart";
 
 main() {
-  var (final x1) = 42;
-//     ^^^^^
-// [analyzer] unspecified
-// [cfe] unspecified
+  var (x1) = 1;
+  Expect.equals(1, x1);
 
-  final (var x2) = 42;
-//       ^^^
-// [analyzer] unspecified
-// [cfe] unspecified
+  final (x2) = 2;
+  Expect.equals(2, x2);
 
-  var [final x3, ...] = [1, 2];
-//     ^^^^^
-// [analyzer] unspecified
-// [cfe] unspecified
+  var (int x3) = 3;
+  Expect.equals(3, x3);
 
-  final {"key1": var x4} = {"key1": 1};
-//               ^^^
-// [analyzer] unspecified
-// [cfe] unspecified
+  final (int x4) = 4;
+  Expect.equals(4, x4);
 
-  var (final num x5, n: String x6) = (3.14, n: "pi");
-//     ^^^^^
-// [analyzer] unspecified
-// [cfe] unspecified
+  var [x5, ...] = [5, 6];
+  Expect.equals(5, x5);
 
-  final (num x7, n: var x8) = (3.14, n: "pi");
-//                  ^^^
-// [analyzer] unspecified
-// [cfe] unspecified
+  final [x6, ...] = [6, 7];
+  Expect.equals(6, x6);
 
-  final Square(:var areaAsInt) = Square(2);
-//              ^^^
-// [analyzer] unspecified
-// [cfe] unspecified
+  var [int x7, ...] = [7, 8];
+  Expect.equals(7, x7);
 
-  var Square(areaAsInt: final int x9) = Square(2);
-//                      ^^^^^
-// [analyzer] unspecified
-// [cfe] unspecified
+  final [int x8, ...] = [8, 9];
+  Expect.equals(8, x8);
+
+  var {"key1": x9} = {"key1": 9};
+  Expect.equals(9, x9);
+
+  final {"key1": x10} = {"key1": 10};
+  Expect.equals(10, x10);
+
+  var {"key1": int x11} = {"key1": 11};
+  Expect.equals(11, x11);
+
+  final {"key1": int x12} = {"key1": 12};
+  Expect.equals(12, x12);
+
+  var (num x13, n: x14) = (13, n: 14);
+  Expect.equals(13, x13);
+  Expect.equals(14, x14);
+
+  final (x15, n: int x16) = (15, n: 16);
+  Expect.equals(15, x15);
+  Expect.equals(16, x16);
+
+  final Square(:areaAsInt) = Square(2);
+  Expect.equals(4, areaAsInt);
+
+  var Square(areaAsInt: final int x17) = Square(2);
+  Expect.equals(4, x17);
 }
