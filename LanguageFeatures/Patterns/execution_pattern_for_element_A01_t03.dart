eefd7b649021ef90b1009b21640f8483bfcc5900
sgrekhov <sgrekhov22@gmail.com>
2023-02-19 10:21:48 +0200

    #1401. Execution of pattern-for statement and element. Switch expression

---
+++ LanguageFeatures/Patterns/execution_pattern_for_element_A01_t03.dart
@@ -0,0 +1,28 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion
+/// Likewise, a collection element of the form:
+///
+/// for (<patternVariableDeclaration>; <condition>; <increment>) <element>
+/// Is executed like a traditional for loop though is more likely to declare
+/// multiple variables
+///
+/// @description Check that pattern-for element is executed like a traditional
+/// for loop. Test map pattern
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=patterns
+
+import "../../Utils/expect.dart";
+
+main() {
+  var list = [
+    -1,
+    for (var {"k1": a, "k2": b} = [0, 1]; a <= 8;
+      {"k1": a, "k2": b} = {"k1": b, "k2": a + b}) a,
+    13
+  ];
+  Expect.listEquals([-1, 0, 1, 1, 2, 3, 5, 8, 13], list);
+}
---
+++ LanguageFeatures/Patterns/execution_pattern_for_statement_A01_t04.dart
@@ -0,0 +1,27 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion
+/// A statement of the form:
+///
+/// for (<patternVariableDeclaration>; <condition>; <increment>) <statement>
+///
+/// Is executed like a traditional for loop though is more likely to declare
+/// multiple variables.
+///
+/// @description Check that pattern-for statement is executed like a traditional
+/// for loop. Test record pattern
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=patterns,records
+
+import "../../Utils/expect.dart";
+
+main() {
+  String log = "";
+  for (var (a, b) = (0, 1); a <= 13; (a, b) = (b, a + b)) {
+    log += "$a;";
+  }
+  Expect.equals("0;1;1;2;3;5;8;13;", log);
+}
