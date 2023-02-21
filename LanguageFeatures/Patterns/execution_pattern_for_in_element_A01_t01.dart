5e5327f5074afb25027b5eb73aa0f88d95189f2d
sgrekhov <sgrekhov22@gmail.com>
2023-02-21 11:52:27 +0200

    #1401. Pattern-for-in statement and element tests

---
+++ LanguageFeatures/Patterns/execution_pattern_for_in_element_A01_t01.dart
@@ -0,0 +1,117 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion
+/// A collection element of the form:
+///
+/// for (<keyword> <pattern> in <expression>) <element>
+///
+/// Where <keyword> is var or final is treated like so:
+/// 1. Let I be the static type of <expression>, inferred using context type
+///   schema Iterable<P> where P is the context type schema of <pattern>.
+/// 2. Calculate the element type of I:
+///     i. If I implements Iterable<T> for some T then E is T.
+///     ii. Else if I is dynamic then E is dynamic.
+///     iii. Else it is a compile-time error.
+/// 3. Type check <pattern> with matched value type E.
+/// 4. If there are no compile-time errors, then execution proceeds as the
+///   following code, where id1 and id2 are fresh identifiers and append() is
+///   an operation to add an element to the surrounding collection being built:
+/// ```dart
+/// I id1 = <expression>;
+/// Iterator<E> id2 = id1.iterator;
+/// while (id2.moveNext()) {
+///   <keyword> <pattern> = id2.current;
+///   append(<element>);
+/// }
+/// ```
+/// @description Checks that if `I` implements `Iterable<T>` for some `T` then
+/// `E` is `T`. Test `T` inferred from `<pattern>` to `<expression>` and from
+/// `<expression>` to `<pattern>`
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=patterns,records
+
+import "../../Utils/expect.dart";
+import "../../Utils/static_type_helper.dart";
+import "patterns_lib.dart";
+
+main() {
+  var l1 = [
+    0,
+    for (var (num v1) in [1, 2, 3]) v1,
+    4
+  ];
+  Expect.listEquals([0, 1, 2, 3, 4], l1);
+  l1.expectStaticType<Exactly<List<num>>>();
+
+  var l2 = [
+    0,
+    for (final (v2) in <num>[1, 2, 3]) v2,
+    4
+  ];
+  Expect.listEquals([0, 1, 2, 3, 4], l2);
+  l2.expectStaticType<Exactly<List<num>>>();
+
+  var l3 = [
+    0,
+    for (var <num>[v3] in [[1], [2], [3]]) v3,
+    4
+  ];
+  Expect.listEquals([0, 1, 2, 3, 4], l3);
+  l3.expectStaticType<Exactly<List<num>>>();
+
+  var l4 = [
+    0,
+    for (final [v4] in <List<num>>[[1], [2], [3]]) v4,
+    4
+  ];
+  Expect.listEquals([0, 1, 2, 3, 4], l4);
+  l4.expectStaticType<Exactly<List<num>>>();
+
+  var l5 = [
+    0,
+    for (var <String, num>{"k1": v5} in [{"k1": 1}]) v5,
+    2
+  ];
+  Expect.listEquals([0, 1, 2], l5);
+  l5.expectStaticType<Exactly<List<num>>>();
+
+  var l6 = [
+    0,
+    for (final {"k1": v6} in <Map<String, num>>[{"k1": 1}]) v6,
+    2
+  ];
+  Expect.listEquals([0, 1, 2], l6);
+  l6.expectStaticType<Exactly<List<num>>>();
+
+  var l7 = [
+    0,
+    for (var (num v7, n: num v8) in [(1, n: 2)]) v7,
+    2
+  ];
+  Expect.listEquals([0, 1, 2], l7);
+  l7.expectStaticType<Exactly<List<num>>>();
+
+  var l8 = [
+    1,
+    for (final (v9, n: v10) in <(num, {num n})>[(1, n: 2)]) v10,
+    3
+  ];
+  Expect.listEquals([1, 2, 3], l8);
+  l8.expectStaticType<Exactly<List<num>>>();
+
+  var l9 = [
+    for (var Square<Centimeter>(area: v11) in [Square(1)]) v11,
+  ];
+  Expect.isTrue(l9[0] == 1);
+  l9.expectStaticType<Exactly<List<Unit<Centimeter>>>>();
+
+  var l10 = [
+    for (final Square(area: v12) in <Square<Centimeter>>[Square<Centimeter>(1)])
+      v12
+  ];
+  Expect.isTrue(l10[0] == 1);
+  l10.expectStaticType<Exactly<List<Unit<Centimeter>>>>();
+}
