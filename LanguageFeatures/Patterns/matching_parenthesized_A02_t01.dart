07279694bd8d95a8e5aea258624fdfb3feb01f3f
Sergey G. Grekhov <sgrekhov22@gmail.com>
2023-03-09 20:46:50 +0200

    #1401. Matching tests for identifier and parenthesized pattern (#1915)

---
+++ LanguageFeatures/Patterns/matching_parenthesized_A02_t01.dart
@@ -0,0 +1,40 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion At runtime, a pattern is matched against a value. This determines
+/// whether or not the match fails and the pattern refutes the value. If the
+/// match succeeds, the pattern may also destructure data from the object or
+/// bind variables.
+///
+/// Refutable patterns usually occur in a context where match refutation causes
+/// execution to skip over the body of code where any variables bound by the
+/// pattern are in scope. If a pattern match failure occurs in an irrefutable
+/// context, a runtime error is thrown.
+///
+/// To match a pattern p against a value v:
+/// ...
+/// Parenthesized: Match the subpattern against v and succeed if it matches.
+///
+/// @description Checks that if subpattern of a parenthesized pattern doesn't
+/// match, then pattern itself doesn't match too
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=patterns
+
+import "../../Utils/expect.dart";
+
+main() {
+  Expect.throws(() {
+    int v = 42;
+    (<dynamic>[v]) = ["42"] as dynamic;
+  });
+  Expect.throws(() {
+    String v = "42";
+    (<dynamic>[v]) = [42] as dynamic;
+  });
+  Expect.throws(() {
+    int v = 0;
+    (<dynamic>[v]) = [null] as dynamic;
+  });
+}
---
+++ LanguageFeatures/Patterns/matching_variable_A03_t01.dart
@@ -0,0 +1,43 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion At runtime, a pattern is matched against a value. This determines
+/// whether or not the match fails and the pattern refutes the value. If the
+/// match succeeds, the pattern may also destructure data from the object or
+/// bind variables.
+///
+/// Refutable patterns usually occur in a context where match refutation causes
+/// execution to skip over the body of code where any variables bound by the
+/// pattern are in scope. If a pattern match failure occurs in an irrefutable
+/// context, a runtime error is thrown.
+///
+/// To match a pattern p against a value v:
+/// ...
+/// Variable:
+/// i. Let T be the static type of the variable p declares or assigns to.
+/// ii. If the runtime type of v is not a subtype of T then the match fails.
+/// iii. Otherwise, store v in p's variable and the match succeeds.
+///
+/// @description Checks that if the runtime type of `v` is a subtype of the
+/// static type of the variable `p` declares or assigns to, then `v` is stored
+/// in `p`s variable and match succeeds. Test an irrefutable context
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=patterns
+
+import "../../Utils/expect.dart";
+
+main() {
+  var <dynamic>[int v1] = [42] as dynamic;
+  Expect.equals(42, v1);
+
+  final <dynamic>[num? v2] = [null] as dynamic;
+  Expect.isNull(v2);
+
+  var <dynamic>[num v3] = [3.14] as dynamic;
+  Expect.equals(3.14, v3);
+
+  final <dynamic>[num v4] = [42] as dynamic;
+  Expect.equals(42, v4);
+}
