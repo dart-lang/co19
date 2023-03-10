4c55ff53fa31ee9b8ea90004362f952f234d9665
Sergey G. Grekhov <sgrekhov22@gmail.com>
2023-03-10 11:40:29 +0200

    #1401. [Patterns] If case statement and element tests added (#1782)
    Add if-case statement and element tests

---
+++ LanguageFeatures/Patterns/if_case_A01_t01.dart
@@ -0,0 +1,210 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion We replace the existing ifStatement rule with:
+///
+/// ifStatement ::= ifCondition statement ('else' statement)?
+/// ifCondition :== 'if' '(' expression ( 'case' guardedPattern )? ')'
+///
+/// When the condition has no guardedPattern, it behaves as it does today. If
+/// there is a guardedPattern, then the expression is evaluated and matched
+/// against the subsequent pattern. If it matches, the then branch is executed
+/// with any variables the pattern defines in scope. Otherwise, the else branch
+/// is executed if there is one.
+///
+/// A guard is also allowed:
+///
+/// if (json case [int x, int y] when x == y) {
+///   print('Was on coordinate x-y intercept');
+/// } else {
+///   throw FormatException('Invalid JSON.');
+/// }
+///
+/// @description Checks if-case statement with different patterns and a guard
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=patterns,records
+
+import "../../Utils/expect.dart";
+import "patterns_lib.dart";
+
+String test(int value) {
+  if (value case var a1 && 0 || var a1 && 1 when a1 == 0) {
+    return "logical-or-1";
+  }
+  if (value case 1 || 2) {
+    return "logical-or-2";
+  }
+  if (value case var a2 && > 10 && var b2 && < 15 when a2 == 14) {
+    return "logical-and-1";
+  }
+  if (value case > 12 && < 17) {
+    return "logical-and-2";
+  }
+  if (value case <= 25 when false) {
+    return "relational-1";
+  }
+  if (value case < 24 when true) {
+    return "relational-2";
+  }
+  if (value case 30 when 1 > 2) {
+    return "constant-1";
+  }
+  if (value case 30 when 1 < 2) {
+    return "constant-2";
+  }
+  if (value case (var a3 && 40) when a3 == 42) {
+    return "parenthesized-1";
+  }
+  if (value case (40)) {
+    return "parenthesized-2";
+  } else {
+    return "default";
+  }
+}
+
+String testCast(num value) {
+  if (value case var c1 as int when c1 == 42) {
+    return "cast-1 =$c1";
+  }
+  if (value case var c2 as int when c2 > 4) {
+    return "cast-2 =$c2";
+  } else {
+    return "default";
+  }
+}
+
+String testNullCheck(int? value) {
+  if (value case var a1? when a1 > 0) {
+    return "null-check-1";
+  }
+  if (value case var a2?) {
+    return "null-check-2";
+  } else {
+    return "default";
+  }
+}
+
+String testNullAssert(int? value) {
+  if (value case var a1! when a1 > 0) {
+    return "null-assert-1";
+  }
+  return "default";
+}
+
+String testVariable(int value) {
+  if (value case var a1 when a1 > 0) {
+    return "variable-1";
+  }
+  if (value case var a2 when a2 < 0) {
+    return "variable-2";
+  } else {
+    return "default";
+  }
+}
+
+String testList(List<int> list) {
+  if (list case [1, var a] when a > 0) {
+    return "list-1";
+  }
+  if (list case [1, final b, ...] when b < 0) {
+    return "list-2";
+  }
+  if (list case [1, _, ...var r] when !r.isEmpty) {
+    return "list-3";
+  } else {
+    return "default";
+  }
+}
+
+String testMap(Map<String, int> map) {
+  if (map case {"key1": 1, "key2": var a} when a > 0) {
+    return "map-1";
+  }
+  if (map case {"key1": 1, "key2": final b} when b < 0) {
+    return "map-2";
+  } else {
+    return "default";
+  }
+}
+
+String testRecord(Record record) {
+  if (record case (1, int a) when a > 0) {
+    return "record-1";
+  }
+  if (record case (1, final int b) when b < 0) {
+    return "record-2";
+  }
+  if (record case (1, 2, n: int c) when c > 0) {
+    return "record-3";
+  }
+  if (record case (1, 2, n: final int d) when d < 0) {
+    return "record-4";
+  } else {
+    return "default";
+  }
+}
+
+String testObject(Shape shape) {
+  if (shape case Square(sizeAsInt: int a) when a > 2) {
+    return "object-1";
+  }
+  if (shape case Square(areaAsInt: final int b) when b < 4 && b >= 1) {
+    return "object-2";
+  }
+  if (shape case Circle(:int sizeAsInt) when sizeAsInt > 1) {
+    return "object-3";
+  } else {
+    return "default";
+  }
+}
+
+main() {
+  Expect.equals("logical-or-1", test(0));
+  Expect.equals("logical-or-2", test(1));
+  Expect.equals("logical-and-1", test(14));
+  Expect.equals("logical-and-2", test(13));
+  Expect.equals("default", test(24));
+  Expect.equals("relational-2", test(23));
+  Expect.equals("constant-2", test(30));
+  Expect.equals("parenthesized-2", test(40));
+
+  Expect.equals("cast-1 =42", testCast(42));
+  Expect.equals("cast-2 =41", testCast(41));
+  Expect.equals("default", testCast(3));
+
+  Expect.equals("null-check-2", testNullCheck(0));
+  Expect.equals("null-check-1", testNullCheck(1));
+  Expect.equals("default", testNullCheck(null));
+
+  Expect.equals("null-assert-1", testNullAssert(1));
+  Expect.equals("default", testNullAssert(0));
+  Expect.throws(() {testNullAssert(null);});
+
+  Expect.equals("variable-1", testVariable(1));
+  Expect.equals("variable-2", testVariable(-1));
+  Expect.equals("default", testVariable(0));
+
+  Expect.equals("list-1", testList([1, 2]));
+  Expect.equals("list-2", testList([1, -1]));
+  Expect.equals("list-3", testList([1, 0, 3]));
+  Expect.equals("default", testList([1, 0]));
+
+  Expect.equals("map-1", testMap({"key1": 1, "key2": 2}));
+  Expect.equals("map-2", testMap({"key1": 1, "key2": -3}));
+  Expect.equals("default", testMap({"key1": 1, "key2": 0}));
+
+  Expect.equals("record-1", testRecord((1, 2)));
+  Expect.equals("record-2", testRecord((1, -3)));
+  Expect.equals("record-3", testRecord((1, 2, n: 3)));
+  Expect.equals("record-4", testRecord((1, 2, n: -4)));
+  Expect.equals("default", testRecord((1, 0)));
+  Expect.equals("default", testRecord((1, 2, n: 0)));
+
+  Expect.equals("object-1", testObject(Square(3)));
+  Expect.equals("object-2", testObject(Square(1)));
+  Expect.equals("default", testObject(Square(0)));
+  Expect.equals("object-3", testObject(Circle(2)));
+  Expect.equals("default", testObject(Circle(1)));
+}
---
+++ LanguageFeatures/Patterns/if_case_A02_t01.dart
@@ -0,0 +1,200 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Since Dart allows if elements inside collection literals, we also
+/// support if-case elements. We replace the existing ifElement rule with:
+//
+// ifElement ::= ifCondition element ('else' element)?
+// The semantics follow the statement form. If there is no guardedPattern, then
+// it behaves as before. When there is a guardedPattern, if the expression
+// matches the pattern (and the guard returns true) then we evaluate and yield
+// the then element into the surrounding collection. Otherwise, we evaluate and
+// yield the else element if there is one.
+///
+/// @description Checks if-case element with different patterns and an optional
+/// guard in a list literal
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=patterns,records
+
+import "../../Utils/expect.dart";
+import "patterns_lib.dart";
+
+List<int> testLogicalOr1(int value) => [
+    1,
+    if (value case var a && 0 || var a && 1 when a == 0) 2 else 3,
+    4
+  ];
+
+List<int> testLogicalOr2(int value) {
+  return [1, if (value case 1 || 2) 2 else 3, 4];
+}
+
+List<int> testLogicalAnd1(int value) => [
+    1,
+    if (value case var a && > 10 && var b && < 15 when a == 14) 2 else 3,
+    4
+  ];
+
+List<int> testLogicalAnd2(int value) {
+  return [1, if (value case > 12 && < 17) 2 else 3, 4];
+}
+
+List<int> testRelational(int value, bool guard) {
+  return [1, if (value case < 25 when guard) 2 else 3, 4];
+}
+
+List<int> testConstant(int value, int guard) {
+  return [1, if (value case 30 when guard == 42) 2 else 3, 4];
+}
+
+List<int> testParenthesized1(int value, int guard) {
+  return [1, if (value case (var a && 40) when a == guard) 2 else 3, 4];
+}
+
+List<int> testParenthesized2(int value) => [
+    1,
+    if (value case (42)) 2 else 3,
+    4
+  ];
+
+List<int> testCast(num value) {
+  return [1, if (value case var c1 as int when c1 == 42) 2 else 3, 4];
+}
+
+List<int> testNullCheck1(int? value) {
+  return [1, if (value case var a? when a > 0) 2 else 3, 4];
+}
+
+List<int> testNullCheck2(int? value) => [1, if (value case var a?) 2 else 3, 4];
+
+List<int> testNullAssert(int? value) {
+  return [1, if (value case var a! when a > 0) 2 else 3, 4];
+}
+
+List<int> testVariable(int value) {
+  return [1, if (value case var a when a > 0) 2 else 3, 4];
+}
+
+List<int> testList1(List<int> list) {
+  return [1, if (list case [1, var a] when a > 0) 2 else 3, 4];
+}
+
+List<int> testList2(List<int> list) {
+  return [1, if (list case [1, final b, ...] when b < 0) 2 else 3, 4];
+}
+
+List<int> testList3(List<int> list) {
+  return [1, if (list case [1, _, ...var r] when !r.isEmpty) 2 else 3, 4];
+}
+
+List<int> testMap1(Map<String, int> map) {
+  return [1, if (map case {"key1": 1, "key2": var a} when a > 0) 2 else 3, 4];
+}
+
+List<int> testMap2(Map<String, int> map) => [
+    1,
+    if (map case {"key1": 1, "key2": final b, ...} when b < 0) 2 else 3,
+    4
+  ];
+
+List<int> testRecord1(Record record) {
+  return [1, if (record case (1, int a) when a > 0) 2 else 3, 4];
+}
+
+List<int> testRecord2(Record record) => [
+    1,
+    if (record case (1, 2, n: final int d) when d < 0) 2 else 3,
+    4
+  ];
+
+List<int> testObject1(Shape shape) {
+  return [1, if (shape case Square(sizeAsInt: var a) when a > 2) 2 else 3, 4];
+}
+
+List<int> testObject2(Shape shape) => [
+    1,
+    if (shape case Circle(: final sizeAsInt) when sizeAsInt > 1) 2 else 3,
+    4
+  ];
+
+List<int> testObject3(Shape shape) {
+  return [1, if (shape case Square(sizeAsInt: 1)) 2 else 3, 4];
+}
+
+main() {
+    Expect.listEquals([1, 2, 4], testLogicalOr1(0));
+    Expect.listEquals([1, 3, 4], testLogicalOr1(1));
+    Expect.listEquals([1, 2, 4], testLogicalOr2(1));
+    Expect.listEquals([1, 2, 4], testLogicalOr2(2));
+    Expect.listEquals([1, 3, 4], testLogicalOr2(3));
+    Expect.listEquals([1, 2, 4], testLogicalAnd1(14));
+    Expect.listEquals([1, 3, 4], testLogicalAnd1(13));
+    Expect.listEquals([1, 3, 4], testLogicalAnd1(10));
+    Expect.listEquals([1, 3, 4], testLogicalAnd1(15));
+    Expect.listEquals([1, 2, 4], testLogicalAnd2(15));
+    Expect.listEquals([1, 3, 4], testLogicalAnd2(10));
+    Expect.listEquals([1, 3, 4], testLogicalAnd2(20));
+    Expect.listEquals([1, 2, 4], testRelational(20, true));
+    Expect.listEquals([1, 3, 4], testRelational(20, false));
+    Expect.listEquals([1, 3, 4], testRelational(25, true));
+    Expect.listEquals([1, 2, 4], testConstant(30, 42));
+    Expect.listEquals([1, 3, 4], testConstant(30, 41));
+    Expect.listEquals([1, 3, 4], testConstant(31, 42));
+    Expect.listEquals([1, 2, 4], testParenthesized1(40, 40));
+    Expect.listEquals([1, 3, 4], testParenthesized1(42, 40));
+    Expect.listEquals([1, 3, 4], testParenthesized1(40, 42));
+    Expect.listEquals([1, 2, 4], testParenthesized2(42));
+    Expect.listEquals([1, 3, 4], testParenthesized2(40));
+    Expect.listEquals([1, 2, 4], testCast(42));
+    Expect.listEquals([1, 3, 4], testCast(41));
+    Expect.listEquals([1, 2, 4], testNullCheck1(1));
+    Expect.listEquals([1, 3, 4], testNullCheck1(-1));
+    Expect.listEquals([1, 3, 4], testNullCheck1(null));
+    Expect.listEquals([1, 2, 4], testNullCheck2(1));
+    Expect.listEquals([1, 2, 4], testNullCheck2(-1));
+    Expect.listEquals([1, 3, 4], testNullCheck2(null));
+    Expect.listEquals([1, 2, 4], testNullAssert(1));
+    Expect.listEquals([1, 3, 4], testNullAssert(-1));
+    Expect.throws(() {testNullAssert(null);});
+    Expect.listEquals([1, 2, 4], testVariable(1));
+    Expect.listEquals([1, 3, 4], testVariable(-1));
+    Expect.listEquals([1, 2, 4], testList1([1, 42]));
+    Expect.listEquals([1, 3, 4], testList1([2, 42]));
+    Expect.listEquals([1, 3, 4], testList1([1, 0]));
+    Expect.listEquals([1, 3, 4], testList1([1, 42, 0]));
+    Expect.listEquals([1, 2, 4], testList2([1, -42]));
+    Expect.listEquals([1, 3, 4], testList2([2, -42]));
+    Expect.listEquals([1, 3, 4], testList2([1, 0]));
+    Expect.listEquals([1, 2, 4], testList2([1, -42, 0]));
+    Expect.listEquals([1, 2, 4], testList3([1, 2, 3]));
+    Expect.listEquals([1, 3, 4], testList3([2, 2]));
+    Expect.listEquals([1, 3, 4], testList3([1, 0]));
+    Expect.listEquals([1, 2, 4], testList3([1, -42, 0]));
+    Expect.listEquals([1, 2, 4], testMap1({"key1": 1, "key2": 2}));
+    Expect.listEquals([1, 3, 4], testMap1({"key1": 2, "key2": 2}));
+    Expect.listEquals([1, 3, 4], testMap1({"key1": 1, "key2": -2}));
+    Expect.listEquals([1, 3, 4], testMap1({"key1": 1, "key2": 2, "key3": 3}));
+    Expect.listEquals([1, 2, 4], testMap2({"key1": 1, "key2": -2}));
+    Expect.listEquals([1, 3, 4], testMap2({"key1": 2, "key2": -2}));
+    Expect.listEquals([1, 3, 4], testMap2({"key1": 1, "key2": 2}));
+    Expect.listEquals([1, 2, 4], testMap2({"key1": 1, "key2": -2, "key3": 3}));
+    Expect.listEquals([1, 2, 4], testRecord1((1, 2)));
+    Expect.listEquals([1, 3, 4], testRecord1((2, 2)));
+    Expect.listEquals([1, 3, 4], testRecord1((1, -2)));
+    Expect.listEquals([1, 2, 4], testRecord2((1, 2, n: -1)));
+    Expect.listEquals([1, 3, 4], testRecord2((2, 2, n: 1)));
+    Expect.listEquals([1, 3, 4], testRecord2((1, 2, n: 1)));
+    Expect.listEquals([1, 3, 4], testRecord2((1, n: 1)));
+    Expect.listEquals([1, 3, 4], testRecord2((1, 2, n: 1, n2: 2)));
+    Expect.listEquals([1, 2, 4], testObject1(Square(3)));
+    Expect.listEquals([1, 3, 4], testObject1(Circle(3)));
+    Expect.listEquals([1, 3, 4], testObject1(Square(1)));
+    Expect.listEquals([1, 2, 4], testObject2(Circle(3)));
+    Expect.listEquals([1, 3, 4], testObject2(Circle(1)));
+    Expect.listEquals([1, 3, 4], testObject2(Square(3)));
+    Expect.listEquals([1, 2, 4], testObject3(Square(1)));
+    Expect.listEquals([1, 3, 4], testObject3(Circle(1)));
+    Expect.listEquals([1, 3, 4], testObject3(Square(3)));
+}
