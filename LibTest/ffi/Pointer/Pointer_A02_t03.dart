84004ec2beefcd4a5012b1559d3cd56399fdb33e
sgrekhov <sgrekhov22@gmail.com>
2023-04-06 11:45:11 +0300

    Fixes #2006. `Pointer` can have top types as type arguments

--- LibTest/ffi/Pointer/Pointer_A02_t03.dart
+++ LibTest/ffi/Pointer/Pointer_A02_t03.dart
@@ -3,10 +3,9 @@
 // BSD-style license that can be found in the LICENSE file.
 
 /// @assertion Pointer<T extends NativeType>
-///  Represents a pointer into the native C memory. Cannot be extended.
+/// Represents a pointer into the native C memory. Cannot be extended.
 ///
-/// @description Checks that it is a compile time error if pointer class is a
-/// top type
+/// @description Checks that it is not an error if pointer class is a top type
 /// @author sgrekhov@unipro.ru
 /// @issue 47746
 
@@ -15,28 +14,12 @@ import 'package:ffi/ffi.dart';
 
 void main() {
   Pointer<Int16> p = calloc<Int16>();
-  Pointer<dynamic> p1 = Pointer.fromAddress(p.address);
-//        ^^^^^^^
-// [cfe] unspecified
-// [analyzer] unspecified
-
-  Pointer<void> p2 = Pointer.fromAddress(p.address);
-//        ^^^^
-// [cfe] unspecified
-// [analyzer] unspecified
-
-  Pointer<Null> p3 = Pointer.fromAddress(p.address);
-//        ^^^^
-// [cfe] unspecified
-// [analyzer] unspecified
-
-  Pointer<Object?> p4 = Pointer.fromAddress(p.address);
-//        ^^^^^^^
-// [cfe] unspecified
-// [analyzer] unspecified
-
-  Pointer<Never> p5 = Pointer.fromAddress(p.address);
-//        ^^^^^
-// [cfe] unspecified
-// [analyzer] unspecified
+  try {
+    Pointer<dynamic> p1 = Pointer.fromAddress(p.address);
+    Pointer<void> p2 = Pointer.fromAddress(p.address);
+    Pointer<Object?> p3 = Pointer.fromAddress(p.address);
+    Pointer<Never> p4 = Pointer.fromAddress(p.address);
+  } finally {
+    calloc.free(p);
+  }
 }
