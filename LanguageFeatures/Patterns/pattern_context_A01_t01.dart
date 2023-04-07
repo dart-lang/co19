badd48c866bdd34c29aad12c7ec1de380ec2ebce
Sergey G. Grekhov <sgrekhov22@gmail.com>
2023-04-07 17:48:33 +0300

    Fixes #2008. Split pattern_context_A01_t01.dart test to multiple methods (#2015)

--- LanguageFeatures/Patterns/pattern_context_A01_t01.dart
+++ LanguageFeatures/Patterns/pattern_context_A01_t01.dart
@@ -18,44 +18,69 @@
 
 import "patterns_lib.dart";
 
-main() {
+test1() {
   var x1 || x1 = 42;
 //       ^
 // [analyzer] unspecified
 // [cfe] unspecified
+}
 
+test2() {
   final (int x2) || (int x2) = 42;
 //               ^
 // [analyzer] unspecified
 // [cfe] unspecified
+}
 
+test3() {
   var [x3, ...] || [x3, _] = [1, 2];
 //              ^
 // [analyzer] unspecified
 // [cfe] unspecified
+}
 
+test4() {
   final {"key1": int x4} || {"key1": int x4} = {"key1": 1};
 //                       ^
 // [analyzer] unspecified
 // [cfe] unspecified
+}
 
+test5() {
   var (num x5, n: String x6) || (num x5, n: String x6) = (3.14, n: "pi");
 //                           ^
 // [analyzer] unspecified
 // [cfe] unspecified
+}
 
+test6() {
   final Square(:areaAsInt) || Square(:areaAsInt) = Square(2);
 //                         ^
 // [analyzer] unspecified
 // [cfe] unspecified
+}
 
+test7() {
   var (x6 || x6) = 42;
 //        ^
 // [analyzer] unspecified
 // [cfe] unspecified
+}
 
+test8() {
   final ((int x7) || (int x7)) = 42;
 //                ^
 // [analyzer] unspecified
 // [cfe] unspecified
+}
+
+main() {
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+  test6();
+  test7();
+  test8();
 }
