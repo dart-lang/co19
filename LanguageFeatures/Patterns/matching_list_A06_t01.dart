30e6be72134e3c2a341b6af21a2eab4478b76134
Sergey G. Grekhov <sgrekhov22@gmail.com>
2023-04-25 11:11:45 +0300

    Fixes #2027. Hint expectation added (#2028)
    Hint expectation added

--- LanguageFeatures/Patterns/matching_list_A06_t01.dart
+++ LanguageFeatures/Patterns/matching_list_A06_t01.dart
@@ -91,6 +91,8 @@ String test1(MisbehavingList<int> ml) =>
     [_, _] => "2",
     [_, _, ...] => "2+",
     _ => "any"
+//    ^^
+// [analyzer] HINT.UNREACHABLE_SWITCH_CASE
   };
 
 String test2(MisbehavingList<int> ml) {
