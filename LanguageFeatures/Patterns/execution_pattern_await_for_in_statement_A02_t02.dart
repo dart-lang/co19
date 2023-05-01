1d7920b18cc8373638038a1df78d0d7976baa051
sgrekhov <sgrekhov22@gmail.com>
2023-05-01 16:26:44 +0300

    #1401. Async for-in tests added

--- LanguageFeatures/Patterns/execution_pattern_await_for_in_statement_A02_t02.dart
+++ LanguageFeatures/Patterns/execution_pattern_await_for_in_statement_A02_t02.dart
@@ -25,9 +25,11 @@
 ///   { <statement> }
 /// }
 /// ```
-/// @description Checks that if `I` implements `Iterable<T>` for some `T` then
-/// `E` is `T`. Test `T` inferred from `<pattern>` to `<expression>` and from
-/// `<expression>` to `<pattern>`
+/// @description Checks that in an async for-in loop if `I` is `dynamic` and
+/// runtime type of `I` is `Stream<T>` where `T` is assignable to `<pattern>`
+/// required type then for-in statement works as expected. In particular, if `I`
+/// is `dynamic` then element type is `dynamic` and execution of the body does
+/// take place
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=patterns,records
@@ -36,105 +38,104 @@ import "../../Utils/expect.dart";
 import "../../Utils/static_type_helper.dart";
 import "patterns_lib.dart";
 
-extension Storing on Object? {
-  static dynamic stored;
-  void get store => stored = this;
-}
-
-main() {
+main() async {
   String log = "";
-  for (var (num v1) in [1, 2, 3]..store) {
+  await for (var (num v1) in Stream.fromIterable([1, 2, 3]) as dynamic) {
     v1.expectStaticType<Exactly<num>>();
     log += "$v1;";
   }
   Expect.equals("1;2;3;", log);
-  Expect.isTrue(Storing.stored is List<num>);
-  Storing.stored.add(42);
-  Storing.stored.add(3.14);
 
   log = "";
-  for (final (v2) in <num>[1, 2, 3]) {
-    v2.expectStaticType<Exactly<num>>();
+  await for (final (v2) in Stream<num>.fromIterable([1, 2, 3]) as dynamic) {
+    Expect.throws(() {
+      v2.whatever;
+    }); // v2 is dynamic
     log += "$v2;";
   }
   Expect.equals("1;2;3;", log);
   log = "";
 
   log = "";
-  for (var <num>[v3] in [[1], [2], [3]]..store) {
+  await for (var <num>[v3] in Stream.fromIterable([
+    [1],
+    [2],
+    [3]
+  ]) as dynamic) {
     v3.expectStaticType<Exactly<num>>();
     log += "$v3;";
   }
   Expect.equals("1;2;3;", log);
-  Expect.isTrue(Storing.stored is List<List<num>>);
-  Storing.stored.first.add(42);
-  Storing.stored.first.add(3.14);
 
   log = "";
-  for (final [v4] in <List<num>>[[1], [2], [3]]) {
-    v4.expectStaticType<Exactly<num>>();
+  await for (final [v4] in Stream<List<num>>.fromIterable([
+    [1],
+    [2],
+    [3]
+  ]) as dynamic) {
+    Expect.throws(() {
+      v4.whatever;
+    });
     log += "$v4;";
   }
   Expect.equals("1;2;3;", log);
 
   log = "";
-  for (var <String, num>{"k1": v5} in [{"k1": 1}]..store) {
+  await for (var <String, num>{"k1": v5} in Stream.fromIterable([
+    {"k1": 1}
+  ]) as dynamic) {
     v5.expectStaticType<Exactly<num>>();
     log += "$v5;";
   }
   Expect.equals("1;", log);
-  Expect.isTrue(Storing.stored is List<Map<String, num>>);
-  Expect.isTrue(Storing.stored.first is Map<String, num>);
-  Storing.stored.first["answer"] = 42;
-  Storing.stored.first["pi"] = 3.14;
 
   log = "";
-  for (final {"k1": v6} in <Map<String, num>>[{"k1": 1}]) {
-    v6.expectStaticType<Exactly<num>>();
+  await for (final {"k1": v6} in Stream<Map<String, num>>.fromIterable([
+    {"k1": 1}
+  ]) as dynamic) {
+    Expect.throws(() {
+      v6.whatever;
+    });
     log += "$v6;";
   }
   Expect.equals("1;", log);
 
   log = "";
-  for (var (num v7, n: num v8) in [(1, n: 2)]..store) {
+  await for (var (num v7, n: num v8)
+      in Stream.fromIterable([(1, n: 2)]) as dynamic) {
     v7.expectStaticType<Exactly<num>>();
     v8.expectStaticType<Exactly<num>>();
     log += "$v7;$v8;";
   }
   Expect.equals("1;2;", log);
-  Expect.isTrue(Storing.stored is List<(num, {num n})>);
 
   log = "";
-  for (final (v9, n: v10) in <(num, {num n})>[(1, n: 2)]) {
-    v9.expectStaticType<Exactly<num>>();
-    v10.expectStaticType<Exactly<num>>();
+  await for (final (v9, n: v10)
+      in Stream<(num, {num n})>.fromIterable([(1, n: 2)]) as dynamic) {
+    Expect.throws(() {
+      v9.whatever;
+    });
+    Expect.throws(() {
+      v10.whatever;
+    });
     log += "$v9;$v10;";
   }
   Expect.equals("1;2;", log);
 
   log = "";
-  for (var Square<Centimeter>(area: v11) in [Square(1)]..store) {
+  await for (var Square<Centimeter>(area: v11)
+      in Stream.fromIterable([Square<Centimeter>(1)]) as dynamic) {
     v11.expectStaticType<Exactly<Unit<Centimeter>>>();
     log += "$v11;";
   }
   Expect.equals("1;", log);
-  Expect.isTrue(Storing.stored is List<Square<Centimeter>>);
 
   log = "";
-  for (final Square(area: v12) in <Square<Centimeter>>[Square<Centimeter>(1)]) {
-    v12.expectStaticType<Exactly<Unit<Centimeter>>>();
+  await for (final Square(area: v12)
+      in Stream<Square<Centimeter>>.fromIterable([Square<Centimeter>(1)])
+          as dynamic) {
+    v12.expectStaticType<Exactly<Unit<MetricUnits>>>();
     log += "$v12;";
   }
   Expect.equals("1;", log);
-
-  log = "";
-  for (var (num v1 && v2) in [1, 2, 3]..store) {
-    v1.expectStaticType<Exactly<num>>();
-    v2.expectStaticType<Exactly<num>>();
-    log += "$v1&&$v2;";
-  }
-  Expect.equals("1&&1;2&&2;3&&3;", log);
-  Expect.isTrue(Storing.stored is List<num>);
-  Storing.stored.add(42);
-  Storing.stored.add(3.14);
 }
