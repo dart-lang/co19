34b9ef25166b658ac3340bdd3ec473223ffc6348
sgrekhov <sgrekhov22@gmail.com>
2023-05-03 11:07:58 +0300

    #1401. Async for-in element tests added

---
+++ LanguageFeatures/Patterns/execution_pattern_await_for_in_element_A03_t02.dart
@@ -0,0 +1,132 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion
+/// A collection element of the form:
+///
+/// for (<keyword> <pattern> in <expression>) <element>
+///
+/// Where <keyword> is var or final is treated like so:
+/// 1. Let I be the static type of <expression>, inferred using context type
+///   schema Iterable<P> where P is the context type schema of <pattern>.
+/// 2. Calculate the element type of I:
+///     i. If I implements Iterable<T> for some T then E is T.
+///     ii. Else if I is dynamic then E is dynamic.
+///     iii. Else it is a compile-time error.
+/// 3. Type check <pattern> with matched value type E.
+/// 4. If there are no compile-time errors, then execution proceeds as the
+///   following code, where id1 and id2 are fresh identifiers and append() is
+///   an operation to add an element to the surrounding collection being built:
+/// ```dart
+/// I id1 = <expression>;
+/// Iterator<E> id2 = id1.iterator;
+/// while (id2.moveNext()) {
+///   <keyword> <pattern> = id2.current;
+///   append(<element>);
+/// }
+/// ```
+/// @description Checks that in a async for-in element if `I` is `dynamic` and a
+/// runtime type of `I` is `Stream<T>` where `T` is assignable to `<pattern>`
+/// required type then async for-in element works as expected
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=patterns,records
+
+import "../../Utils/expect.dart";
+import "../../Utils/static_type_helper.dart";
+import "patterns_lib.dart";
+
+main() async {
+  var l1 = [
+    0,
+    await for (var (num v1) in Stream.fromIterable([1, 2, 3]) as dynamic) v1,
+    4
+  ];
+  Expect.listEquals([0, 1, 2, 3, 4], l1);
+  l1.expectStaticType<Exactly<List<num>>>();
+
+  var l2 = [
+    0,
+    await for (final (v2) in Stream<num>.fromIterable([1, 2, 3]) as dynamic)
+      (1 > 2 ? v2.whatever : null, v2).$2,
+    4
+  ];
+  Expect.listEquals([0, 1, 2, 3, 4], l2);
+  l2.expectStaticType<Exactly<List<dynamic>>>();
+
+  var l3 = [
+    0,
+    await for (var <num>[v3] in Stream.fromIterable([
+      [1],
+      [2],
+      [3]
+    ]) as dynamic) v3,
+    4
+  ];
+  Expect.listEquals([0, 1, 2, 3, 4], l3);
+  l3.expectStaticType<Exactly<List<num>>>();
+
+  var l4 = [
+    0,
+    await for (final [v4] in Stream<List<num>>.fromIterable([
+      [1],
+      [2],
+      [3]
+    ]) as dynamic) (1 > 2 ? v4.whatever : null, v4).$2,
+    4
+  ];
+  Expect.listEquals([0, 1, 2, 3, 4], l4);
+  l4.expectStaticType<Exactly<List<dynamic>>>();
+
+  var l5 = [
+    0,
+    await for (var <String, num>{"k1": v5} in Stream.fromIterable([
+      {"k1": 1}]) as dynamic) v5,
+    2
+  ];
+  Expect.listEquals([0, 1, 2], l5);
+  l5.expectStaticType<Exactly<List<num>>>();
+
+  var l6 = [
+    0,
+    await for (final {"k1": v6} in Stream<Map<String, num>>.fromIterable([
+      {"k1": 1}]) as dynamic) (1 > 2 ? v6.whatever : null, v6).$2,
+    2
+  ];
+  Expect.listEquals([0, 1, 2], l6);
+  l6.expectStaticType<Exactly<List<dynamic>>>();
+
+  var l7 = [
+    0,
+    await for (var (num v7, n: num v8)
+        in Stream.fromIterable([(1, n: 2)]) as dynamic) v7,
+    2
+  ];
+  Expect.listEquals([0, 1, 2], l7);
+  l7.expectStaticType<Exactly<List<num>>>();
+
+  var l8 = [
+    1,
+    await for (final (v9, n: v10)
+        in Stream<(num, {num n})>.fromIterable([(1, n: 2)]) as dynamic)
+      (1 > 2 ? v10.whatever : null, v10).$2,
+    3
+  ];
+  Expect.listEquals([1, 2, 3], l8);
+  l8.expectStaticType<Exactly<List<dynamic>>>();
+
+  var l9 = [
+    await for (var Square<Centimeter>(area: v11)
+        in Stream.fromIterable([Square<Centimeter>(1)]) as dynamic) v11, ];
+  Expect.isTrue(l9[0] == 1);
+  l9.expectStaticType<Exactly<List<Unit<Centimeter>>>>();
+
+  var l10 = [
+    await for (final Square(area: v12)
+        in Stream<Square<Centimeter>>.fromIterable([Square<Centimeter>(1)])
+            as dynamic) v12
+  ];
+  Expect.isTrue(l10[0] == 1);
+  l10.expectStaticType<Exactly<List<Unit<MetricUnits>>>>();
+}
---
+++ LanguageFeatures/Patterns/execution_pattern_await_for_in_element_A03_t08.dart
@@ -0,0 +1,75 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion
+/// A collection element of the form:
+///
+/// for (<keyword> <pattern> in <expression>) <element>
+///
+/// Where <keyword> is var or final is treated like so:
+/// 1. Let I be the static type of <expression>, inferred using context type
+///   schema Iterable<P> where P is the context type schema of <pattern>.
+/// 2. Calculate the element type of I:
+///     i. If I implements Iterable<T> for some T then E is T.
+///     ii. Else if I is dynamic then E is dynamic.
+///     iii. Else it is a compile-time error.
+/// 3. Type check <pattern> with matched value type E.
+/// 4. If there are no compile-time errors, then execution proceeds as the
+///   following code, where id1 and id2 are fresh identifiers and append() is
+///   an operation to add an element to the surrounding collection being built:
+/// ```dart
+/// I id1 = <expression>;
+/// Iterator<E> id2 = id1.iterator;
+/// while (id2.moveNext()) {
+///   <keyword> <pattern> = id2.current;
+///   append(<element>);
+/// }
+/// ```
+/// @description Checks that in an async for-in element it is a compile-time
+/// error if `I` doesn't implement `Stream<T>` and in not `dynamic`. Test a map
+/// literal
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=patterns,records
+
+import "patterns_lib.dart";
+
+main() async {
+  var m1 = {
+    await for (var (int v1) in [42]) "k$v1": v1
+//                             ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  };
+  var m2 = {
+    await for (final <int>[v2] in "42") "k$v2": v2
+//                                ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  };
+  var m3 = {
+    await for (var <String, int>{"k1": v3} in [42]) "k$v3": v3
+//                                            ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  };
+  var m4 = {
+    await for (final (int v4,) in "42") "k$v4": v4
+//                                ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  };
+  var m5 = {
+    await for (var (n: v5) in [42]) "k$v5": v5
+//                            ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  };
+  var m6 = {
+    await for (final Square(area: v6) in "42") "k$v6": v6
+//                                       ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  };
+}
