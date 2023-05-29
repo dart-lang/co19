6233bd1fa1e4449b5a0b766ae51f20a4c3577899
sgrekhov <sgrekhov22@gmail.com>
2023-05-29 10:14:34 +0300

    Implement review notes

--- LanguageFeatures/Inline-classes/static_analysis_member_invocation_A05_t03.dart
+++ LanguageFeatures/Inline-classes/static_analysis_member_invocation_A05_t03.dart
@@ -2,9 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion If Dm is a method with function type F, and args exists, the
-/// static analysis of the inline member invocation is the same as that of an
-/// invocation with argument part args of a function with type [T1/X1 .. Ts/Xs]F
+/// @assertion Constructor tear-off of inline classes happens in the same way as
+/// for regular classes
+/// @note Inline classes specification doesn't contain this statement but
+/// assumes it
 ///
 /// @description Checks static type of an inline class constructor tear-off
 /// @author sgrekhov22@gmail.com
@@ -16,15 +17,37 @@ import "../../Utils/static_type_helper.dart";
 inline class IC1 {
   final int id;
   IC1(this.id);
+  IC1.n1(this.id, [String s = ""]);
+  IC1.n2(this.id, {String s = ""});
+  IC1.n3(this.id, {required String s});
 }
 
 inline class IC2<T extends num> {
   final T id;
-  IC2.x(this.id);
+  IC2(this.id);
+  IC2.n1(this.id, [String s = ""]);
+  IC2.n2(this.id, {String s = ""});
+  IC2.n3(this.id, {required String s});
 }
 
 main() {
   IC1.new.expectStaticType<Exactly<IC1 Function(int)>>();
-  IC2<num>.x.expectStaticType<Exactly<IC2 Function(num)>>();
-  IC2.x.expectStaticType<Exactly<IC2<T> Function<T extends num>(T)>>();
+  IC1.n1.expectStaticType<Exactly<IC1 Function(int, [String])>>();
+  IC1.n2.expectStaticType<Exactly<IC1 Function(int, {String s})>>();
+  IC1.n3.expectStaticType<Exactly<IC1 Function(int, {required String s})>>();
+
+  IC2<num>.new.expectStaticType<Exactly<IC2 Function(num)>>();
+  IC2<int>.n1.expectStaticType<Exactly<IC2<int> Function(int, [String])>>();
+  IC2<int>.n2.expectStaticType<Exactly<IC2<int> Function(int, {String s})>>();
+  IC2<int>
+      .n3
+      .expectStaticType<Exactly<IC2<int> Function(int, {required String s})>>();
+
+  IC2.new.expectStaticType<Exactly<IC2<T> Function<T extends num>(T)>>();
+  IC2.n1
+      .expectStaticType<Exactly<IC2<T> Function<T extends num>(T, [String])>>();
+  IC2.n2.expectStaticType<
+      Exactly<IC2<T> Function<T extends num>(T, {String s})>>();
+  IC2.n3.expectStaticType<
+      Exactly<IC2<T> Function<T extends num>(T, {required String s})>>();
 }
