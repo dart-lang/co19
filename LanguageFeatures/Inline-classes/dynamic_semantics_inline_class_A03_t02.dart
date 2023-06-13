af819efdca4c32bd743da4ed8762594255158673
sgrekhov <sgrekhov22@gmail.com>
2023-06-13 14:24:18 +0300

    Implement review recommendations

--- LanguageFeatures/Inline-classes/dynamic_semantics_inline_class_A03_t02.dart
+++ LanguageFeatures/Inline-classes/dynamic_semantics_inline_class_A03_t02.dart
@@ -24,81 +24,71 @@ inline class V2<T> {
   V2(this.id);
 }
 
-String test(Object instance) {
-  String log = "";
-  try {
-    instance as num;
-    log += "as num;";
-  } catch (_) {
-    log += "throw;";
-  }
+bool testAsInt(Object instance) {
   try {
     instance as int;
-    log += "as int;";
+    return true;
   } catch (_) {
-    log += "throw;";
+    return false;
   }
-  try {
-    instance as (int,);
-    log += "as (int,);";
-  } catch (_) {
-    log += "throw;";
-  }
-  try {
-    instance as V1;
-    log += "as V1;";
-  } catch (_) {
-    log += "throw;";
-  }
-  try {
-    instance as List<num>;
-    log += "as List<num>;";
-  } catch (_) {
-    log += "throw;";
-  }
-  try {
-    instance as List<int>;
-    log += "as List<int>;";
-  } catch (_) {
-    log += "throw;";
-  }
-  try {
-    instance as V2<List<num>>;
-    log += "as V2<List<num>>;";
-  } catch (_) {
-    log += "throw;";
-  }
-  try {
-    instance as V2<List<int>>;
-    log += "as V2<List<int>>;";
-  } catch (_) {
-    log += "throw;";
-  }
-  try {
-    instance as V2<List<(int,)>>;
-    log += "as V2<List<(int,)>>;";
-  } catch (_) {
-    log += "throw;";
-  }
-  return log;
 }
 
-main() {
+bool testAsV1(Object instance) {
+  try {
+    instance as V1;
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
+
+bool testAsT<T>(Object instance) {
+  try {
+    instance as T;
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
+
+bool testAsV2<T>(Object instance) {
+  try {
+    instance as V2<T>;
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
+
+void main() {
   int i = 42;
   var v1 = V1(i);
-  String log1 = test(i);
-  String log2 = test(v1);
-  Expect.equals(log1, log2);
+  var v2 = V2<num>(i);
 
-  List<int> l1 = [];
-  var v2 = V2<List<int>>(l1);
-  String log3 = test(l1);
-  String log4 = test(v2);
-  Expect.equals(log3, log4);
+  List<int> l = [];
+  var v3 = V2<List<int>>(l);
 
-  List<(int,)> l2 = [];
-  var v3 = V2<List<(int,)>>(l2);
-  String log5 = test(l2);
-  String log6 = test(v3);
-  Expect.equals(log5, log6);
+  var objects = [
+    true,
+    3.14,
+    i,
+    v1,
+    v2,
+    l,
+    v3,
+    "String",
+    #foo,
+    <String>[],
+    {'a': () {}},
+    {42,},
+    (),
+    (42,),
+    StringBuffer('Hello')
+  ];
+
+  for (var o in objects) {
+    Expect.equals(testAsInt(o), testAsV1(o));
+    Expect.equals(testAsT<int>(o), testAsV2<int>(o));
+    Expect.equals(testAsT<List<int>>(o), testAsV2<List<int>>(o));
+  }
 }
