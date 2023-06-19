da4057ee643e367e068073b62013197eebffe1be
sgrekhov <sgrekhov22@gmail.com>
2023-06-19 13:13:53 +0300

    #2080. Add correct member overrides tests. Part 1.

---
+++ Language/Interfaces/Correct_Member_Overrides/correct_member_overrides_A01_t01.dart
@@ -0,0 +1,54 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Let m and m′ be member signatures with the same name id. Then m
+/// is a correct override of m′ iff the following criteria are all satisfied:
+/// • m and m′ are both methods, both getters, or both setters.
+///
+/// @description Checks that it is a compile-time error if `m` and `m′` are not
+/// both getters or methods
+/// @author sgrekhov22@gmail.com
+
+interface class I1 {
+  int get m => 42;
+}
+
+interface class I2 {
+  int m() => 42;
+}
+
+class C1 implements I1 {
+  int m() => 42;
+//    ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+class C2 implements I2 {
+  int get m => 42;
+//        ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+mixin M1 implements I1 {
+  int m() => 0;
+//    ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+mixin M2 implements I2 {
+  int get m => 0;
+//        ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  print(C1);
+  print(C2);
+  print(M1);
+  print(M2);
+}
---
+++ Language/Interfaces/Correct_Member_Overrides/correct_member_overrides_A03_t08.dart
@@ -0,0 +1,44 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Let m and m′ be member signatures with the same name id. Then m
+/// is a correct override of m′ iff the following criteria are all satisfied:
+/// ...
+/// • If m and m′ are both methods or both setters: Let F be the function type
+/// of m except that the parameter type is the built-in class Object for each
+/// parameter of m which is covariant-by-declaration. Let F′ be the function
+/// type of m′. F must then be a subtype of F′.
+///
+/// @description Checks that it is not an error if `m` and `m′` are both methods
+/// and function type `m` is a subtype of `m′.
+/// @author sgrekhov22@gmail.com
+
+interface class I1 {
+  void m1(num v1) {}
+  void m2(num v1, [num v2 = 0]) {}
+  void m3(num v1, {num v2 = 0}) {}
+}
+
+interface class I2 {
+  void m1(covariant int v1) {}
+  void m2(num v1, [covariant int v2 = 0]) {}
+  void m3(num v1, {covariant int v2 = 0}) {}
+}
+
+class C implements I1, I2 {
+  void m1(covariant int i) {}
+  void m2(num v1, [covariant Object v2 = ""]) {}
+  void m3(num v1, {covariant int v2 = 0}) {}
+}
+
+mixin M implements I1, I2 {
+  void m1(num i) {}
+  void m2(num v1, [Object v2 = ""]) {}
+  void m3(num v1, {int v2 = 0}) {}
+}
+
+main() {
+  print(C);
+  print(M);
+}
