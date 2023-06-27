d779021ae6d96971f1fbfb5774c39312fdf19f45
Sergey G. Grekhov <sgrekhov22@gmail.com>
2023-06-27 09:18:22 +0300

    Fixes #2099. Add subtyping tests for function type with required named arguments

---
+++ LanguageFeatures/Subtyping/dynamic/generated/named_function_types_fail_class_member_fail_A06_t03.dart
@@ -0,0 +1,116 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion A type T0 is a subtype of a type T1 (written T0 <: T1) when:
+/// Named Function Types:
+/// T0 is U0 Function<X0 extends B00, ..., Xk extends B0k>(V0 x0, ..., Vn xn,
+///   {r0n+1 Vn+1 xn+1, ..., r0m Vm xm}) where r0j is empty or required for j in
+///   n+1...m
+/// and T1 is U1 Function<Y0 extends B10, ..., Yk extends B1k>(S0 y0, ...,
+///   Sn yn, {r1n+1 Sn+1 yn+1, ..., r1q Sq yq}) where r1j is empty or required
+///   for j in n+1...q
+/// and {yn+1, ... , yq} subsetof {xn+1, ... , xm}
+/// and Si[Z0/Y0, ..., Zk/Yk] <: Vi[Z0/X0, ..., Zk/Xk] for i in 0...n
+/// and Si[Z0/Y0, ..., Zk/Yk] <: Tj[Z0/X0, ..., Zk/Xk] for i in n+1...q, yj = xi
+/// and for each j such that r0j is required, then there exists an i in n+1...q
+///   such that xj = yi, and r1i is required
+/// and U0[Z0/X0, ..., Zk/Xk] <: U1[Z0/Y0, ..., Zk/Yk]
+/// and B0i[Z0/X0, ..., Zk/Xk] === B1i[Z0/Y0, ..., Zk/Yk] for i in 0...k
+/// where the Zi are fresh type variables with bounds B0i[Z0/X0, ..., Zk/Xk]
+///
+/// @description Check that if `T0` has a required named argument and `T1` has
+/// not required named argument of the same type and with the same name, then
+/// anyway `T0` is a subtype of `T1`.
+/// @author sgrekhov22@gmail.com
+///
+/// @description Check that if type T0 is not a subtype of a type T1, then
+/// instance of T0 cannot be assigned to the mixin member of type T1
+/// @author sgrekhov@unipro.ru
+///
+/// This test is generated from test_types/named_function_types_fail_A061.dart and 
+/// test_cases/class_member_fail_x03.dart. Don't modify it! 
+/// If you need to change this test, then change one of the files above and then 
+/// run generator/generator.dart to regenerate the tests.
+
+import '../../utils/common.dart';
+import '../../../../Utils/expect.dart';
+
+typedef void F0({required int i});
+typedef void F1({int i});
+
+void f0Instance({required int i}) {}
+void f1Instance({int i = 0}) {}
+
+F0 t0Instance = f0Instance;
+F1 t1Instance = f1Instance;
+
+const t1Default = f1Instance;
+
+mixin class ClassMemberSuper1_t03 {
+  F1 m = t1Default;
+
+  void set superSetter(F1 val) {}
+}
+
+class ClassMember1_t03 extends Object with ClassMemberSuper1_t03 {
+
+  test1() {
+    m = forgetType(t0Instance);
+  }
+
+  test2() {
+    superSetter = forgetType(t0Instance);
+  }
+}
+
+class ClassMemberSuper2_t03<X> {
+  X m;
+
+  ClassMemberSuper2_t03(X x) : m = x {}
+
+  void set superSetter(X val) {}
+}
+
+class ClassMember2_t03<X> extends ClassMemberSuper2_t03<X> {
+
+  ClassMember2_t03(X x): super(x) {}
+
+  test1() {
+    m = forgetType(t0Instance);
+  }
+
+  test2() {
+    superSetter = forgetType(t0Instance);
+  }
+}
+
+main() {
+  Expect.throws(() {
+    new ClassMember1_t03().m = forgetType(t0Instance);
+  }, (e) => e is TypeError);
+  Expect.throws(() {
+    new ClassMember1_t03().superSetter = forgetType(t0Instance);
+  }, (e) => e is TypeError);
+  Expect.throws(() {
+    new ClassMember1_t03().test1();
+  }, (e) => e is TypeError);
+  Expect.throws(() {
+    new ClassMember1_t03().test2();
+  }, (e) => e is TypeError);
+
+  // Test type parameters
+
+  Expect.throws(() {
+    new ClassMember2_t03<F1>(t1Instance).m = forgetType(t0Instance);
+  }, (e) => e is TypeError);
+  Expect.throws(() {
+    new ClassMember2_t03<F1>(t1Instance).superSetter = forgetType(t0Instance);
+  }, (e) => e is TypeError);
+  Expect.throws(() {
+    new ClassMember2_t03<F1>(t1Instance).test1();
+  }, (e) => e is TypeError);
+  Expect.throws(() {
+    new ClassMember2_t03<F1>(t1Instance).test2();
+  }, (e) => e is TypeError);
+}
---
+++ LanguageFeatures/Subtyping/static/generated/named_function_types_class_member_A06_t02.dart
@@ -0,0 +1,82 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion A type T0 is a subtype of a type T1 (written T0 <: T1) when:
+/// Named Function Types:
+/// T0 is U0 Function<X0 extends B00, ..., Xk extends B0k>(V0 x0, ..., Vn xn,
+///   {r0n+1 Vn+1 xn+1, ..., r0m Vm xm}) where r0j is empty or required for j in
+///   n+1...m
+/// and T1 is U1 Function<Y0 extends B10, ..., Yk extends B1k>(S0 y0, ...,
+///   Sn yn, {r1n+1 Sn+1 yn+1, ..., r1q Sq yq}) where r1j is empty or required
+///   for j in n+1...q
+/// and {yn+1, ... , yq} subsetof {xn+1, ... , xm}
+/// and Si[Z0/Y0, ..., Zk/Yk] <: Vi[Z0/X0, ..., Zk/Xk] for i in 0...n
+/// and Si[Z0/Y0, ..., Zk/Yk] <: Tj[Z0/X0, ..., Zk/Xk] for i in n+1...q, yj = xi
+/// and for each j such that r0j is required, then there exists an i in n+1...q
+///   such that xj = yi, and r1i is required
+/// and U0[Z0/X0, ..., Zk/Xk] <: U1[Z0/Y0, ..., Zk/Yk]
+/// and B0i[Z0/X0, ..., Zk/Xk] === B1i[Z0/Y0, ..., Zk/Yk] for i in 0...k
+/// where the Zi are fresh type variables with bounds B0i[Z0/X0, ..., Zk/Xk]
+///
+/// @description Check that if `T0` has a named argument and `T1` has a required
+/// named argument of the same type, then T0 is a subtype of T1.
+/// @author sgrekhov22@gmail.com
+///
+/// @description Check that if type T0 is a subtype of a type T1, then instance
+/// of T0 can be assigned to the superclass member of type T1
+/// @author sgrekhov@unipro.ru
+///
+/// This test is generated from test_types/named_function_types_A06.dart and 
+/// test_cases/class_member_x02.dart. Don't modify it! 
+/// If you need to change this test, then change one of the files above and then 
+/// run generator/generator.dart to regenerate the tests.
+
+typedef void F0({int i});
+typedef void F1({required int i});
+
+void f0Instance({int i = 0}) {}
+void f1Instance({required int i}) {}
+
+F0 t0Instance = f0Instance;
+
+const t1Default = f1Instance;
+
+class ClassMemberSuper1_t02 {
+  F1 m = t1Default;
+
+  ClassMemberSuper1_t02(dynamic value) {
+    m = value;
+  }
+
+  ClassMemberSuper1_t02.named(dynamic value) {
+    m = value;
+  }
+
+  ClassMemberSuper1_t02.short(this.m);
+
+  void set superSetter(F1 val) {}
+}
+
+class ClassMember1_t02 extends ClassMemberSuper1_t02 {
+
+  ClassMember1_t02() : super(t0Instance) {}
+
+  ClassMember1_t02.named() : super.named(t0Instance) {}
+
+  ClassMember1_t02.short() : super.short(t0Instance);
+
+  test() {
+    m = t0Instance;
+    superSetter = t0Instance;
+  }
+}
+
+main() {
+  ClassMember1_t02 c1 = new ClassMember1_t02();
+  c1 = new ClassMember1_t02.short();
+  c1 = new ClassMember1_t02.named();
+  c1.m = t0Instance;
+  c1.test();
+  c1.superSetter = t0Instance;
+}
