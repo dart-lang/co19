0d6cfc94c18485341ce78e9b18fdaf3f8681ae56
sgrekhov <sgrekhov22@gmail.com>
2023-07-11 14:13:17 +0300

    #2117. Update accessible_instance_member_t* tests

--- Language/Expressions/Method_Invocation/Super_Invocation/accessible_instance_member_t01.dart
+++ Language/Expressions/Method_Invocation/Super_Invocation/accessible_instance_member_t01.dart
@@ -2,15 +2,17 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion Let Sstatic be the superclass of the immediately enclosing class.
-/// It is a static type warning if Sstatic does not have an accessible instance
-/// member named m unless Sstatic or a superinterface of Sstatic is annotated
-/// with an annotation denoting a constant identical to the constant @proxy
-/// deﬁned in dart:core.
-/// @description Checks that it is a compile error if S does not have
-/// an instance member named m.
+/// @assertion Let Ssuper be the superclass of the immediately enclosing class
+/// for i, and let L be the library that contains i. Let the declaration D be
+/// the result of looking up the method m in Ssuper with respect to L, and let F
+/// be the static type of D. Otherwise, if the method lookup failed, let the
+/// declaration D be the result of looking up the getter m with respect to L in
+/// Ssuper, and let F be the return type of D. If both lookups failed, a
+/// compile-time error occurs
+///
+/// @description Checks that it is a compile error if `S` does not have
+/// an instance member named `m`.
 /// @author msyabro
-
 
 class S {}
 
