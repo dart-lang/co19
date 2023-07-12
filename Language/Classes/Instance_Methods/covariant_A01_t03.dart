28e906995425325a20170bec5a3f06eb0af27fad
Sergey G. Grekhov <sgrekhov22@gmail.com>
2023-07-12 16:26:44 +0300

    #2087. Add tests for parameters covariant-by-declaration (#2121)
    Add tests for parameters covariant-by-declaration

---
+++ Language/Classes/Instance_Methods/covariant_A01_t03.dart
@@ -0,0 +1,38 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion For each parameter p of m where covariant is present, it is a
+/// compile-time error if there exists a direct or indirect superinterface of C
+/// which has an accessible method signature m′′ with the same name as m, such
+/// that m′′ has a parameter p′′ that corresponds to p, unless the type of p is
+/// a subtype or a supertype of the type of p′′
+///
+/// @description Checks that it is not an error if there is a member with a
+/// covariant parameter `p` and there is another member with the same name but
+/// with the parameter which is a supertype of `p`
+/// @author sgrekhov22@gmail.com
+
+class A {
+  void m1(num a) {}
+  void m2([num a = 0]) {}
+  void m3({num a = 0}) {}
+  void m4({required num a}) {}
+
+  void set s(num n) {}
+  void operator +(num n) {}
+}
+
+class C extends A {
+  void m1(covariant int a) {}
+  void m2([covariant int a = 1]) {}
+  void m3({covariant int a = 1}) {}
+  void m4({required covariant int a}) {}
+
+  void set s(covariant int s) {}
+  void operator +(covariant int n) {}
+}
+
+main() {
+  print(C);
+}
