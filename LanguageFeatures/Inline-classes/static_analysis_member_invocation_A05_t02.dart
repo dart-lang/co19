5cb9df6022bfaba63f52bd812974bfae73691633
sgrekhov <sgrekhov22@gmail.com>
2023-07-26 12:37:55 +0300

    #2142. Change static analysis member invocation tests to be extension type tests

--- LanguageFeatures/Inline-classes/static_analysis_member_invocation_A05_t02.dart
+++ LanguageFeatures/Inline-classes/static_analysis_member_invocation_A05_t02.dart
@@ -2,9 +2,17 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion If Dm is a method with function type F, and args exists, the
-/// static analysis of the inline member invocation is the same as that of an
-/// invocation with argument part args of a function with type [T1/X1 .. Ts/Xs]F
+/// @assertion Consider an invocation of the extension type member m on the
+/// receiver expression e according to the extension type declaration V with the
+/// actual type arguments T1, ..., Ts.
+/// ...
+/// V has an extension type member m with a uniquely determined declaration Dm
+/// ...
+/// Assume that Dm is a method with function type F, and typeArgs is provided. A
+/// compile-time error occurs if F is not a generic function type where typeArgs
+/// is a list of actual type arguments that conform to the declared bounds. If
+/// no error occurred, the invocation has the static type which is a non-generic
+/// function type where typeArgs are substituted into the function type.
 ///
 /// @description Checks that it is a compile-time error to specify a wrong
 /// type argument of a torn-off method
@@ -12,16 +20,13 @@
 
 // SharedOptions=--enable-experiment=inline-class
 
-inline class IC1 {
-  final int id;
-  IC1(this.id);
-
+extension type ET1(int id) {
   int foo<T1, T2 extends num>() => 42;
 }
 
 main() {
-  IC1 ic1 = IC1(42);
-  ic1.foo<String, String>;
+  ET1 et1 = ET1(42);
+  et1.foo<String, String>;
 //                ^^^^^^
 // [analyzer] unspecified
 // [cfe] unspecified
--- LanguageFeatures/Inline-classes/static_analysis_member_invocation_A06_t01.dart
+++ LanguageFeatures/Inline-classes/static_analysis_member_invocation_A06_t01.dart
@@ -2,35 +2,29 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion We say that an inline class declaration DV has a member named n
-/// in the case where DV declares a member named n, and in the case where DV has
-/// no such declaration, but DV has a direct superinterface V that has a member
-/// named n. In both cases, the member declaration named n that DV has is said
-/// declaration.
+/// @assertion We say that an extension type declaration DV has a non-extension
+/// type member named n in the case where DV does not declare a member named n,
+/// but DV has a direct extension type superinterface V that has a non-extension
+/// type member named n, or DV has a direct non-extension type superinterface T
+/// whose interface contains a member signature named n.
 ///
-/// @description Checks that if inline class `V` has a superinterface with a
-/// member `m` then this member is also presents in `V`
+/// @description Checks that if an extension type `ET` has a superinterface with
+/// a member `m` then this member is also presents in `ET`
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=inline-class
 
-inline class V1 {
-  final int id;
-  V1(this.id);
-
+class I {
   void m1() {}
   int get m2 => 42;
   void set m3(int val) {}
 }
 
-inline class V implements V1 {
-  final int id;
-  V(this.id);
-}
+extension type ET(int id) implements I {}
 
 main() {
-  V v = V(42);
-  v.m1();
-  v.m2;
-  v.m3 = 42;
+  ET et = ET(42);
+  et.m1();
+  et.m2;
+  et.m3 = 42;
 }
