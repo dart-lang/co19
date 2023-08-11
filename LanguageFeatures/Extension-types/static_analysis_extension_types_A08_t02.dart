e9161540f8b28da6d96690f0f77451a49eeb69d6
sgrekhov <sgrekhov22@gmail.com>
2023-08-11 10:30:37 +0200

    Fixes #2173. Fix tests checking use of await for extension types

--- LanguageFeatures/Extension-types/static_analysis_extension_types_A08_t02.dart
+++ LanguageFeatures/Extension-types/static_analysis_extension_types_A08_t02.dart
@@ -5,27 +5,37 @@
 /// @assertion It is a compile-time error if await e occurs, and the static type
 /// of e is an extension type which is not a subtype of Future<T> for any T.
 ///
-/// @description Checks that it is not an error if `await e` occurs and the
-/// static type of `e` is an extension type which is a subtype of `Future<T>`
-/// for some `T`.
+/// @description Checks that it is a compile-time error if `await e` occurs, and
+/// the static type of `e` is an extension type which is not a subtype of
+/// `Future<T>`.
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=inline-class
 
-import "../../Utils/expect.dart";
-
-Future<int> f = Future<int>.value(0);
+extension type V0(Future<int> id) {}
 
 extension type V1(Future<int> id) {}
 
 extension type V2<T extends Future<Object>>(T id) {}
 
+extension type V3(Future<int> id) implements V0 {}
+
 main() async {
   V1 v1 = V1(Future<int>.value(42));
-  var _v1 = await v1;
-  Expect.equals(42, _v1);
+  await v1;
+//      ^^
+// [analyzer] unspecified
+// [cfe] unspecified
 
   V2<Future<String>> v2 = V2(Future<String>.value("42"));
-  var _v2 = await v2;
-  Expect.equals("42", _v2);
+  await v2;
+//      ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  V3 v3 = V3(Future<int>.value(42));
+  await v3;
+//      ^^
+// [analyzer] unspecified
+// [cfe] unspecified
 }
