f5a60b3169161f0b746e2e598524d9f1912b7dd5
sgrekhov <sgrekhov22@gmail.com>
2023-10-19 13:59:30 +0300

    Fixes #2317. Add tests for extension type type parameters in non-covariant position

---
+++ LanguageFeatures/Extension-types/superinterfaces_of_extension_type_A09_t02.dart
@@ -0,0 +1,28 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Assume that DV is an extension type declaration named Name, and
+/// V1 occurs as one of the <type>s in the <interfaces> of DV. In this case we
+/// say that V1 is a superinterface of DV.
+/// ...
+/// A compile-time error occurs if a type variable declared by DV occurs in a
+/// non-covariant position in V1.
+///
+/// @description Checks that it is a compile-time error if a type variable
+/// declared by `DV` occurs in a non-covariant position in `V1`.
+/// @author sgrekhov22@gmail.com
+/// @issue 53803
+
+// SharedOptions=--enable-experiment=inline-class
+
+extension type ET1<T>(int _) {}
+
+extension type ET2<T>(int _) implements ET1<void Function(T)> {}
+//             ^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+main() {
+  print(ET2);
+}
