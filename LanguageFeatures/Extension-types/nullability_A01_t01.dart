532c2ae2d9d29ad3a1be6fcf9a456f9e134004ac
sgrekhov <sgrekhov22@gmail.com>
2023-10-20 15:48:06 +0300

    More nullability tests

--- LanguageFeatures/Extension-types/nullability_A01_t01.dart
+++ LanguageFeatures/Extension-types/nullability_A01_t01.dart
@@ -25,6 +25,8 @@ extension type ET4(void _) {}
 
 extension type ET5(FutureOr<Null> _) {}
 
+extension type ET6(Never _) {}
+
 main() {
   ET1 et11 = null;
 //           ^^^^
@@ -55,4 +57,10 @@ main() {
 // [analyzer] unspecified
 // [cfe] unspecified
   ET5? et52 = null;
+
+  ET6 et61 = null;
+//           ^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  ET6? et62 = null;
 }
