3d8e931412a97962593b9d9df8004e39226a1291
sgrekhov <sgrekhov22@gmail.com>
2023-11-01 13:43:17 +0400

    Fixes #2339. Add tests that extension can't have name `type`

---
+++ LanguageFeatures/Extension-types/syntax_A13_t01.dart
@@ -0,0 +1,37 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion A rule for <extensionTypeDeclaration> is added to the grammar,
+/// along with some rules for elements used in extension type declarations:
+///
+/// <extensionTypeDeclaration> ::=
+///   'extension' 'type' 'const'? <typeIdentifier> <typeParameters>?
+///   <representationDeclaration> <interfaces>?
+///   '{'
+///     (<metadata> <extensionTypeMemberDeclaration>)*
+///   '}'
+///
+/// <representationDeclaration> ::=
+///   ('.' <identifierOrNew>)? '(' <metadata> <type> <identifier> ')'
+///
+/// <identifierOrNew> ::= <identifier> | 'new'
+///
+/// <extensionTypeMemberDeclaration> ::= <classMemberDefinition>
+///
+/// @description Checks that it is not an error if an extension type has the
+/// name `type` or 'on'
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=inline-class
+
+import "../../Utils/expect.dart";
+
+extension type type(int id) {}
+
+extension type on(int id) {}
+
+main() {
+  Expect.equals(42, type(42).id);
+  Expect.equals(42, on(42).id);
+}
