4f27f32080202247ff1155435f208c72e379cd52
sgrekhov <sgrekhov22@gmail.com>
2023-11-14 17:45:48 +0200

    Implement review recommendations

--- Language/Classes/Constructors/Factories/accessible_class_A01_t03.dart
+++ Language/Classes/Constructors/Factories/accessible_class_A01_t03.dart
@@ -12,15 +12,17 @@
 /// const? factory
 ///     N(T1 x1 . . . , Tn xn, {Tn+1 xn+1=d1, . . . , Tn+k xn+k=dk}) = R;
 /// where R is of one of the forms T<S1 . . . , Sp> or T<S1 . . . , Sp>.id.
-/// ...
-/// The redirectee constructor for this declaration is the constructor k′
-/// denoted by R.
-/// ...
-/// It is a compile error if the function type of k' is not a subtype of the
-/// type of k.
 ///
-/// @description Checks that it is a compile-time error is if a factory
-/// constructor redirects to a null
+/// It is a compile-time error if T does not denote a class accessible in the
+/// current scope. If T does denote such a class D, it is a compile-time error
+/// if R does not denote a constructor. Otherwise, it is a compile-time error if
+/// R denotes a generative constructor and D is abstract.
+///
+/// @description Checks that a compile error is produced if `T` does not denote
+/// a class accessible in the current scope.
+///
+/// @description Checks that it is a compile-time error if `T` does not denote a
+/// class accessible in the current scope
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=inline-class
--- Language/Classes/Constructors/Factories/return_type_A03_t01.dart
+++ Language/Classes/Constructors/Factories/return_type_A03_t01.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion It is a dynamic type error if a factory returns a non-null object
-/// whose type is not a subtype of its actual return type.
+/// @assertion It is a dynamic type error if a factory returns an object whose
+/// type is not a subtype of its actual return type.
 ///
 /// @description Checks that it is a dynamic type error if a factory returns a
 /// non-null object  whose type is not a subtype of its actual return type.
