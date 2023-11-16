4e7dbff6233949c5691b2f6bc72cdddb5bfb9c41
sgrekhov <sgrekhov22@gmail.com>
2023-11-16 10:34:36 +0200

    #2371. Add tests for factory constructor generating an enum value

---
+++ LanguageFeatures/Enhanced-Enum/factory_constructor_A01_t01.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if the constructor implicitly invoked
+/// by an enumerated enum value declaration is a factory constructor.
+///
+/// @description Check that It is a compile-time error if the constructor
+/// implicitly invoked by an enumerated enum value declaration is a factory
+/// constructor
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=inline-class
+
+enum E1 {
+  e1,
+  e2.foo();
+//   ^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  const E1();
+  const factory E1.foo() = ET1.foo;
+}
+
+extension type const ET1(E1 it) implements E1 {
+  const ET1.foo() : this(E1.e1);
+}
+
+void main() {
+  print(identical(E1.e1, E1.e2));
+}
---
+++ LanguageFeatures/Enhanced-Enum/factory_constructor_A01_t02.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if the constructor implicitly invoked
+/// by an enumerated enum value declaration is a factory constructor.
+///
+/// @description Check that It is a compile-time error if the constructor
+/// implicitly invoked by an enumerated enum value declaration is a factory
+/// constructor
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=inline-class
+
+enum E {
+  e1.foo();
+//   ^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  const E();
+  const factory E.foo() = ET.foo;
+}
+
+extension type const ET(E it) implements E {
+  const ET.foo() : this(E.e1);
+}
+
+void main() {
+  print(E.e1);
+}
