1338f625d77e450b18a5171af423698b2bf695e1
sgrekhov <sgrekhov22@gmail.com>
2023-12-19 16:03:08 +0200

    #2419. Add missing int ~/ double constants expression

--- Language/Expressions/Constants/math_operators_t09.dart
+++ Language/Expressions/Constants/math_operators_t09.dart
@@ -79,11 +79,12 @@ class C {
     aq = 5 ~/ 1,
     ar = 0.2 ~/ 2,
     as = 1 ~/ (2 + 1);
+    at = 3 ~/ 1.1;
 
     final a, b, c, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w,
       x, y, z,
       aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar,
-      as;
+      as, at;
 }
 
 main() {
