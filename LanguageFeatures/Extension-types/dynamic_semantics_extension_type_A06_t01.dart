d855f0e12434e001ca2377a765a10c8cecbd7aab
sgrekhov <sgrekhov22@gmail.com>
2024-01-02 12:05:01 +0400

    #2420. Add more dynamic semantics extension type tests

---
+++ LanguageFeatures/Extension-types/dynamic_semantics_extension_type_A06_t01.dart
@@ -0,0 +1,61 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Let R0 be the extension type erasure of V0. At run time, the type
+/// V0 has the same representation and semantics as R0. In particular, they
+/// behave identically with respect to is, is!, as, and ==, both when V0 and R0
+/// are used as types, and when they occur as subterms of another type.
+///
+/// @description Check that an extension type erasure is not performed
+/// statically outside of switch expressions and statements
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=inline-class
+
+extension type BoolET1(bool _) {}
+extension type BoolET2(bool _) implements bool {}
+extension type const ConstBoolET1(bool _) {}
+extension type const ConstBoolET2(bool _) implements bool {}
+
+main() {
+  var _false = BoolET1(false);
+  var _true = BoolET2(true);
+  const True = ConstBoolET1(true);
+  const False = ConstBoolET2(false);
+
+  if (_true) {}
+  if (_false) {}
+//    ^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  if (False) {}
+  if (True) {}
+//    ^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  for (;_true;) {
+    break;
+  }
+  for (;_false;) {}
+//      ^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  switch (42) {
+    case _ when False:
+  }
+  switch (42) {
+    case _ when _false:
+//              ^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  }
+  var _ = switch(42) {
+    >0 when True => 0,
+//          ^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+    _ when _true => 1,
+    _ => 2
+  };
+}
---
+++ LanguageFeatures/Extension-types/dynamic_semantics_extension_type_A06_t04.dart
@@ -0,0 +1,35 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Let R0 be the extension type erasure of V0. At run time, the type
+/// V0 has the same representation and semantics as R0. In particular, they
+/// behave identically with respect to is, is!, as, and ==, both when V0 and R0
+/// are used as types, and when they occur as subterms of another type.
+///
+/// @description Check that at run time, an extension type has the same
+/// representation and semantics as its erasure
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=inline-class
+
+import '../../Utils/expect.dart';
+
+extension type IterableET1<T>(Iterable<T> id) {}
+extension type IterableET2<T>(Iterable<T> id) implements Iterable<T> {}
+extension type const ConstIterableET1<T>(Iterable<T> _) {}
+extension type const ConstIterableET2<T>(Iterable<T> _)
+    implements Iterable<T> {}
+
+
+main() {
+  dynamic l1 = IterableET1(<int>[]);
+  dynamic l2 = IterableET2(<int>[]);
+  const dynamic cl1 = ConstIterableET1(<int>[]);
+  const dynamic cl2 = ConstIterableET2(<int>[]);
+
+  Expect.identical(l1.id, l1);
+  Expect.identical(l2.id, l2);
+  Expect.identical(cl1.id, cl1);
+  Expect.identical(cl2.id, cl2);
+}
