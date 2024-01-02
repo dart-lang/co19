2ea77cad7fc789a8f06e904fdfd99c199477967c
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-01-02 13:32:10 +0200

    Fixes #2436. Add tests for external and redirecting constructors with initializing formals (#2460)

---
+++ Language/Classes/Constructors/Generative_Constructors/initializing_formal_parameter_A02_t03.dart
@@ -0,0 +1,35 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error for an initializing formal parameter
+/// to occur in a redirecting or external constructor.
+///
+/// @description Checks that it is a compile-time error when the initializing
+/// formal parameter occurs in an external constructor.
+/// @author sgrekhov22@gmail.com
+/// @issue 54485
+
+class C {
+  int x;
+
+  external C({required this.x});
+//            ^^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+enum E {
+  a(x: 1);
+
+  final int x;
+  external const E({required this.x});
+//                  ^^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  print(C);
+  print(E);
+}
