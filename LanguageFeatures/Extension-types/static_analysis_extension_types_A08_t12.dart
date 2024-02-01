c7fbf4465e493eb24a184fe5dc9ac8ca5cbe3bce
sgrekhov <sgrekhov22@gmail.com>
2024-02-01 12:46:33 +0200

    Fix #2490. Update incompatible with await tests according to the changed rule

--- LanguageFeatures/Extension-types/static_analysis_extension_types_A08_t12.dart
+++ LanguageFeatures/Extension-types/static_analysis_extension_types_A08_t12.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -11,24 +11,21 @@
 /// Consider an expression of the form await e. A compile-time error occurs if
 /// the static type of e is incompatible with await.
 ///
-/// @description Checks that it is not an error if `await e` occurs and the
-/// static type of `e` is `S?`, and `S` is compatible with await.
+/// @description Checks that it is not an error if `await e` occurs, and the
+/// static type of `e` is `X & B` and `B` does not derive a future type but
+/// compatible with await, and `X` is incompatible with await.
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=inline-class
 
-import "../../Utils/expect.dart";
+extension type NumET(num _) implements num {}
 
-extension type V1(Future<int> _) implements Future<int> {}
+test<X extends NumET>(X x) async {
+  if (x is int) {
+    await x;
+  }
+}
 
-extension type V2<T extends Future<Object>>(T _) implements Future<Object>{}
-
-main() async {
-  V1? v1 = V1(Future<int>.value(42));
-  var _v1 = await v1;
-  Expect.equals(42, _v1);
-
-  V2<Future<String>>? v2 = V2(Future<String>.value("42"));
-  var _v2 = await v2;
-  Expect.equals("42", _v2);
+main() {
+  test(NumET(42));
 }
