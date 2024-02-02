5125c01a89fb018a335c74e900ca82e98d58c7c4
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-02-02 13:42:40 +0200

    Fix #2490. Update incompatible with await tests according to the changed rule (#2516)
    Update incompatible with await tests according to the changed rule

--- LanguageFeatures/Extension-types/static_analysis_extension_types_A08_t07.dart
+++ LanguageFeatures/Extension-types/static_analysis_extension_types_A08_t07.dart
@@ -6,16 +6,13 @@
 /// of the following criteria holds:
 /// - T is an extension type that does not implement Future.
 /// - T is S?, and S is incompatible with await.
-/// - T is X & B, and either:
-///   - B is incompatible with await, or
-///   - B does not derive a future type, and X is incompatible with await.
+/// - T is X & B, and B is incompatible with await.
 /// - T is a type variable with bound S, and S is incompatible with await.
 /// Consider an expression of the form await e. A compile-time error occurs if
 /// the static type of e is incompatible with await.
 ///
 /// @description Checks that it is a compile-time error if `await e` occurs, and
-/// the static type of `e` is `X & B` and `B` does not derive a future type, and
-/// `X` is incompatible with await.
+/// the static type of `e` is `X & B` and `B` is incompatible with await.
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=inline-class
