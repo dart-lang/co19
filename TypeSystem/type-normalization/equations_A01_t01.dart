36429f75c000bc8a58e057b3a24cb3a5e3485cce
sgrekhov <sgrekhov22@gmail.com>
2024-02-07 10:42:11 +0200

    #2522. Move type-normalization tests to TypeSystem dir

---
+++ TypeSystem/type-normalization/equations_A01_t01.dart
@@ -0,0 +1,23 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion The NORM relation defines the canonical representative of classes
+/// of equivalent types...
+/// This is based on the following equations:
+///   T?? == T?
+///
+/// @description Checks that T?? == T?
+/// @author sgrekhov@unipro.ru
+
+// Requirements=nnbd-strong
+
+class T {}
+class A<X> {}
+class B<X> implements A<X?> {}
+
+class C extends B<T?> implements A<T?> {}
+
+main() {
+  new C();
+}
---
+++ TypeSystem/type-normalization/equations_A13_t01.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion The NORM relation defines the canonical representative of classes
+/// of equivalent types...
+/// This is based on the following equations:
+///   X extend Never == Never
+///
+/// @description Checks that X extend Never == Never
+/// @author sgrekhov@unipro.ru
+
+// Requirements=nnbd-strong
+
+class A<X> {}
+class B<X extends Never> implements A<X> {}
+
+class C extends B<Never> implements A<Never> {}
+
+main() {
+  new C();
+}
