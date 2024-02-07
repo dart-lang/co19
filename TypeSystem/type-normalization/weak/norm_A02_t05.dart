329af7188df8a7aa1d49b1c93cb2f8a3244c51e3
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-02-07 18:29:02 +0200

    #2522. Move type-normalization weak tests to TypeSystem dir (#2526)

---
+++ TypeSystem/type-normalization/weak/norm_A02_t05.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion
+/// NORM(FutureOr<T>) =
+///  let S be NORM(T)
+///  if S is a top type then S
+///  if S is Object then S
+///  if S is Object* then S
+///  if S is Never then Future<Never>
+///  if S is Null then Future<Null>?
+///  else FutureOr<S>
+///
+/// @description Checks that if NORM(T) is Null then
+/// NORM(FutureOr<T>) = Future<Null>?
+/// @author sgrekhov@unipro.ru
+
+// Requirements=nnbd-weak
+
+import 'dart:async';
+
+class A<X> {}
+class B<X> implements A<X> {}
+
+class C extends B<FutureOr<Null>> implements A<Future<Null>?> {}
+
+main() {
+  new C();
+}
