3402776262f44c72be688b2b3e6f360b6d2cab97
sgrekhov <sgrekhov22@gmail.com>
2024-03-05 13:31:58 +0200

    #2559. Add tests checking that `augment` is a built-in identifier

---
+++ Language/Expressions/Identifier_Reference/built_in_identifier_A26_t02.dart
@@ -0,0 +1,50 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a syntax error if a built-in identifier is used as the
+/// declared name of a prefix, class, mixin, enum, type parameter, type alias,
+/// or extension.
+///
+/// @description Checks that it is a compile-time error if a built-in identifier
+/// `augment` is used as the declared name of a type variable.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+class C<augment> {
+//      ^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+mixin M<augment> {
+//      ^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+enum E<augment> {
+//     ^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  e1;
+}
+
+void foo<augment>() {}
+//       ^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+extension Ext<augment> on List {}
+//            ^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+main() {
+  print(C);
+  print(M);
+  print(E);
+  print(foo);
+  print(List);
+}
