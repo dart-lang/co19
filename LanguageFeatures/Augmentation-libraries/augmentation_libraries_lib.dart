379f6f89cc067032c7236351b870d83ef46f3ba5
sgrekhov <sgrekhov22@gmail.com>
2024-03-06 09:17:06 +0200

    #2559. Add tests for augment libraries visibility

---
+++ LanguageFeatures/Augmentation-libraries/augmentation_libraries_lib.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @description Common library for augmentation libraries tests
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+library augmentation_libraries_lib;
+
+class AL {}
---
+++ LanguageFeatures/Augmentation-libraries/defining_augmentation_A02_t03_lib.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Augmentations do not share an import scope with the main library
+/// or each other. The libraries one augmentation imports are visible only to
+/// that file.
+///
+/// @description Checks that imports in an augmentation library are not visible
+/// in another augmentation library
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+library augment 'defining_augmentation_A02_t03_main.dart';
+
+import 'augmentation_libraries_lib.dart';
