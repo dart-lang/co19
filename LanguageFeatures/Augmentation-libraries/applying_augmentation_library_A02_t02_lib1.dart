587a44d3d6fda6f5e3189dd02b82a49a93ffd138
sgrekhov <sgrekhov22@gmail.com>
2024-03-08 10:39:41 +0700

    #2559. Add applying augmentation library tests

---
+++ LanguageFeatures/Augmentation-libraries/applying_augmentation_library_A02_t02_lib1.dart
@@ -0,0 +1,23 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if:
+/// ...
+/// - The library referenced in a `library augment` directive does not have an
+///   `import augment` directive pointing back to this augmentation.
+///
+/// @description Check that it is a compile-time error if a library referenced
+/// in an `library augment` directive is not an does not have a `import augment`
+/// directive pointing back to this augmentation.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+library augment 'applying_augmentation_library_A02_t02.dart';
+//               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+// 'applying_augmentation_library_A02_t02.dart' is pointing back to
+// 'applying_augmentation_library_A02_t02_lib2.dart' but not to this library
