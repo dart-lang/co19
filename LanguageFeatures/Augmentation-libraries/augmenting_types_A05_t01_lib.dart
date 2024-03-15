07f7a4d3e874b6bb40c836f2943fa4e728adb761
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-03-16 05:00:00 +0700

    #2559. Add augmenting types tests. Part 4 (#2571)

---
+++ LanguageFeatures/Augmentation-libraries/augmenting_types_A05_t01_lib.dart
@@ -0,0 +1,27 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if:
+/// ...
+/// - The type parameters of the type augmentation do not match the original
+///   type's type parameters. This means there must be the same number of type
+///   parameters with the same bounds and names.
+///
+/// @description Checks that it is a compile-time error if an augmenting type
+/// declares wrong number of type parameters
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+library augment 'augmenting_types_A05_t01.dart';
+
+augment class C {}
+//            ^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+augment class C<T, X> {}
+//              ^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
