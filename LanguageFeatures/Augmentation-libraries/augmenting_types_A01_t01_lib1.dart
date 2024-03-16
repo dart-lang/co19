31b4631b54f56ea4bb33ad3c1565b844befbe7a2
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-03-16 19:03:40 +0700

    #2559. Add augmenting types tests. Part 1 (#2568)

---
+++ LanguageFeatures/Augmentation-libraries/augmenting_types_A01_t01_lib1.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if:
+///
+/// - The augmenting type and corresponding type are not the same kind: class,
+///   mixin, enum, or extension. You can't augment a class with a mixin, etc.
+///
+/// @description Checks that it is a compile-time error if an augmenting type
+/// and the corresponding type are not the same kind
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+library augment 'augmenting_types_A01_t01.dart';
+
+augment mixin C {}
+//            ^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+augment class M {}
+//            ^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+augment class E {}
+//            ^
+// [analyzer] unspecified
+// [cfe] unspecified
