de5a71e24f88128bd3219ccb04f729f0bc29f951
sgrekhov <sgrekhov22@gmail.com>
2024-04-02 11:57:19 +0300

    Update assert, expect errors where they textually occurs

--- LanguageFeatures/Augmentation-libraries/augmenting_types_A06_t05_lib.dart
+++ LanguageFeatures/Augmentation-libraries/augmenting_types_A06_t05_lib.dart
@@ -2,13 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion A class, enum, extension, extension type, or mixin augmentation
-/// may specify extends, implements, on, and with clauses (when generally
-/// supported). The types in these clauses are appended to the original
-/// declarations clauses of the same kind, and if that clause did not exist
-/// previously then it is added with the new types. All regular rules apply
-/// after this appending process, so you cannot have multiple extends on a
-/// class, or an on clause on an enum, etc
+/// @assertion A class, enum, extension type, or mixin augmentation may specify
+/// extends, implements, on, and with clauses (when generally supported). The
+/// types in these clauses are appended to the original declarations clauses of
+/// the same kind, and if that clause did not exist previously then it is added
+/// with the new types. All regular rules apply after this appending process, so
+/// you cannot have multiple extends on a class, or an on clause on an enum, etc
 ///
 /// @description Checks that it is a compile-time error if a class, extension
 /// type, mixin or enum augment specifies `implements` clause but doesn't
@@ -20,7 +19,13 @@
 augment library 'augmenting_types_A06_t05.dart';
 
 augment class C implements I {}
+//            ^
+// [analyzer] unspecified
+// [cfe] unspecified
 
 augment mixin M implements I {}
 
-augment enum E implements I {}
+augment enum E implements I {e1;}
+//           ^
+// [analyzer] unspecified
+// [cfe] unspecified
