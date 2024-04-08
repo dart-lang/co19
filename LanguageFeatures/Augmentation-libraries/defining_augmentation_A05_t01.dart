7b90f92ae278571208288801e84dd0aa84ca1c7c
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-04-08 16:46:53 +0300

    #2559. Update/add new defining augmentation tests (#2595)
    Update/add new defining augmentation tests

--- LanguageFeatures/Augmentation-libraries/defining_augmentation_A05_t01.dart
+++ LanguageFeatures/Augmentation-libraries/defining_augmentation_A05_t01.dart
@@ -2,7 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion It is a compile-time error if a library tries to augment itself
+/// @assertion It is a compile-time error if:
+/// ...
+/// - There is a cycle in the graph whose edges are the import augment
+///   directives of an augmented library and of any augmentation libraries which
+///   are directly or indirectly reachable from there via said edges.
 ///
 /// @description Checks that it is a compile-time error if a library tries to
 /// augment itself
---
+++ LanguageFeatures/Augmentation-libraries/defining_augmentation_A07_t01_lib.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if:
+/// ...
+/// - An import augment directive has a <uri> that denotes an entity which is
+///   not a library augmentation
+///
+/// @description Checks that it is a compile-time error if an `import augment`
+/// directive has an <uri> that denotes an entity which is not a library
+/// augmentation
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+library defining_augmentation_A07_t01_lib;
