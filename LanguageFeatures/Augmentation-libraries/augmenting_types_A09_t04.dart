835fa65c73b0b94890e49d095aa95c7caf674011
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-04-08 15:36:59 +0300

    #2559. Add more augmenting types tests for `with` clause (#2594)

---
+++ LanguageFeatures/Augmentation-libraries/augmenting_types_A09_t04.dart
@@ -0,0 +1,38 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion A class, enum, extension type, or mixin augmentation may specify
+/// extends, implements, on, and with clauses (when generally supported). The
+/// types in these clauses are appended to the original declarations clauses of
+/// the same kind, and if that clause did not exist previously then it is added
+/// with the new types. All regular rules apply after this appending process, so
+/// you cannot have multiple extends on a class, or an on clause on an enum, etc
+///
+/// @description Checks that it is a compile-time error if augment of a class or
+/// enum specifies a `with` clause which is incompatible with existing members
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+import augment 'augmenting_types_A09_t04_lib.dart';
+
+abstract interface class I {
+  int get foo;
+}
+
+mixin M implements I {}
+
+abstract class C {
+  String foo();
+}
+
+enum E {
+  e1;
+  String foo() => "E";
+}
+
+main() {
+  print(C);
+  print(E);
+}
