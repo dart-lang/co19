1d318c29c90efa64daf90f7d34b222d2af2c7834
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-04-09 11:19:27 +0300

    #2559. Add augmenting types tests. Part 3 (#2570)
    Add augmenting types tests. Part 3

---
+++ LanguageFeatures/Augmentation-libraries/augmenting_types_A03_t01.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if:
+/// ...
+/// - The augmenting type declares an extends clause, but one was already
+///   present. We don't allow overwriting an existing extends, but one can be
+///   filled in if it wasn't present originally.
+///
+/// @description Checks that it is a compile-time error if an augmenting type
+/// declares an extends clause, but one was already present
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+import augment 'augmenting_types_A03_t01_lib.dart';
+
+class A {}
+class B extends A {}
+class C extends B {}
+
+main() {
+  print(C);
+}
---
+++ LanguageFeatures/Augmentation-libraries/augmenting_types_A03_t03_lib.dart
@@ -0,0 +1,38 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if:
+/// ...
+/// - The augmenting type declares an extends clause, but one was already
+///   present. We don't allow overwriting an existing extends, but one can be
+///   filled in if it wasn't present originally.
+///
+/// @description Checks that it is a compile-time error if an augmenting type
+/// declares an extends clause, but one was already present. Test
+/// `extends Object` case
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+augment library 'augmenting_types_A03_t03.dart';
+
+augment class C1 extends Object {}
+//               ^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+augment class C1 extends ObjectAlias {}
+//               ^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+augment class C2 extends Object {}
+//               ^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+augment class C2 extends ObjectAlias {}
+//               ^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
