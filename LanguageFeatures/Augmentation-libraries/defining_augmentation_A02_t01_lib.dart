425e7efae5335eab349451eed3f96d0420e0cdd2
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-04-09 11:44:10 +0300

    #2559. Add tests for augment libraries visibility (#2561)
    Add tests for augment libraries visibility{

---
+++ LanguageFeatures/Augmentation-libraries/defining_augmentation_A02_t01_lib.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Augmentations do not share an import scope with the main library
+/// or each other. The libraries one augmentation imports are visible only to
+/// that file.
+///
+/// @description Checks that imports in an augmentation library are not visible
+/// in a main library
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+augment library 'defining_augmentation_A02_t01.dart';
+
+import 'augmentation_libraries_lib.dart';
+
+class C implements AL {}
---
+++ LanguageFeatures/Augmentation-libraries/defining_augmentation_A02_t06_augmented_lib.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Augmentations do not share an import scope with the main library
+/// or each other. The libraries one augmentation imports are visible only to
+/// that file.
+///
+/// @description Checks that if augment library does exports then augmented
+/// library implicitly exports it
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+import augment 'defining_augmentation_A02_t06_lib.dart';
