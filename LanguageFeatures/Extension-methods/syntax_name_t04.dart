2ab67e8c0a2be525d8f96daa85af8c4b5225bab0
sgrekhov <sgrekhov22@gmail.com>
2024-05-06 15:22:12 +0300

    Fixes #2638. Add extension methods test for type aliases

--- LanguageFeatures/Extension-methods/syntax_name_t04.dart
+++ LanguageFeatures/Extension-methods/syntax_name_t04.dart
@@ -2,22 +2,20 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion an extension declaration is a top-level declaration with a grammar
-/// similar to:
+/// @assertion an extension declaration is a top-level declaration with a
+/// grammar similar to:
 /// <extension> ::=
 ///   `extension' <identifier>? <typeParameters>? `on' <type> `?'? `{'
 ///     memberDeclaration*
 ///   `}'
 /// Such a declaration introduces its name (the identifier) into the surrounding
 /// scope. The name does not denote a type, but it can be used to denote the
-/// extension itself in various places. The name can be hidden or shown in import
-/// or export declarations.
+/// extension itself in various places. The name can be hidden or shown in
+/// import export declarations.
 ///
 /// @description Check that the name can be hidden or shown in import or export
 /// declarations.
 /// @author sgrekhov@unipro.ru
-
-
 
 import "../../Utils/expect.dart";
 import "my_fancy_list_lib.dart" show MyFancyList;
--- LanguageFeatures/Extension-methods/syntax_name_t05.dart
+++ LanguageFeatures/Extension-methods/syntax_name_t05.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -13,18 +13,14 @@
 /// extension itself in various places. The name can be hidden or shown in
 /// import export declarations.
 ///
-/// @description Check that it is a compile-time error to create a type alias
-/// for an extension
-/// @author sgrekhov22@gmail.com
+/// @description Check that the name does not denote a type
+/// @author sgrekhov@unipro.ru
 
-class A {}
-extension Ext on A {}
-
-typedef ExtAlias = Ext;
-//                 ^^^
-// [analyzer] unspecified
-// [cfe] unspecified
+extension MyFancyList<T> on List<T> {}
 
 main() {
-  print(A);
+  MyFancyList<String> list = ["Lily", "was", "here"];
+//^
+// [analyzer] unspecified
+// [cfe] unspecified
 }
