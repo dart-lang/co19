9d8ff49c2d186a985c4d42ff87e0a4f49bb36020
sgrekhov <sgrekhov22@gmail.com>
2024-05-07 11:45:15 +0300

    #2142. Add tests checking type parameter vs representation variable name conflict

---
+++ LanguageFeatures/Extension-types/syntax_A14_t01.dart
@@ -0,0 +1,39 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion A rule for <extensionTypeDeclaration> is added to the grammar,
+/// along with some rules for elements used in extension type declarations:
+///
+/// <extensionTypeDeclaration> ::=
+///   'extension' 'type' 'const'? <typeIdentifier> <typeParameters>?
+///   <representationDeclaration> <interfaces>?
+///   '{'
+///     (<metadata> <extensionTypeMemberDeclaration>)*
+///   '}'
+///
+/// <representationDeclaration> ::=
+///   ('.' <identifierOrNew>)? '(' <metadata> <type> <identifier> ')'
+///
+/// <identifierOrNew> ::= <identifier> | 'new'
+///
+/// <extensionTypeMemberDeclaration> ::= <classMemberDefinition>
+///
+/// @description Checks that it is a compile-time error if representation
+/// variable of an extension type has the same name as one of type parameters
+/// @author sgrekhov22@gmail.com
+
+extension type ET1<T>(int T) {}
+//                        ^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+extension type ET2<X, Y extends num>(int Y) {}
+//                                       ^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+main() {
+  print(ET1);
+  print(ET2);
+}
