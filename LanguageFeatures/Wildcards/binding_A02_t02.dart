aaffc6f809f18b6bc17558f2ff4d8f892141ee32
sgrekhov <sgrekhov22@gmail.com>
2024-05-13 17:15:59 +0200

    Implement review recommendations

--- LanguageFeatures/Wildcards/binding_A02_t02.dart
+++ LanguageFeatures/Wildcards/binding_A02_t02.dart
@@ -22,19 +22,34 @@ class C<T> {
   const C.constConstructor3(T _, T _, [T? _, T? _]);
 
   factory C.factoryConstructor1(_, int _, var _) = C.constructor1;
-  factory C.factoryConstructor2(_, int _, var _, [_, int _ = 0, var _ = ""])
+  factory C.factoryConstructor2(_, int _, var _, [_, int _, var _])
       = C.constructor2;
   factory C.factoryConstructor3(T _, T _, [T? _, T? _]) = C.constructor3;
+
+  C.redirectingConstructor1(_, int _, var _) : this.constConstructor1(1, 2, 3);
+  C.redirectingConstructor2(_, int _, var _, [_, int _ = 0, var _ = ""])
+      : this.constConstructor1(1, 2, 3);
+  C.redirectingConstructor3(T _, T _, [T? _, T? _])
+      : this.constructor1(1, 2, 3);
 }
 
 enum E<T> {
   e1.constructor1("1", 2, "3"),
   e2.constructor2("4", 5, "6", 7, 8, "9"),
-  e3<int>.constructor3(1, 2, null, 4])();
+  e3<int>.constructor3(1, 2, null, 4),
+  e4.redirectingConstructor1(1, 2, 3),
+  e5.redirectingConstructor2(1, 2, 3, 4, 5),
+  e6<int>.redirectingConstructor3(1, 2, 3);
 
   const E.constructor1(_, int _, var _);
   const E.constructor2(_, int _, var _, [_, int _ = 0, var _ = ""]);
   const E.constructor3(T _, T _, [T? _, T? _]);
+
+  const E.redirectingConstructor1(_, int _, var _) : this.constructor1(1, 2, 3);
+  const E.redirectingConstructor2(_, int _, var _, [_, int _ = 0, var _ = ""])
+      : this.constructor1(1, 2, 3);
+  const E.redirectingConstructor3(T _, T _, [T? _, T? _])
+      : this.constructor1(1, 2, 3);
 }
 
 extension type ET<T>(int id) {
@@ -48,8 +63,14 @@ extension type ET<T>(int id) {
 
   factory ET.factoryConstructor1(int _, int _, var _) = ET.constructor1;
   factory ET.factoryConstructor2(
-      int _, int _, var _, [_, int _ = 0, var _ = ""]) = ET.constructor2;
+      int _, int _, var _, [_, int _, var _]) = ET.constructor2;
   factory ET.factoryConstructor3(int _, T _, [T? _, T? _]) = ET.constructor3;
+
+  ET.redirectingConstructor1(_, int _, var _) : this.constConstructor1(1, 2, 3);
+  ET.redirectingConstructor2(_, int _, var _, [_, int _ = 0, var _ = ""])
+      : this.constConstructor1(1, 2, 3);
+  ET.redirectingConstructor3(T _, T _, [T? _, T? _])
+      : this.constructor1(1, 2, 3);
 }
 
 main() {
@@ -62,16 +83,22 @@ main() {
   C.factoryConstructor1("1", 2, true);
   C.factoryConstructor2("1", 2, true, "3", 4, "5");
   C<int>.factoryConstructor3(1, 2, 3, 4);
+  C.redirectingConstructor1("1", 2, true);
+  C.redirectingConstructor2("1", 2, true, "3", 4, "5");
+  C<int>.redirectingConstructor3(1, 2, 3, 4);
 
   print(E);
 
   ET.constructor1(1, 2, "3");
   ET.constructor2(1, 2, true, "3", 4, "5");
   ET<int>.constructor3(1, 2, 3, 4);
-  const ET.constConstructor1("1", 2, true);
-  const ET.constConstructor2("1", 2, true, "3", 4, "5");
+  const ET.constConstructor1(1, 2, true);
+  const ET.constConstructor2(1, 2, true, "3", 4, "5");
   const ET<int>.constConstructor3(1, 2, null, 4);
-  ET.factoryConstructor1("1", 2, true);
-  ET.factoryConstructor2("1", 2, true, "3", 4, "5");
+  ET.factoryConstructor1(1, 2, true);
+  ET.factoryConstructor2(1, 2, true, "3", 4, "5");
   ET<String>.factoryConstructor3(1, "2", null, "4");
+  ET.redirectingConstructor1("1", 2, true);
+  ET.redirectingConstructor2("1", 2, true, "3", 4, "5");
+  ET<int>.redirectingConstructor3(1, 2, 3, 4);
 }
--- LanguageFeatures/Wildcards/binding_A02_t03.dart
+++ LanguageFeatures/Wildcards/binding_A02_t03.dart
@@ -14,26 +14,27 @@
 
 import '../../Utils/expect.dart';
 
-typedef int F1(_, int _, var _);
-typedef int F2(_, int _, var _, [_, int _ = 0, var _ = ""]);
+typedef int F1(_, int _);
+typedef int F2(_, int _, [_, int _]);
 typedef int F3<T>(T _, T _, [T? _, T? _]);
 
-typedef F4 = int Function(_, int _, var _);
-typedef F5 = int Function(_, int _, var _, [_, int _ = 0, var _ = ""]);
-typedef F6 = int Function<T>(T _, T _, [T? _, T? _]);
+typedef F4 = int Function(dynamic _, int _);
+typedef F5 = int Function(dynamic _, int _, [dynamic _, int _]);
+typedef F6<T> = int Function(T _, T _, [T? _, T? _]);
 
 main() {
-  F1 f1 = (_, int _, var _) => 1;
-  F2 f2 = (_, int _, var _, [_, int _ = 0, var _ = ""]) => 2;
+  // Let's check that type aliases above work as intended
+  F1 f1 = (_, int _) => 1;
+  F2 f2 = (var _, int _, [var _, int _ = 0]) => 2;
   F3<int> f3 = (int _, int _, [int? _, int? _]) => 3;
-  F4 f4 = (_, int _, var _) => 4;
-  F5 f5 = (_, int _, var _, [_, int _ = 0, var _ = ""]) => 5;
+  F4 f4 = (_, int _) => 4;
+  F5 f5 = (var _, int _, [var _, int _ = 0]) => 5;
   F6<int> f6 = (int _, int _, [int? _, int? _]) => 6;
 
-  Expect.equals(1, f1(1, 2, 3));
-  Expect.equals(2, f2(1, 2, 3, 4, 5, "6"));
+  Expect.equals(1, f1(1, 2));
+  Expect.equals(2, f2(1, 2, 3, 4));
   Expect.equals(3, f3(1, 2, null, 4));
-  Expect.equals(4, f4(1, 2, 3));
-  Expect.equals(5, f5(1, 2, 3, 4, 5, "6"));
+  Expect.equals(4, f4(1, 2));
+  Expect.equals(5, f5(1, 2, 3, 4));
   Expect.equals(6, f6(1, 2, null, 4));
 }
