7b552456f79389f2e9dc4d82383d81be635cf580
sgrekhov <sgrekhov22@gmail.com>
2024-05-15 09:39:24 +0200

    #2641. Add tests for unchanged type declarations named `_`

---
+++ LanguageFeatures/Wildcards/unchanged_A03_t04.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Other declarations: top-level variables, top-level function
+/// names, type names, member names, etc. are unchanged. They can be named `_`
+/// as they are today.
+///
+/// @description Checks that a type named `_` is still accessible and can be
+/// used.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=wildcard-variables
+
+extension type _(int id) {}
+
+main() {
+  print(_);
+}
