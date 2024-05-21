54eef7954074220a9db1c891b4b5f39e9d6799f0
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-05-21 13:26:18 +0300

    #2641. Check that it is still an error to declare more than one extension member named _ (#2676)

---
+++ LanguageFeatures/Wildcards/other_declarations_A05_t42.dart
@@ -0,0 +1,41 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Other declarations: top-level variables, top-level function
+/// names, type names, member names, etc. are unchanged. They can be named `_`
+/// as they are today.
+///
+/// @description Checks that it is a compile-time error to declare more than one
+/// member named `_`. Test an instance getter vs other extension members
+/// conflict.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=wildcard-variables
+
+class A {}
+
+extension E1 on A {
+  int get _ => 1;
+  int _() => 1;
+//    ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+extension E2 on A {
+  int get _ => 1;
+  int get _ => 6;
+//        ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+extension E3 on A {
+  int get _ => 1;
+  void set _(int v) {} // Ok
+}
+
+main() {
+  print(A);
+}
