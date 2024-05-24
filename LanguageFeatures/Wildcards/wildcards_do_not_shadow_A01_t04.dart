924d5d58f41e3f830ca0f5aaca07dcaf3429926e
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-05-24 10:45:21 +0300

    #2641. Add wildcards do not shadow tests (#2683)
    Add wildcards do not shadow tests

---
+++ LanguageFeatures/Wildcards/wildcards_do_not_shadow_A01_t04.dart
@@ -0,0 +1,50 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Here is an interesting example:
+/// ```
+/// class C {
+///   var _ = 'field';
+///
+///   test() {
+///     var _ = 'local';
+///
+///     _ = 'assign';
+///   }
+/// }
+/// ```
+// This program is valid and assigns to the field, not the local.
+///
+/// @description Checks that a local variable named `_` doesn't shadow a
+/// top-level method named `_` but a class field does.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=wildcard-variables
+
+import '../../Utils/expect.dart';
+
+String _log = "";
+
+void _(String v) {_log = "top-level $v";}
+
+test() {
+  var _ = "";
+  _ = "assign1";
+  Expect.equals("top-level assign1", _log);
+}
+
+class C {
+  var _ = "field";
+
+  void test() {
+    _ = "assign2";
+    Expect.equals("assign2", _);
+  }
+}
+
+main() {
+  test();
+  C().test();
+  Expect.equals("top-level assign1", _log);
+}
---
+++ LanguageFeatures/Wildcards/wildcards_do_not_shadow_A02_t01.dart
@@ -0,0 +1,42 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Here is an interesting example:
+/// ```
+/// class C {
+///   var _ = 'field';
+///
+///   test() {
+///     var _ = 'local';
+///
+///     _ = 'assign';
+///   }
+/// }
+/// ```
+// This program is valid and assigns to the field, not the local.
+///
+/// @description Checks that a top-level variable named `_` can be assigned to
+/// a local variable named `_`.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=wildcard-variables
+
+import '../../Utils/expect.dart';
+
+int _ = 42;
+
+test1() {
+  int _ = _;
+  Expect.equals(42, _);
+}
+
+test2() {
+  var _ = _++;
+  Expect.equals(43, _);
+}
+
+main() {
+  test1();
+  test2();
+}
