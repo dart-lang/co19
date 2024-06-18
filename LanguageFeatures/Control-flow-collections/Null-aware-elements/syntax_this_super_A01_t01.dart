e44ca42e758a2bbcfebb4e12beebedca6f8963db
sgrekhov <sgrekhov22@gmail.com>
2024-06-18 20:14:33 +0300

    #2694. Rename syntax tests for expressions

--- LanguageFeatures/Control-flow-collections/Null-aware-elements/syntax_this_super_A01_t01.dart
+++ LanguageFeatures/Control-flow-collections/Null-aware-elements/syntax_this_super_A01_t01.dart
@@ -25,6 +25,7 @@ import '../../../Utils/expect.dart';
 class A {
   String? foo(int i) => "A.foo($i)";
   String? operator [](int index) => "A[$index]";
+  String? call(int? v) => "A($v)";
 }
 
 class C extends A {
@@ -32,44 +33,64 @@ class C extends A {
     var l = [
       ?this.foo(1),
       ?this[1],
+      ?this(1),
       ?super.foo(2),
-      ?super[2]
+      ?super[2],
+      ?super(2)
     ];
-    Expect.listEquals(["A.foo(1)", "A[1]", "A.foo(2)", "A[2]"], l);
+    Expect.listEquals(
+        ["A.foo(1)", "A[1]", "A(1)", "A.foo(2)", "A[2]", "A(2)"], l);
+
     var s = {
       ?this.foo(1),
       ?this[1],
+      ?this(1),
       ?super.foo(2),
-      ?super[2]
+      ?super[2],
+      ?super(2)
     };
-    Expect.setEquals(["A.foo(1)", "A[1]", "A.foo(2)", "A[2]"], s);
+    Expect.setEquals(
+        {"A.foo(1)", "A[1]", "A(1)", "A.foo(2)", "A[2]", "A(2)"}, s);
+
     var m = {
-      ?this.foo(1): "",
-      ?this[1]: "",
-      ?super.foo(2): "",
-      ?super[2]: "",
+      ?this.foo(1): "1",
+      ?this[1]: "2",
+      ?this(1): "3",
+      ?super.foo(2): "4",
+      ?super[2]: "5",
+      ?super(2): "6",
       "1": ?this.foo(1),
       "2": ?this[1],
-      "3": ?super.foo(2),
-      "4": ?super[2],
+      "3": ?this(1),
+      "4": ?super.foo(2),
+      "5": ?super[2],
+      "6": ?super(2),
       ?this.foo(3): ?this.foo(3),
       ?this[3]: ?this[3],
+      ?this(3): ?this(3),
       ?super.foo(4): ?super.foo(4),
-      ?super[4]: ?super[4]
+      ?super[4]: ?super[4],
+      ?super(4): ?super(4)
     };
     Expect.mapEquals({
-      "A.foo(1)": "",
-      "A[1]": "",
-      "A.foo(2)": "",
-      "A[2]": "",
+      "A.foo(1)": "1",
+      "A[1]": "2",
+      "A(1)": "3",
+      "A.foo(2)": "4",
+      "A[2]": "5",
+      "A(2)": "6",
       "1": "A.foo(1)",
       "2": "A[1]",
-      "3": "A.foo(2)",
-      "4": "A[2]",
+      "3": "A(1)",
+      "4": "A.foo(2)",
+      "5": "A[2]",
+      "6": "A(2)",
       "A.foo(3)": "A.foo(3)",
       "A[3]": "A[3]",
+      "A(3)": "A(3)",
       "A.foo(4)": "A.foo(4)",
-      "A[4]": "A[4]"
+      "A[4]": "A[4]",
+      "A(4)": "A(4)"
     }, m);
   }
 }
