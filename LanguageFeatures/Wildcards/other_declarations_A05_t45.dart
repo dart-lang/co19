cd57dbc81e54a5928a4671153aff4bcf55161cb6
sgrekhov <sgrekhov22@gmail.com>
2024-06-28 11:11:53 +0300

    Fixes #2529. Add test checking wildcard parameter with no default value

---
+++ LanguageFeatures/Wildcards/other_declarations_A05_t45.dart
@@ -0,0 +1,142 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Other declarations: top-level variables, top-level function
+/// names, type names, member names, etc. are unchanged. They can be named `_`
+/// as they are today.
+///
+/// @description Checks that it is still a compile-time error to declare a
+/// function with an optional non-nullable wildcard parameter with no default
+/// value.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=wildcard-variables
+
+void topLevelFunction1([int _]) {}
+//                          ^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+void topLevelFunction2({int _}) {}
+//                          ^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+class C {
+  static void staticMethod1([int _]) {}
+//                               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void staticMethod2({int _}) {}
+//                               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void instanceMethod1([int _]) {}
+//                          ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void instanceMethod2({int _}) {}
+//                          ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+mixin M {
+  static void staticMethod1([int _]) {}
+//                               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void staticMethod2({int _}) {}
+//                               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void instanceMethod1([int _]) {}
+//                          ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void instanceMethod2({int _}) {}
+//                          ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+enum E {
+  e0;
+  static void staticMethod1([int _]) {}
+//                               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void staticMethod2({int _}) {}
+//                               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void instanceMethod1([int _]) {}
+//                          ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void instanceMethod2({int _}) {}
+//                          ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+exnetsion type ET(int i) {
+  static void staticMethod1([int _]) {}
+//                               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void staticMethod2({int _}) {}
+//                               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void instanceMethod1([int _]) {}
+//                          ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void instanceMethod2({int _}) {}
+//                          ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+class A {}
+
+extension Ext on A {
+  static void staticMethod1([int _]) {}
+//                               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void staticMethod2({int _}) {}
+//                               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void instanceMethod1([int _]) {}
+//                          ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void instanceMethod2({int _}) {}
+//                          ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  var f1 = ([int _]) {};
+//               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  var f2 = ({int _}) {};
+//               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  print(topLevelFunction1);
+  print(topLevelFunction2);
+  print(C);
+  print(M);
+  print(E);
+  print(ET);
+  print(A);
+}
