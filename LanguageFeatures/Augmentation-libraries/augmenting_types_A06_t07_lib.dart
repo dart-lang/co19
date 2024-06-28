49576f52267b646bde497608a17b5c9e8d4480cf
sgrekhov <sgrekhov22@gmail.com>
2024-06-28 15:38:01 +0300

    Remove excessive parameter

--- LanguageFeatures/Augmentation-libraries/augmenting_types_A06_t07_lib.dart
+++ LanguageFeatures/Augmentation-libraries/augmenting_types_A06_t07_lib.dart
@@ -22,6 +22,3 @@ augment library 'augmenting_types_A06_t07.dart';
 augment class C1 extends A {}
 
 augment abstract class C2 extends A {}
-
-// [error line 28, column 47]
-// [analyzer] unspecified
