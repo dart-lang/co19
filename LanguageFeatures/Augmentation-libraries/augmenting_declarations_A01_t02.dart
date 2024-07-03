0538504c6224914764bef0e7ad22c2ca2e568d48
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-07-03 10:28:59 +0300

    #2559. Add augmenting declarations tests (#2565)
    Add merge order and augmenting declarations tests

---
+++ LanguageFeatures/Augmentation-libraries/augmenting_declarations_A01_t02.dart
@@ -0,0 +1,23 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if:
+/// - An augmenting declaration has no corresponding original declaration to
+///   apply to.
+///
+/// @description Checks that it is a compile-time error if an augmenting
+/// declaration has no corresponding original declaration to apply to
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+augment void foo() {}
+//           ^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+main() {
+  void foo() {}
+  print(foo);
+}
