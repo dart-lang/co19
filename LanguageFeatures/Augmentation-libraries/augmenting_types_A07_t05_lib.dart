4cf262e1f73092f2d4e52a3ed5744eefa7faafed
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-07-26 14:52:24 +0200

    #2559. Update assertions in augmenting types tests. Part 3 (#2784)

--- LanguageFeatures/Augmentation-libraries/augmenting_types_A07_t05_lib.dart
+++ LanguageFeatures/Augmentation-libraries/augmenting_types_A07_t05_lib.dart
@@ -10,19 +10,16 @@
 /// after this appending process, so you cannot have multiple `extends` on a
 /// class, or an `on` clause on an enum, etc.
 ///
-/// @description Checks that it is a compile-time error if a class, extension
-/// type, mixin or enum augment specifies `implements` clause but doesn't
-/// implement this interface
+/// @description Checks that an augment of an extension type may specify a base,
+/// final or sealed class in an `implements` clause
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=macros
 
-augment library 'augmenting_types_A07_t03.dart';
+augment library 'augmenting_types_A07_t05.dart';
 
-augment class C implements I {}
+augment extension type ET1(FinalClass _) implements FinalClass {}
 
-augment mixin M implements I {}
+augment extension type ET2(BaseClass _) implements BaseClass {}
 
-augment enum E implements I {
-  augment e1;
-}
+augment extension type ET3(SealedClass _) implements SealedClass {}
