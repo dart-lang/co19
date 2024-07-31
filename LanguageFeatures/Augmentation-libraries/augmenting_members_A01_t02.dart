c54aff0e6e90f2ef487221f14547e8dc6670344c
sgrekhov <sgrekhov22@gmail.com>
2024-07-31 13:31:51 +0300

    #2559. Add tests for `augmented` expression in abstract and external members

---
+++ LanguageFeatures/Augmentation-libraries/augmenting_members_A01_t02.dart
@@ -0,0 +1,61 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if:
+/// ...
+/// An augmenting declaration uses `augmented` when the augmented
+/// declaration has no concrete implementation. Note that all external
+/// declarations are assumed to have an implementation provided by another
+/// external source, and otherwise they will throw a runtime error when called.
+/// An `abstract` variable introduces no implementation.
+///
+/// @description Checks that it is a compile-time error if an augmenting
+/// declaration uses `augmented` when the augmented declaration has no concrete
+/// implementation. Test static members.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+import augment 'augmenting_members_A01_t02_lib.dart';
+
+class C {
+  static void abstractMethod();
+  static String get abstractGetter;
+  static void set abstractSetter(String v);
+}
+
+mixin M {
+  static void abstractMethod();
+  static String get abstractGetter;
+  static void set abstractSetter(String v);
+}
+
+enum E {
+  e0;
+  static void abstractMethod();
+  static String get abstractGetter;
+  static void set abstractSetter(String v);
+}
+
+class A {}
+
+extension Ext on A {
+  static void abstractMethod();
+  static String get abstractGetter;
+  static void set abstractSetter(String v);
+}
+
+extension type ET(String _) {
+  static void abstractMethod();
+  static String get abstractGetter;
+  static void set abstractSetter(String v);
+}
+
+main() {
+  print(C);
+  print(M);
+  print(E);
+  print(A);
+  print(ET);
+}
---
+++ LanguageFeatures/Augmentation-libraries/augmenting_members_A01_t03_lib.dart
@@ -0,0 +1,126 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if:
+/// ...
+/// An augmenting declaration uses `augmented` when the augmented
+/// declaration has no concrete implementation. Note that all external
+/// declarations are assumed to have an implementation provided by another
+/// external source, and otherwise they will throw a runtime error when called.
+/// An `abstract` variable introduces no implementation.
+///
+/// @description Checks that it is a not an error if an augmenting
+/// declaration uses `augmented` when the augmented declaration is an external.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+augment library 'augmenting_members_A01_t03.dart';
+import '../../Utils/expect.dart';
+
+String _log = "";
+
+augment class C {
+  augment String externalMethod() {
+    Expect.throws(() {augmented();});
+    return "Augmented";
+  }
+  augment String get externalGetter {
+    Expect.throws(() {augmented;});
+    return "Augmented";
+  }
+  augment void set externalSetter(String v) {
+    Expect.throws(() {augmented = v;});
+    _log = "externalSetter = $v";
+  }
+  augment String get externalVariable {
+    Expect.throws(() {augmented;});
+    return "Augmented";
+  }
+  augment void set externalVariable(String v) {
+    Expect.throws(() {augmented = v;});
+    _log = "externalVariable = $v";
+  }
+  augment String get externalFinalVariable {
+    Expect.throws(() {augmented;});
+    return "Augmented";
+  }
+}
+
+augment mixin M {
+  augment String externalMethod() {
+    Expect.throws(() {augmented();});
+    return "Augmented";
+  }
+  augment String get externalGetter {
+    Expect.throws(() {augmented;});
+    return "Augmented";
+  }
+  augment void set externalSetter(String v) {
+    Expect.throws(() {augmented = v;});
+    _log = "externalSetter = $v";
+  }
+  augment String get externalVariable {
+    Expect.throws(() {augmented;});
+    return "Augmented";
+  }
+  augment void set externalVariable(String v) {
+    Expect.throws(() {augmented = v;});
+    _log = "externalVariable = $v";
+  }
+  augment String get externalFinalVariable {
+    Expect.throws(() {augmented;});
+    return "Augmented";
+  }
+}
+
+augment enum E {
+  augment e0;
+  augment String externalMethod() {
+    Expect.throws(() {augmented();});
+    return "Augmented";
+  }
+  augment String get externalGetter {
+    Expect.throws(() {augmented;});
+    return "Augmented";
+  }
+  augment void set externalSetter(String v) {
+    Expect.throws(() {augmented = v;});
+    _log = "externalSetter = $v";
+  }
+  augment String get externalFinalVariable {
+    Expect.throws(() {augmented;});
+    return "Augmented";
+  }
+}
+
+augment extension Ext {
+  augment String externalMethod() {
+    Expect.throws(() {augmented();});
+    return "Augmented";
+  }
+  augment String get externalGetter {
+    Expect.throws(() {augmented;});
+    return "Augmented";
+  }
+  augment void set externalSetter(String v) {
+    Expect.throws(() {augmented = v;});
+    _log = "externalSetter = $v";
+  }
+}
+
+augment extension type ET(String _) {
+  augment String externalMethod() {
+    Expect.throws(() {augmented();});
+    return "Augmented";
+  }
+  augment String get externalGetter {
+    Expect.throws(() {augmented;});
+    return "Augmented";
+  }
+  augment void set externalSetter(String v) {
+    Expect.throws(() {augmented = v;});
+    _log = "externalSetter = $v";
+  }
+}
