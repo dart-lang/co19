cfc73fa203dd8acceede40c6a822328ae7d652f4
sgrekhov <sgrekhov22@gmail.com>
2024-08-19 16:25:00 +0300

    #2559. Add constructor augmentation tests. Part 7. Redirecting generative constructors

---
+++ LanguageFeatures/Augmentation-libraries/augmenting_constructors_A18_t01_lib.dart
@@ -0,0 +1,28 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Redirecting generative constructors
+/// ...
+/// It is a compile-time error if:
+/// - The augmented constructor has any initializers or a body.
+///
+/// @description Checks that it is a compile-time error to declare an augmenting
+/// redirecting generative constructor if the augmented constructor has
+/// initialising formals.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+augment library 'augmenting_constructors_A18_t01.dart';
+
+augment class C {
+  augment C.foo(this.x, {this.y}): this(x);
+//              ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  augment C.bar({required this.y}): this.foo(0);
+//               ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
---
+++ LanguageFeatures/Augmentation-libraries/augmenting_constructors_A18_t02.dart
@@ -0,0 +1,28 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Redirecting generative constructors
+/// ...
+/// It is a compile-time error if:
+/// - The augmented constructor has any initializers or a body.
+///
+/// @description Checks that it is a compile-time error to declare an augmenting
+/// redirecting generative constructor if the augmented constructor has a
+/// non-empty initializer list.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+import augment 'augmenting_constructors_A18_t02_lib.dart';
+
+class C {
+  int x, y;
+  C(this.x, [this.y = 0]);
+  C.foo(int x, {int y = 0}): assert(x > 0);
+  C.bar({required int x}): x = x, y = 0;
+}
+
+main() {
+  print(C);
+}
