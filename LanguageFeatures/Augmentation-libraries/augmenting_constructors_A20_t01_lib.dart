6f237f122ded16abb0c9ca1e1103c5ef8e4886f3
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-08-20 17:58:00 +0300

    #2559. Add constructor augmentation tests. Part 8. Redirecting factory constructors (#2826)
    Add constructor augmentation tests. Part 8. Redirecting factory constructors. Note that several tests use declarations of factory constructors with no body (and no redirection), even though that is currently a syntax error. The grammar will be adjusted to allow them.

---
+++ LanguageFeatures/Augmentation-libraries/augmenting_constructors_A20_t01_lib.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Redirecting factory constructors
+/// ...
+/// It is a compile-time error if:
+/// - The augmented constructor has a body.
+///
+/// @description Checks that it is a compile-time error if the augmented
+/// constructor has a body.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+augment library 'augmenting_constructors_A20_t01.dart';
+
+augment class C {
+  augment factory C.bar(int x, [int y]) = C;
+//                                      ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  augment factory C.baz(int x, {int y}) = C.foo;
+//                                      ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  augment factory C.qux(int x, [int y]) = D;
+//                                      ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
