81db6926c87981f0ba2530fa03379a999a729f7b
sgrekhov <sgrekhov22@gmail.com>
2024-08-21 11:00:43 +0300

    Fix syntax error.

--- LanguageFeatures/Augmentation-libraries/augmenting_constructors_A16_t03_lib.dart
+++ LanguageFeatures/Augmentation-libraries/augmenting_constructors_A16_t03_lib.dart
@@ -31,7 +31,7 @@ augment class C {
     Expect.equals(1, c2.x);
     Expect.equals(1, c2.y);
     return augmented(x + 1, y + 1);
-  };
+  }
 
   augment factory C.bar(int x, {int y = 0}) {
     var c1 = augmented(0);
@@ -42,7 +42,7 @@ augment class C {
     Expect.equals(1, c2.x);
     Expect.equals(1, c2.y);
     return augmented(x + 1, y: y + 1);
-  };
+  }
 
   augment factory C.bar({required int x}) {
     var c1 = augmented(x: 0);
@@ -50,7 +50,7 @@ augment class C {
     Expect.equals(0, c1.x);
     Expect.equals(0, c1.y);
     return augmented(x: x + 1);
-  };
+  }
 }
 
 augment extension type ET {
@@ -59,26 +59,26 @@ augment extension type ET {
     et1.expectStaticType<Exactly<ET>>();
     Expect.equals(0, et11.id);
     return augmented(x + 1);
-  };
+  }
 
   augment factory ET.bar([int x]) {
     var et1 = augmented(1);
     et1.expectStaticType<Exactly<ET>>();
     Expect.equals(1, et1.id);
     return augmented(x + 1);
-  };
+  }
 
   augment factory ET.baz({int x}) {
     var et1 = augmented(x: 1);
     et1.expectStaticType<Exactly<ET>>();
     Expect.equals(1, et1.id);
     return augmented(x: x + 1);
-  };
+  }
 
   augment factory ET.qux({required int x}) {
     var et1 = augmented(x: 1);
     et1.expectStaticType<Exactly<ET>>();
     Expect.equals(1, et1.id);
     return augmented(x: x + 1);
-  };
+  }
 }
