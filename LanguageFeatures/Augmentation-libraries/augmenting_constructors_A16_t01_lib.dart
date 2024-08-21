76a4e2cad802eaadc12550d09268243c8d715994
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-08-21 15:39:29 +0300

    #2559. Add constructor augmentation tests. Part 6. Non-redirecting factory constructors (#2821)
    Add constructor augmentation tests. Part 6. Non-redirecting factory constructors

---
+++ LanguageFeatures/Augmentation-libraries/augmenting_constructors_A16_t01_lib.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion A non-redirecting factory constructor marked `augment` works in
+/// the same way as a normal function augmentation.
+/// If it has a body, it replaces the body of the augmented constructor
+/// (if present), and it may invoke the augmented body by calling
+/// `augmented(arguments)`.
+///
+/// @description Checks that augmenting non-redirecting factory constructor may
+/// add a body to an introductory factory constructor.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+augment library 'augmenting_constructors_A16_t01.dart';
+
+augment class C {
+  augment factory C.foo() => C(0);
+}
+
+augment extension type ET {
+  augment factory ET.foo() => ET(0);
+}
