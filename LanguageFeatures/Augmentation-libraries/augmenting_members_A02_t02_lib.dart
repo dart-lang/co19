e80817a90877482912acc52af28592142ec4506d
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-09-02 18:57:29 +0300

    #2559. Add augmenting members tests. Part 2 (#2799)
    Add augmenting members tests. Part 2

---
+++ LanguageFeatures/Augmentation-libraries/augmenting_members_A02_t02_lib.dart
@@ -0,0 +1,65 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion It is a compile-time error if:
+/// ...
+/// A non-writable variable declaration is augmented with a setter.
+///
+/// @description Checks that it is a compile-time error if an implicitly induced
+/// getter of a `late final` variable declaration with an initializer is
+/// augmented with a setter.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+augment library 'augmenting_members_A02_t02.dart';
+
+/**/augment void set topLevelVariable(String _) {}
+//  ^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+augment class C {
+  augment static void set staticVariable(String _) {}
+//^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  augment void set instanceVariable(String _) {}
+//^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+augment mixin M {
+  augment static void set staticVariable(String _) {}
+//^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  augment void set instanceVariable(String _) {}
+//^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+augment enum E {
+  augment e0;
+  augment static void set staticVariable(String _) {}
+//^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+augment extension Ext {
+  augment static void set staticVariable(String _) {}
+//^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+augment extension type ET {
+  augment static void set staticVariable(String _) {}
+//^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
---
+++ LanguageFeatures/Augmentation-libraries/augmenting_members_A04_t04.dart
@@ -0,0 +1,84 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion A final variable can be augmented with a non-final augmenting
+/// variable, and that will not add any setter. An augmenting variable
+/// declaration only affects the initializer expression, not setters.
+///
+/// @description Checks that if a final variable is augmented with a non-final
+/// augmenting variable then it is still a compile-time error to write to it.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+import augment 'augmenting_members_A04_t04_lib.dart';
+
+final String topLevelVariable = "topLevelVariable";
+
+class C {
+  static final String staticVariable = "staticVariable";
+  final String instanceVariable = "instanceVariable";
+}
+
+mixin M {
+  static final String staticVariable = "staticVariable";
+  final String instanceVariable = "instanceVariable";
+}
+
+enum E {
+  e0;
+  static final String staticVariable = "staticVariable";
+  final String instanceVariable = "staticVariable";
+}
+
+class A {}
+
+extension Ext on A {
+  static final String staticVariable = "staticVariable";
+}
+
+extension type ET(String _) {
+  static final String staticVariable = "staticVariable";
+}
+
+class MA = Object with M;
+
+main() {
+  topLevelVariable = "";
+//^^^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  C.staticVariable = "";
+//  ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  C().instanceVariable = "";
+//    ^^^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  M.staticVariable = "";
+//  ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  MA().instanceVariable = "";
+//     ^^^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  E.staticVariable = "";
+//  ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  E.e0.instanceVariable = "";
+//     ^^^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  Ext.staticVariable = "";
+//  ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  ET.staticVariable = "";
+//  ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
