994a06bc701338721f35bd16adca46cb08544587
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-09-23 18:50:20 +0300

    #2846. Add unquoted imports tests for keywords (#2886)
    Add unquoted imports tests for keywords

---
+++ LanguageFeatures/Unquoted-imports/test_package2/lib/keywords/when.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion We allow all reserved words and identifiers, including built-in
+/// identifiers and contextual keywords as path components.
+///
+/// @description Library file to test identifier `when`.
+/// @author sgrekhov22@gmail.com
+
+String libraryId = "when";
