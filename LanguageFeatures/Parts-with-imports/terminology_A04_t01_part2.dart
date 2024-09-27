7f3439d87e1b683aca827ce5941f36bd468cdd72
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-09-27 14:04:20 +0300

    #2825. Add more grammar and terminology tests (#2885)

--- LanguageFeatures/Parts-with-imports/terminology_A04_t01_part2.dart
+++ LanguageFeatures/Parts-with-imports/terminology_A04_t01_part2.dart
@@ -2,11 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion It’s a compile-time error if a `part` directive denotes a file
-/// which is not a part file.
+/// @assertion It’s a compile-time error if a part file is a sub-part of itself.
+/// That is, if the includes relation has a cycle.
 ///
-/// @description Check that it is a compile-time error if a `part` directive
-/// denotes a file which is not a part file.
+/// @description Check that it is a compile-time error if a part file is a
+/// sub-part of itself.
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=enhanced-parts
---
+++ LanguageFeatures/Parts-with-imports/terminology_A05_t02.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion A Dart file is either a library file or a part file, each having
+/// its own grammar.
+///
+/// @description Checks that it is a compile-time error if an `import` refers to
+/// an entity which is not a library
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+part 'terminology_A05_t02_lib1.dart';
+part 'terminology_A05_t02_lib2.dart';
+
+main() {
+}
