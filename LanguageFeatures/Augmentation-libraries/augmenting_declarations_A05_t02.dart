018582933a22aab3cf0ab4fccc3a21e760949eb0
sgrekhov <sgrekhov22@gmail.com>
2024-10-01 12:24:22 +0300

    #2559. Add tests for adding body to static members

---
+++ LanguageFeatures/Augmentation-libraries/augmenting_declarations_A05_t02.dart
@@ -0,0 +1,77 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Because of augmentations, non-abstract class, mixin, mixin class,
+/// enum, extension type, and extension declarations are now allowed to contain
+/// abstract member declarations, as long as those members are equipped with a
+/// body by an augmentation declaration.
+///
+/// @description Checks that it is not an error if a body of an abstract static
+/// member is provided by an augmentation.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+import '../../Utils/expect.dart';
+part 'augmenting_declarations_A05_t02_lib.dart';
+
+class C {
+  static String abstractMethod();
+  static String get abstractGetter;
+  static void set abstractSetter(String v);
+}
+
+mixin M {
+  static String abstractMethod();
+  static String get abstractGetter;
+  static void set abstractSetter(String v);
+}
+
+enum E {
+  e0;
+  static String abstractMethod();
+  static String get abstractGetter;
+  static void set abstractSetter(String v);
+}
+
+class A {}
+
+extension Ext on A {
+  static String abstractMethod();
+  static String get abstractGetter;
+  static void set abstractSetter(String v);
+}
+
+extension type ET(String _) {
+  static String abstractMethod();
+  static String get abstractGetter;
+  static void set abstractSetter(String v);
+}
+
+main() {
+  Expect.equals("abstractMethod", C.abstractMethod());
+  Expect.equals("abstractGetter", C.abstractGetter);
+  C.abstractSetter = "a";
+  Expect.equals("abstractSetter = a", _log);
+
+  Expect.equals("abstractMethod", M.abstractMethod());
+  Expect.equals("abstractGetter", M.abstractGetter);
+  M.abstractSetter = "b";
+  Expect.equals("abstractSetter = b", _log);
+
+  Expect.equals("abstractMethod", E.abstractMethod());
+  Expect.equals("abstractGetter", E.abstractGetter);
+  E.abstractSetter = "c";
+  Expect.equals("abstractSetter = c", _log);
+
+  Expect.equals("abstractMethod", Ext.abstractMethod());
+  Expect.equals("abstractGetter", Ext.abstractGetter);
+  Ext.abstractSetter = "d";
+  Expect.equals("abstractSetter = d", _log);
+
+  Expect.equals("abstractMethod", ET.abstractMethod());
+  Expect.equals("abstractGetter", ET.abstractGetter);
+  ET.abstractSetter = "e";
+  Expect.equals("abstractSetter = e", _log);
+}
---
+++ LanguageFeatures/Augmentation-libraries/augmenting_declarations_A05_t04.dart
@@ -0,0 +1,103 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Because of augmentations, non-abstract class, mixin, mixin class,
+/// enum, extension type, and extension declarations are now allowed to contain
+/// abstract member declarations, as long as those members are equipped with a
+/// body by an augmentation declaration.
+///
+/// @description Checks that it is still a compile-time error if a body of an
+/// abstract member of a non-abstract class is not provided by an augmentation.
+/// Test static members.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+part 'augmenting_declarations_A05_t04_lib.dart';
+
+class C {
+  static String abstractMethod();
+//                              ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static String get abstractGetter;
+//                                ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void set abstractSetter(String v);
+//                                        ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+mixin M {
+  static String abstractMethod();
+//                              ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static String get abstractGetter;
+//                                ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void set abstractSetter(String v);
+//                                        ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+enum E {
+  e0;
+  static String abstractMethod();
+//                              ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static String get abstractGetter;
+//                                ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void set abstractSetter(String v);
+//                                        ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+class A {}
+
+extension Ext on A {
+  static String abstractMethod();
+//                              ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static String get abstractGetter;
+//                                ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void set abstractSetter(String v);
+//                                        ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+extension type ET(String _) {
+  static String abstractMethod();
+//                              ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static String get abstractGetter;
+//                                ^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void set abstractSetter(String v);
+//                                        ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  print(C);
+  print(M);
+  print(E);
+  print(A);
+  print(ET);
+}
