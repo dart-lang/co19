4cbb92c2477ad793162a4b0aff1fb6efb088e1a9
sgrekhov <sgrekhov22@gmail.com>
2024-10-03 11:40:08 +0300

    Fixes #2906. Add digit separators 'parse' tests

---
+++ LanguageFeatures/Digit-separators/parse_A01_t01.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion `num.parse()/tryParse()`, `int.parse()/tryParse()` and
+/// `double.parse()/tryParse()` don't accept separators.
+///
+/// @description Check that it is a run-time error if string, parsed by
+/// `int.parse()/tryParse()` contains `_`.
+/// @author sgrekhov22@gmail.com
+
+import '../../Utils/expect.dart';
+
+main() {
+  Expect.throws(() {
+    int.parse("1_2");
+  });
+  Expect.throws(() {
+    int.parse("3__4");
+  });
+  Expect.throws(() {
+    int.parse("0x5_6", radix: 16);
+  });
+  Expect.throws(() {
+    int.parse("0x7__8", radix: 16);
+  });
+  Expect.isNull(int.tryParse("1_2"));
+  Expect.isNull(int.tryParse("3__4"));
+  Expect.isNull(int.tryParse("0x5_6", radix: 16));
+  Expect.isNull(int.tryParse("0x7__8", radix: 16));
+}
---
+++ LanguageFeatures/Digit-separators/parse_A03_t03.dart
@@ -0,0 +1,56 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion `num.parse()/tryParse()`, `int.parse()/tryParse()` and
+/// `double.parse()/tryParse()` don't accept separators.
+///
+/// @description Check that it is a run-time error if string, parsed by
+/// `num.parse()/tryParse()` contains `_`.
+/// @author sgrekhov22@gmail.com
+
+import '../../Utils/expect.dart';
+
+main() {
+  Expect.throws(() {
+    num.parse("-1_2");
+  });
+  Expect.throws(() {
+    num.parse("-3__4");
+  });
+  Expect.throws(() {
+    num.parse("-0x5_6");
+  });
+  Expect.throws(() {
+    num.parse("-0x7__8");
+  });
+
+  Expect.throws(() {
+    num.parse("-5_6.7_8");
+  });
+  Expect.throws(() {
+    num.parse("-9__0.1__2");
+  });
+  Expect.throws(() {
+    num.parse("-1.2e1_0");
+  });
+  Expect.throws(() {
+    num.parse("-1.2_3e+1");
+  });
+  Expect.throws(() {
+    num.parse("-1_2.3e-1");
+  });
+
+  Expect.isNull(num.tryParse("-1_2"));
+  Expect.isNull(num.tryParse("-3__4"));
+  Expect.isNull(num.tryParse("-0x5_6"));
+  Expect.isNull(num.tryParse("-0x7__8"));
+
+  Expect.isNull(num.tryParse("-1_2"));
+  Expect.isNull(num.tryParse("-3__4"));
+  Expect.isNull(num.tryParse("-5_6.7_8"));
+  Expect.isNull(num.tryParse("-9__0.1__2"));
+  Expect.isNull(num.tryParse("-1.2e1_0"));
+  Expect.isNull(num.tryParse("-1.2_3e+1"));
+  Expect.isNull(num.tryParse("-1_2.3e-1"));
+}
