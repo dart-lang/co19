03e4f17f90ac1fafc4846cadb8dfdd38d6643c1e
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-11-19 14:52:31 +0200

    Fixes #2983. Change expected error in generic_parameter_inference_A01_t05.dart (#2985)
    * Fixes #2983. Change expected error in generic_parameter_inference_A01_t05.dart
    
    * Change to unspecified

--- LanguageFeatures/Extension-methods/generic_parameter_inference_A01_t05.dart
+++ LanguageFeatures/Extension-methods/generic_parameter_inference_A01_t05.dart
@@ -5,12 +5,13 @@
 /// @assertion If both the extension and the method is generic, then inference
 /// must infer the extension type parameters first, to figure out whether the
 /// extension applies, and only then start inferring method type parameters. As
-/// mentioned above, the inference is similar to other cases of chained inference.
-/// @description Check that two-step type inference forces correct type parameter
-/// choice and error is thrown if method parameter is of incorrect type.
+/// mentioned above, the inference is similar to other cases of chained
+/// inference.
+///
+/// @description Check that two-step type inference forces correct type
+/// parameter choice and error is reported if method parameter is of incorrect
+/// type.
 /// @author iarkh@unipro.ru
-
-
 
 extension SuperList<T> on List<T> {
   void checkme1<R>(R i) {}
@@ -23,11 +24,10 @@ main() {
   list.checkme1<int>(42);
   list.checkme2(42);
 //     ^^^^^^^^
-// [analyzer] COMPILE_TIME_ERROR.COULD_NOT_INFER
-// [cfe] Inferred type argument 'int' doesn't conform to the bound 'T' of the type variable 'R' on 'SuperList|checkme2'.
+// [analyzer] unspecified
+// [cfe] unspecified
   list.checkme2<int>(42);
-//     ^
-// [cfe] Type argument 'int' doesn't conform to the bound 'T' of the type variable 'R' on 'SuperList|checkme2'.
 //              ^^^
-// [analyzer] COMPILE_TIME_ERROR.TYPE_ARGUMENT_NOT_MATCHING_BOUNDS
+// [analyzer] unspecified
+// [cfe] unspecified
 }
