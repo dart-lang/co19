3e8abbe2b077d10be6eddcb29d645099d86ee7cb
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-12-16 16:21:13 +0200

    #2559. Add more augmented expression tests (#3016)

---
+++ LanguageFeatures/Augmentation-libraries/augmented_expression_A11_t28.dart
@@ -0,0 +1,61 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion The exact result of an `augmented` expression depends on what is
+/// being augmented, but it generally follows the same rules as any normal
+/// identifier:
+/// ...
+/// - Augmenting non-redirecting generative constructors: Unlike other
+///   functions, `augmented` has no special meaning in non-redirecting
+///   generative constructors. It is still a reserved word inside the body of
+///   these constructors, since they are within the scope of an augmenting
+///   declaration.
+///
+/// @description Checks that it is a compile-time error for an augmenting
+/// non-redirecting generative constructor to initialize a member named
+/// `augmented` in the initializer list.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+class C {
+  int augmented;
+  C(this.augmented);
+}
+
+augment class C {
+  C.foo() : augmented = 1;
+//          ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+enum E {
+  a0(0);
+  final int augmented;
+  const E(this.augmented);
+}
+
+augment enum E {
+  a1.foo();
+  const E.foo() : augmented = 1;
+//                ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+extension type ET(int augmented) {}
+
+augment extension type ET {
+  ET.foo() : augmented = 1 {}
+//           ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  print(C);
+  print(E);
+  print(ET);
+}
