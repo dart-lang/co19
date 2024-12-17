d23755b04b17f2908fa9b2466b978046940c85a4
Sergey G. Grekhov <sgrekhov22@gmail.com>
2024-12-17 16:08:14 +0200

    #2559. Add metadata tests. Part 1. (#3017)
    Add metadata tests. Part 1.

---
+++ LanguageFeatures/Augmentation-libraries/augmented_expression_A03_t33.dart
@@ -0,0 +1,268 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion The exact result of an `augmented` expression depends on what is
+/// being augmented, but it generally follows the same rules as any normal
+/// identifier:
+/// ...
+/// - Augmenting fields: Within an augmenting variable declaration, `augmented`
+///   can only be used in an initializer expression, and refers to the augmented
+///   variable's initializing expression, which is immediately evaluated.
+///
+/// It is a compile-time error to use `augmented` in an augmenting variable's
+/// initializer if the member being augmented is not a variable declaration with
+/// an initializing expression.
+///
+/// @description Checks that it is a compile-time error if in an augmenting
+/// scope an augmenting field has a metadata named `augmented`.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+class augmented {
+  const augmented();
+}
+
+@augmented()
+var topLevelVariable = "Original";
+@augmented()
+final finalTopLevelVariable = "Original";
+
+@augmented()
+augment var topLevelVariable = "Augment";
+
+@augmented()
+augment final finalTopLevelVariable = "Augment";
+
+
+class C {
+  @augmented()
+  static var staticVariable1 = "Original";
+  @augmented()
+  static final finalStaticVariable1 = "Original";
+  @augmented()
+  var instanceVariable1 = "Original";
+  @augmented()
+  final finalInstanceVariable1 = "Original";
+
+  @augmented()
+  augment static var staticVariable1;
+  @augmented()
+  augment static final finalStaticVariable1;
+  @augmented()
+  augment var instanceVariable1;
+  @augmented()
+  augment final finalInstanceVariable1;
+}
+
+augment class C {
+  @augmented() augment static var staticVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() augment static final finalStaticVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() augment var instanceVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() augment final finalInstanceVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() static var staticVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() static final finalStaticVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() var instanceVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() final finalInstanceVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+mixin M {
+  @augmented()
+  static var staticVariable1 = "Original";
+  @augmented()
+  static final finalStaticVariable1 = "Original";
+  @augmented()
+  var instanceVariable1 = "Original";
+  @augmented()
+  final finalInstanceVariable1 = "Original";
+
+  @augmented()
+  augment static var staticVariable1;
+  @augmented()
+  augment static final finalStaticVariable1;
+  @augmented()
+  augment var instanceVariable1;
+  @augmented()
+  augment final finalInstanceVariable1;
+}
+
+augment mixin M {
+  @augmented() augment static var staticVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() augment static final finalStaticVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() augment var instanceVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() augment final finalInstanceVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() static var staticVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() static final finalStaticVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() var instanceVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() final finalInstanceVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+enum E {
+  e0;
+
+  @augmented()
+  static var staticVariable1 = "Original";
+  @augmented()
+  static final finalStaticVariable1 = "Original";
+  @augmented()
+  final finalInstanceVariable1 = "Original";
+
+  @augmented()
+  augment static var staticVariable1;
+  @augmented()
+  augment static final finalStaticVariable1;
+  @augmented()
+  augment final finalInstanceVariable1;
+}
+
+augment enum E {
+  e1;
+
+  @augmented() augment static var staticVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() augment static final finalStaticVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() augment final finalInstanceVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() static var staticVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() static final finalStaticVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() final finalInstanceVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+class A {}
+
+extension Ext on A {
+  @augmented()
+  static var staticVariable1 = "Original";
+  @augmented()
+  static final finalStaticVariable1 = "Original";
+
+  @augmented()
+  augment static var staticVariable1;
+  @augmented()
+  augment static final finalStaticVariable1;
+}
+
+augment extension Ext {
+  @augmented() augment static var staticVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() augment static final finalStaticVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() static var staticVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() static final finalStaticVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+extension type ET(int _) {
+  @augmented()
+  static var staticVariable1 = "Original";
+  @augmented()
+  static final finalStaticVariable1 = "Original";
+
+  @augmented()
+  augment static var staticVariable1;
+  @augmented()
+  augment static final finalStaticVariable1;
+}
+
+augment extension type ET {
+  @augmented() augment static var staticVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() augment static final finalStaticVariable1 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() static var staticVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() static final finalStaticVariable2 = "Augment";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  print(topLevelVariable);
+  print(finalTopLevelVariable);
+  print(C);
+  print(M);
+  print(E);
+  print(A);
+  print(ET);
+}
---
+++ LanguageFeatures/Augmentation-libraries/augmented_expression_A05_t26.dart
@@ -0,0 +1,130 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion The exact result of an `augmented` expression depends on what is
+/// being augmented, but it generally follows the same rules as any normal
+/// identifier:
+/// ...
+/// - Augmenting operators: When augmenting an operator, `augmented` refers to
+///   the augmented operator method, which must be immediately invoked using
+///   function call syntax. For example when augmenting `operator +` you would
+///   use `augmented(1)` to call the augmented operator, and when augmenting
+///   `operator []=` you would use the `augmented(key, value)` syntax.
+///
+/// @description Checks that it is a compile-time error if in an augmenting
+/// scope an augmenting operator has a metadata named `augmented`.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=macros
+
+class augmented {
+  const augmented();
+}
+
+class C {
+  @augmented()
+  String operator +(Object? other) => "Original + $other";
+
+  @augmented()
+  augment String operator +(Object? other);
+}
+
+augment class C {
+  @augmented() augment String operator +(Object? other) => "Augment + $other";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() String operator -(Object? other) => "Augment - $other";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+mixin M {
+  @augmented()
+  String operator +(Object? other) => "Original + $other";
+
+  @augmented()
+  augment String operator +(Object? other);
+}
+
+augment mixin M {
+  @augmented() augment String operator +(Object? other) => "Augment + $other";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() String operator -(Object? other) => "Augment - $other";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+enum E {
+  e0;
+  @augmented()
+  String operator +(Object? other) => "Original + $other";
+
+  @augmented()
+  augment String operator +(Object? other);
+}
+
+augment enum E {
+  e1;
+  @augmented() augment String operator +(Object? other) => "Augment + $other";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() String operator -(Object? other) => "Augment - $other";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+class A {}
+
+extension Ext on A {
+  @augmented()
+  String operator +(Object? other) => "Original + $other";
+
+  @augmented()
+  augment String operator +(Object? other);
+}
+
+augment extension Ext {
+  @augmented() augment String operator +(Object? other) => "Augment + $other";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() String operator -(Object? other) => "Augment - $other";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+extension type ET(int _) {
+  @augmented()
+  String operator +(Object? other) => "Original + $other";
+
+  @augmented()
+  augment String operator +(Object? other);
+}
+
+augment extension type ET {
+  @augmented() augment String operator +(Object? other) => "Augment + $other";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  @augmented() String operator -(Object? other) => "Augment - $other";
+// ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  print(C);
+  print(M);
+  print(E);
+  print(A);
+  print(ET);
+}
