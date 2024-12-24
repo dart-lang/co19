a1bc2c90f17fee23f0996bb3e7606e83ec7342ad
sgrekhov <sgrekhov22@gmail.com>
2024-12-24 13:14:03 +0200

    #2976. Add constant expressions tests

---
+++ LanguageFeatures/Static-access-shorthand/constant_expression_A07_t01.dart
@@ -0,0 +1,54 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Given an expression that is a prefix of
+/// `<staticMemberShorthandHead> <selector>*`, whose assigned shorthand context
+/// denotes a declaration D, and where the identifier or `new` of the
+/// `<staticMemberShorthandHead>` denotes a static declaration or constructor
+/// declaration S when looked up on D.
+/// ...
+/// - An expression of `.id(arguments)` or `.new(arguments)` is a constant
+///   expression if (and only if) it occurs in a constant context, S declares a
+///   constant constructor, every expression in arguments (which then occurs in
+///   a constant context too) is a constant expression, and inferred type
+///   arguments, if any, are all constant types.
+///
+/// @description Checks that an expression of the form `.id(arguments)` or
+/// `.new(arguments)` is not a constant expression if inferred type arguments
+/// are not constant types.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=enum-shorthands
+
+class C<X, Y> {
+  const C();
+  const C.id();
+}
+
+extension type const ET<X, T>(int _) {
+  const ET.id(this._);
+}
+
+void foo<T>() {
+  C<int, T> c1 = const .new();
+//                     ^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  C<T, int> c2 = const .id();
+//                     ^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  ET<int, T> et1 = const .new(0);
+//                       ^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  ET<T, int> et2 = const .id(0);
+//                       ^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  foo<int>();
+}
---
+++ LanguageFeatures/Static-access-shorthand/constant_expression_A09_t01.dart
@@ -0,0 +1,84 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Whether such an expression followed by more selectors is a
+/// constant expression depends on the concrete selectors and types, but can use
+/// the current rules which recursively asks about the receiver being a constant
+/// function.
+///
+/// @description Checks that a constant expression shorthand followed by `!`
+/// selector is still a constant.
+/// @author sgrekhov22@gmail.com
+/// @issue 59800
+
+// SharedOptions=--enable-experiment=enum-shorthands
+
+import '../../Utils/expect.dart';
+
+class C {
+  final String value;
+  const C(this.value);
+  const C.id(this.value);
+  const factory C.f(String value) = C;
+
+  static const C instance = const C("C instance");
+}
+
+mixin M on C {
+  static const M instance = const MC("M instance");
+}
+
+class MC extends C with M {
+  const MC(super.value);
+}
+
+enum E {
+  e0("e0");
+  final String value;
+  const E(this.value);
+
+  static const E instance = E.e0;
+}
+
+extension type const ET(int value) {
+  const ET.id(this.value);
+  const factory ET.f(int value) = ET;
+
+  static const zero = const ET(0);
+}
+
+main() {
+  const C c1 = const .new("one")!; // ignore: unnecessary_non_null_assertion
+  Expect.equals("one", c1.value);
+
+  const C c2 = const .id("two")!; // ignore: unnecessary_non_null_assertion
+  Expect.equals("two", c2.value);
+
+  const C c3 = const .f("three")!; // ignore: unnecessary_non_null_assertion
+  Expect.equals("three", c3.value);
+
+  const C c4 = .instance!; // ignore: unnecessary_non_null_assertion
+  Expect.equals("C instance", c4.value);
+
+  const M m = .instance!; // ignore: unnecessary_non_null_assertion
+  Expect.equals("M instance", m.value);
+
+  const E e1 = .e0!; // ignore: unnecessary_non_null_assertion
+  Expect.equals(e1, E.e0);
+
+  const E e2 = .instance!; // ignore: unnecessary_non_null_assertion
+  Expect.equals("e0", e2.value);
+
+  const ET et1 = const .new(1)!; // ignore: unnecessary_non_null_assertion
+  Expect.equals(1, et1.value);
+
+  const ET et2 = const .id(2)!; // ignore: unnecessary_non_null_assertion
+  Expect.equals(2, et2.value);
+
+  const ET et3 = const .f(3)!; // ignore: unnecessary_non_null_assertion
+  Expect.equals(3, et3.value);
+
+  const ET et4 = .zero!; // ignore: unnecessary_non_null_assertion
+  Expect.equals(0, et4.value);
+}
