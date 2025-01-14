1398986c060a347f9eaa9739e8a04da7236ed206
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-01-14 12:42:53 +0200

    #2976. Add patterns tests (#3031)
    Add patterns tests

---
+++ LanguageFeatures/Static-access-shorthand/patterns_A02_t11.dart
@@ -0,0 +1,108 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion If a static member shorthand expression occurs elsewhere in a
+/// pattern where a constant expression is generally allowed, like
+/// `const (big ? .big : .little)` or `< .one`, except for the relational
+/// pattern `== e`, it's treated as a normal constant expression, using the
+/// context type it's given. The expression of `const (...)` will have the
+/// matched value type as context type. The relational pattern expressions,
+/// other than for `==` and `!=`, will have the parameter type of the
+/// corresponding operator of the matched value type as context type.
+///
+/// @description Checks that if a static member shorthand expression occurs in a
+/// record pattern it's treated as a normal constant expression, using the
+/// context type it's given.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=enum-shorthands
+
+import '../../Utils/expect.dart';
+
+class C {
+  final int value;
+  const C(this.value);
+
+  static const C one = C(1);
+
+  @override
+  bool operator ==(Object other) {
+    if (other is C) {
+      return other.value == value;
+    }
+    return false;
+  }
+}
+
+mixin M on C {
+  static const M one = MC(1);
+}
+
+class MC extends C with M {
+  const MC(super.value);
+}
+
+enum E {
+  e1, e2;
+
+  static const E one = E.e1;
+}
+
+extension type const ET(int value) implements int {
+  static const ET one = ET(1);
+}
+
+main() {
+  String res = "";
+  if ((C(1),) case (.one,)) {
+    res = "C Ok";
+  }
+  Expect.equals("C Ok", res);
+
+  if ((x: C(1)) case (x: .one)) {
+    res = "C Ok again";
+  }
+  Expect.equals("C Ok again", res);
+
+  M m = MC(1);
+  if ((m,) case (.one,)) {
+    res = "M Ok";
+  }
+  Expect.equals("M Ok", res);
+
+  if ((x: m) case (x: .one)) {
+    res = "M Ok again";
+  }
+  Expect.equals("M Ok again", res);
+
+  if ((E.e1,) case (.one,)) {
+    res = "E Ok";
+  }
+  Expect.equals("E Ok", res);
+
+  if ((x: E.e1) case (x: .one)) {
+    res = "E Ok again";
+  }
+  Expect.equals("E Ok again", res);
+
+  if ((E.e1,) case (.e1,)) {
+    res = "E Ok";
+  }
+  Expect.equals("E Ok", res);
+
+  if ((x: E.e1) case (x: .e1)) {
+    res = "E Ok again";
+  }
+  Expect.equals("E Ok again", res);
+
+  if ((ET(1),) case (.one,)) {
+    res = "ET Ok";
+  }
+  Expect.equals("ET Ok", res);
+
+  if ((x: ET(1)) case (x: .one)) {
+    res = "ET Ok again";
+  }
+  Expect.equals("ET Ok again", res);
+}
---
+++ LanguageFeatures/Static-access-shorthand/patterns_A02_t12.dart
@@ -0,0 +1,90 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion If a static member shorthand expression occurs elsewhere in a
+/// pattern where a constant expression is generally allowed, like
+/// `const (big ? .big : .little)` or `< .one`, except for the relational
+/// pattern `== e`, it's treated as a normal constant expression, using the
+/// context type it's given. The expression of `const (...)` will have the
+/// matched value type as context type. The relational pattern expressions,
+/// other than for `==` and `!=`, will have the parameter type of the
+/// corresponding operator of the matched value type as context type.
+///
+/// @description Checks that if a static member shorthand expression occurs in
+/// an object pattern it's treated as a normal constant expression, using the
+/// context type it's given.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=enum-shorthands
+
+import '../../Utils/expect.dart';
+
+class C {
+  final int value;
+  const C(this.value);
+
+  static const C one = C(1);
+
+  @override
+  bool operator ==(Object other) {
+    if (other is C) {
+      return other.value == value;
+    }
+    return false;
+  }
+}
+
+mixin M on C {
+  static const M one = MC(1);
+}
+
+class MC extends C with M {
+  const MC(super.value);
+}
+
+enum E {
+  e1, e2;
+
+  static const E one = E.e1;
+}
+
+extension type const ET(int value) implements int {
+  static const ET one = ET(1);
+}
+
+class Container {
+  C c = C(1);
+  M m = MC(1);
+  E e = E.e1;
+  ET et = ET(1);
+}
+
+main() {
+  String res = "";
+  Container c = Container();
+  if (c case Container(c: .one)) {
+    res = "C Ok";
+  }
+  Expect.equals("C Ok", res);
+
+  if (c case Container(m: .one)) {
+    res = "M Ok";
+  }
+  Expect.equals("M Ok", res);
+
+  if (c case Container(e: .one)) {
+    res = "E Ok";
+  }
+  Expect.equals("E Ok", res);
+
+  if (c case Container(e: .e1)) {
+    res = "E Ok again";
+  }
+  Expect.equals("E Ok again", res);
+
+  if (c case Container(et: .one)) {
+    res = "ET Ok";
+  }
+  Expect.equals("ET Ok", res);
+}
