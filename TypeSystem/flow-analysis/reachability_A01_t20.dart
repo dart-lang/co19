55384eb5ddd086b59c61b32d6fd38b303c209225
sgrekhov <sgrekhov22@gmail.com>
2025-02-10 16:14:13 +0200

    #3057. Add more flow analysis tests for type Never. Part 2.

---
+++ TypeSystem/flow-analysis/reachability_A01_t20.dart
@@ -0,0 +1,34 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion
+/// - Variable or getter: If `N` is an expression of the form `x` where the
+///   type of `x` is `T` then:
+/// If `T <: Never` then:
+///   - Let `after(N) = unreachable(before(N))`.
+/// Otherwise:
+///   - Let `after(N) = before(N)`.
+///
+/// @description Checks that the code is unreachable after a variable of type
+/// `Never`. Test an `if` statement in a list literal.
+/// @author sgrekhov22@gmail.com
+
+void test(Never n) {
+  late int i;
+  bool b = (() => true)();
+  if (b) {
+    [
+      if (n is String)        // The code after this point is unreachable
+        i = 42  // Variable is initialized in a dead code. This leaves it definitely unassigned
+    ];
+  }
+  i; // It is an error to read a local late variable when it is definitely unassigned.
+//^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  print(test);
+}
