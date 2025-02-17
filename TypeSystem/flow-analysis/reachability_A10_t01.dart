7462554d8be19c5c821abf427b874fcc76890552
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-02-17 15:27:26 +0200

    #3057. Add flow analysis tests for throw expression (#3076)
    Add flow analysis tests for throw expression

---
+++ TypeSystem/flow-analysis/reachability_A10_t01.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion throw: If `N` is a throw expression of the form `throw E1`, then:
+/// - Let `before(E1) = before(N)`
+/// - Let `after(N) = unreachable(after(E1))`
+///
+/// @description Checks that code after a throw expression is unreachable.
+/// @author sgrekhov22@gmail.com
+
+main() {
+  late int i;
+  if ((() => false)()) {
+    throw "Dead code after this point";
+    i = 42; // Variable is initialized in a dead code. This leaves it definitely unassigned
+  }
+  i;  // It is an error to read a local late variable when it is definitely unassigned.
+//^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
