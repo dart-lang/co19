1001acd2f4a948805a1ea7447d434e8f23146876
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-03-06 15:07:15 +0200

    #3057. Add reachability tests for operators. Part 2. (#3099)

---
+++ TypeSystem/flow-analysis/reachability_A20_t34.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion - Binary operator: All binary operators other than `==`, `&&`,
+/// `||`, and `??` are handled as calls to the appropriate `operator` method.
+///
+/// @description Checks that if the static type of the expression of the form
+/// `E1 << E2` is `Never` then `E2` is still reachable.
+/// @author sgrekhov22@gmail.com
+
+class C<T extends Never> {
+  T operator <<(int x) => throw "C";
+}
+
+main() {
+  late int i;
+  try {
+    C() << (i = 42);
+  } catch (_) {}
+  i; // Not definitely unassigned
+}
