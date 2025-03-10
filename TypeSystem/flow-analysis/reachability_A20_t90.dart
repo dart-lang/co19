a30a7d4ae2dc4d5b91d53b95ecdca482fdde705f
sgrekhov <sgrekhov22@gmail.com>
2025-03-10 11:52:27 +0200

    #3057. Add reachability tests for operators. Part 4.

---
+++ TypeSystem/flow-analysis/reachability_A20_t90.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion - Binary operator: All binary operators other than `==`, `&&`,
+/// `||`, and `??` are handled as calls to the appropriate `operator` method.
+///
+/// @description Checks that if the static type of the expression of the form
+/// `E1 -= E2` is `Never` `after(N) = unreachable(after(E2))`.
+/// @author sgrekhov22@gmail.com
+
+class C {
+  Never operator -(int other) => throw "";
+}
+
+void test() {
+  late int i;
+  if (2 > 1) {
+    C c = C();
+    c -= 0;
+    i = 42;
+  }
+  i; // Definitely unassigned
+//^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  print(test);
+}
---
+++ TypeSystem/flow-analysis/reachability_A20_t92.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion - Binary operator: All binary operators other than `==`, `&&`,
+/// `||`, and `??` are handled as calls to the appropriate `operator` method.
+///
+/// @description Checks that if the static type of the expression of the form
+/// `E1 -= E2` static type of `E1`is not `Never` then `after(N) = after(E2)`.
+/// This is tested by detecting that `i = 42` is considered to be guaranteed to
+/// have been executed when `i;` is executed.
+/// @author sgrekhov22@gmail.com
+
+class C {
+  int n;
+  C(this.n);
+  C operator -(int other) => C(n - other);
+}
+
+main() {
+  int i;
+  C c = C(1);
+  c -= (i = 42);
+  i; // Definitely assigned
+}
