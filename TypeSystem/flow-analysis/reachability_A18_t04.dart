a77a4e33883bd573e3bfb5e1a508f592f9db6d38
sgrekhov <sgrekhov22@gmail.com>
2025-03-11 10:22:55 +0200

    #3057. Add more reachability tests for && and || operators

---
+++ TypeSystem/flow-analysis/reachability_A18_t04.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion - Shortcut and: If `N` is a shortcut "and" expression of the form
+/// `E1 && E2`, then:
+/// - Let `before(E1) = before(N)`.
+/// - Let `before(E2) = split(true(E1))`.
+/// - Let `true(N) = unsplit(true(E2))`.
+/// - Let `false(N) = merge(split(false(E1)), false(E2))`.
+///
+/// @description Checks that in an expression of the form `E1 && E2` if `E1` is
+/// `false` then `E2` is never executed.
+/// @author sgrekhov22@gmail.com
+
+Never foo() => throw "Never";
+
+main() {
+  late int i;
+  if (2 > 1) {
+    false && foo();
+    i = 42;
+  }
+  i; // Not definitely unassigned
+}
