9585c289c4dc89fb1db451110c48f39d4e7b3768
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-03-12 13:16:44 +0200

    #3057. Add more reachability tests for && and || operators (#3104)
    Add more reachability tests for && and || operators

---
+++ TypeSystem/flow-analysis/reachability_A19_t04.dart
@@ -0,0 +1,26 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion - Shortcut or: If `N` is a shortcut "or" expression of the form
+/// `E1 || E2`, then:
+/// - Let `before(E1) = before(N)`.
+/// - Let `before(E2) = split(false(E1))`.
+/// - Let `false(N) = unsplit(false(E2))`.
+/// - Let `true(N) = merge(split(true(E1)), true(E2))`.
+///
+/// @description Checks that in an expression of the form `E1 || E2`, if `E1` is
+/// `true` and the static type of `E2` is `Never`, then the code after
+/// `E1 || E2` is not considered dead code.
+/// @author sgrekhov22@gmail.com
+
+Never foo() => throw "Never";
+
+main() {
+  late int i;
+  if (2 > 1) {
+    true || foo();
+    i = 42;
+  }
+  i; // Not definitely unassigned
+}
