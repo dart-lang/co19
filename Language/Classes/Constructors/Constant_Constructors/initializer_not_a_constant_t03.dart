d337dd8ffb412dd43e015fa89b5b3c40d4dcc173
sgrekhov <sgrekhov22@gmail.com>
2025-04-08 14:14:21 +0300

    Minor change in comments

--- Language/Classes/Constructors/Constant_Constructors/initializer_not_a_constant_t03.dart
+++ Language/Classes/Constructors/Constant_Constructors/initializer_not_a_constant_t03.dart
@@ -5,6 +5,7 @@
 /// @assertion It is a compile-time error if a constant constructor is declared
 /// by a class C if any instance variable declared in C is initialized with an
 /// expression that is not a constant expression
+///
 /// @description Checks that compile error is produced if class declares a
 /// constant constructor and has instance variable which is initialised by static
 /// non-constant expression.
