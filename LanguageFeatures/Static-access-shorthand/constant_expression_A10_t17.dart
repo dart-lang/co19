c1c8ff8200505078c22fa4dbac99f11aa3e07f4a
sgrekhov <sgrekhov22@gmail.com>
2025-04-25 10:38:34 +0300

    #3122. Expect an error in case of non-constant in a constant expression

--- LanguageFeatures/Static-access-shorthand/constant_expression_A10_t17.dart
+++ LanguageFeatures/Static-access-shorthand/constant_expression_A10_t17.dart
@@ -45,9 +45,15 @@ main() {
 //                    ^
 // [analyzer] unspecified
 // [cfe] unspecified
-  const M m = 2 < 1 ? .one : .two; //`false ? variable : const` is a constant
+  const M m = 2 < 1 ? .one : .two;
+//                    ^
+// [analyzer] unspecified
+// [cfe] unspecified
 
-  const E e = 2 > 1 ? .one : .two; // `true ? const : variable` is a constant
+  const E e = 2 > 1 ? .one : .two;
+//                           ^
+// [analyzer] unspecified
+// [cfe] unspecified
 
   const ET et = 2 < 1 ? .one : .two;
 //                             ^
