afc9da94335e5b62c5de0ba70080baae78c88e65
sgrekhov <sgrekhov22@gmail.com>
2025-05-15 16:06:25 +0300

    #3057. Add more tests for promotion in switch

---
+++ TypeSystem/flow-analysis/promotion_switch_A04_t01.dart
@@ -0,0 +1,62 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion For now (May, 2025) promotion in switch statement and
+/// expressions are not specified yet.
+/// TODO (sgrekhov): update when specified
+///
+/// @description Checks that pattern match adds type of interest to the switch
+/// statement/expression variable.
+/// @author sgrekhov22@gmail.com
+
+import '../../Utils/static_type_helper.dart';
+
+test1(Object? x) {
+  switch (x) {
+    case int _:
+  }
+  x = 42;
+  x.expectStaticType<Exactly<int>>();
+}
+
+test2(Object? x) {
+  if (1 > 2) {
+    switch (x) {
+      case int v:
+    }
+  }
+  x = 42;
+  x.expectStaticType<Exactly<int>>();
+}
+
+test3(Object? x) {
+  var y = switch (x) {
+    int v => 42,
+    _ => 0
+  };
+  x = 42;
+  x.expectStaticType<Exactly<int>>();
+}
+
+test4(Object? x) {
+  if (1 > 2) {
+    var y = switch (x) {
+      int v => 42,
+      _ => 0
+    };
+  }
+  x = 42;
+  x.expectStaticType<Exactly<int>>();
+}
+
+main() {
+  test1(1);
+  test1(null);
+  test2(1);
+  test2(null);
+  test3(1);
+  test3(null);
+  test4(1);
+  test4(null);
+}
---
+++ TypeSystem/flow-analysis/promotion_switch_A05_t03.dart
@@ -0,0 +1,53 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion For now (May, 2025) promotion in switch statement and
+/// expressions are not specified yet.
+/// TODO (sgrekhov): update when specified
+///
+/// @description Checks that pattern match promotes the variable when a case
+/// scope is shared but cases have the same type.
+/// @author sgrekhov22@gmail.com
+/// @issue 60708
+
+import '../../Utils/static_type_helper.dart';
+
+test1(Object? x) {
+  switch (x) {
+    case int v when (x = 42) > 0:
+    case int v:
+      x.expectStaticType<Exactly<int>>();
+    case _:
+      x.expectStaticType<Exactly<Object?>>();
+  }
+}
+
+test2(Object? x) {
+  switch (x) {
+    case int _ when (x = 42) > 0:
+    case int v:
+      x.expectStaticType<Exactly<int>>();
+    case _:
+      x.expectStaticType<Exactly<Object?>>();
+  }
+}
+
+test3(Object? x) {
+  switch (x) {
+    case int _ when (x = 42) > 0:
+    case int _:
+      x.expectStaticType<Exactly<int>>();
+    case _:
+      x.expectStaticType<Exactly<Object?>>();
+  }
+}
+
+main() {
+  test1(1);
+  test1(null);
+  test2(1);
+  test2(null);
+  test3(1);
+  test3(null);
+}
