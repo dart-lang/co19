e540484e99430b5188deefe0e529f60c1f556376
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-07-03 19:51:31 +0300

    #3180. Add `NullRejectionException` tests

---
+++ LibTest/js_interop/NullRejectionException/nullRejectionException_t02.dart
@@ -0,0 +1,33 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Exception for when the promise is rejected with a `null` or
+/// `undefined` value.
+///
+/// @description Checks that this exception occurs when the promise is rejected
+/// with an `undefined` value.
+/// @author sgrekhov22@gmail.com
+
+import 'dart:async';
+import 'dart:js_interop';
+import 'dart:js_interop_unsafe';
+import '../../../Utils/expect.dart';
+import '../js_utils.dart';
+
+main() {
+  eval(r'''
+    globalThis.promise = new Promise(function(resolve, reject) {
+      reject(undefined);
+    });
+  ''');
+  JSPromise promise = globalContext["promise"] as JSPromise;
+  asyncStart();
+  promise.toDart.then((v) {
+    Expect.fail("NullRejectionException expected");
+  }).onError((e, st) {
+    Expect.isTrue(e is NullRejectionException);
+    Expect.isTrue((e as NullRejectionException).isUndefined);
+    asyncEnd();
+  });
+}
