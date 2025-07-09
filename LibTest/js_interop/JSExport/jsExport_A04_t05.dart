7d10b89c5e8b16e270192b68d68c22945db233b7
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-07-09 12:29:08 +0300

    #3180. Add JSExport tests. Part 3.

---
+++ LibTest/js_interop/JSExport/jsExport_A04_t05.dart
@@ -0,0 +1,33 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Classes and mixins in the hierarchy of the annotated class are
+/// included only if they are annotated as well or specific members in them are
+/// annotated. If a superclass does not have an annotation anywhere, its members
+/// are not included.
+///
+/// @description Checks that it is a compile-time error if a superclass is
+/// annotated with `@JSExport` but the class itself is not and this class is
+/// passed to `createJSInteropWrapper`.
+/// @author sgrekhov22@gmail.com
+
+import 'dart:js_interop';
+
+@JSExport()
+class A {
+  int aVariable = 42;
+  String aMethod(String v) => "aMethod($v);";
+  String get aGetter => "aGetter";
+  void set aSetter(bool value) {}
+}
+
+class C extends A {
+}
+
+void main() {
+  createJSInteropWrapper<C>(C());
+//                       ^
+// [analyzer] unspecified
+// [web] unspecified
+}
