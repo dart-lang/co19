a5b892d475d9c8a014b833f3c7c199a58e37ee77
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-08-04 12:27:13 +0300

    Add forgotten file

--- LibTest/js_interop/module.js
+++ LibTest/js_interop/module.js
@@ -8,3 +8,14 @@ export function B() {}
 export function answer() {
   return 42;
 }
+
+export class C {
+  foo() {
+    return "C.foo() from module.js";
+  }
+  static bar() {
+    return "C.bar() from module.js";
+  }
+}
+
+globalThis.C = C;
