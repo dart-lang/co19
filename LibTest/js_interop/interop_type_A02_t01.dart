277ffe2a54d4b4edb12be5388ff7cfe182b383fd
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-08-18 12:55:55 +0300

    Implement review recommendations 2

--- LibTest/js_interop/interop_type_A02_t01.dart
+++ LibTest/js_interop/interop_type_A02_t01.dart
@@ -7,30 +7,43 @@
 /// Interop types are either a "JS type" provided by Dart or an extension type
 /// wrapping an interop type.
 ///
-/// @description Check that it is a compile-time error to declare an external
-/// constructor on a JS interop extension type whose representation type is not
-/// a subtype of [JSObject]. Test [JSAny].
+/// @description Check that it is not an error to declare an external
+/// constructor on a JS interop extension type whose representation type is
+/// [JSAny].
 /// @author sgrekhov22@gmail.com
-/// @issue 61314
 
 import 'dart:js_interop';
+import 'dart:js_interop_unsafe';
+import '../../Utils/expect.dart';
+import 'js_utils.dart';
 
-extension type MyAny1._(JSAny _) implements JSAny {
-  external MyAny1.fromDart(String value);
-//^
-// [analyzer] unspecified
-// [web] unspecified
+extension type MyAny._(JSAny _) implements JSAny {
+  external MyAny.fromDart(String value);
+  external String foo();
 }
 
-@JS('Number')
+@JS('MyAny')
 extension type MyAny2._(JSAny _) implements JSAny {
   external MyAny2(num value);
-//^
-// [analyzer] unspecified
-// [web] unspecified
+  external num foo();
 }
 
 main() {
-  print(MyAny1);
-  print(MyAny2);
+  eval(r'''
+    class MyAny {
+      constructor(v) {
+        this.v = v;
+      }
+      foo() {
+        return this.v;
+      }
+    }
+    globalThis.MyAny = MyAny;
+  ''');
+
+  MyAny ma = MyAny.fromDart("x");
+  Expect.equals("x", ma.foo());
+
+  MyAny2 ma2 = MyAny2(42);
+  Expect.equals(42, ma2.foo());
 }
