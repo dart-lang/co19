d4eaebdca87e91022c81ba1cd488631efcd59318
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-09-05 11:19:15 +0300

    #3180. Add tests for `FutureOfVoidToJSPromise`

---
+++ LibTest/js_interop/FutureOfVoidToJSPromise/toJS_A02_t01.dart
@@ -0,0 +1,62 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion JSPromise<JSAny?> get toJS
+/// A JSPromise that either resolves once this [Future] completes or rejects
+/// with an object that contains its error.
+///
+/// The rejected object contains the original error as a [JSBoxedDartObject] in
+/// the property `error` and the original `stack` trace as a [String] in the
+/// property stack.
+///
+/// @description Checks that if the [Future] completes with an error then the
+/// rejected object contains the original error as a [JSBoxedDartObject] in the
+/// property `error` and the original stack trace as a [String] in the property
+/// `stack`.
+/// @author sgrekhov22@gmail.com
+
+import 'dart:async';
+import 'dart:js_interop';
+import 'dart:js_interop_unsafe';
+import '../../../Utils/expect.dart';
+import '../js_utils.dart';
+
+final completer = Completer<void>();
+
+void complete() {
+  completer.complete();
+}
+
+extension type Error(JSObject _) implements JSObject {
+  @JS()
+  external JSBoxedDartObject get error;
+  @JS()
+  external String get stack;
+}
+
+main() {
+  globalContext["complete"] = complete.toJS;
+  Future<void> future = Future.error(
+    "The error",
+    StackTrace.fromString("Stack trace"),
+  );
+  JSPromise promise = future.toJS;
+  globalContext["promise"] = promise;
+  eval(r'''
+    globalThis.promise.then((v) => {
+      globalThis.result = v;
+      globalThis.complete();
+    }, (e) => {
+      globalThis.error = e;
+      globalThis.complete();
+    });
+  ''');
+  asyncStart();
+  completer.future.then((_) {
+    Error error = globalContext["error"] as Error;
+    Expect.equals("The error", error.error.toDart);
+    Expect.equals("Stack trace", error.stack);
+    asyncEnd();
+  });
+}
