a01ba87c6c811e0260acaad3f73808d829b59418
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-09-05 19:55:52 +0300

    #3180. Add tests for `FutureOfVoidToJSPromise` (#3303)

---
+++ LibTest/js_interop/FutureOfVoidToJSPromise/toJS_A01_t02.dart
@@ -0,0 +1,46 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion JSPromise<JSAny?> get toJS
+/// A JSPromise that either resolves once this [Future] completes or rejects
+/// with an object that contains its error.
+///
+/// The rejected object contains the original error as a [JSBoxedDartObject] in
+/// the property `error` and the original `stack` trace as a [String] in the
+/// property stack.
+///
+/// @description Checks that [JSPromise] created by this property completes and
+/// the value with which the [Future] was completed is ignored. Test a [Future]
+/// created by [Future.value] constructor.
+/// @author sgrekhov22@gmail.com
+
+import 'dart:async';
+import 'dart:js_interop';
+import 'dart:js_interop_unsafe';
+import '../../../Utils/expect.dart';
+import '../js_utils.dart';
+
+final completer = Completer<void>();
+
+void complete() {
+  completer.complete();
+}
+
+main() {
+  globalContext["complete"] = complete.toJS;
+  Future<void> future = Future.value("Success".toJS);
+  JSPromise promise = future.toJS;
+  globalContext["promise"] = promise;
+  eval(r'''
+    globalThis.promise.then((v) => {
+      globalThis.result = v;
+      globalThis.complete();
+    });
+  ''');
+  asyncStart();
+  completer.future.then((_) {
+    Expect.isNull(globalContext["result"]);
+    asyncEnd();
+  });
+}
---
+++ LibTest/js_interop/FutureOfVoidToJSPromise/toJS_A01_t03.dart
@@ -0,0 +1,48 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion JSPromise<JSAny?> get toJS
+/// A JSPromise that either resolves once this [Future] completes or rejects
+/// with an object that contains its error.
+///
+/// The rejected object contains the original error as a [JSBoxedDartObject] in
+/// the property `error` and the original `stack` trace as a [String] in the
+/// property stack.
+///
+/// @description Checks that if the [Future] completes with an error then
+/// `onRejected` callback of JavaScript `Promise.then()` is called.
+/// @author sgrekhov22@gmail.com
+
+import 'dart:async';
+import 'dart:js_interop';
+import 'dart:js_interop_unsafe';
+import '../../../Utils/expect.dart';
+import '../js_utils.dart';
+
+final completer = Completer<void>();
+
+void complete() {
+  completer.complete();
+}
+
+main() {
+  globalContext["complete"] = complete.toJS;
+  Future<void> future = Future.error("The error");
+  JSPromise promise = future.toJS;
+  globalContext["promise"] = promise;
+  eval(r'''
+    globalThis.promise.then((v) => {
+      globalThis.result = "onFulfilled";
+      globalThis.complete();
+    }, (e) => {
+      globalThis.result = "onRejected";
+      globalThis.complete();
+    });
+  ''');
+  asyncStart();
+  completer.future.then((_) {
+    Expect.equals("onRejected", (globalContext["result"] as JSString).toDart);
+    asyncEnd();
+  });
+}
