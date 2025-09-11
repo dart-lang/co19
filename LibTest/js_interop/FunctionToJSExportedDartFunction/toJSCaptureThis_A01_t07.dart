d414379eeb3ba7a4715e8675ff90b56421407eb0
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-09-11 12:13:35 +0300

    #3180. Add more tests for `toJSCaptureThis` (#3308)
    Add more tests for `toJSCaptureThis`.

---
+++ LibTest/js_interop/FunctionToJSExportedDartFunction/toJSCaptureThis_A01_t07.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion JSExportedDartFunction get toJSCaptureThis
+/// A callable JavaScript function that wraps this [Function] and captures the
+/// `this` value when called.
+///
+/// Identical to `toJS`, except the resulting [JSExportedDartFunction] will pass
+/// `this` from JavaScript as the first argument to the converted [Function].
+/// Any [Function] that is converted with this member should take in an extra
+/// parameter at the beginning of the parameter list to handle this.
+///
+/// @description Check that this property returns a JavaScript function that
+/// wraps this [Function] and captures the `this` value when called. Check that
+/// a [JSString] can be passed as the value of `this`.
+/// @author sgrekhov22@gmail.com
+
+import 'dart:js_interop';
+import 'dart:js_interop_unsafe';
+import '../../../Utils/expect.dart';
+
+extension type JSStringOrJSStringObject._(JSAny _) implements JSAny {
+  @JS('toString')
+  external String toStringJS();
+}
+
+void foo(JSStringOrJSStringObject pThis) {
+  Expect.equals("Value of this", pThis.toString());
+}
+
+main() {
+  globalContext["jsFoo"] = foo.toJSCaptureThis;
+  JSFunction jsFoo = globalContext["jsFoo"] as JSFunction;
+  jsFoo.callAsFunction("Value of this".toJS);
+}
---
+++ LibTest/js_interop/FunctionToJSExportedDartFunction/toJSCaptureThis_A01_t09.dart
@@ -0,0 +1,40 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion JSExportedDartFunction get toJSCaptureThis
+/// A callable JavaScript function that wraps this [Function] and captures the
+/// `this` value when called.
+///
+/// Identical to `toJS`, except the resulting [JSExportedDartFunction] will pass
+/// `this` from JavaScript as the first argument to the converted [Function].
+/// Any [Function] that is converted with this member should take in an extra
+/// parameter at the beginning of the parameter list to handle this.
+///
+/// @description Check that this property returns a JavaScript function that
+/// wraps this [Function] and captures the `this` value when called. Check that
+/// `null` can be passed as the value of `this`.
+/// @author sgrekhov22@gmail.com
+
+import 'dart:js_interop';
+import 'dart:js_interop_unsafe';
+import '../../../Utils/expect.dart';
+
+void foo(JSAny? pThis) {
+  if (isJS) {
+    // Test runner runs `dart2js` tests in non-strict mode. In this case
+    // (see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call#parameters)
+    // `null` is replaced by the global object.
+    Expect.equals(globalContext, pThis);
+  }
+  if (isWasm) {
+    // `dart2wasm` tests are run in the strict mode.
+    Expect.isNull(pThis);
+  }
+}
+
+main() {
+  globalContext["jsFoo"] = foo.toJSCaptureThis;
+  JSFunction jsFoo = globalContext["jsFoo"] as JSFunction;
+  jsFoo.callAsFunction(null);
+}
