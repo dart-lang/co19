654d2a61a878117bab06e6e9cfc9c6346e9ee0a0
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-09-18 11:08:08 +0300

    Implement review recommendations

---
+++ LibTest/js_interop/Int16ListToJSInt16Array/toJS_t03.dart
@@ -0,0 +1,43 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion JSInt16Array get toJS
+/// Converts this [Int16List] to a [JSInt16Array] by either casting,
+/// unwrapping, or cloning the [Int16List].
+///
+/// > [!NOTE]
+/// > Depending on whether code is compiled to JavaScript or Wasm, this
+/// > conversion will have different semantics.
+/// > When compiling to JavaScript, all typed lists are the equivalent
+/// > JavaScript typed arrays, and therefore this method simply casts.
+/// > When compiling to Wasm, this [Int16List] may or may not be a wrapper
+/// > depending on if it was converted from JavaScript or instantiated in
+/// > Dart. If it's a wrapper, this method unwraps it. If it's instantiated in
+/// > Dart, this method clones this [Int16List]'s values into a new
+/// > [JSInt16Array].
+/// > Avoid assuming that modifications to this [Int16List] will affect the
+/// > [JSInt16Array] and vice versa unless it was instantiated in JavaScript.
+///
+/// @description Check that when compiling to JavaScript this operation is a
+/// cast and returns the same object. When compiling to Wasm this operation is a
+/// wrapping and returns not the same object.
+/// @author sgrekhov22@gmail.com
+
+import 'dart:js_interop';
+import 'dart:typed_data';
+import '../../../Utils/expect.dart';
+
+main() {
+  Int16List l = Int16List.fromList([42]);
+  if (isJS) {
+    // This is a cast. Object is the same
+    Expect.identical(l, l.toJS);
+    Expect.identical(l, l.toJS.toDart);
+  }
+  if (isWasm) {
+    // This is a wrapping/unwrapping. It's not the same object
+    Expect.notIdentical(l, l.toJS);
+    Expect.listEquals(l.toList(), l.toJS.toDart.toList());
+  }
+}
---
+++ LibTest/js_interop/Uint8ClampedListToJSUint8ClampedArray/toJS_t03.dart
@@ -0,0 +1,44 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion JSUint8ClampedArray get toJS
+/// Converts this [Uint8ClampedList] to a [JSUint8ClampedArray] by either
+/// casting, unwrapping, or cloning the [Uint8ClampedList].
+///
+/// > [!NOTE]
+/// > Depending on whether code is compiled to JavaScript or Wasm, this
+/// > conversion will have different semantics.
+/// > When compiling to JavaScript, all typed lists are the equivalent
+/// > JavaScript typed arrays, and therefore this method simply casts.
+/// > When compiling to Wasm, this [Uint8ClampedList] may or may not be a
+/// > wrapper depending on if it was converted from JavaScript or instantiated
+/// > in Dart. If it's a wrapper, this method unwraps it. If it's instantiated
+/// > in Dart, this method clones this [Uint8ClampedList]'s values into a new
+/// > [JSUint8ClampedArray].
+/// > Avoid assuming that modifications to this [Uint8ClampedList] will affect
+/// > the [JSUint8ClampedArray] and vice versa unless it was instantiated in
+/// > JavaScript.
+///
+/// @description Check that when compiling to JavaScript this operation is a
+/// cast and returns the same object. When compiling to Wasm this operation is a
+/// wrapping and returns not the same object.
+/// @author sgrekhov22@gmail.com
+
+import 'dart:js_interop';
+import 'dart:typed_data';
+import '../../../Utils/expect.dart';
+
+main() {
+  Uint8ClampedList l = Uint8ClampedList.fromList([42]);
+  if (isJS) {
+    // This is a cast. Object is the same
+    Expect.identical(l, l.toJS);
+    Expect.identical(l, l.toJS.toDart);
+  }
+  if (isWasm) {
+    // This is a wrapping/unwrapping. It's not the same object
+    Expect.notIdentical(l, l.toJS);
+    Expect.listEquals(l.toList(), l.toJS.toDart.toList());
+  }
+}
