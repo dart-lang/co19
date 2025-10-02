0c8d2bab9e52b29ac7b0275cacdc58da5120e864
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-10-02 12:35:03 +0300

    #3315. Add tests for `covariant` keyword (#3328)
    Add tests for `covariant` keyword

---
+++ LanguageFeatures/primary-constructors/static_processing_A08_t02.dart
@@ -0,0 +1,78 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion The following errors apply to formal parameters of a declaring
+/// constructor, be it in the header or in the body. Let `p` be a formal
+/// parameter of a declaring constructor in a class, enum, or extension type
+/// declaration `D` named `C`:
+/// ...
+/// A compile-time error occurs if `p` has both of the modifiers `covariant` and
+/// `final`.
+///
+/// @description Check that it is a compile-time error if a formal parameters of
+/// a declaring constructor has both of the modifiers `covariant` and `final`.
+/// Test extension types.
+/// @author sgrekhov22@gmail.com
+
+// TODO (sgrekhov) Add `declaring-constructors` experimental flag
+
+extension type ET1(covariant final String x) {}
+//                           ^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+// Representation variable is always final even without `final` keyword
+extension type ET2([covariant String x = ""]) {}
+//                  ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+extension type ET3({covariant final String x = ""}) {}
+//                            ^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+extension type ET4({required covariant String x}) {}
+//                           ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+extension type ET5 {
+  this(covariant String x);
+//     ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+extension type ET6 {
+  this([covariant final String x = ""]);
+//                ^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+extension type ET7 {
+  this({covariant String x = ""});
+//      ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+extension type ET8 {
+  this({required covariant final String x});
+//                         ^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  print(ET1);
+  print(ET2);
+  print(ET3);
+  print(ET4);
+  print(ET5);
+  print(ET6);
+  print(ET7);
+  print(ET8);
+}
