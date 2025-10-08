d18f7eb65c30c817b369d865380fd82930d41839
sgrekhov <sgrekhov22@gmail.com>
2025-10-08 11:20:24 +0300

    Fixes #3340. Remove support for conditional parts

--- LanguageFeatures/Parts-with-imports/grammar_A02_t01.dart
+++ LanguageFeatures/Parts-with-imports/grammar_A02_t01.dart
@@ -2,36 +2,39 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion We extend the grammar of part files to allow import, export and
-/// part file directives. We allow part files directives to use a configurable
-/// URI like the other two. We restrict the part of directive to only allow the
-/// string version.
-///
-/// -- Changed "<uri>" to "<configurableUri>".
-/// <partDirective> ::= <metadata> `part' <configurableUri> `;'
+/// @assertion We extend the grammar of part files to allow `import`, `export`
+/// and `part` file directives. We restrict the part of directive to only allow
+/// the string version.
 ///
 /// -- Removed "<dottedIdentifier>" as option, retaining only "<uri>".
 /// <partHeader> ::= <metadata> `part' `of' <uri> `;'
 ///
 /// -- Added "<importOrExport>* <partDirective>*"
 /// <partDeclaration> ::=
-///   <partHeader> <importOrExport>* <partDirective>*
-///   (<metadata> <topLevelDeclaration>)* <EOF>
+///   <partHeader> <importOrExport>* <partDirective>* (<metadata>
+///   <topLevelDeclaration>)* <EOF>
 ///
-/// The grammar change is small, mainly adding import, export and part
+/// The grammar change is small, mainly adding `import`, `export` and `part`
 /// directives to part files.
 ///
-/// @description Check that a `part` directive may contain a '<configurableUri>'
+/// @description Check that it is a compile-time error if a `part` directive
+/// contains a '<configurableUri>'.
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=enhanced-parts
 
-import '../../Utils/expect.dart';
-
 part 'grammar_A02_t01_part1.dart'
+//   ^
+// [analyzer] unspecified
+// [cfe] unspecified
         if (dart.library.io) 'grammar_A02_t01_part2.dart'
+//      ^^
+// [analyzer] unspecified
+// [cfe] unspecified
         if (dart.library.collection) 'grammar_A02_t01_part3.dart';
+//      ^^
+// [analyzer] unspecified
+// [cfe] unspecified
 
 main() {
-  Expect.isTrue(partId() == "part2" || partId() == "part3");
 }
