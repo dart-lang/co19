54bc135244ad546295a8bd1f6e78165f85b9b40f
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-10-08 18:57:02 +0300

    Fixes #3340. Remove support for conditional parts (#3341)

--- LanguageFeatures/Parts-with-imports/grammar_A03_t01.dart
+++ LanguageFeatures/Parts-with-imports/grammar_A03_t01.dart
@@ -2,23 +2,19 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion We extend the grammar of part files to allow import, export and
-/// part file directives. We allow part files directives to use a configurable
-/// URI like the other two. We restrict the part of directive to only allow the
-/// string version.
-///
-/// -- Changed "<uri>" to "<configurableUri>".
-/// <partDirective> ::= <metadata> `part' <configurableUri> `;'
+/// @assertion We extend the grammar of part files to allow `import`, `export`
+/// and `part` file directives. We restrict the part of directive to only allow
+/// the string version.
 ///
 /// -- Removed "<dottedIdentifier>" as option, retaining only "<uri>".
 /// <partHeader> ::= <metadata> `part' `of' <uri> `;'
 ///
 /// -- Added "<importOrExport>* <partDirective>*"
 /// <partDeclaration> ::=
-///   <partHeader> <importOrExport>* <partDirective>*
-///   (<metadata> <topLevelDeclaration>)* <EOF>
+///   <partHeader> <importOrExport>* <partDirective>* (<metadata>
+///   <topLevelDeclaration>)* <EOF>
 ///
-/// The grammar change is small, mainly adding import, export and part
+/// The grammar change is small, mainly adding `import`, `export` and `part`
 /// directives to part files.
 ///
 /// @description Check that it is a compile-time error to use a
