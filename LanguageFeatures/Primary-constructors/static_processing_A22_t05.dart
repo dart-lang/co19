8bdc5424ecfffc11bdd1d29cfba07c9e5ece7105
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-11-20 17:08:39 +0200

    #3315. Update tests for formal parameters. Part 4. (#3407)
    Update tests for formal parameters. Part 4.

--- LanguageFeatures/Primary-constructors/static_processing_A22_t05.dart
+++ LanguageFeatures/Primary-constructors/static_processing_A22_t05.dart
@@ -2,49 +2,37 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion The semantics of the declaring constructor is found in the
-/// following steps, where `D` is the class, extension type, or enum declaration
-/// in the program that includes a declaring constructor `k`, and `D2` is the
-/// result of the derivation of the semantics of `D`. The derivation step will
-/// delete elements that amount to the declaring constructor. Semantically, it
-/// will add a new constructor `k2`, and it will add zero or more instance
-/// variable declarations.
+/// @assertion The semantics of the primary constructor is found in the
+/// following steps, where `D` is the class, mixin class, extension type, or
+/// enum declaration in the program that includes a primary constructor `k`, and
+/// `D2` is the result of the derivation of the semantics of `D`. The derivation
+/// step will delete elements that amount to the primary constructor.
+/// Semantically, it will add a new constructor `k2`, and it will add zero or
+/// more instance variable declarations.
 /// ...
-/// Otherwise, a formal parameter (named or positional) of the form` var T p` or
+/// The formal parameter list `L2` of `k2` is identical to `L`, except that each
+/// formal parameter is processed as follows.
+/// ...
+/// Otherwise, a formal parameter (named or positional) of the form `var T p` or
 /// `final T p` where `T` is a type and `p` is an identifier is replaced in `L2`
 /// by `this.p`, along with its default value, if any. Next, a semantic instance
 /// variable declaration corresponding to the syntax `T p;` or `final T p;` is
 /// added to `D2`. It includes the modifier `final` if the parameter in `L` has
-/// the modifier `final`, or `D` is an extension type declaration and `k` is a
-/// declaring header constructor. In all cases, if `p` has the modifier
+/// the modifier `final` and `D` is not an `extension type` declaration; if `D`
+/// is an extension type declaration then the name of `p` specifies the name of
+/// the representation variable. In all cases, if `p` has the modifier
 /// `covariant` then this modifier is removed from the parameter in `L2`, and it
 /// is added to the instance variable declaration named `p`.
 ///
-/// @description Check that it is a compile-time error if a declaring
-/// constructor contains a formal parameter of the form `var T p` or
-/// `final T p` and the enclosing class/enum/extension type contains a static
-/// member named `p`. Test mandatory parameters.
+/// @description Check that it is a compile-time error if a primary constructor
+/// contains a formal parameter of the form `var T p` or `final T p` and the
+/// enclosing class/enum/extension type contains a static member named `p`. Test
+/// mandatory parameters.
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=primary-constructors
 
-class C1<T>(var String v1, final T v2, var bool v3) {
-  static String get v1 => "";
-//                  ^^
-// [analyzer] unspecified
-// [cfe] unspecified
-  static T v2;
-//         ^^
-// [analyzer] unspecified
-// [cfe] unspecified
-  static void v3() {}
-//            ^^
-// [analyzer] unspecified
-// [cfe] unspecified
-}
-
-class C2<T> {
-  this(final String v1, var T v2, final bool v3);
+class C<T>(var String v1, final T v2, var bool v3) {
   static String get v1 => "";
 //                  ^^
 // [analyzer] unspecified
@@ -73,45 +61,8 @@ extension type ET2(String v) {
 // [cfe] unspecified
 }
 
-extension type ET3<T> {
-  this(final T v);
-
-  static void v() {}
-//            ^
-// [analyzer] unspecified
-// [cfe] unspecified
-}
-
-extension type ET4 {
-  this(final String v);
-
-  static String get v => "";
-//                  ^
-// [analyzer] unspecified
-// [cfe] unspecified
-}
-
-enum E1<T>(final T v1, final String v2, final bool v3) {
+enum E<T>(final T v1, final String v2, final bool v3) {
   e0<int>(1, "E1", true);
-
-  static final T v1;
-//               ^^
-// [analyzer] unspecified
-// [cfe] unspecified
-  static String get v2 => "";
-//                  ^^
-// [analyzer] unspecified
-// [cfe] unspecified
-  static void v3() {}
-//            ^^
-// [analyzer] unspecified
-// [cfe] unspecified
-}
-
-enum E2<T> {
-  e0<int>(1, "E1", true);
-
-  this(final T v1, final String v2, final bool v3);
 
   static final T v1;
 //               ^^
@@ -128,12 +79,8 @@ enum E2<T> {
 }
 
 main() {
-  print(C1);
-  print(C2);
+  print(C);
   print(ET1);
   print(ET2);
-  print(ET3);
-  print(ET4);
-  print(E1);
-  print(E2);
+  print(E);
 }
