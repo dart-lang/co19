0d1231e54ba405ce3e37c7a1e0061d9f939e3cff
sgrekhov <sgrekhov22@gmail.com>
2025-11-25 12:26:03 +0200

    #3182. Update augmenting_constructors_A05-08* tests

--- LanguageFeatures/Augmentations/augmenting_constructors_A07_t02.dart
+++ LanguageFeatures/Augmentations/augmenting_constructors_A07_t02.dart
@@ -2,24 +2,24 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion It is a compile-time error if:
-/// ...
-/// - A non-redirecting constructor augments a constructor which is not
-///   potentially non-redirecting.
+/// @assertion It's a compile-time error if an augmentation is complete and any
+/// declaration before it in the augmentation chain is also complete.
 ///
 /// @description Checks that it is a compile-time error if a non-redirecting
 /// factory constructor augments a factory constructor which is not potentially
 /// non-redirecting.
 /// @author sgrekhov22@gmail.com
 
-// SharedOptions=--enable-experiment=macros
-
-part 'augmenting_constructors_A07_t02_lib.dart';
+// SharedOptions=--enable-experiment=augmentations
 
 class C {
   C();
   factory C.foo() = C;
-//^^^^^
+}
+
+augment class C {
+  augment factory C.foo() => C();
+//                ^^^^^
 // [analyzer] unspecified
 // [cfe] unspecified
 }
@@ -27,6 +27,13 @@ class C {
 extension type ET(int id) {
   ET.foo(this.id);
   factory ET.bar(int id) = ET.foo;
+}
+
+augment extension type ET {
+  augment factory ET.bar(int id) => ET.foo(id);
+//                ^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
 }
 
 main() {
