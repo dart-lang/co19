57071fcbf95c6753d8d8cad6df17c6b92e5e4df0
sgrekhov <sgrekhov22@gmail.com>
2025-11-25 12:37:33 +0200

    Fix assertions and descriptions

--- LanguageFeatures/Augmentations/augmenting_constructors_A06_t01.dart
+++ LanguageFeatures/Augmentations/augmenting_constructors_A06_t01.dart
@@ -2,14 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion The general rule is that compile-time errors apply to semantic
-/// definitions whenever possible. In other words, if the library is
-/// syntactically well-formed enough that augmentations can be applied, then
-/// they should be. And if doing so eliminates what would otherwise be a
-/// compile-time error, then that error should not be reported.
+/// @assertion It's a compile-time error if an augmentation is complete and any
+/// declaration before it in the augmentation chain is also complete.
 ///
-/// @description Checks that it is a compile-time error if the resulting
-/// constructor has a redirecting initializer and initializer list elements.
+/// @description Checks that it is a compile-time error if the introductory
+/// constructor has a redirecting initializer and the augmenting has initializer
+/// list elements.
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=augmentations
