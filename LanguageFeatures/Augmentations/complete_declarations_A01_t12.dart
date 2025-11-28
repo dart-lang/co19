7eba23aec7478d5cb9d8834edd23a0cb65b160a5
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-11-28 12:01:38 +0200

    #3182. Rename some augmenting constructors tests (#3422)

--- LanguageFeatures/Augmentations/complete_declarations_A01_t12.dart
+++ LanguageFeatures/Augmentations/complete_declarations_A01_t12.dart
@@ -2,8 +2,19 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion It's a compile-time error if an augmentation is complete and any
-/// declaration before it in the augmentation chain is also complete.
+/// @assertion More precisely, a function or constructor declaration
+/// (introductory or augmenting) is incomplete if all of:
+/// - It has no body. That means no `{ ... }` or `=> ...;` but only `;`.
+/// - The function is not marked external. An external function is considered to
+///   have a body, just not one that is visible as Dart code.
+/// - There is no redirection, initializer list, initializing formals, field
+///   parameters, or super parameters. Obviously, this only applies to
+///   constructor declarations.
+///
+/// If a declaration is not incomplete then it is complete.
+///
+/// It's a compile-time error if an augmentation is complete and any declaration
+/// before it in the augmentation chain is also complete.
 ///
 /// @description Checks that it is a compile-time error if both an introductory
 /// and an augmenting non-redirecting factory constructors have bodies.
---
+++ LanguageFeatures/Augmentations/complete_declarations_A01_t13.dart
@@ -0,0 +1,45 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion More precisely, a function or constructor declaration
+/// (introductory or augmenting) is incomplete if all of:
+/// - It has no body. That means no `{ ... }` or `=> ...;` but only `;`.
+/// - The function is not marked external. An external function is considered to
+///   have a body, just not one that is visible as Dart code.
+/// - There is no redirection, initializer list, initializing formals, field
+///   parameters, or super parameters. Obviously, this only applies to
+///   constructor declarations.
+///
+/// If a declaration is not incomplete then it is complete.
+///
+/// It's a compile-time error if an augmentation is complete and any declaration
+/// before it in the augmentation chain is also complete.
+///
+/// @description Checks that it is a compile-time error to declare an augmenting
+/// redirecting generative constructor if the augmented constructor has a super
+/// initializer.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=augmentations
+
+class A {
+  int x;
+  A(this.x);
+}
+
+class C extends A {
+  C() : super(0);
+  C.foo() : super(0);
+}
+
+augment class C {
+  augment C.foo(): this();
+//                 ^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  print(C);
+}
