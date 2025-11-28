cc2f055ea93cda688ad1c6a9ed1e77e718656eac
sgrekhov <sgrekhov22@gmail.com>
2025-11-28 10:23:09 +0200

    #3182. Add tests for constructors with initializing formals

---
+++ LanguageFeatures/Augmentations/augmenting_constructors_A09_t03.dart
@@ -0,0 +1,82 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion An incomplete constructor can be completed by adding an
+/// initializer list and/or a body, or by adding a redirection.
+///
+/// @description Checks that constructor with the initializing formals can be
+/// augmented.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=augmentations
+
+import '../../Utils/expect.dart';
+
+class C {
+  int x;
+  C(this.x);
+  C.foo([this.x]);
+  C.bar({this.x});
+  C.baz({required this.x});
+}
+
+augment class C {
+  augment C(int x);
+  augment C.foo([int x = 1]);
+  augment C.bar({int x = 2});
+  augment C.baz({required int x});
+}
+
+enum E {
+  e0(0), e1.foo(), e2.foo(2), e3.bar(), e4.bar(x: 3), e5.baz(x: 4);
+  final int x;
+  const E(this.x);
+  const E.foo([this.x]);
+  const E.bar({this.x});
+  const E.baz({required this.x});
+}
+
+augment enum E {
+  augment const E(int x);
+  augment const E.foo([int x = 1]);
+  augment const E.bar({int x = 2});
+  augment const E.baz({required int x});
+}
+
+extension type ET._(int x) {
+  ET(this.x);
+  ET.foo([this.x]);
+  ET.bar({this.x});
+  ET.baz({required this.x});
+}
+
+augment extension type ET {
+  augment ET(int x);
+  augment ET.foo([int x = 1]);
+  augment ET.bar({int x = 2});
+  augment ET.baz({required int x});
+}
+
+main() {
+  Expect.equals(0, C(0).x);
+  Expect.equals(1, C.foo().x);
+  Expect.equals(2, C.foo(2).x);
+  Expect.equals(2, C.bar().x);
+  Expect.equals(3, C.bar(x: 3).x);
+  Expect.equals(4, C.baz(x: 4).x);
+
+  Expect.equals(0, E.e0.x);
+  Expect.equals(1, E.e1.x);
+  Expect.equals(2, E.e2.x);
+  Expect.equals(2, E.e3.x);
+  Expect.equals(3, E.e4.x);
+  Expect.equals(4, E.e5.x);
+
+  Expect.equals(0, ET(0).x);
+  Expect.equals(1, ET.foo().x);
+  Expect.equals(2, ET.foo(2).x);
+  Expect.equals(2, ET.bar().x);
+  Expect.equals(3, ET.bar(x: 3).x);
+  Expect.equals(4, ET.baz(x: 4).x);
+}
--- LanguageFeatures/Augmentations/complete_declarations_A01_t11.dart
+++ LanguageFeatures/Augmentations/complete_declarations_A01_t11.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -16,67 +16,96 @@
 /// It's a compile-time error if an augmentation is complete and any declaration
 /// before it in the augmentation chain is also complete.
 ///
-/// @description Checks that it is a compile-time error if augmentation adds a
-/// body to an already completed declaration. Test declarations in different
-/// part files.
+/// @description Checks that it is a compile-time error if both the introductory
+/// and the augmenting constructors have initializing formals.
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=augmentations
 
-part 'complete_declarations_A01_t10_lib1.dart';
-part 'complete_declarations_A01_t10_lib3.dart';
-
 class C {
-  C() {}
-  C.id() {}
-
-  augment C() {}
-//            ^
-// [analyzer] unspecified
-// [cfe] unspecified
-  augment C.id() {}
-//               ^
-// [analyzer] unspecified
-// [cfe] unspecified
+  int x;
+  C(this.x);
+  C.foo([this.x]);
+  C.bar({this.x});
+  C.baz({required this.x});
 }
 
 augment class C {
-  augment C() {}
-//            ^
+  augment C(this.x);
+//        ^
 // [analyzer] unspecified
 // [cfe] unspecified
-  augment C.id() {}
-//               ^
+  augment C.foo([this.x = 0]);
+//        ^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  augment C.bar({this.x = 0});
+//        ^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  augment C.baz({required this.x});
+//        ^^^^^
 // [analyzer] unspecified
 // [cfe] unspecified
 }
 
-extension type ET._(int id) {
-  ET(this.id) {}
-  ET.foo(this.id) {}
+enum E {
+  e0(0);
 
-  augment ET(int id) {}
-//                   ^
+  final int x;
+  const C(this.x);
+  const C.foo([this.x]);
+  const C.bar({this.x});
+  const C.baz({required this.x});
+}
+
+augment enum E {
+  augment const E(this.x);
+//              ^
 // [analyzer] unspecified
 // [cfe] unspecified
-  augment ET.foo(this.id) {}
-//                        ^
+  augment const E.foo([this.x = 0]);
+//              ^^^^^
 // [analyzer] unspecified
 // [cfe] unspecified
+  augment const E.bar({this.x = 0});
+//              ^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  augment const E.baz({required this.x});
+//              ^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+extension type ET._(int x) {
+  ET(this.x);
+  ET.foo([this.x]);
+  ET.bar({this.x});
+  ET.baz({required this.x});
 }
 
 augment extension type ET {
-  augment ET.new(int id) {}
-//                       ^
+  augment ET(this.x);
+//        ^^
 // [analyzer] unspecified
 // [cfe] unspecified
-  augment ET.foo(this.id) {}
-//                        ^
+  augment ET.foo([this.x = 0]);
+//        ^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  augment ET.bar({this.x = 0});
+//        ^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  augment ET.baz({required this.x});
+//        ^^^^^^
 // [analyzer] unspecified
 // [cfe] unspecified
 }
 
 main() {
   print(C);
+  print(E);
   print(ET);
 }
