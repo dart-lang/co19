98912aba55e7d6997154edcbb950101bd66e5dd0
Sergey G. Grekhov <sgrekhov22@gmail.com>
2025-12-03 18:46:25 +0200

    #3182. Update `augmenting_functions_A05-07*` test (#3430)

--- LanguageFeatures/Augmentations/augmenting_functions_A05_t01.dart
+++ LanguageFeatures/Augmentations/augmenting_functions_A05_t01.dart
@@ -2,73 +2,164 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion It is a compile-time error if:
+/// @assertion A top-level function, static method, or instance method may be
+/// augmented to provide default values for optional parameters.
 /// ...
-/// - The augmenting function specifies any default values.
+/// An optional formal parameter has the default value `d` if exactly one
+/// declaration of that formal parameter in the augmentation chain specifies a
+/// default value, and it is `d`. An optional formal parameter does not have an
+/// explicitly specified default value if none of its declarations in the
+/// augmentation chain specifies a default value. The default value is
+/// introduced implicitly with the value `null` in the case where the parameter
+/// has a nullable declared type, and no default values for that parameter are
+/// specified in the augmentation chain.
 ///
-/// @description Checks that it is a compile-time error if a function
-/// augmentation specifies any default values.
+/// @description Checks that an augmentation may provide default values for
+/// optional parameters.
 /// @author sgrekhov22@gmail.com
-/// @issue 55569
 
-// SharedOptions=--enable-experiment=macros
+// SharedOptions=--enable-experiment=augmentations
 
-part 'augmenting_functions_A05_t01_lib.dart';
+import '../../Utils/expect.dart';
 
-void topLevelFunction1([int i = 1]) {}
-void topLevelFunction2({int i = 1}) {}
+int topLevelFunction1([int i]) => i;
+int topLevelFunction2({int i}) => i;
+
+augment int topLevelFunction1([int i = 1]);
+augment int topLevelFunction2({int i = 2});
 
 class C {
-  static void staticMethod1([int i = 1]) {}
-  static void staticMethod2({int i = 1}) {}
-  void instanceMethod1([int i = 1]) {}
-  void instanceMethod2([covariant int i = 1]) {}
-  void instanceMethod3({int i = 1}) {}
-  void instanceMethod4({covariant int i = 1}) {}
+  static int staticMethod1([int i]) => i;
+  static int staticMethod2({int i}) => i;
+  int instanceMethod1([int i]) => i;
+  int instanceMethod2([covariant int i]) => i;
+  int instanceMethod3({int i}) => i;
+  int instanceMethod4({covariant int i}) => i;
+}
+
+augment class C {
+  augment static int staticMethod1([int i = 1]);
+  augment static int staticMethod2({int i = 2});
+  augment int instanceMethod1([int i = 3]);
+  augment int instanceMethod2([covariant int i = 4]);
+  augment int instanceMethod3({int i = 5});
+  augment int instanceMethod4({covariant int i = 6});
 }
 
 mixin M {
-  static void staticMethod1([int i = 1]) {}
-  static void staticMethod2({int i = 1}) {}
-  void instanceMethod1([int i = 1]) {}
-  void instanceMethod2([covariant int i = 1]) {}
-  void instanceMethod3({int i = 1}) {}
-  void instanceMethod4({covariant int i = 1}) {}
+  static int staticMethod1([int i]) => i;
+  static int staticMethod2({int i}) => i;
+  int instanceMethod1([int i]) => i;
+  int instanceMethod2([covariant int i]) => i;
+  int instanceMethod3({int i}) => i;
+  int instanceMethod4({covariant int i}) => i;
+}
+
+augment mixin M {
+  augment static int staticMethod1([int i = 1]);
+  augment static int staticMethod2({int i = 2});
+  augment int instanceMethod1([int i = 3]);
+  augment int instanceMethod2([covariant int i = 4]);
+  augment int instanceMethod3({int i = 5});
+  augment int instanceMethod4({covariant int i = 6});
 }
 
 enum E {
   e1;
+  static int staticMethod1([int i]) => i;
+  static int staticMethod2({int i}) => i;
+  int instanceMethod1([int i]) => i;
+  int instanceMethod2([covariant int i]) => i;
+  int instanceMethod3({int i}) => i;
+  int instanceMethod4({covariant int i}) => i;
+}
 
-  static void staticMethod1([int i = 1]) {}
-  static void staticMethod2({int i = 1}) {}
-  void instanceMethod1([int i = 1]) {}
-  void instanceMethod2([covariant int i = 1]) {}
-  void instanceMethod3({int i = 1}) {}
-  void instanceMethod4({covariant int i = 1}) {}
+augment enum E {
+  ;
+  augment static int staticMethod1([int i = 1]);
+  augment static int staticMethod2({int i = 2});
+  augment int instanceMethod1([int i = 3]);
+  augment int instanceMethod2([covariant int i = 4]);
+  augment int instanceMethod3({int i = 5});
+  augment int instanceMethod4({covariant int i = 6});
 }
 
 class A {}
 
 extension Ext on A {
-  static void staticMethod1([int i = 1]) {}
-  static void staticMethod2({int i = 1}) {}
-  void instanceMethod1([int i = 1]) {}
-  void instanceMethod2({int i = 1}) {}
+  static int staticMethod1([int i]) => i;
+  static int staticMethod2({int i}) => i;
+  int instanceMethod1([int i]) => i;
+  int instanceMethod2([covariant int i]) => i;
+  int instanceMethod3({int i}) => i;
+  int instanceMethod4({covariant int i}) => i;
+}
+
+augment extension Ext {
+  augment static int staticMethod1([int i = 1]);
+  augment static int staticMethod2({int i = 2});
+  augment int instanceMethod1([int i = 3]);
+  augment int instanceMethod2([covariant int i = 4]);
+  augment int instanceMethod3({int i = 5});
+  augment int instanceMethod4({covariant int i = 6});
 }
 
 extension type ET(int _) {
-  static void staticMethod1([int i = 1]) {}
-  static void staticMethod2({int i = 1}) {}
-  void instanceMethod1([int i = 1]) {}
-  void instanceMethod2({int i = 1}) {}
+  static int staticMethod1([int i]) => i;
+  static int staticMethod2({int i}) => i;
+  int instanceMethod1([int i]) => i;
+  int instanceMethod2([covariant int i]) => i;
+  int instanceMethod3({int i}) => i;
+  int instanceMethod4({covariant int i}) => i;
 }
 
+augment extension type ET {
+  augment static int staticMethod1([int i = 1]);
+  augment static int staticMethod2({int i = 2});
+  augment int instanceMethod1([int i = 3]);
+  augment int instanceMethod2([covariant int i = 4]);
+  augment int instanceMethod3({int i = 5});
+  augment int instanceMethod4({covariant int i = 6});
+}
+
+class MA = Object with M;
+
 main() {
-  print(topLevelFunction1);
-  print(topLevelFunction2);
-  print(C);
-  print(M);
-  print(E);
-  print(A);
-  print(ET);
+  Expect.equals(1, topLevelFunction1());
+  Expect.equals(2, topLevelFunction2());
+
+  Expect.equals(1, C.staticMethod1());
+  Expect.equals(2, C.staticMethod2());
+  Expect.equals(3, C().instanceMethod1());
+  Expect.equals(4, C().instanceMethod2());
+  Expect.equals(5, C().instanceMethod3());
+  Expect.equals(6, C().instanceMethod4());
+
+  Expect.equals(1, M.staticMethod1());
+  Expect.equals(2, M.staticMethod2());
+  Expect.equals(3, MA().instanceMethod1());
+  Expect.equals(4, MA().instanceMethod2());
+  Expect.equals(5, MA().instanceMethod3());
+  Expect.equals(6, MA().instanceMethod4());
+
+  Expect.equals(1, E.staticMethod1());
+  Expect.equals(2, E.staticMethod2());
+  Expect.equals(3, E.e1.instanceMethod1());
+  Expect.equals(4, E.e1.instanceMethod2());
+  Expect.equals(5, E.e1.instanceMethod3());
+  Expect.equals(6, E.e1.instanceMethod4());
+
+  Expect.equals(1, Ext.staticMethod1());
+  Expect.equals(2, Ext.staticMethod2());
+  Expect.equals(3, A().e1.instanceMethod1());
+  Expect.equals(4, A().e1.instanceMethod2());
+  Expect.equals(5, A().e1.instanceMethod3());
+  Expect.equals(6, A().e1.instanceMethod4());
+
+  Expect.equals(1, ET.staticMethod1());
+  Expect.equals(2, ET.staticMethod2());
+  Expect.equals(3, ET(0).e1.instanceMethod1());
+  Expect.equals(4, ET(0).e1.instanceMethod2());
+  Expect.equals(5, ET(0).e1.instanceMethod3());
+  Expect.equals(6, ET(0).e1.instanceMethod4());
 }
--- LanguageFeatures/Augmentations/augmenting_functions_A05_t02.dart
+++ LanguageFeatures/Augmentations/augmenting_functions_A05_t02.dart
@@ -2,84 +2,226 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion It is a compile-time error if:
+/// @assertion A top-level function, static method, or instance method may be
+/// augmented to provide default values for optional parameters.
 /// ...
-/// - The augmenting function specifies any default values.
+/// An optional formal parameter has the default value `d` if exactly one
+/// declaration of that formal parameter in the augmentation chain specifies a
+/// default value, and it is `d`. An optional formal parameter does not have an
+/// explicitly specified default value if none of its declarations in the
+/// augmentation chain specifies a default value. The default value is
+/// introduced implicitly with the value `null` in the case where the parameter
+/// has a nullable declared type, and no default values for that parameter are
+/// specified in the augmentation chain.
 ///
-/// @description Checks that it is a compile-time error if a function
-/// augmentation specifies any default values.
+/// @description Checks that it is not an error if a function augmentation
+/// doesn't specify default values for optional parameters. In this case the
+/// default value is `null` if the parameter has a nullable declared type.
 /// @author sgrekhov22@gmail.com
-/// @issue 55569
 
-// SharedOptions=--enable-experiment=macros
+// SharedOptions=--enable-experiment=augmentations
 
-part 'augmenting_functions_A05_t02_lib.dart';
+import '../../Utils/expect.dart';
 
-void topLevelFunction1([int? i]) {}
-void topLevelFunction2({int? i}) {}
-void topLevelFunction3({required int? i}) {}
+int? topLevelFunction1([int? i]) => i;
+int? topLevelFunction2({int? i}) => i;
+topLevelFunction3({i}) => i;
+topLevelFunction4({i}) => i;
 
 class C {
-  static void staticMethod1([int? i]) {}
-  static void staticMethod2({int? i}) {}
-  static void staticMethod3({required int? i}) {}
-  void instanceMethod1([int? i]) {}
-  void instanceMethod2([covariant int? i]) {}
-  void instanceMethod3({int? i}) {}
-  void instanceMethod4({covariant int? i}) {}
-  void instanceMethod5({required int? i}) {}
+  static int? staticMethod1([int? i]) => i;
+  static int? staticMethod2({int? i}) => i;
+  static staticMethod3([i]) => i;
+  static staticMethod4({i}) => i;
+  int? instanceMethod1([int? i]) => i;
+  int? instanceMethod2([covariant int? i]) => i;
+  int? instanceMethod3({int? i}) => i;
+  int? instanceMethod4({covariant int? i}) => i;
+  instanceMethod5([i]) => i;
+  instanceMethod6({i}) => i;
+}
+
+augment class C {
+  augment static int? staticMethod1([int? i]);
+  augment static int? staticMethod2({int? i});
+  augment static staticMethod3([i]);
+  augment static staticMethod4({i});
+  augment int? instanceMethod1([int? i]);
+  augment int? instanceMethod2([covariant int? i]);
+  augment int? instanceMethod3({int? i});
+  augment int? instanceMethod4({covariant int? i});
+  augment instanceMethod5([i]);
+  augment instanceMethod6({i});
 }
 
 mixin M {
-  static void staticMethod1([int? i]) {}
-  static void staticMethod2({int? i}) {}
-  static void staticMethod3({required int? i}) {}
-  void instanceMethod1([int? i]) {}
-  void instanceMethod2([covariant int? i]) {}
-  void instanceMethod3({int? i}) {}
-  void instanceMethod4({covariant int? i}) {}
-  void instanceMethod5({required int? i}) {}
+  static int? staticMethod1([int? i]) => i;
+  static int? staticMethod2({int? i}) => i;
+  static staticMethod3([i]) => i;
+  static staticMethod4({i}) => i;
+  int? instanceMethod1([int? i]) => i;
+  int? instanceMethod2([covariant int? i]) => i;
+  int? instanceMethod3({int? i}) => i;
+  int? instanceMethod4({covariant int? i}) => i;
+  instanceMethod5([i]) => i;
+  instanceMethod6({i}) => i;
+}
+
+augment mixin M {
+  augment static int? staticMethod1([int? i]);
+  augment static int? staticMethod2({int? i});
+  augment static staticMethod3([i]);
+  augment static staticMethod4({i});
+  augment int? instanceMethod1([int? i]);
+  augment int? instanceMethod2([covariant int? i]);
+  augment int? instanceMethod3({int? i});
+  augment int? instanceMethod4({covariant int? i});
+  augment instanceMethod5([i]);
+  augment instanceMethod6({i});
 }
 
 enum E {
   e1;
+  static int? staticMethod1([int? i]) => i;
+  static int? staticMethod2({int? i}) => i;
+  static staticMethod3([i]) => i;
+  static staticMethod4({i}) => i;
+  int? instanceMethod1([int? i]) => i;
+  int? instanceMethod2([covariant int? i]) => i;
+  int? instanceMethod3({int? i}) => i;
+  int? instanceMethod4({covariant int? i}) => i;
+  instanceMethod5([i]) => i;
+  instanceMethod6({i}) => i;
+}
 
-  static void staticMethod1([int? i]) {}
-  static void staticMethod2({int? i}) {}
-  static void staticMethod3({required int? i}) {}
-  void instanceMethod1([int? i]) {}
-  void instanceMethod2([covariant int? i]) {}
-  void instanceMethod3({int? i}) {}
-  void instanceMethod4({covariant int? i}) {}
-  void instanceMethod5({required int? i}) {}
+augment enum E {
+  ;
+  augment static int? staticMethod1([int? i]);
+  augment static int? staticMethod2({int? i});
+  augment static staticMethod3([i]);
+  augment static staticMethod4({i});
+  augment int? instanceMethod1([int? i]);
+  augment int? instanceMethod2([covariant int? i]);
+  augment int? instanceMethod3({int? i});
+  augment int? instanceMethod4({covariant int? i});
+  augment instanceMethod5([i]);
+  augment instanceMethod6({i});
 }
 
 class A {}
 
 extension Ext on A {
-  static void staticMethod1([int? i]) {}
-  static void staticMethod2({int? i}) {}
-  static void staticMethod3({required int? i}) {}
-  void instanceMethod1([int? i]) {}
-  void instanceMethod2({int? i}) {}
-  void instanceMethod3({required int? i}) {}
+  static int? staticMethod1([int? i]) => i;
+  static int? staticMethod2({int? i}) => i;
+  static staticMethod3([i]) => i;
+  static staticMethod4({i}) => i;
+  int? instanceMethod1([int? i]) => i;
+  int? instanceMethod2([covariant int? i]) => i;
+  int? instanceMethod3({int? i}) => i;
+  int? instanceMethod4({covariant int? i}) => i;
+  instanceMethod5([i]) => i;
+  instanceMethod6({i}) => i;
+}
+
+augment extension Ext {
+  augment static int? staticMethod1([int? i]);
+  augment static int? staticMethod2({int? i});
+  augment static staticMethod3([i]);
+  augment static staticMethod4({i});
+  augment int? instanceMethod1([int? i]);
+  augment int? instanceMethod2([covariant int? i]);
+  augment int? instanceMethod3({int? i});
+  augment int? instanceMethod4({covariant int? i});
+  augment instanceMethod5([i]);
+  augment instanceMethod6({i});
 }
 
 extension type ET(int _) {
-  static void staticMethod1([int? i]) {}
-  static void staticMethod2({int? i}) {}
-  static void staticMethod3({required int? i}) {}
-  void instanceMethod1([int? i]) {}
-  void instanceMethod2({int? i}) {}
-  void instanceMethod3({required int? i}) {}
+  static int? staticMethod1([int? i]) => i;
+  static int? staticMethod2({int? i}) => i;
+  static staticMethod3([i]) => i;
+  static staticMethod4({i}) => i;
+  int? instanceMethod1([int? i]) => i;
+  int? instanceMethod2([covariant int? i]) => i;
+  int? instanceMethod3({int? i}) => i;
+  int? instanceMethod4({covariant int? i}) => i;
+  instanceMethod5([i]) => i;
+  instanceMethod6({i}) => i;
 }
 
+augment extension type ET {
+  augment static int? staticMethod1([int? i]);
+  augment static int? staticMethod2({int? i});
+  augment static staticMethod3([i]);
+  augment static staticMethod4({i});
+  augment int? instanceMethod1([int? i]);
+  augment int? instanceMethod2([covariant int? i]);
+  augment int? instanceMethod3({int? i});
+  augment int? instanceMethod4({covariant int? i});
+  augment instanceMethod5([i]);
+  augment instanceMethod6({i});
+}
+
+class MA = Object with M;
+
 main() {
-  print(topLevelFunction1);
-  print(topLevelFunction2);
-  print(C);
-  print(M);
-  print(E);
-  print(A);
-  print(ET);
+  Expect.isNull(topLevelFunction1());
+  Expect.isNull(topLevelFunction2());
+  Expect.isNull(topLevelFunction3());
+  Expect.isNull(topLevelFunction4());
+
+  Expect.isNull(C.staticMethod1());
+  Expect.isNull(C.staticMethod2());
+  Expect.isNull(C.staticMethod3());
+  Expect.isNull(C.staticMethod4());
+  Expect.isNull(C().instanceMethod1());
+  Expect.isNull(C().instanceMethod2());
+  Expect.isNull(C().instanceMethod3());
+  Expect.isNull(C().instanceMethod4());
+  Expect.isNull(C().instanceMethod5());
+  Expect.isNull(C().instanceMethod6());
+
+  Expect.isNull(M.staticMethod1());
+  Expect.isNull(M.staticMethod2());
+  Expect.isNull(M.staticMethod3());
+  Expect.isNull(M.staticMethod4());
+  Expect.isNull(MA().instanceMethod1());
+  Expect.isNull(MA().instanceMethod2());
+  Expect.isNull(MA().instanceMethod3());
+  Expect.isNull(MA().instanceMethod4());
+  Expect.isNull(MA().instanceMethod5());
+  Expect.isNull(MA().instanceMethod6());
+
+  Expect.isNull(E.staticMethod1());
+  Expect.isNull(E.staticMethod2());
+  Expect.isNull(E.staticMethod3());
+  Expect.isNull(E.staticMethod4());
+  Expect.isNull(E.e1.instanceMethod1());
+  Expect.isNull(E.e1.instanceMethod2());
+  Expect.isNull(E.e1.instanceMethod3());
+  Expect.isNull(E.e1.instanceMethod4());
+  Expect.isNull(E.e1.instanceMethod5());
+  Expect.isNull(E.e1.instanceMethod6());
+
+  Expect.isNull(Ext.staticMethod1());
+  Expect.isNull(Ext.staticMethod2());
+  Expect.isNull(Ext.staticMethod3());
+  Expect.isNull(Ext.staticMethod4());
+  Expect.isNull(A().instanceMethod1());
+  Expect.isNull(A().instanceMethod2());
+  Expect.isNull(A().instanceMethod3());
+  Expect.isNull(A().instanceMethod4());
+  Expect.isNull(A().instanceMethod5());
+  Expect.isNull(A().instanceMethod6());
+
+  Expect.isNull(ET.staticMethod1());
+  Expect.isNull(ET.staticMethod2());
+  Expect.isNull(ET.staticMethod3());
+  Expect.isNull(ET.staticMethod4());
+  Expect.isNull(ET(0).instanceMethod1());
+  Expect.isNull(ET(0).instanceMethod2());
+  Expect.isNull(ET(0).instanceMethod3());
+  Expect.isNull(ET(0).instanceMethod4());
+  Expect.isNull(ET(0).instanceMethod5());
+  Expect.isNull(ET(0).instanceMethod6());
 }
