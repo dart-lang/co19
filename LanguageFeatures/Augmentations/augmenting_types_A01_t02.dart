2de03d2463e4e659f663ebab8f4f513c1c51417c
sgrekhov <sgrekhov22@gmail.com>
2025-12-22 15:46:53 +0200

    #3182. Update assertions in augmenting_types_A01_*.dart

--- LanguageFeatures/Augmentations/augmenting_types_A01_t02.dart
+++ LanguageFeatures/Augmentations/augmenting_types_A01_t02.dart
@@ -2,19 +2,15 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion It's a compile-time error if a library contains two top-level
-/// declarations with the same name, and:
-/// - Neither is an augmenting declaration, or
-/// - one of the declarations is a class-like declarations and the other is not
-///   of the same kind, meaning that at either one is a class, mixin, enum,
-///   extension or extension type declaration, and the other is not the same
-///   kind of declaration.
+/// @assertion It's a compile-time error if an augmentation doesn't have the
+/// same kind as the introductory declaration. For example, augmenting a `class`
+/// with a `mixin`, an `enum` with a function, a method with a getter, etc.
 ///
 /// @description Checks that it is a compile-time error if an augmented
 /// declaration is a type alias and an augmenting declaration is not
 /// @author sgrekhov22@gmail.com
 
-// SharedOptions=--enable-experiment=macros
+// SharedOptions=--enable-experiment=augmentations
 
 part 'augmenting_types_A01_t02_lib1.dart';
 part 'augmenting_types_A01_t02_lib2.dart';
--- LanguageFeatures/Augmentations/augmenting_types_A01_t05_lib.dart
+++ LanguageFeatures/Augmentations/augmenting_types_A01_t05_lib.dart
@@ -2,18 +2,14 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion It's a compile-time error if a library contains two top-level
-/// declarations with the same name, and:
-/// - Neither is an augmenting declaration, or
-/// - one of the declarations is a class-like declarations and the other is not
-///   of the same kind, meaning that at either one is a class, mixin, enum,
-///   extension or extension type declaration, and the other is not the same
-///   kind of declaration.
+/// @assertion It's a compile-time error if an augmentation doesn't have the
+/// same kind as the introductory declaration. For example, augmenting a `class`
+/// with a `mixin`, an `enum` with a function, a method with a getter, etc.
 ///
 /// @description Checks that `augment class C = ...` is a syntax error.
 /// @author sgrekhov22@gmail.com
 
-// SharedOptions=--enable-experiment=macros
+// SharedOptions=--enable-experiment=augmentations
 
 part of 'augmenting_types_A01_t05.dart';
 
