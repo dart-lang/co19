c5b89a2fc332f0966e1b5fbb1ce817197a73c15b
Sergey G. Grekhov <sgrekhov22@gmail.com>
2026-01-05 12:57:32 +0200

    Fixes #3473. Update expected errors positions, fix typos (#3486)

--- LanguageFeatures/Primary-constructors/static_processing_A02_t08.dart
+++ LanguageFeatures/Primary-constructors/static_processing_A02_t08.dart
@@ -16,35 +16,35 @@
 // SharedOptions=--enable-experiment=primary-constructors
 
 enum E1.someName(int _) {
-  e0.someName(1);
-  static int someName = 0;
-//           ^^^^^^^^
+//      ^^^^^^^^
 // [analyzer] unspecified
 // [cfe] unspecified
+  e0.someName(1);
+  static int someName = 0;
 }
 
 enum E2.someName(int _) {
+//      ^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
   e0.someName(2);
   static int get someName => 0;
-//               ^^^^^^^^
-// [analyzer] unspecified
-// [cfe] unspecified
 }
 
-enum ET3._someName(int _) {
+enum E3._someName(int _) {
+//      ^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
   e0._someName(3);
   static void _someName(int _) {}
-//            ^^^^^^^^^
-// [analyzer] unspecified
-// [cfe] unspecified
 }
 
-enum ET4.someName(int _) {
-  e4.someName(4);
-  static void set someName(int _) {}
-//                ^^^^^^^^
+enum E4.someName(int _) {
+//      ^^^^^^^^
 // [analyzer] unspecified
 // [cfe] unspecified
+  e4.someName(4);
+  static void set someName(int _) {}
 }
 
 main() {
