2055d8fadc7cf4133bcbb1b2b44d2e317381bb95
sgrekhov <sgrekhov22@gmail.com>
2026-01-13 11:04:54 +0200

    Fixes #3535. Add additional test for conflicting members.

--- LanguageFeatures/Extension-types/static_analysis_extension_types_A12_t03.dart
+++ LanguageFeatures/Extension-types/static_analysis_extension_types_A12_t03.dart
@@ -2,9 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion It is a compile-time error if DV is an extension type
-/// declaration, and DV has a non-extension type member named m as well as an
-/// extension type member named m, for any m.
+/// @assertion It is a compile-time error if `DV` is an extension type
+/// declaration, and `DV` has a non-extension type member named `m` which is not
+/// precluded by `DV` as well as an extension type member named `m` which is not
+/// precluded by `DV`, for any `m`. In case of conflicts, `DV` must declare a
+/// member named `m` to resolve the conflict.
 ///
 /// @description Checks that it is a compile-time error if an extension type
 /// declaration has a non-extension type member named `m` as well as an
