393287694b6a827bfa2e00b3c3bf67f0ddf88c05
Sergey G. Grekhov <sgrekhov22@gmail.com>
2026-01-20 17:54:33 +0200

    Fixes #3556. Use `unspecified` expected error (#3557)
    Use `unspecified` expected error

--- Language/Generics/function_A02_t01.dart
+++ Language/Generics/function_A02_t01.dart
@@ -7,9 +7,9 @@
 /// [f<T1, . . . , Tl>(...)], where the static type of [f] is a generic function
 /// type with formal type parameters [X1 extends B1], . . . , [Xm extends Bm].
 /// It is a compile-time error if [m <> l].
+///
 /// @description checks that error appears if number of type parameters is
 /// @author iarkh@unipro.ru
-
 
 void testme1<T>() {}
 T1? testme2<T1, T2>(T2 t) {}
@@ -20,49 +20,41 @@ main() {
   testme1<int>();
   testme1<dynamic>();
   testme1<int, dynamic>();
-//^
-// [cfe] Expected 1 type arguments.
 //       ^^^^^^^^^^^^^^
-// [analyzer] COMPILE_TIME_ERROR.WRONG_NUMBER_OF_TYPE_ARGUMENTS_METHOD
+// [cfe] unspecified
+// [analyzer] unspecified
   testme1<dynamic, int, int>();
-//^
-// [cfe] Expected 1 type arguments.
 //       ^^^^^^^^^^^^^^^^^^^
-// [analyzer] COMPILE_TIME_ERROR.WRONG_NUMBER_OF_TYPE_ARGUMENTS_METHOD
+// [cfe] unspecified
+// [analyzer] unspecified
 
   testme2(1);
   testme2<int>(12);
-//^
-// [cfe] Expected 2 type arguments.
 //       ^^^^^
-// [analyzer] COMPILE_TIME_ERROR.WRONG_NUMBER_OF_TYPE_ARGUMENTS_METHOD
+// [cfe] unspecified
+// [analyzer] unspecified
   testme2<dynamic>(333);
-//^
-// [cfe] Expected 2 type arguments.
 //       ^^^^^^^^^
-// [analyzer] COMPILE_TIME_ERROR.WRONG_NUMBER_OF_TYPE_ARGUMENTS_METHOD
+// [cfe] unspecified
+// [analyzer] unspecified
   testme2<int, dynamic>(289);
   testme2<dynamic, int, int>(1);
-//^
-// [cfe] Expected 2 type arguments.
 //       ^^^^^^^^^^^^^^^^^^^
-// [analyzer] COMPILE_TIME_ERROR.WRONG_NUMBER_OF_TYPE_ARGUMENTS_METHOD
+// [cfe] unspecified
+// [analyzer] unspecified
 
   testme3();
   testme3<int>();
-//^
-// [cfe] Expected 3 type arguments.
 //       ^^^^^
-// [analyzer] COMPILE_TIME_ERROR.WRONG_NUMBER_OF_TYPE_ARGUMENTS_METHOD
+// [cfe] unspecified
+// [analyzer] unspecified
   testme3<int, dynamic>();
-//^
-// [cfe] Expected 3 type arguments.
 //       ^^^^^^^^^^^^^^
-// [analyzer] COMPILE_TIME_ERROR.WRONG_NUMBER_OF_TYPE_ARGUMENTS_METHOD
+// [cfe] unspecified
+// [analyzer] unspecified
   testme3<dynamic, int, int>();
   testme3<dynamic, int, int, String>();
-//^
-// [cfe] Expected 3 type arguments.
 //       ^^^^^^^^^^^^^^^^^^^^^^^^^^^
-// [analyzer] COMPILE_TIME_ERROR.WRONG_NUMBER_OF_TYPE_ARGUMENTS_METHOD
+// [cfe] unspecified
+// [analyzer] unspecified
 }
