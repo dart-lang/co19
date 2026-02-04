ee47d771126e32d193e6420b76c9513db540e59e
sgrekhov <sgrekhov22@gmail.com>
2026-02-04 10:20:49 +0200

    Fixes #3574. Update expected error location in `static_processing_A03_t02.dart`

--- LanguageFeatures/Primary-constructors/static_processing_A03_t02.dart
+++ LanguageFeatures/Primary-constructors/static_processing_A03_t02.dart
@@ -40,11 +40,11 @@ base mixin M7.someName(); // Constructor is "trivial" but it's not a "mixin clas
 // [cfe] unspecified
 
 mixin class M8.someName() { // Constructor is "non-trivial", it has in-body initializer list
-//             ^^^^^^^^
-// [analyzer] unspecified
-// [cfe] unspecified
   int x;
   this: x = 0;
+//^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
 }
 
 main() {
