149e7021273f56ccfad1a513b484072b78c45dab
sgrekhov <sgrekhov22@gmail.com>
2026-02-10 16:01:07 +0200

    #3182. Add tests for augmentation of primary constructors. Part 2.

---
+++ LanguageFeatures/Augmentations/augmenting_constructors_A10_t17.dart
@@ -0,0 +1,99 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion We say that an augmenting function or constructor's signature
+/// matches if:
+/// - It has the same number of type parameters with the same type parameter
+///   names (same identifiers) and bounds (after type annotation inheritance),
+///   if any (same types, even if they may not be written exactly the same in
+///   case one of the declarations needs to refer to a type using an import
+///   prefix).
+/// - The return type (if not omitted) is the same as the augmented
+///   declaration's return type.
+/// - It has the same number of positional and optional parameters as the
+///   augmented declaration.
+/// - It has the same set of named parameter names as the augmented declaration.
+/// - For all corresponding pairs of parameters:
+///   - They have the same type (if not omitted in the augmenting declaration).
+///   - They have the same `required` and `covariant` modifiers.
+/// - For all positional parameters:
+///   - The augmenting function's parameter name is `_`, or
+///   - The augmenting function's parameter name is the same as the name of the
+///     corresponding positional parameter in every preceding declaration that
+///     doesn't have `_` as its name.
+/// ...
+/// It is a compile-time error if:
+/// - The signature of the augmenting function does not match the signature of
+///   the augmented function.
+///
+/// @description Checks that it is not an error if an augmentation of a primary
+/// constructor omits types of declaring parameters.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=augmentations,primary-constructors
+
+import '../../Utils/expect.dart';
+import '../../Utils/static_type_helper.dart';
+
+class C1(var int x, [final int y = 0]) {}
+
+augment class C1 {
+  augment C1(x, [y]);
+}
+
+class C2({required final int x, var int y = 0}) {}
+
+augment class C2 {
+  augment C2({required x, y});
+}
+
+enum E1(final int x, [final int y = 0]) {
+  e0(0), e1(1, 2);
+}
+
+augment enum E1 {
+  ;
+  augment E1(x, [y]);
+}
+
+enum E2({final required int x, final int y = 0}) {
+  e0(x: 1), e1(x: 1, y: 2);
+}
+
+augment enum E2 {
+  ;
+  augment E2({required x, y});
+}
+
+extension type ET(int x) {}
+
+augment extension type ET(int x) {
+  augment ET(x);
+}
+
+main() {
+  C1.new.expectStaticType<Exactly<C1 Function(int x, [int y])>>();
+  C2.new.expectStaticType<Exactly<C2 Function({required int x, int y})>>();
+  ET.new.expectStaticType<Exactly<ET Function(int x)>>();
+
+  Expect.equals(1, C1(1).x);
+  Expect.equals(0, C1(1).y);
+  Expect.equals(1, C1(1, 2).x);
+  Expect.equals(2, C1(1, 2).y);
+  Expect.equals(1, C2(x: 1).x);
+  Expect.equals(0, C2(x: 1).y);
+  Expect.equals(1, C2(x: 1, y: 2).x);
+  Expect.equals(2, C2(x: 1, y: 2).y);
+
+  Expect.equals(1, E1.e0.x);
+  Expect.equals(0, E1.e0.y);
+  Expect.equals(1, E1.e1.x);
+  Expect.equals(2, E1.e1.y);
+  Expect.equals(1, E2.e0.x);
+  Expect.equals(0, E2.e0.y);
+  Expect.equals(1, E2.e1.x);
+  Expect.equals(2, E2.e1.y);
+
+  Expect.equals(1, ET(1).x);
+}
---
+++ LanguageFeatures/Augmentations/augmenting_constructors_A10_t20.dart
@@ -0,0 +1,126 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion We say that an augmenting function or constructor's signature
+/// matches if:
+/// - It has the same number of type parameters with the same type parameter
+///   names (same identifiers) and bounds (after type annotation inheritance),
+///   if any (same types, even if they may not be written exactly the same in
+///   case one of the declarations needs to refer to a type using an import
+///   prefix).
+/// - The return type (if not omitted) is the same as the augmented
+///   declaration's return type.
+/// - It has the same number of positional and optional parameters as the
+///   augmented declaration.
+/// - It has the same set of named parameter names as the augmented declaration.
+/// - For all corresponding pairs of parameters:
+///   - They have the same type (if not omitted in the augmenting declaration).
+///   - They have the same `required` and `covariant` modifiers.
+/// - For all positional parameters:
+///   - The augmenting function's parameter name is `_`, or
+///   - The augmenting function's parameter name is the same as the name of the
+///     corresponding positional parameter in every preceding declaration that
+///     doesn't have `_` as its name.
+/// ...
+/// It is a compile-time error if:
+/// - The signature of the augmenting function does not match the signature of
+///   the augmented function.
+///
+/// @description Checks that it is not an error if primary constructor formal
+/// parameters implicitly have type `dynamic` and the augmenting constructor
+/// specifies it explicitly.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=augmentations,primary-constructors
+
+import '../../Utils/expect.dart';
+
+class C1(x) {}
+
+augment class C1 {
+  augment C1(dynamic x) {
+    if (1 > 2) {
+      x.checkDynamic;
+    }
+  }
+}
+
+class C2([x]) {}
+
+augment class C2 {
+  augment C2([dynamic x]) {
+    if (1 > 2) {
+      x.checkDynamic;
+    }
+  }
+}
+
+class C3({x}) {}
+
+augment class C3 {
+  augment C3({dynamic x}) {
+    if (1 > 2) {
+      x.checkDynamic;
+    }
+  }
+}
+
+class C4({required x}) {}
+
+augment class C4 {
+  augment C4({required dynamic x}) {
+    if (1 > 2) {
+      x.checkDynamic;
+    }
+  }
+}
+
+enum E1(x) {
+  e0(0);
+}
+
+augment enum E1 {
+  ;
+  augment const E1(dynamic x);
+}
+
+enum E2([x]) {
+  e0;
+}
+
+augment enum E2 {
+  ;
+  augment const E2([dynamic x]);
+}
+
+enum E3({x}) {
+  e0;
+}
+
+augment enum E3 {
+  ;
+  augment const E3({dynamic x});
+}
+
+enum E4({required x}) {
+  e0(x: 1);
+}
+
+augment enum E4 {
+  ;
+  augment const E4({required dynamic x});
+}
+
+main() {
+  Expect.equals(1, C1(1).x);
+  Expect.isNull(C2().x);
+  Expect.equals(1, C2(1).x);
+  Expect.isNull(C3().x);
+  Expect.equals(1, C3(x: 1).x);
+  Expect.equals(1, C4(x: 1).x);
+  Expect.equals(0, E1.e0.x);
+  Expect.isNull(E2.e0.x);
+  Expect.isNull(E3.e0.x);
+  Expect.equals(1, E4.e0.x);
+}
