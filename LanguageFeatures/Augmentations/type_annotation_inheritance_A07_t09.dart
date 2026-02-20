e2870efc5d7cf5052613f417a2f5bbee8043cbf8
sgrekhov <sgrekhov22@gmail.com>
2026-02-20 14:06:56 +0200

    Add missing cases

---
+++ LanguageFeatures/Augmentations/type_annotation_inheritance_A07_t09.dart
@@ -0,0 +1,40 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion An augmenting declaration may omit type annotations for a return
+/// type, variable type, parameter type, or type parameter bound. In the last
+/// case, that includes omitting the `extends` keyword. For a variable, a `var`
+/// keyword replaces the type if the variable isn't `final`.
+///
+/// If a type annotation or type parameter bound is omitted in the augmenting
+/// declaration, it is inferred to be the same as the corresponding type
+/// annotation or type parameter bound in the declaration being augmented.
+///
+/// @description Checks that it not an error if an augmentation omits the type
+/// of a variable that augments a setter. Test the case when the type is
+/// obtained via override inference.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=augmentations
+
+abstract class A {
+  void set foo(int _);
+  int get foo();
+}
+
+class C implements A {
+  abstract var foo;
+}
+
+augment class C {
+  augment var foo;
+}
+
+augment class C {
+  augment set foo(_) {}
+}
+
+main() {
+  C().foo = 42;
+}
