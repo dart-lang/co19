c83fa804d180253f8b9df866e542f4af78f5b98d
Sergey G. Grekhov <sgrekhov22@gmail.com>
2026-02-20 13:04:24 +0200

    #3315. Add tests for wildcarded parameters (#3623)
    Add tests for wildcarded parameters

---
+++ LanguageFeatures/Primary-constructors/static_processing_A24_t10.dart
@@ -0,0 +1,42 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion The semantics of the primary constructor is found in the
+/// following steps, where `D` is the class, mixin class, extension type, or
+/// enum declaration in the program that includes a primary constructor `k`, and
+/// `D2` is the result of the derivation of the semantics of `D`. The derivation
+/// step will delete elements that amount to the primary constructor.
+/// Semantically, it will add a new constructor `k2`, and it will add zero or
+/// more instance variable declarations.
+/// ...
+/// If there is a primary constructor body part that contains an initializer
+/// list then `k2` has an initializer list with the same elements in the same
+/// order. If that body part has a function body then `k2` has the same function
+/// body.
+///
+/// @description Check that it is not an error if an instance variable which
+/// is implicitly induced by a declaring parameter with the name `_` is accessed
+/// in the body of the primary constructor.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=primary-constructors
+
+import '../../Utils/expect.dart';
+
+class C(var int _) {
+  this {
+    _++;
+  }
+}
+
+extension type ET(int _) {
+  this {
+    assert(_ > 0);
+  }
+}
+
+main() {
+  Expect.equals(1, C(0)._);
+  ET(1);
+}
