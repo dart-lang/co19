40ac4078ce301a60a3f8068104cfe70967b99eda
sgrekhov <sgrekhov22@gmail.com>
2026-03-27 14:33:25 +0300

    #3182. Add more tests for augmenting parameter with the name `_`

---
+++ LanguageFeatures/Augmentations/augmenting_variables_getters_setters_A02_t08.dart
@@ -0,0 +1,180 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion We say that an augmenting function or constructor's signature
+/// matches an introductory signature if:
+/// - It has the same number of type parameters with the same type parameter
+///   names (same identifiers) and bounds (after type annotation inheritance),
+///   if any (same types, even if they may not be written exactly the same in
+///   case one of the declarations needs to refer to a type using an import
+///   prefix).
+/// - The return type (if not omitted) is the same as the augmented
+///   declaration's return type.
+/// - It has the same number of positional parameters as the introductory
+///   declaration, and the same number of those are optional.
+/// - It has the same set of named parameter names as the augmented declaration.
+/// - For each corresponding pair of parameters:
+///   - They have the same name. This is trivial for named parameters, but may
+///     fail to hold for positional parameters.
+///   - They have the same type (or the augmenting declaration omits the type).
+///   - They both have the modifier `covariant`, or none of them have it.
+///   - They both have the modifier `required`. or none of them have it.
+/// - For all positional parameters:
+///   - The augmenting function's parameter name is `_`, or
+///   - The augmenting function's parameter name is the same as the name of the
+///     corresponding positional parameter in every preceding declaration that
+//      doesn't have `_` as its name.
+/// ...
+/// It's a compile-time error if:
+/// - The signature of the augmenting getter or setter does not match the
+///   signature of the corresponding introductory getter or setter.
+///
+/// @description Checks that it is a compile-time error if the parameter name of
+/// an augmenting setter doesn't exactly match the original implicit setter.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=augmentations
+
+void set topLevelSetter1(int x);
+augment void set topLevelSetter1(int _x) {}
+//                                  ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+void set topLevelSetter2(int _x) {}
+augment void set topLevelSetter2(int x);
+//                                   ^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+class C {
+  static void set staticSetter1(int x);
+  augment static void set staticSetter1(int _x) {}
+//                                          ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void set staticSetter2(int x) {}
+  augment static void set staticSetter2(int _x);
+//                                          ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  void set instanceSetter1(int x);
+  augment void set instanceSetter1(int _x) {}
+//                                     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void set instanceSetter2(int x) {}
+  augment void set instanceSetter2(int _x);
+//                                     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+mixin M {
+  static void set staticSetter1(int x);
+  augment static void set staticSetter1(int _x) {}
+//                                          ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void set staticSetter2(int x) {}
+  augment static void set staticSetter2(int _x);
+//                                          ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  void set instanceSetter1(int x);
+  augment void set instanceSetter1(int _x) {}
+//                                     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void set instanceSetter2(int x) {}
+  augment void set instanceSetter2(int _x);
+//                                     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+enum E {
+  e0;
+  static void set staticSetter1(int x);
+  augment static void set staticSetter1(int _x) {}
+//                                          ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void set staticSetter2(int x) {}
+  augment static void set staticSetter2(int _x);
+//                                          ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  void set instanceSetter1(int x);
+  augment void set instanceSetter1(int _x) {}
+//                                     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void set instanceSetter2(int x) {}
+  augment void set instanceSetter2(int _x);
+//                                     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+class A {}
+
+extension Ext on A {
+  static void set staticSetter1(int x);
+  augment static void set staticSetter1(int _x) {}
+//                                          ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void set staticSetter2(int x) {}
+  augment static void set staticSetter2(int _x);
+//                                          ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  void set instanceSetter1(int x);
+  augment void set instanceSetter1(int _x) {}
+//                                     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void set instanceSetter2(int x) {}
+  augment void set instanceSetter2(int _x);
+//                                     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+extension type ET(int _) {
+  static void set staticSetter1(int x);
+  augment static void set staticSetter1(int _x) {}
+//                                          ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  static void set staticSetter2(int x) {}
+  augment static void set staticSetter2(int _x);
+//                                          ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  void set instanceSetter1(int x);
+  augment void set instanceSetter1(int _x) {}
+//                                     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void set instanceSetter2(int x) {}
+  augment void set instanceSetter2(int _x);
+//                                     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  print(C);
+  print(M);
+  print(E);
+  print(A);
+  print(ET);
+}
