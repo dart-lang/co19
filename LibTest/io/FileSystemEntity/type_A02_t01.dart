68af4b502a0194b9408f74e599df3e14415c5361
sgrekhov <sgrekhov22@gmail.com>
2026-05-05 12:48:53 +0300

    #3779. Add tests for `FileSystemEntity.type()`

---
+++ LibTest/io/FileSystemEntity/type_A02_t01.dart
@@ -0,0 +1,35 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion type static method
+/// ```
+/// Future<FileSystemEntityType> type(
+///   String path,
+///   {bool followLinks = true}
+/// )
+/// ```
+/// Finds the type of file system object that a path points to.
+///
+/// Returns a [Future<FileSystemEntityType>] that completes with the same
+/// results as [typeSync].
+///
+/// @description Checks that this method returns [FileSystemEntityType.file] if
+/// `path` points to a file.
+/// @author sgrekhov22@gmail.com
+
+import 'dart:io';
+import '../../../Utils/expect.dart';
+import '../file_utils.dart';
+
+main() async {
+  await inSandbox(_main);
+}
+
+void _main(Directory sandbox) async {
+  final file = getTempFileSync(parent: sandbox);
+  Expect.equals(
+    FileSystemEntityType.file,
+    await FileSystemEntity.type(file.path),
+  );
+}
