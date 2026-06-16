759d62686e14f760ddd27174fc85906e15ac7c2c
sgrekhov <sgrekhov22@gmail.com>
2026-06-16 15:21:15 +0300

    #3847. Add inference tests for generic methods

---
+++ TypeSystem/inference/method_override_inference_A02_t02.dart
@@ -0,0 +1,183 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion A method `m` of a class `C` is subject to override inference if
+/// it is missing one or more component types of its signature, and one or more
+/// of the direct superinterfaces of `C` has a member named `m` (that is, `C.m`
+/// overrides one or more declarations). Each missing type is filled in with the
+/// corresponding type from the combined member signature `s` of `m` in the
+/// direct superinterfaces of `C`.
+///
+/// A compile-time error occurs if `s` does not exist.
+///
+/// @description Checks that it is a compile-time error occurs if `s` does not
+/// exist. Test generic classes.
+/// @author sgrekhov22@gmail.com
+
+mixin class A<T extends num> {
+  T? m1() => null;
+  void m2(T v) {}
+  void m3([T? v]) {}
+  void m4({T? v}) {}
+  void m5({required T v}) {}
+}
+
+mixin class B<T extends String> {
+  T? m1() => null;
+  void m2(T v) {}
+  void m3([T? v]) {}
+  void m4({T? v}) {}
+  void m5({required T v}) {}
+}
+
+class C1 extends A implements B {
+  m1() => 0;
+//^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m2(v) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m3([v]) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m4({v}) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m5({required v}) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+class C2 with A implements B {
+  m1() => 0;
+//^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m2(v) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m3([v]) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m4({v}) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m5({required v}) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+mixin M on A implements B {
+  m1() => '0';
+//^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m2(v) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m3([v]) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m4({v}) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m5({required v}) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+mixin class MC implements A, B {
+  m1() => '0';
+//^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m2(v) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m3([v]) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m4({v}) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m5({required v}) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+enum E1 implements A, B {
+  e0;
+
+  m1() => '0';
+//^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m2(v) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m3([v]) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m4({v}) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m5({required v}) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+enum E2 with A implements B {
+  e0;
+
+  m1() => '0';
+//^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m2(v) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m3([v]) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m4({v}) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  void m5({required v}) {}
+//     ^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
+
+main() {
+  print(C1);
+  print(C2);
+  print(M);
+  print(MC);
+  print(E1);
+  print(E2);
+}
--- TypeSystem/inference/method_override_inference_A05_t03.dart
+++ TypeSystem/inference/method_override_inference_A05_t03.dart
@@ -8,29 +8,30 @@
 /// explicitly if needed.
 ///
 /// @description Check that missing components of a method signature are
-/// inferred as a combined member signature form a direct superinterface.
+/// inferred as a combined member signature form a direct superinterface. Test
+/// generics.
 /// @author sgrekhov22@gmail.com
 
 import '../../Utils/static_type_helper.dart';
 
-mixin class A {
-  num m1() => 3.14;
-  void m2(num v) {}
-  void m3([num v = 0]) {}
-  void m4({num v = 0}) {}
-  void m5({required num v}) {}
+mixin class A<T> {
+  T? m1() => null;
+  void m2(T v) {}
+  void m3([T? v]) {}
+  void m4({T? v}) {}
+  void m5({required T v}) {}
 }
 
-mixin class B {
-  int m1() => 42;
-  void m2(int v) {}
-  void m3([int v = 0]) {}
-  void m4({int v = 0}) {}
-  void m5({required int v}) {}
+mixin class B<T> {
+  T? m1() => null;
+  void m2(T v) {}
+  void m3([T? v]) {}
+  void m4({T? v}) {}
+  void m5({required T v}) {}
 }
 
 
-class C1 extends A implements B {
+class C1 extends A<num> implements B<int> {
   m1() => 0;
   void m2(v) {}
   void m3([v = 1]) {}
@@ -38,7 +39,7 @@ class C1 extends A implements B {
   void m5({required v}) {}
 }
 
-class C2 implements A, B {
+class C2 implements A<num>, B<int> {
   m1() => 0;
   void m2(v) {}
   void m3([v = 1]) {}
@@ -46,7 +47,7 @@ class C2 implements A, B {
   void m5({required v}) {}
 }
 
-class C3 with A implements B {
+class C3 with A<num> implements B<int> {
   m1() => 0;
   void m2(v) {}
   void m3([v = 1]) {}
@@ -54,7 +55,7 @@ class C3 with A implements B {
   void m5({required v}) {}
 }
 
-class C4 with B implements A {
+class C4 with B<int> implements A<num> {
   m1() => 0;
   void m2(v) {}
   void m3([v = 1]) {}
@@ -62,7 +63,7 @@ class C4 with B implements A {
   void m5({required v}) {}
 }
 
-mixin M implements A, B {
+mixin M implements A<num>, B<int> {
   m1() => 0;
   void m2(v) {}
   void m3([v = 1]) {}
@@ -70,7 +71,7 @@ mixin M implements A, B {
   void m5({required v}) {}
 }
 
-mixin class MC implements A, B {
+mixin class MC implements A<num>, B<int> {
   m1() => 0;
   void m2(v) {}
   void m3([v = 1]) {}
@@ -78,7 +79,7 @@ mixin class MC implements A, B {
   void m5({required v}) {}
 }
 
-enum E1 implements A, B {
+enum E1 implements A<num>, B<int> {
   e0;
 
   m1() => 0;
@@ -91,45 +92,45 @@ enum E1 implements A, B {
 class MA = Object with M;
 
 main() {
-  C1().m1.expectStaticType<Exactly<int Function()>>();
+  C1().m1.expectStaticType<Exactly<int? Function()>>();
   C1().m2.expectStaticType<Exactly<void Function(num)>>();
-  C1().m3.expectStaticType<Exactly<void Function([num])>>();
-  C1().m4.expectStaticType<Exactly<void Function({num v})>>();
+  C1().m3.expectStaticType<Exactly<void Function([num?])>>();
+  C1().m4.expectStaticType<Exactly<void Function({num? v})>>();
   C1().m5.expectStaticType<Exactly<void Function({required num v})>>();
 
-  C2().m1.expectStaticType<Exactly<int Function()>>();
+  C2().m1.expectStaticType<Exactly<int? Function()>>();
   C2().m2.expectStaticType<Exactly<void Function(num)>>();
-  C2().m3.expectStaticType<Exactly<void Function([num])>>();
-  C2().m4.expectStaticType<Exactly<void Function({num v})>>();
+  C2().m3.expectStaticType<Exactly<void Function([num?])>>();
+  C2().m4.expectStaticType<Exactly<void Function({num? v})>>();
   C2().m5.expectStaticType<Exactly<void Function({required num v})>>();
 
-  C3().m1.expectStaticType<Exactly<int Function()>>();
+  C3().m1.expectStaticType<Exactly<int? Function()>>();
   C3().m2.expectStaticType<Exactly<void Function(num)>>();
-  C3().m3.expectStaticType<Exactly<void Function([num])>>();
-  C3().m4.expectStaticType<Exactly<void Function({num v})>>();
+  C3().m3.expectStaticType<Exactly<void Function([num?])>>();
+  C3().m4.expectStaticType<Exactly<void Function({num? v})>>();
   C3().m5.expectStaticType<Exactly<void Function({required num v})>>();
 
-  C4().m1.expectStaticType<Exactly<int Function()>>();
+  C4().m1.expectStaticType<Exactly<int? Function()>>();
   C4().m2.expectStaticType<Exactly<void Function(num)>>();
-  C4().m3.expectStaticType<Exactly<void Function([num])>>();
-  C4().m4.expectStaticType<Exactly<void Function({num v})>>();
+  C4().m3.expectStaticType<Exactly<void Function([num?])>>();
+  C4().m4.expectStaticType<Exactly<void Function({num? v})>>();
   C4().m5.expectStaticType<Exactly<void Function({required num v})>>();
 
-  MA().m1.expectStaticType<Exactly<int Function()>>();
+  MA().m1.expectStaticType<Exactly<int? Function()>>();
   MA().m2.expectStaticType<Exactly<void Function(num)>>();
-  MA().m3.expectStaticType<Exactly<void Function([num])>>();
-  MA().m4.expectStaticType<Exactly<void Function({num v})>>();
+  MA().m3.expectStaticType<Exactly<void Function([num?])>>();
+  MA().m4.expectStaticType<Exactly<void Function({num? v})>>();
   MA().m5.expectStaticType<Exactly<void Function({required num v})>>();
 
-  MC().m1.expectStaticType<Exactly<int Function()>>();
+  MC().m1.expectStaticType<Exactly<int? Function()>>();
   MC().m2.expectStaticType<Exactly<void Function(num)>>();
-  MC().m3.expectStaticType<Exactly<void Function([num])>>();
-  MC().m4.expectStaticType<Exactly<void Function({num v})>>();
+  MC().m3.expectStaticType<Exactly<void Function([num?])>>();
+  MC().m4.expectStaticType<Exactly<void Function({num? v})>>();
   MC().m5.expectStaticType<Exactly<void Function({required num v})>>();
 
-  E1.e0.m1.expectStaticType<Exactly<int Function()>>();
+  E1.e0.m1.expectStaticType<Exactly<int? Function()>>();
   E1.e0.m2.expectStaticType<Exactly<void Function(num)>>();
-  E1.e0.m3.expectStaticType<Exactly<void Function([num])>>();
-  E1.e0.m4.expectStaticType<Exactly<void Function({num v})>>();
+  E1.e0.m3.expectStaticType<Exactly<void Function([num?])>>();
+  E1.e0.m4.expectStaticType<Exactly<void Function({num? v})>>();
   E1.e0.m5.expectStaticType<Exactly<void Function({required num v})>>();
 }
