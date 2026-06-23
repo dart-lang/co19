18a4087fdb98ee7a6235e471f9fb60f53b3445d5
sgrekhov <sgrekhov22@gmail.com>
2026-06-23 10:37:35 +0300

    #3847. Add tests for non-final variable inference

---
+++ TypeSystem/inference/getter_setter_override_inference_A06_t03.dart
@@ -0,0 +1,66 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion The type of a non-final field which overrides both a setter and a
+/// getter is inferred to be the parameter type of the combined member signature
+/// of said setter in the direct superinterfaces, if this type is the same as
+/// the return type of the combined member signature of said getter in the
+/// direct superinterfaces. If the types are not the same then inference fails
+/// with an error.
+///
+/// @description Check that the type of a non-final field which overrides both a
+/// setter and a getter is inferred to be the parameter type of said setter in
+/// the direct superinterface, not the type `dynamic`.
+/// @author sgrekhov22@gmail.com
+
+mixin class A {
+  void set m1(num _) {}
+  num get m1 => 0;
+  void set m2(num _) {}
+  num get m2 => 42;
+}
+
+class C1 extends A {
+  var m1 = 0;
+  var m2 = 0;
+}
+
+class C2 with A {
+  var m1 = 0;
+  var m2 = 0;
+}
+
+mixin M on A {
+  var m1 = 0;
+  var m2 = 0;
+}
+
+class MA = A with M;
+
+main() {
+  C1().m1.testNotDynamic;
+//        ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  C1().m2.testNotDynamic;
+//        ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  C2().m1.testNotDynamic;
+//        ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  C2().m2.testNotDynamic;
+//        ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  MA().m1.testNotDynamic;
+//        ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+  MA().m2.testNotDynamic;
+//        ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+}
---
+++ TypeSystem/inference/getter_setter_override_inference_A06_t11.dart
@@ -0,0 +1,50 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion The type of a non-final field which overrides both a setter and a
+/// getter is inferred to be the parameter type of the combined member signature
+/// of said setter in the direct superinterfaces, if this type is the same as
+/// the return type of the combined member signature of said getter in the
+/// direct superinterfaces. If the types are not the same then inference fails
+/// with an error.
+///
+/// @description Check that the type of a non-final field which implements both
+/// a setter and a getter is inferred to be the parameter type of said setter in
+/// the direct superinterface. Test late variables.
+/// @author sgrekhov22@gmail.com
+
+import '../../Utils/static_type_helper.dart';
+
+class A {
+  void set m1(num _) {}
+  num get m1 => 0;
+  void set m2(num _) {}
+  num get m2 => 42;
+}
+
+class C implements A {
+  late var m1 = 0;
+  late var m2 = 0;
+}
+
+mixin M implements A {
+  late var m1 = 0;
+  late var m2 = 0;
+}
+
+mixin class MC implements A {
+  late var m1 = 0;
+  late var m2 = 0;
+}
+
+class MA = A with M;
+
+main() {
+  C().m1.expectStaticType<Exactly<num>>();
+  C().m2.expectStaticType<Exactly<num>>();
+  MA().m1.expectStaticType<Exactly<num>>();
+  MA().m2.expectStaticType<Exactly<num>>();
+  MC().m1.expectStaticType<Exactly<num>>();
+  MC().m2.expectStaticType<Exactly<num>>();
+}
