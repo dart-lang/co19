1f60fc475c782803d5073716971291bf59722b49
Sergey G. Grekhov <sgrekhov22@gmail.com>
2026-06-29 11:02:26 +0300

    Fixes #3915. Add tests for augmentation with `with` clause (#3924)
    Add tests for augmentation with `with` clause

---
+++ LanguageFeatures/Augmentations/augmenting_class_like_declarations_A02_t27.dart
@@ -0,0 +1,49 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion A class, enum, extension type, mixin, or mixin class augmentation
+/// may specify `extends`, `implements` and `with` clauses (when otherwise
+/// supported). The types in these clauses are appended to the introductory
+/// declarations' clauses of the same kind, and if that clause did not exist
+/// previously, then it is added with the new types.
+///
+/// @description Checks that an augment of a class may specify a `with` clause
+/// and invocations of members of 'super' works as expected.
+/// @author sgrekhov22@gmail.com
+
+// SharedOptions=--enable-experiment=augmentations
+
+import '../../Utils/expect.dart';
+
+StringBuffer log = StringBuffer();
+
+mixin M1 {
+  String v = 'M1';
+  String get g => 'M1';
+  String m() => 'M1';
+  void set s(String v) {
+    log.write('$v;');
+  }
+}
+
+mixin M2 on M1 {
+  late String v = '${super.v} and M2';
+  String get g => '${super.g} and M2';
+  String m() => '${super.m()} and M2';
+  void set s(String v) {
+    log.write('${super.s = v} and M2');
+  }
+}
+
+class C with M1 {}
+
+augment class C with M2 {}
+
+main() {
+  Expect.equals('M1 and M2', C().v);
+  Expect.equals('M1 and M2', C().g);
+  Expect.equals('M1 and M2', C().m());
+  C().s = 'M1';
+  Expect.equals('M1;M1 and M2', log.toString());
+}
