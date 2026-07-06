8780f8d15247d66576e65e230dea6220b313cf76
sgrekhov <sgrekhov22@gmail.com>
2026-07-06 12:45:36 +0300

    #3847. Add tests for local function return type inference

---
+++ TypeSystem/inference/local_return_type_inference_A01_t01.dart
@@ -0,0 +1,27 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion With null safety, local function body inference is changed so
+/// that the local function name is not considered available for inference while
+/// performing inference on the body. As a result, any recursive calls to the
+/// function for which the result type is required for inference to complete
+/// will no longer be treated as having return type `dynamic`, but will instead
+/// result in an inference failure.
+///
+/// @description Checks that recursive calls in a function literal result in an
+/// inference failure and compile-time error.
+/// @author sgrekhov22@gmail.com
+/// @issue 63743
+
+main() {
+  foo(int count) {
+    if (count > 0) {
+      return foo(--count);
+//           ^^^
+// [analyzer] unspecified
+// [cfe] unspecified
+    }
+    return 3.14;
+  }
+}
---
+++ TypeSystem/inference/local_return_type_inference_A02_t02.dart
@@ -0,0 +1,141 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion With null safety, local function body inference is changed so
+/// that the local function name is not considered available for inference while
+/// performing inference on the body. As a result, any recursive calls to the
+/// function for which the result type is required for inference to complete
+/// will no longer be treated as having return type `dynamic`, but will instead
+/// result in an inference failure.
+///
+/// @description Checks that the omitted return type of a non-local function is
+/// inferred to be `dynamic`.
+/// @author sgrekhov22@gmail.com
+
+import '../../Utils/static_type_helper.dart';
+
+f1() {
+  if (1 > 2) {
+    return 42;
+  }
+  return 3.14;
+}
+
+class C {
+  static sf1() {
+    if (1 > 2) {
+      return 42;
+    }
+    return 3.14;
+  }
+
+  f1() {
+    if (1 > 2) {
+      return 42;
+    }
+    return 3.14;
+  }
+}
+
+mixin M {
+  static sf1() {
+    if (1 > 2) {
+      return 42;
+    }
+    return 3.14;
+  }
+
+  f1() {
+    if (1 > 2) {
+      return 42;
+    }
+    return 3.14;
+  }
+}
+
+mixin class MC {
+  static sf1() {
+    if (1 > 2) {
+      return 42;
+    }
+    return 3.14;
+  }
+
+  f1() {
+    if (1 > 2) {
+      return 42;
+    }
+    return 3.14;
+  }
+}
+
+enum E {
+  e0;
+
+  static sf1() {
+    if (1 > 2) {
+      return 42;
+    }
+    return 3.14;
+  }
+
+  f1() {
+    if (1 > 2) {
+      return 42;
+    }
+    return 3.14;
+  }
+}
+
+class A {}
+
+extension Ext on A {
+  static sf1() {
+    if (1 > 2) {
+      return 42;
+    }
+    return 3.14;
+  }
+
+  f1() {
+    if (1 > 2) {
+      return 42;
+    }
+    return 3.14;
+  }
+}
+
+extension type ET(int _) {
+  static sf1() {
+    if (1 > 2) {
+      return 42;
+    }
+    return 3.14;
+  }
+
+  f1() {
+    if (1 > 2) {
+      return 42;
+    }
+    return 3.14;
+  }
+}
+
+class MA = Object with M;
+
+main() {
+  f1.expectStaticType<Exactly<dynamic Function()>>();
+  C.sf1.expectStaticType<Exactly<dynamic Function()>>();
+  C().f1.expectStaticType<Exactly<dynamic Function()>>();
+  M.sf1.expectStaticType<Exactly<dynamic Function()>>();
+  MA().f1.expectStaticType<Exactly<dynamic Function()>>();
+  MC.sf1.expectStaticType<Exactly<dynamic Function()>>();
+  MC().f1.expectStaticType<Exactly<dynamic Function()>>();
+  E.sf1.expectStaticType<Exactly<dynamic Function()>>();
+  E.e0.f1.expectStaticType<Exactly<dynamic Function()>>();
+  Ext.sf1.expectStaticType<Exactly<dynamic Function()>>();
+  A().f1.expectStaticType<Exactly<dynamic Function()>>();
+  ET.sf1.expectStaticType<Exactly<dynamic Function()>>();
+  ET(0).f1.expectStaticType<Exactly<dynamic Function()>>();
+}
