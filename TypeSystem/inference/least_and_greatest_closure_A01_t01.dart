792ac890c81f6f4f3b102a4baa9eb8a8924823d5
Sergey G. Grekhov <sgrekhov22@gmail.com>
2026-07-13 10:57:03 +0300

    #3847. Add least and greatest closure tests. Part 1. (#3944)
    Add least and greatest closure tests. Part 1.

---
+++ TypeSystem/inference/least_and_greatest_closure_A01_t01.dart
@@ -0,0 +1,29 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Given a type `S` and a set of type variables `L` consisting of
+/// the variables `X0, ..., Xn`, we define the least and greatest closure of `S`
+/// with respect to `L` as follows.
+/// ...
+/// - If `S` is `X` where `X` is in `L`:
+///   - The least closure of `S` with respect to `L` is `Never`
+///   - The greatest closure of `S` with respect to `L` is `Object?`
+///
+/// @description Check that if `S` is `X` where `X` is in `L`, then the greatest
+/// closure of `S` with respect to `L` is `Object?`.
+/// @author sgrekhov22@gmail.com
+
+import '../../Utils/static_type_helper.dart';
+
+class C<X> {}
+
+test<X extends C<X>, Y extends C<Y>>(X x, Y y) {
+  var v = 1 > 2 ? x : y;
+  v.expectStaticType<Exactly<C<Object?>>>();
+}
+
+main() {
+  C<Never> c = C<Never>();
+  test(c, c);
+}
---
+++ TypeSystem/inference/least_and_greatest_closure_A03_t01.dart
@@ -0,0 +1,27 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Given a type `S` and a set of type variables `L` consisting of
+/// the variables `X0, ..., Xn`, we define the least and greatest closure of `S`
+/// with respect to `L` as follows.
+/// ...
+/// - If `S` is a base type (or in general, if it does not contain any variable
+///   from `L`)
+///   - The least closure of `S` is `S`
+///   - The greatest closure of `S` is `S`
+///
+/// @description Check that if `S` is a base type then the greatest closure of
+/// `S` is `S`.
+/// @author sgrekhov22@gmail.com
+
+import '../../Utils/static_type_helper.dart';
+
+test <X extends int, Y extends int>(X x, Y y) {
+  var v = 1 > 2 ? x : y;
+  v.expectStaticType<Exactly<int>>();
+}
+
+main() {
+  test(1, 2);
+}
