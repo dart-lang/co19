86489d5447ae771d18844973bf621e9a66e8d7bb
sgrekhov <sgrekhov22@gmail.com>
2026-07-20 11:47:04 +0300

    Fixes #4005. Expect an error in case of late variable in enum, extension or extension type

--- LanguageFeatures/Augmentations/augmenting_variables_getters_setters_A01_t16.dart
+++ LanguageFeatures/Augmentations/augmenting_variables_getters_setters_A01_t16.dart
@@ -17,74 +17,52 @@
 /// augmented by a setter. This is true whether the getter or setter is
 /// explicitly declared or implicitly declared using a variable declaration.
 ///
-/// @description Checks that an incomplete getter may be augmented by a
-/// `late final` variable with an initializing expression.
+/// @description Checks that it is a compile-time error if an incomplete getter
+/// of an enum, extension or extension type is augmented by a `late final`
+/// variable with an initializing expression.
 /// @author sgrekhov22@gmail.com
 
 // SharedOptions=--enable-experiment=augmentations
 
-import '../../Utils/expect.dart';
-
-int get topLevelGetter;
-augment late final int topLevelGetter = 1;
-
-class C {
-  static int get staticGetter;
-  int get instanceGetter;
-}
-
-augment class C {
-  augment static late final int staticGetter = 2;
-  augment late final int instanceGetter = 3;
-}
-
-mixin M {
-  static int get staticGetter;
-  int get instanceGetter;
-}
-
-augment mixin M {
-  augment static late final int staticGetter = 4;
-  augment late final int instanceGetter = 5;
-}
-
 enum E {
   e0;
-  static int get staticGetter;
+  int get instanceGetter;
 }
 
 augment enum E {
   ;
-  augment static late final int staticGetter = 6;
+  augment late final int instanceGetter = 1;
+//        ^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
 }
 
 class A {}
 
 extension Ext on A {
-  static int get staticGetter;
+  int get instanceGetter;
 }
 
 augment extension Ext {
-  augment static late final int staticGetter = 8;
+  augment late final int instanceGetter = 2;
+//                       ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
 }
 
 extension type ET(int _) {
-  static int get staticGetter;
+  int get instanceGetter;
 }
 
 augment extension type ET {
-  augment static late final int staticGetter = 10;
+  augment late final int instanceGetter = 3;
+//                       ^^^^^^^^^^^^^^
+// [analyzer] unspecified
+// [cfe] unspecified
 }
 
-class MA = Object with M;
-
 main() {
-  Expect.equals(1, topLevelGetter);
-  Expect.equals(2, C.staticGetter);
-  Expect.equals(3, C().instanceGetter);
-  Expect.equals(4, M.staticGetter);
-  Expect.equals(5, MA().instanceGetter);
-  Expect.equals(6, E.staticGetter);
-  Expect.equals(8, Ext.staticGetter);
-  Expect.equals(10, ET.staticGetter);
+  print(E);
+  print(A);
+  print(ET);
 }
