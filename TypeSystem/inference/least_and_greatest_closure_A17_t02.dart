c11bc22bfadde4a329b48d6604ecd4cb8f782637
Sergey G. Grekhov <sgrekhov22@gmail.com>
2026-07-24 10:52:19 +0300

    #3847. Add more test for least and greatest closure (#4138)
    Co-authored-by: Erik Ernst <eernst@google.com>

---
+++ TypeSystem/inference/least_and_greatest_closure_A17_t02.dart
@@ -0,0 +1,44 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Given a type `S` and a set of type variables `L` consisting of
+/// the variables `X0, ..., Xn`, we define the least and greatest closure of `S`
+/// with respect to `L` as follows.
+/// ...
+/// - if `S` is `T Function<X0 extends B0, ..., Xk extends Bk>(T0 x0, ..., Tn xn,
+///   [Tn+1 xn+1, ..., Tm xm])` or `T Function<X0 extends B0, ...,
+///   Xk extends Bk>(T0 x0, ..., Tn xn, {Tn+1 xn+1, ..., Tm xm})` and `L`
+///   contains any free type variables from any of the `Bi`:
+///   - The least closure of `S` with respect to `L` is `Never`
+///   - The greatest closure of `S` with respect to `L` is `Function`
+///
+/// @description Check that if `S` is
+/// `T Function<X0 extends B0, ..., Xk extends Bk>(T0 x0, ..., Tn xn,
+/// {Tn+1 xn+1, ..., Tm xm})` and `L` contains any free type variables from any
+/// of the `Bi` then the greatest closure of `S` with respect to `L` is
+/// `Function`.
+/// @author sgrekhov22@gmail.com
+
+import '../../Utils/static_type_helper.dart';
+
+test1<
+  X extends void Function<T extends X>(int, {num n}),
+  Y extends void Function<T extends Y>(int, {num n})
+>(X x, Y y) {
+  var v = 1 > 2 ? x : y;
+  v.expectStaticType<Exactly<Function>>();
+}
+
+test2<
+  X extends void Function<T1 extends X, T2 extends num>(int, {num n}),
+  Y extends void Function<T1 extends Y, T2 extends num>(int, {num n})
+>(X x, Y y) {
+  var v = 1 > 2 ? x : y;
+  v.expectStaticType<Exactly<Function>>();
+}
+
+main() {
+  print(test1);
+  print(test2);
+}
---
+++ TypeSystem/inference/least_and_greatest_closure_A19_t01.dart
@@ -0,0 +1,38 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion Given a type `S` and a set of type variables `L` consisting of
+/// the variables `X0, ..., Xn`, we define the least and greatest closure of `S`
+/// with respect to `L` as follows.
+/// ...
+/// - if `S` is `(T0 x0, ..., Tn xn,  {Tn+1 xn+1, ..., Tm xm})`:
+///   - The least closure of `S` with respect to `L` is
+///     `(U0 x0, ..., Un1 xn, {Un+1 xn+1, ..., Um xm})` where:
+///     - `Ui` is the least closure of `Ti` with respect to `L`
+///   - The greatest closure of `S` with respect to `L` is
+///     `(U0 x0, ..., Un1 xn, {Un+1 xn+1, ..., Um xm})` where:
+///     - `Ui` is the greatest closure of `Ti` with respect to `L`
+///
+/// @description Check that if `S` is
+/// `(T0 x0, ..., Tn xn,  {Tn+1 xn+1, ..., Tm xm})` then the greatest closure of
+/// `S` with respect to `L` is `(U0 x0, ..., Un1 xn, {Un+1 xn+1, ..., Um xm})`
+/// where `Ui` is the greatest closure of `Ti` with respect to `L`.
+/// @author sgrekhov22@gmail.com
+
+import '../../Utils/static_type_helper.dart';
+
+test1<X extends (X, {X n}),Y extends (Y, {Y n})>(X x, Y y) {
+  var v = 1 > 2 ? x : y;
+  v.expectStaticType<Exactly<(Object?, {Object? n})>>();
+}
+
+test2<X extends (int, {num? n}),Y extends (int, {num? n})>(X x, Y y) {
+  var v = 1 > 2 ? x : y;
+  v.expectStaticType<Exactly<(int, {num? n})>>();
+}
+
+main() {
+  print(test1);
+  print(test2);
+}
