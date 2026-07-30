56c8b1705db317afed75ddb8d1827d7fb34dec51
sgrekhov <sgrekhov22@gmail.com>
2026-07-30 10:07:15 +0300

    #4173. Rename existing tests, update assertions

--- TypeSystem/upper-lower-bounds/up_A08_t02.dart
+++ TypeSystem/upper-lower-bounds/up_A08_t02.dart
@@ -2,18 +2,14 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @assertion We define the upper bound of two types T1 and T2 to be UP(T1,T2)
-/// as follows.
-/// - UP(X1 & B1, T2) =
-///   - T2 if X1 <: T2
-///   - otherwise X1 if T2 <: X1
-///   - otherwise UP(B1a, T2) where B1a is the greatest closure of B1 with
-///     respect to X1, as defined in inference.md.
-/// - UP(T1, X2 & B2) =
-///   - X2 if T1 <: X2
-///   - otherwise T1 if X2 <: T1
-///   - otherwise UP(T1, B2a) where B2a is the greatest closure of B2 with
-///     respect to X2, as defined in inference.md.
+/// @assertion We define the upper bound of two types `T1` and `T2` to be
+/// UP(`T1`,`T2`) as follows.
+/// ...
+/// - UP(`X1 & B1`, `T2`) =
+///   - `T2` if `X1 <: T2`
+///   - otherwise `X1` if `T2` <: `X1`
+///   - otherwise UP(`B1a`, `T2`) where `B1a` is the greatest closure of `B1`
+///     with respect to `X1`, as defined in inference.md.
 ///
 /// @description Check that upper bound of intersection types is calculated as
 /// expected
