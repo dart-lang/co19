e0f3c96c70da16c4d074cbf7491f0f1d06ff03fd
Sergey G. Grekhov <sgrekhov22@gmail.com>
2026-08-04 18:07:19 +0400

    #3847. Add tests for the constraint solving (#4172)

---
+++ TypeSystem/inference/constraint_solving_A02_t02.dart
@@ -0,0 +1,28 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// @assertion The constraint solution for a type variable `X` with respect to a
+/// constraint set `C` is the type schema defined as follows:
+/// - let `Mb <: X <: Mt` be the merge of `C` with respect to `X`.
+/// - If `Mb` is known (that is, it does not contain `_`) then the solution is
+///   `Mb`
+/// - Otherwise, if `Mt` is known (that is, it does not contain `_`) then the
+///   solution is `Mt`
+/// - Otherwise, if `Mb` is not `_` then the solution is `Mb`
+/// - Otherwise the solution is `Mt`
+///
+/// Note that the constraint solution is a type schema, and hence may contain
+/// occurrences of the unknown type.
+///
+/// @description Check that if `Mb` is known and `Mt` is unknown then the
+/// solution is `Mb`.
+/// @author sgrekhov22@gmail.com
+
+import '../../Utils/static_type_helper.dart';
+
+X f<X>(X x) => x;
+
+void main() {
+  f(1).expectStaticType<Exactly<int>>(); // int <: X <: _
+}
