2c505ab3adf4c509bf5d575e7da95004e6dd23b9
Erik Ernst <eernst@google.com>
2026-08-24 15:03:10 +0200

    Update up_A13_t02.dart
    Similar to t01.

--- TypeSystem/upper-lower-bounds/up_A13_t02.dart
+++ TypeSystem/upper-lower-bounds/up_A13_t02.dart
@@ -23,8 +23,8 @@ void f1(FutureOr<Object> x, Object y) {
   var v = (1 > 2) ? x : y; // MORETOP(FutureOr<Object>, Object) = false
   // Object and FutureOr<Object> are subtypes of each other, which means that we
   // can't see the difference using `expectStaticType()` function.
-  v.expectStaticType<Exactly<Object>>(); // Check that `v` is `Object`
-  v = confirmObjectContext(); // Check that `v` is not `FutureOr<Object>`
+  v.expectStaticType<Exactly<Object>>(); // Check that `v`'s type is `OBJECT`.
+  v = confirmObjectContext(); // Check that `v` is not `FutureOr<Object>`.
 }
 
 void f2(FutureOr<FutureOr<Object>> x, Object y) {
@@ -37,7 +37,6 @@ void f3(FutureOr<FutureOr<Object>> x, FutureOr<Object> y) {
   // MORETOP(FutureOr<FutureOr<Object>>, FutureOr<Object>) =
   // MORETOP(FutureOr<Object>, Object) = false
   var v = (1 > 2) ? x : y;
-  v.expectStaticType<Exactly<FutureOr<Object>>>();
   v.expectStaticType<Exactly<Object>>();
   v = confirmFutureOrObjectContext();
 }
