87aaa8b8fee224878dc6de4d681066246c569c01
Sergey G. Grekhov <sgrekhov22@gmail.com>
2026-08-24 17:30:58 +0300

    #4173. Add tests for intersection types (#4191)

--- TypeSystem/upper-lower-bounds/up_lib.dart
+++ TypeSystem/upper-lower-bounds/up_lib.dart
@@ -15,9 +15,19 @@ class C {}
 class D<T0, T1> {}
 
 enum E { e0 }
+enum E2 { e0 }
 
 extension type ET(int _) {}
+extension type ET2(ET _) implements ET {}
 
-typedef FPositional = int Function<X extends num>(X s, [int i]);
-typedef FNamed = int Function<X extends num>(X s, {int i});
-typedef Rec = (int, String, {bool b});
+typedef FPositional = num Function<X extends num>(X x, [int i]);
+typedef FPositional2 = int Function<X extends num>(X x, [num i]);
+typedef FNamed = num Function<X extends num>(X x, {int i});
+typedef FNamed2 = int Function<X extends num>(X x, {num i});
+typedef Rec = (num, Pattern, {bool b});
+typedef Rec2 = (int, String, {bool b});
+
+num fPositional<X extends num>(X x, [int i = 0]) => x + i;
+int fPositional2<X extends num>(X x, [num i = 0]) => (x + i).toInt();
+num fNamed<X extends num>(X x, {int i = 0}) => x + i;
+int fNamed2<X extends num>(X x, {num i = 0}) => (x + i).toInt();
